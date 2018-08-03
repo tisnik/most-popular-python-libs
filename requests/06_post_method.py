3572ca853485a2317b9204e8c3198d3cb29056b4
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-08-03 11:54:15 +0200

    6th example: basic usage of HTTP POST method

---
+++ requests/06_post_method.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+import requests
+
+# adresa s testovaci REST API sluzbou
+URL = "https://httpbin.org/post"
+
+# poslani HTTP dotazu typu GET
+response = requests.post(URL)
+
+# vypis odpovedi v plain textu
+print(response.text)
