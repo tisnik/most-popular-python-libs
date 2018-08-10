b7eae4370fa69e46e0c03013355dabc27023fdd5
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-08-10 08:59:08 +0200

    15th example: specifying expected data format in headers

---
+++ requests/15_binary_data_by_header_jpeg.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+import requests
+
+# adresa s testovaci REST API sluzbou
+URL = "https://httpbin.org/image"
+
+# hlavicka posilana v dotazu
+headers = {'accept': 'image/jpeg'}
+
+# poslani HTTP dotazu typu GET
+response = requests.get(URL, headers=headers)
+
+# precteni hlavicek
+headers = response.headers
+
+# vypis typu internetoveho media
+print("Typ dat:", headers.get("content-type"))
+
+# vypis delky dat predanych v tele
+print("Delka dat:", headers.get("content-length"))
+
+print(response.raw)
+
+with open("test2.jpg", 'wb') as fout:
+    for block in response.iter_content(chunk_size=128):
+        fout.write(block)
