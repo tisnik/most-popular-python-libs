15008b7fe38e95617c01099bb73047bf404658e1
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-08-10 08:56:51 +0200

    12th example: reading binary PNG images via HTTP

---
+++ requests/12_binary_data_png_image.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+import requests
+
+# adresa s testovaci REST API sluzbou
+URL = "https://httpbin.org/image/png"
+
+# poslani HTTP dotazu typu GET
+response = requests.get(URL)
+
+# precteni hlavicek
+headers = response.headers
+
+# vypis typu internetoveho media
+print("Typ dat:", headers.get("content-type"))
+
+# vypis delky dat predanych v tele
+print("Delka dat:", headers.get("content-length"))
+
+print(response.raw)
+
+with open("test1.png", 'wb') as fout:
+    for block in response.iter_content(chunk_size=128):
+        fout.write(block)
