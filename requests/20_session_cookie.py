ac4783aba31e54aa4db2e9276e3fd4ee2f5ebaa4
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-08-10 09:05:27 +0200

    20th example: session cookie

---
+++ requests/20_session_cookie.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+import requests
+import json
+
+
+def set_cookie(session, name, value):
+    # adresa s testovaci REST API sluzbou
+    URL = "https://httpbin.org/cookies/set/{name}/{value}".format(name=name, value=value)
+
+    # hlavicka posilana v dotazu
+    headers = {'accept': 'application/json'}
+
+    # poslani HTTP dotazu typu GET
+    return session.get(URL, headers=headers)
+
+
+def delete_cookie(session, name):
+    # adresa s testovaci REST API sluzbou
+    URL = "https://httpbin.org/cookies/delete?{name}=".format(name=name)
+
+    # hlavicka posilana v dotazu
+    headers = {'accept': 'application/json'}
+
+    # poslani HTTP dotazu typu GET
+    return session.get(URL, headers=headers)
+
+
+def print_response(response):
+    # precteni hlavicek
+    headers = response.headers
+
+    print("-" * 60)
+
+    # vypis hlavicek
+    print("Headers:")
+
+    for header_name, header_value in headers.items():
+        print("{:40s} {}".format(header_name, header_value))
+
+    print("-" * 60)
+
+    print("Content:")
+
+    # zpracovani odpovedi, ktera prisla ve formatu JSON
+    data = response.text
+
+    # zpracovani odpovedi, ktera prisla ve formatu JSON
+    data = response.json()
+
+    print(json.dumps(data, indent=4, sort_keys=True))
+
+    print("-" * 60)
+
+
+def print_session_cookies(session):
+    cookies = session.cookies
+    print("Session cookies:")
+
+    for cookie_name, cookie_value in cookies.items():
+        print("{:40s} {}".format(cookie_name, cookie_value))
+
+    print("-" * 60)
+
+
+session = requests.Session()
+
+print("*** set cookie 'foo'=6 ***")
+response = set_cookie(session, "foo", "6")
+print_response(response)
+print_session_cookies(session)
+print()
+
+print("*** set cookie 'bar'=7 ***")
+response = set_cookie(session, "bar", "7")
+print_response(response)
+print_session_cookies(session)
+print()
+
+print("*** set cookie 'foo'=42 ***")
+response = set_cookie(session, "foo", "42")
+print_response(response)
+print_session_cookies(session)
+print()
+
+print("*** delete cookie 'foo' ***")
+response = delete_cookie(session, "foo")
+print_response(response)
+print_session_cookies(session)
+print()
+
+print("*** delete cookie 'baz' ***")
+response = delete_cookie(session, "baz")
+print_response(response)
+print_session_cookies(session)
+print()
+
