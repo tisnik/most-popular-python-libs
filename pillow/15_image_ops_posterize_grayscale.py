45ec6abecb9c8243aecb03ebd5518d439cee447f
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-08-15 19:53:18 +0200

    Pillow example #15: posterization for grayscale images

---
+++ pillow/15_image_ops_posterize_grayscale.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+from PIL import Image
+from PIL import ImageMath
+from PIL import ImageOps
+
+filename = "Lenna.png"
+
+try:
+    test_image = Image.open(filename)
+    test_image.load()
+    test_image.show()
+    grayscale_image = ImageMath.eval("convert(src, 'L')", src=test_image)
+
+    for bits in range(8, 0, -1):
+        modified_image = ImageOps.posterize(grayscale_image, bits)
+        modified_image.show()
+
+except Exception as e:
+    print(e)
