467112af7009e7702f22fb938365b2b31b5e3107
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-08-15 19:50:18 +0200

    Pillow example #08: simple convolution filter

---
+++ pillow/08_simple_filter.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+from PIL import Image, ImageFilter
+
+filename = "Lenna.png"
+
+
+def print_image_info(image):
+    print("Format:     {fmt}".format(fmt=image.format))
+    size = image.size
+
+    print("Resolution: {width}x{height} px".format(width=size[0],
+                                                   height=size[1]))
+
+    print("Mode:       {mode}".format(mode=image.mode))
+
+
+try:
+    test_image = Image.open(filename)
+    test_image.load()
+
+    print("Original:")
+    print_image_info(test_image)
+    print()
+
+    blurred_image = test_image.filter(ImageFilter.BLUR)
+
+    print("Filtered image:")
+    print_image_info(blurred_image)
+
+    test_image.show()
+    blurred_image.show()
+
+except Exception as e:
+    print(e)
