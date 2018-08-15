18f3a57c5cd6e62ebcc148e798154be6432bdb9d
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-08-15 19:51:54 +0200

    Pillow example #11: converting image to BW

---
+++ pillow/11_convert_image_to_black_white.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+from PIL import Image
+from PIL import ImageMath
+
+filename = "Lenna.png"
+
+try:
+    test_image = Image.open(filename)
+    test_image.load()
+    modified_image = ImageMath.eval("convert(src, '1')", src=test_image)
+
+    test_image.show()
+    modified_image.show()
+
+except Exception as e:
+    print(e)
