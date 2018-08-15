57891aea30d30f52cde83500140a2aa837e53fcd
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-08-15 19:52:26 +0200

    Pillow example #13: configurable autocontrast function

---
+++ pillow/13_image_ops_autocontrast_cutoff.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+from PIL import Image
+from PIL import ImageOps
+
+filename = "Lenna.png"
+
+try:
+    test_image = Image.open(filename)
+    test_image.load()
+    modified_image_1 = ImageOps.autocontrast(test_image, cutoff=0)
+    modified_image_2 = ImageOps.autocontrast(test_image, cutoff=50)
+    modified_image_3 = ImageOps.autocontrast(test_image, cutoff=75)
+
+    test_image.show()
+    modified_image_1.show()
+    modified_image_2.show()
+    modified_image_3.show()
+
+except Exception as e:
+    print(e)
