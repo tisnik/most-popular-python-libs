e809c43a7d0ccaea9a1dcff63b87507c6f1e8ffb
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-08-15 19:47:22 +0200

    Pillow example #03: gathering image info

---
+++ pillow/03_image_info.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+from PIL import Image
+
+filename = "Lenna.png"
+
+try:
+    test_image = Image.open(filename)
+    test_image.load()
+    print("Image loaded")
+    print("Format:     {fmt}".format(fmt=test_image.format))
+
+    size = test_image.size
+    print("Resolution: {width}x{height} px".format(width=size[0],
+                                                   height=size[1]))
+
+    print("Mode:       {mode}".format(mode=test_image.mode))
+
+except Exception as e:
+    print(e)
