b5d8d00cdcffd3bedb036975c0e289df40afd6ea
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-08-15 19:48:30 +0200

    Pillow example #04: Image.save() method

---
+++ pillow/04_save_image.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+from PIL import Image
+
+filename = "Lenna.png"
+
+try:
+    test_image = Image.open(filename)
+    test_image.load()
+
+    test_image.save("Lenna.jpg")
+    test_image.save("Lenna.bmp")
+    test_image.save("Lenna.tiff")
+
+except Exception as e:
+    print(e)
