bc80991a53c442259c9eee1bfbf07cc8d495930d
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-08-16 08:05:42 +0200

    PIL example #12: autocontrast function

---
+++ pil/12_image_ops_autocontrast.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8
+
+from PIL import Image
+from PIL import ImageOps
+
+filename = "Lenna.png"
+
+try:
+    test_image = Image.open(filename)
+    test_image.load()
+    modified_image = ImageOps.autocontrast(test_image)
+
+    test_image.show()
+    modified_image.show()
+
+except Exception as e:
+    print(e)
