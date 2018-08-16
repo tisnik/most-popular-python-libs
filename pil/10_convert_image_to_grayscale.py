604b8295f248a3c76217c86b437f4cb6888356c6
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-08-16 08:04:48 +0200

    PIL example #10: converting image to grayscale

---
+++ pil/10_convert_image_to_grayscale.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8
+
+from PIL import Image
+from PIL import ImageMath
+
+filename = "Lenna.png"
+
+try:
+    test_image = Image.open(filename)
+    test_image.load()
+    modified_image = ImageMath.eval("convert(src, 'L')", src=test_image)
+
+    test_image.show()
+    modified_image.show()
+
+except Exception as e:
+    print(e)
