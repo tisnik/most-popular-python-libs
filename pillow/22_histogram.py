042c8250efd92ed9e316cc6724c3dcaa04b5d864
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-08-23 10:58:16 +0200

    Pillow example #22: histogram for an image

---
+++ pillow/22_histogram.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+from PIL import Image
+
+filename = "Lenna.png"
+
+try:
+    # načtení originálního obrázku Leny
+    test_image = Image.open(filename)
+    test_image.load()
+
+    histogram = test_image.histogram()
+
+    print(histogram[0:255])
+    print()
+    print(histogram[256:511])
+    print()
+    print(histogram[512:767])
+    print()
+
+except Exception as e:
+    print(e)
