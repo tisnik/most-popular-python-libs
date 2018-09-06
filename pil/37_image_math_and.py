219a617ba9c18c7ba079de4d291f45d91e4b01a2
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-09-06 09:12:17 +0200

    PIL example #37: Use of 'and' function from ImageMath module

---
+++ pil/37_image_math_and.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8
+
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageMath
+
+filename = "Lenna.png"
+
+
+def create_second_image(width, height):
+    second_image = Image.new("L", (width, height))
+
+    # objekt umožňující kreslení do obrázku
+    draw = ImageDraw.Draw(second_image)
+
+    # vykreslení čtverců
+    draw.rectangle((0, 0, width/2, height/2), fill=0)
+    draw.rectangle((width/2, 0, width, height/2), fill=127)
+    draw.rectangle((0, height/2, width/2, height), fill=128)
+    draw.rectangle((width/2, height/2, width, height), fill=255)
+
+    del draw
+    return second_image
+
+
+try:
+    # načtení originálního obrázku Leny
+    original_image = Image.open(filename)
+    original_image.load()
+
+    # převod na úrovně šedi
+    grayscale_image = ImageMath.eval("convert(src, 'L')", src=original_image)
+
+    # vytvoření druhého obrázku s maskou
+    second_image = create_second_image(512, 512)
+
+    # aplikace operace
+    result_image = ImageMath.eval("first & second", first=grayscale_image, second=second_image)
+
+    # další převod výsledku na stupně šedi
+    result_image = ImageMath.eval("convert(src, 'L')", src=result_image)
+
+    # zobrazení výsledného obrázku uživateli
+    result_image.show()
+
+    # uložení výsledného obrázku
+    result_image.save("37_image_math_and.png")
+
+except Exception as e:
+    print("Vyjimka: " + e.__str__())
