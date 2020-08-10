180f4a94f0c48f08e320fa30618b2bbc495ad2e1
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-08-10 09:13:51 +0200

    Example 05: incorrect JSON and error handling

---
+++ jq.py/05_error_handling.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+#  Demonstrační příklad k článku:
+#      Zpracování dat uložených ve formátu JSON knihovnou jq.py
+
+import jq
+
+with open("broken.json") as fin:
+    content = fin.read()
+    try:
+        print(jq.compile(".openapi").input(text=content).all())
+    except Exception as e:
+        print(e)
