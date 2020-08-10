cb0be5d36e18ad152fd601678324c2c7e6a1e3d0
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-08-10 09:13:51 +0200

    Example 11: reading summary for all andpoints and HTTP method DELETE

---
+++ jq.py/11_summary_for_all_endpoints_delete.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+#  Demonstrační příklad k článku:
+#      Zpracování dat uložených ve formátu JSON knihovnou jq.py
+
+import jq
+import json
+
+with open("openapi.json") as fin:
+    content = json.load(fin)
+    summaries = jq.compile(".paths[] | .delete.summary").input(content).all()
+    for summary in summaries:
+        print(summary)
