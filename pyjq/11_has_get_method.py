7b4a2afa17768c4ea0a25f2542cd6dd17e8e12e5
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-08-10 09:38:34 +0200

    Example 11: predicate for HTTP method existence

---
+++ pyjq/11_has_get_method.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+#  Demonstrační příklad k článku:
+#      Zpracování dat uložených ve formátu JSON knihovnou pyjq
+
+import pyjq
+import json
+
+with open("openapi.json") as fin:
+    content = json.load(fin)
+
+    for endpoint in pyjq.compile('.paths[]').all(content):
+        print(",".join(endpoint.keys()))
+
+    print("-------------------------")
+
+    for has_get in pyjq.compile('.paths[] | has("get")').all(content):
+        print(has_get)
