82e611ecf01f762cd1f88122da66125b639e9a81
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-08-10 09:38:34 +0200

    Example 03: reading first value

---
+++ pyjq/03_first_value.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+#  Demonstrační příklad k článku:
+#      Zpracování dat uložených ve formátu JSON knihovnou pyjq
+
+import pyjq
+import json
+
+with open("openapi.json") as fin:
+    content = json.load(fin)
+    print(pyjq.compile(".openapi").first(content))
