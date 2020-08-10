d81c4ddee5ef63986f4c7b02cf10d82afd153473
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-08-10 09:13:51 +0200

    Example 07: getting non-existing first value from query results

---
+++ jq.py/07_non_existing_key.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+#  Demonstrační příklad k článku:
+#      Zpracování dat uložených ve formátu JSON knihovnou jq.py
+
+import jq
+import json
+
+with open("openapi.json") as fin:
+    content = json.load(fin)
+
+    # klíč "non_existing_key" ve zdrojovém JSONu neexistuje
+    print(jq.compile(".non_existing_key").input(content).first())
