ca142b1364a8eb16fdf8e2548c1b1faead42677b
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-08-10 09:10:41 +0200

    Example 03: process input in JSON form, output is text (print value)

---
+++ jq.py/03_process_as_json_to_text.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+#  Demonstrační příklad k článku:
+#      Zpracování dat uložených ve formátu JSON knihovnou jq.py
+
+import jq
+import json
+
+with open("openapi.json") as fin:
+    content = json.load(fin)
+    print(jq.compile(".openapi").input(content).text())
