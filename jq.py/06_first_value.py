a199fa2cfa2b693e863daac844a120a384605ca8
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-08-10 09:13:51 +0200

    Example 06: getting first value from query results

---
+++ jq.py/06_first_value.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+#  Demonstrační příklad k článku:
+#      Zpracování dat uložených ve formátu JSON knihovnou jq.py
+
+import jq
+import json
+
+with open("openapi.json") as fin:
+    content = json.load(fin)
+    print(jq.compile(".openapi").input(content).first())
+    print(jq.compile(".info.description").input(content).first())
+    print(jq.compile(".tags").input(content).first())
