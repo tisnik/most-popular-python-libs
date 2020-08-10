3cecb5edc2574aa405948f59613a9084236f437d
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-08-10 09:10:41 +0200

    Example 02: process input in text form

---
+++ jq.py/02_process_as_text.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+#  Demonstrační příklad k článku:
+#      Zpracování dat uložených ve formátu JSON knihovnou jq.py
+
+import jq
+
+with open("openapi.json") as fin:
+    content = fin.read()
+    print(jq.compile(".openapi").input(text=content).all())
