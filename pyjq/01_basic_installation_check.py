254bb3d8757dd2332faee7a06b4bff8889cb3e98
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-08-10 09:38:34 +0200

    Example 01: basic installation of py.jq installation

---
+++ pyjq/01_basic_installation_check.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+#  Demonstrační příklad k článku:
+#      Zpracování dat uložených ve formátu JSON knihovnou pyjq
+
+import pyjq
+
+help(pyjq)
