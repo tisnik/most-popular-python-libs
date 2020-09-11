444a64303210208a221dcb1614c591af8f6d1e5b
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-09-11 09:42:34 +0200

    Fixed issue #8: D100: Missing docstring in public module

--- jq.py/02_process_as_text_to_text.py
+++ jq.py/02_process_as_text_to_text.py
@@ -4,6 +4,7 @@
 #  Demonstrační příklad k článku:
 #      Zpracování dat uložených ve formátu JSON knihovnou jq.py
 
+"""Zpracovnání vstupu ve formě textu s interním parsingem JSONu."""
 import jq
 
 with open("openapi.json") as fin:
