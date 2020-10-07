80fe8f07561b8c5764cdfd8fba81a228ed494db7
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-10-07 09:21:03 +0200

    Updated copyright year

--- jq.py/06_first_value_type.py
+++ jq.py/06_first_value_type.py
@@ -2,7 +2,7 @@
 # vim: set fileencoding=utf-8
 
 #
-#  (C) Copyright 2018  Pavel Tisnovsky
+#  (C) Copyright 2020  Pavel Tisnovsky
 #
 #  All rights reserved. This program and the accompanying materials
 #  are made available under the terms of the Eclipse Public License v1.0
