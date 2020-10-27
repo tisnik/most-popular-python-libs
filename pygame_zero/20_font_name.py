14653ea28e0664f142fa30778d55a758174ad1bf
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-10-27 08:29:07 +0100

    Example 20: using font name

---
+++ pygame_zero/20_font_name.py
@@ -0,0 +1,12 @@
+WIDTH = 480
+HEIGHT = 480
+
+
+def draw():
+    screen.fill("white")
+
+    screen.draw.text("Pygame Zero",
+                     (120, 400),
+                     fontname="freesans",
+                     fontsize=60,
+                     color="navy")
