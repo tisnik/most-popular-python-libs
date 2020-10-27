866bcf05a5d8b789eee88999cdd97f5b1a135656
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-10-27 08:29:07 +0100

    Example 24: text with color gradient

---
+++ pygame_zero/24_color_gradient.py
@@ -0,0 +1,12 @@
+WIDTH = 480
+HEIGHT = 480
+
+
+def draw():
+    screen.fill("black")
+
+    screen.draw.text("Pygame Zero",
+                     (60, HEIGHT/2-20),
+                     fontsize=80,
+                     color="orange",
+                     gcolor="red")
