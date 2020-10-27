b15fbb4ebf4509730d07e07aab75da119759589b
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-10-27 08:29:07 +0100

    Example 14: draw lines

---
+++ pygame_zero/14_draw_lines.py
@@ -0,0 +1,11 @@
+WIDTH = 480
+HEIGHT = 480
+
+BORDER = 20
+
+def draw():
+    screen.fill("white")
+
+    for i in range(0, WIDTH-BORDER+1, 10):
+        color = i * 255 / WIDTH
+        screen.draw.line((BORDER, i), (BORDER+i, HEIGHT-BORDER), (color, 0, 255-color))
