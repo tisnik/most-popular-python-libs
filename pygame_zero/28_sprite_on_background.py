eb4cbc6cdd6b096c4bb456398051e93d36a9f6ec
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-11-02 10:41:20 +0100

    Example #28: sprite drawn on background

---
+++ pygame_zero/28_sprite_on_background.py
@@ -0,0 +1,12 @@
+WIDTH = 480
+HEIGHT = 480
+
+BACKGROUND_COLOR = (0, 0x80, 0x80)
+
+sprite = Actor("sprite1.png")
+sprite.pos = (240, 240)
+
+
+def draw():
+    screen.fill(BACKGROUND_COLOR)
+    sprite.draw()
