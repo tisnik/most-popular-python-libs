b149f90142890ff95ee88ff00ad7a80e2be931bb
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-11-02 10:41:36 +0100

    Example #29: exiting app by pressing Esc

---
+++ pygame_zero/29_print_key_codes.py
@@ -0,0 +1,16 @@
+WIDTH = 480
+HEIGHT = 480
+
+BACKGROUND_COLOR = (0, 0x80, 0x80)
+
+sprite = Actor("sprite1.png")
+sprite.pos = (240, 240)
+
+
+def draw():
+    screen.fill(BACKGROUND_COLOR)
+    sprite.draw()
+
+
+def on_key_down(key, mod, unicode):
+    print(key, mod, unicode)
