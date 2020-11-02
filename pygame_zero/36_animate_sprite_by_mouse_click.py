1deb77a1df457689de4f639c89f0e60c3834dc49
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-11-02 12:10:55 +0100

    Example #36: animate sprite

---
+++ pygame_zero/36_animate_sprite_by_mouse_click.py
@@ -0,0 +1,21 @@
+WIDTH = 480
+HEIGHT = 480
+
+BACKGROUND_COLOR = (0, 0x80, 0x80)
+
+sprite = Actor("sprite1.png")
+sprite.pos = (240, 240)
+
+
+def draw():
+    screen.fill(BACKGROUND_COLOR)
+    sprite.draw()
+
+
+def on_key_down(key, mod, unicode):
+    if key == keys.ESCAPE:
+        exit()
+
+
+def on_mouse_down(pos, button):
+    animation = animate(sprite, pos=pos)
