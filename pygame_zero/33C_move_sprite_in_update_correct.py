6a3f244b55c73a6be4ed9ca3ab23c5f6e6c09341
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-11-04 08:26:53 +0100

    Example 33C: corrected variant of example #31 and example #32

---
+++ pygame_zero/33C_move_sprite_in_update_correct.py
@@ -0,0 +1,43 @@
+WIDTH = 480
+HEIGHT = 480
+
+BACKGROUND_COLOR = (0, 0x80, 0x80)
+
+sprite = Actor("sprite1.png")
+sprite.pos = (240, 240)
+dx = 0
+dy = 0
+
+
+def draw():
+    screen.fill(BACKGROUND_COLOR)
+    sprite.draw()
+
+
+def update():
+    sprite.left += dx
+    sprite.top += dy
+
+
+def on_key_down(key, mod, unicode):
+    global dx, dy
+    if key == keys.ESCAPE:
+        exit()
+    if key == keys.UP:
+        dy = -1
+    if key == keys.DOWN:
+        dy = 1
+    if key == keys.LEFT:
+        dx = -1
+    if key == keys.RIGHT:
+        dx = 1
+
+
+def on_key_up(key, mod):
+    global dx, dy
+    if key == keys.ESCAPE:
+        exit()
+    if key == keys.UP or key == keys.DOWN:
+        dy = 0
+    if key == keys.LEFT or key == keys.RIGHT:
+        dx = 0
