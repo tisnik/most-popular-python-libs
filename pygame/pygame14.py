94d731d6c22c87dcd4abdcc5fa638fdb56d1dc42
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-11-05 08:17:44 +0100

    Fixed issues #430 and #431

--- pygame/pygame14.py
+++ pygame/pygame14.py
@@ -76,7 +76,7 @@ image_surface = (pygame.image.load(os.path.join('images', 'gnome-globe.png')).co
 display.blit(image_surface, ( 25,  25), special_flags=BLEND_ADD)
 display.blit(image_surface, (125,  25), special_flags=BLEND_SUB)
 display.blit(image_surface, (225,  25), special_flags=BLEND_MULT)
-display.blit(image_surface, ( 25, 125), special_flags = BLEND_MIN)
+display.blit(image_surface, ( 25, 125), special_flags=BLEND_MIN)
 display.blit(image_surface, (125, 125), special_flags = BLEND_MAX)
 display.blit(image_surface, (225, 125))
 
