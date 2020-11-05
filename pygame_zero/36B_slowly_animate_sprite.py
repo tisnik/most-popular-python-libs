33cf3929cf65a7e51661925f49269fc6e72167a3
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-11-05 08:20:59 +0100

    Slow sprite animation

---
+++ pygame_zero/36B_slowly_animate_sprite.py
@@ -0,0 +1,18 @@
+WIDTH = 480
+HEIGHT = 480
+
+BACKGROUND_COLOR = (0, 0x80, 0x80)
+
+sprite = Actor("sprite1.png")
+sprite.pos = (0, 240)
+animation = animate(sprite, x=WIDTH, duration=5)
+
+
+def draw():
+    screen.fill(BACKGROUND_COLOR)
+    sprite.draw()
+
+
+def on_key_down(key, mod, unicode):
+    if key == keys.ESCAPE:
+        exit()
