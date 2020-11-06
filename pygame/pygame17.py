9ed17198c88d9663fb0b18e3cebec7b5b55c3fec
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-11-06 09:02:01 +0100

    Fixed issue #299

--- pygame/pygame17.py
+++ pygame/pygame17.py
@@ -47,7 +47,7 @@ pygame.display.set_caption('Pygame test #17')
 # Konstanty s n-ticemi představujícími základní barvy
 BLACK   = (  0,   0,   0)
 RED     = (255,   0,   0)
-BLUE    = (0,   0,   255)
+BLUE = (0,   0,   255)
 
 # Vyplnění plochy okna černou barvou
 display.fill(BLACK)
