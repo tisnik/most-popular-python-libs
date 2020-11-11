163933d5f29350dfb4d64cf7f38dbb0d390fb186
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-11-11 07:41:12 +0100

    Plotnine example #7 (offline variant)

---
+++ plotnine/offline_variants/07_xyplot_show_pp.py
@@ -0,0 +1,10 @@
+from plotnine.data import economics
+
+from plotnine import ggplot, aes, geom_line, labs
+
+g = ggplot(economics) + \
+    aes(x="date", y="uempmed") + \
+    geom_line() + \
+    labs(x="date", y="median duration of unemployment")
+
+g.save("07.png")
