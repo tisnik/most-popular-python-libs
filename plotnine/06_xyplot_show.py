0abc5fbdd2c10733fc7df23c243fbd7797bfc5a8
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-11-11 07:40:16 +0100

    Plotnine example #6

---
+++ plotnine/06_xyplot_show.py
@@ -0,0 +1,6 @@
+from plotnine.data import economics
+
+from plotnine import ggplot, aes, geom_line, labs
+
+g = ggplot(economics) + aes(x="date", y="uempmed") + geom_line() + labs(x="date", y="median duration of unemployment")
+print(g)
