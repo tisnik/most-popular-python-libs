3f02eae483a10da8ec4a5500627a0a57373766e1
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-11-11 07:41:12 +0100

    Plotnine example #21 (offline variant)

---
+++ plotnine/offline_variants/21_lm.py
@@ -0,0 +1,11 @@
+from plotnine.data import economics
+from plotnine import ggplot, aes, facet_grid, labs, geom_point, geom_smooth, xlab, ylab
+
+g=(ggplot(economics)
+        + aes(x="date", y="uempmed")
+        + geom_point()
+        + geom_smooth(color="red", method="lm")
+        + xlab("date (year)")
+        + ylab("unemploynment"))
+
+g.save("21.png")
