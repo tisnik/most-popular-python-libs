3f784318515e80c690f5e394383f3faa328002d5
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-11-11 07:40:16 +0100

    Plotnine example #20

---
+++ plotnine/20_smoothing_span_0_2.py
@@ -0,0 +1,9 @@
+from plotnine.data import economics
+from plotnine import ggplot, aes, facet_grid, labs, geom_point, geom_smooth, xlab, ylab
+
+print(ggplot(economics)
+        + aes(x="date", y="uempmed")
+        + geom_point()
+        + geom_smooth(color="red", span=0.1)
+        + xlab("date (year)")
+        + ylab("unemploynment"))
