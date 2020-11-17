45bf3fa4d919a79d9bcb24f46ecdc334ffdb3393
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-11-17 12:16:19 +0100

    Fixed issue #390

--- pygame/pygame13.py
+++ pygame/pygame13.py
@@ -29,7 +29,7 @@ pygame.display.set_caption('Pygame test #13')
 
 # Konstanty s n-ticemi představujícími základní barvy
 BLACK   = (  0,   0,   0)
-BLUE    = (  0,   0, 255)
+BLUE = (  0,   0, 255)
 CYAN = (  0, 255, 255)
 GREEN = (  0, 255,   0)
 YELLOW = (255, 255,   0)
