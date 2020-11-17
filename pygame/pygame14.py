59abff02040928b40ae733d078febdc600724f1d
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-11-17 11:12:39 +0100

    Fixed issue #417

--- pygame/pygame14.py
+++ pygame/pygame14.py
@@ -35,7 +35,7 @@ GREEN   = (  0, 255,   0)
 YELLOW  = (255, 255,   0)
 RED     = (255,   0,   0)
 MAGENTA = (255,   0, 255)
-WHITE   = (255, 255, 255)
+WHITE = (255, 255, 255)
 
 # Vyplnění plochy okna černou barvou
 display.fill(BLACK)
