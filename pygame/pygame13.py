5699f5f2df6948a3c23bfe57a9035e1f1ad60c62
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-11-18 10:58:05 +0100

    Fixed issue #386

--- pygame/pygame13.py
+++ pygame/pygame13.py
@@ -7,7 +7,10 @@
 #                   Zachování alfa kanálu.
 
 
-import pygame, sys, os, math
+import pygame
+import sys
+import os
+import math
 
 # Nutno importovat kvůli konstantám QUIT atd.
 from pygame.locals import *
