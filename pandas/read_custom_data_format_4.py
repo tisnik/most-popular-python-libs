30490eefa25a06fce3fb0e34b75a2ab8e3eb5709
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-11-24 10:31:38 +0100

    Reading custom data format, 4th approach

---
+++ pandas/read_custom_data_format_4.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+"""Reading data file with custom format. separator + skip rows specification."""
+
+import pandas
+
+df = pandas.read_csv("denni_kurz.txt", sep="|", skiprows=1)
+
+df["kurz"] = pandas.to_numeric(df["kurz"].str.replace(',','.'), errors='coerce')
+
+print("Data frame")
+print("---------------------------")
+print(df)
+print()
+
+print("Column types")
+print("---------------------------")
+print(df.dtypes)
