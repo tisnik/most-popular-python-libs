f3a455497cddb8fc8249794f95042492b0455151
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-11-24 10:31:38 +0100

    Read custom timestamps, second approach

---
+++ pandas/read_custom_timestamps_2.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+"""Reading CSV with custom timestamps format using custom parser."""
+
+import pandas
+import datetime
+
+
+def datetime_parser(raw_data):
+    return datetime.datetime.strptime(raw_data, "%Y/%m/%d %H-%M-%S")
+
+
+df = pandas.read_csv("custom_timestamps.csv",
+                     date_parser=datetime_parser,
+                     parse_dates=["Timestamp"])
+
+
+pandas.to_datetime(df.Timestamp)
+
+print("Data frame")
+print("---------------------------")
+print(df)
+print()
+
+print("Column types")
+print("---------------------------")
+print(df.dtypes)
