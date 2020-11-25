568c6fa441faff3e896a84e1a8a9152eb77379c4
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-11-25 08:27:17 +0100

    Reading data file from internet with parsing

---
+++ pandas/read_data_from_internets_proper_2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+"""Reading data file from internets."""
+
+import pandas
+
+url = "https://www.cnb.cz/cs/financni_trhy/devizovy_trh/kurzy_devizoveho_trhu/denni_kurz.txt"
+df = pandas.read_csv(url, sep="|", skiprows=1)
+
+df["kurz"] = pandas.to_numeric(df["kurz"].str.replace(',','.'), errors='coerce')
+
+print("Data frame")
+print("---------------------------")
+print(df)
+print()
+
+print("Column types")
+print("---------------------------")
+print(df.dtypes)
