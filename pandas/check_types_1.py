6b34761fb41cc52bf3d18e153ffe63a2e835865e
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-11-25 08:27:17 +0100

    Schema check using voluptuous library

---
+++ pandas/check_types_1.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+"""Reading CSV file that contains column with integer values (some are missing)."""
+
+import pandas
+from voluptuous import Schema
+from voluptuous import Invalid
+
+
+def print_data_frame(df):
+    print("Data frame")
+    print("---------------------------")
+    print(df)
+    print()
+
+    print("Column types")
+    print("---------------------------")
+    print(df.dtypes)
+
+
+def validate_item(schema, data):
+    try:
+        print("\n\n")
+        print(schema)
+        print(data)
+        schema(data)
+        print("pass")
+    except Exception as e:
+        print(e)
+
+
+def validate_data_frame(data_frame):
+    print()
+
+    print("Validation")
+    print("---------------------------")
+
+    schema = Schema((int, int, float))
+
+    for record in df.itertuples():
+        validate_item(schema, record)
+
+
+df = pandas.read_csv("missing_integer_values.csv")
+print_data_frame(df)
+validate_data_frame(df)
