362f103f33ac0aa2469ec41379571d2da9be27ee
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-12-01 12:35:57 +0100

    Second variant of Pandas plot

--- pandas/plot_kafka_lags_pandas.py
+++ pandas/plot_kafka_lags_pandas.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import sys
-import csv
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
---
+++ pandas/plot_kafka_lags_pandas_2.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import sys
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Check if command line argument is specified (it is mandatory).
+if len(sys.argv) < 2:
+    print("Usage:")
+    print("  plot_kafka_lags_pandas.py input_file.csv")
+    print("Example:")
+    print("  plot_kafka_lags_pandas.py overall.csv")
+    sys.exit(1)
+
+# First command line argument should contain name of input CSV.
+input_csv = sys.argv[1]
+
+df = pd.read_csv(input_csv)
+
+print(df.info())
+print(df.describe())
+
+# Create new histogram graph
+df.plot(x="Time", y="topic : uploads")
+
+# Try to show the plot on screen
+plt.show()
