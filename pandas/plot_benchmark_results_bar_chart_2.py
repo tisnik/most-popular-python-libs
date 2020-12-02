6c02fe7e524fbfaa5288cf80bfb3677365b2173a
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-12-02 09:04:00 +0100

    Plot a bar chart from data frame

---
+++ pandas/plot_benchmark_results_bar_chart_2.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import sys
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Check if command line argument is specified (it is mandatory).
+if len(sys.argv) < 2:
+    print("Usage:")
+    print("  plot_benchmark_results_bar_chart_2.py ")
+    print("Example:")
+    print("  plot_benchmark_results_bar_chart_2.py data.tsv")
+    sys.exit(1)
+
+# First command line argument should contain name of input CSV.
+input_file = sys.argv[1]
+
+df = pd.read_csv(input_file, sep="\t")
+
+df = df[5:10]
+data_columns = ["ANSI C", "Cython #1", "Cython #2", "Cython #3", "Numba #1/interpret", "Numba #2", "Numba #3", "Numba #4"]
+
+for data_column in data_columns:
+    df[data_column] = pd.to_numeric(df[data_column].str.replace(',', '.'), errors='coerce')
+
+print(df)
+print()
+
+print(df.info())
+print()
+
+print(df.describe())
+print()
+
+
+# Create new histogram graph
+df.plot.bar(x="Height", y=data_columns)
+
+# Try to show the plot on screen
+plt.show()
