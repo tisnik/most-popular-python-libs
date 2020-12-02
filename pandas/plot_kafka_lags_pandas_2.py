92da2a91b6a66b99eb1aacbd21847b2d8b74374b
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-12-02 09:04:00 +0100

    Updated script name

--- pandas/plot_kafka_lags_pandas_2.py
+++ pandas/plot_kafka_lags_pandas_2.py
@@ -7,9 +7,9 @@ import matplotlib.pyplot as plt
 # Check if command line argument is specified (it is mandatory).
 if len(sys.argv) < 2:
     print("Usage:")
-    print("  plot_kafka_lags_pandas.py input_file.csv")
+    print("  plot_kafka_lags_pandas_2.py input_file.csv")
     print("Example:")
-    print("  plot_kafka_lags_pandas.py overall.csv")
+    print("  plot_kafka_lags_pandas_2.py overall.csv")
     sys.exit(1)
 
 # First command line argument should contain name of input CSV.
