460c0cb3d268f4fa613d7e8f60cb494c6082b1e2
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-12-07 08:58:48 +0100

    Working with Series, example #9

---
+++ pandas/series_09.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+import pandas
+
+s = pandas.Series(range(1, 7), ('a', 'b', 'c', 'd', 'e', 'f'))
+
+print("sum", s.sum(), sep="\t")
+print("prod", s.prod(), sep="\t")
+print("min", s.min(), sep="\t")
+print("max", s.max(), sep="\t")
+print("median", s.median(), sep="\t")
+print("std", s.std(), sep="\t")
+print("var" ,s.var(), sep="\t")
+print("quantile", s.quantile(0.01), sep="\t")
