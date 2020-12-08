a4852ad07093852c8a3f4538aa587f8be9ac0c34
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-12-08 11:23:58 +0100

    Working with series, example #15

---
+++ pandas/series_15.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+import pandas
+
+s = pandas.Series((100, 200, 300, 400, 500, 600))
+print(s.dtypes)
+print(s)
+print()
+
+s = s.astype('int32')
+print(s.dtypes)
+print(s)
+print()
+
+s =s.astype('int8')
+print(s.dtypes)
+print(s)
