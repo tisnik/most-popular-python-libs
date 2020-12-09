77195b273dd08490436851338d964199a38b1ad8
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-12-09 08:17:37 +0100

    Plot series example #3

---
+++ pandas/plot_series_03.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+import numpy as np
+import pandas
+import matplotlib.pyplot as plt
+
+# hodnoty na x-ové ose
+r = np.linspace(0, 2*np.pi, 20)
+
+# konstrukce datové struktury Series
+s = pandas.Series(data=np.sin(r), index=r)
+
+# tisk obsahu Series
+print(s)
+
+# vytvoření grafu
+s.plot.bar(grid=True)
+
+# uložení grafu
+plt.savefig("series_plot_03.png")
+
+# vykreslení grafu
+plt.show()
