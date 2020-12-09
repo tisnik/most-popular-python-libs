86f4488679c64e2136f9bf774b68d4b1d52912c8
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-12-09 08:17:37 +0100

    Plot series example #11

---
+++ pandas/plot_series_11.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+import numpy as np
+import pandas
+import matplotlib.pyplot as plt
+
+# hodnoty na x-ové ose
+r = np.linspace(0, 2*np.pi, 100)
+
+# konstrukce datové struktury Series
+s = pandas.Series(data=np.sin(r), index=r)
+
+s2 = s.map(lambda x: x+np.random.rand()/2)
+
+s3 = s2 - s
+
+# vytvoření grafu
+plt.plot(s, "--", s2, "-", s3, "-")
+
+# uložení grafu
+plt.savefig("series_plot_11.png")
+
+# vykreslení grafu
+plt.show()
