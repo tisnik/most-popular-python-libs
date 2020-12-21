09d4b1759ebafe754f6c596f938c3bc7beaa4ae4
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-12-21 08:18:25 +0100

    Displaying references to objects, example #15

---
+++ gc_objgraph/gc15_references.py
@@ -0,0 +1,20 @@
+import gc
+import sys
+import pprint
+
+
+def foo(ref):
+    bar(ref)
+
+
+def bar(ref):
+    print(sys.getrefcount(ref))
+    pprint.pprint(len(gc.get_referrers(ref)))
+    pprint.pprint(gc.get_referrers(ref))
+
+
+x = "Test!"
+pprint.pprint(gc.get_referrers(x))
+print()
+
+foo(x)
