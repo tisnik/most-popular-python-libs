07809379e2f2b96e7b496b4f27937655c7e3e11c
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-12-21 08:18:25 +0100

    Show reference graph, example #3

---
+++ gc_objgraph/objgraph3.py
@@ -0,0 +1,9 @@
+"""Grafické zobrazení referencí."""
+
+import objgraph
+
+x = "Foo"
+y = [x, "bar"]
+
+# zobrazení referencí na dva řetězce uložené v seznamu
+objgraph.show_refs([y], filename='objgraph3.png')
