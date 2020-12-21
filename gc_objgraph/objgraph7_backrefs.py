57ad3111a2cb0fae2fd8f88d6846533ad3547905
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-12-21 08:18:25 +0100

    Show backreferences graph, example #7

---
+++ gc_objgraph/objgraph7_backrefs.py
@@ -0,0 +1,16 @@
+"""Grafické zobrazení referencí."""
+
+import objgraph
+
+class A():
+    def __init__(self, other):
+        self.other = other
+
+# tři objekty, které na sebe navzájem ukazují
+x = A(None)
+y = A(x)
+z = A(y)
+x.other=z
+
+# cyklické reference mezi objekty
+objgraph.show_backrefs([x, y, z], filename='objgraph7_backrefs.png')
