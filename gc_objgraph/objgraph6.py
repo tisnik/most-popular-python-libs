8971aea1d5201ff4165bdb60e911b29c0060aeb4
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-12-21 08:18:25 +0100

    Show reference graph, example #6

---
+++ gc_objgraph/objgraph6.py
@@ -0,0 +1,23 @@
+"""Grafické zobrazení referencí."""
+
+import objgraph
+
+x = {}
+y = {}
+z = {}
+
+# cyklické reference N:N
+x["->x"] = x
+x["->y"] = y
+x["->z"] = z
+
+y["->x"] = x
+y["->y"] = y
+y["->z"] = z
+
+z["->x"] = x
+z["->y"] = y
+z["->z"] = z
+
+# graf s cyklickými referencemi
+objgraph.show_refs([x, y, z], filename='objgraph6.png')
