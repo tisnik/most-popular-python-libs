bcb5158be06254fdb7be69c63371e9e78ed3112c
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-12-29 15:47:12 +0100

    Data frame example #1: loading data frame

---
+++ pandas/data_frame_01_loading.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+import pandas
+
+# přečtení zdrojových dat
+df = pandas.read_csv("tiobe.tsv", sep="\t")
+
+# specifikace indexu - má se získat ze sloupce Language
+df.set_index("Language", inplace=True)
+
+# datový rámec zobrazíme
+print(df)
+print()
+
+# podrobnější informace o datovém rámci
+print(df.dtypes)
+print()
+
+# více podrobnějších informací o datovém rámci
+print(df.info())
+print()
