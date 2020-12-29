bf3447df14d50c2f241849627066961004f65e4c
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-12-29 15:47:12 +0100

    Data frame example #4: column formatting using map

---
+++ pandas/data_frame_04_map_format.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+import pandas
+
+# přečtení zdrojových dat
+df = pandas.read_csv("tiobe.tsv", sep="\t")
+
+# specifikace indexu - má se získat ze sloupce Language
+df.set_index("Language", inplace=True)
+
+# formát hodnot ve sloupci
+df["Ratings"] = df["Ratings"].map("Rating is {:4.1f}%".format)
+
+# datový rámec zobrazíme
+print(df)
+print()
+
+# podrobnější informace o datovém rámci
+print(df.dtypes)
+print()
+
+# více podrobnějších informací o datovém rámci
+print(df.info())
+print()
