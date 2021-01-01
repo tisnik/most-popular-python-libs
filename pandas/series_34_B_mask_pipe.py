709a24f39a522ce0bfbc30e1a7f4578a8fe51df3
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-01-01 10:37:14 +0100

    Modified example #34

---
+++ pandas/series_34_B_mask_pipe.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+import pandas
+
+# přečtení zdrojových dat
+df = pandas.read_csv("tiobe.tsv", sep="\t")
+
+# specifikace indexu - má se získat ze sloupce Language
+df.set_index("Language", inplace=True)
+
+# pro jistotu si datový rámec zobrazíme
+print(df)
+print()
+
+# konstrukce datové struktury Series (datové řady) z datového rámce
+s = pandas.Series(df["Ratings"])
+
+# tisk obsahu původní datové řady
+print(s)
+print()
+
+# maskování hodnot
+results = s.mask(s > 10).mask(s < 2)
+
+# tisk výsledku
+print("Masked (min + max)")
+print(results)
