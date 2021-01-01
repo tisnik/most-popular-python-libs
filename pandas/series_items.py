cf44423dbfb73957e0e44a03a0693a48ae1074ac
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-01-01 10:43:40 +0100

    Iterate over items in serie

--- pandas/series_items.py
+++ pandas/series_items.py
@@ -11,6 +11,7 @@ df.set_index("Language", inplace=True)
 
 # pro jistotu si datový rámec zobrazíme
 print(df)
+print()
 
 # konstrukce datové struktury Series z datového rámce
 s = pandas.Series(df["Ratings"])
