5dd370ad0a9cc69928f22f4d51d09ff2aa1eb569
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-01-06 10:41:45 +0100

    Aggregation of non-normalized table

---
+++ pandas/data_frame_group_by_11_kurzy.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+import pandas
+
+# přečtení zdrojových dat s jejich konverzí do datového rámce
+df = pandas.read_csv("denni_kurz2.txt", sep="|", skiprows=0)
+
+# převod číselných hodnot
+df["kurz"] = pandas.to_numeric(df["kurz"].str.replace(',','.'), errors='coerce')
+
+# datový rámec zobrazíme
+print(df)
+print()
+
+print(df.describe())
