d4364add2bebb6c724d709059c3efb29fed5f1f9
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-01-06 10:42:13 +0100

    Transformation of non-normalized table

---
+++ pandas/data_frame_group_by_14_transform.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+import pandas
+import numpy as np
+
+# přečtení zdrojových dat s jejich konverzí do datového rámce
+df = pandas.read_csv("denni_kurz2.txt", sep="|", skiprows=0)
+
+# převod číselných hodnot
+df["kurz"] = pandas.to_numeric(df["kurz"].str.replace(',','.'), errors='coerce')
+
+df["datum"] = pandas.to_datetime(df["datum"])
+
+# datový rámec zobrazíme
+print(df)
+print()
+
+# rozdělení do skupin podle data
+gb = df.groupby(["kód"])
+
+# nový sloupec
+df["rozdíl"]=gb["kurz"].transform(lambda x:x.max()-x.min())
+
+print(df[0:10])
+print()
+print(df[33:43])
