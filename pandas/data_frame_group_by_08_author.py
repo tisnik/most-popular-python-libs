fb8412068da3dbc5c3a99fcc3fc9cf3f1a28445b
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-01-07 08:25:44 +0100

    Group by data frame by author

--- pandas/data_frame_group_by_08_author.py
+++ pandas/data_frame_group_by_08_author.py
@@ -4,20 +4,20 @@
 import pandas
 import pprint
 
-# přečtení zdrojových dat
+# přečtení zdrojových dat s jejich konverzí do datového rámce
 df = pandas.read_csv("git_log.txt", sep="|")
 
 # datový rámec zobrazíme
 print(df)
 print()
 
-# rozdělení do skupin
+# rozdělení do skupin podle autora (jednoho sloupce)
 gb = df.groupby(["Author"])
 
-# zobrazení skupin
+# zobrazení skupin vytvořených v rámci předchozího kroku
 print("Group by author")
 pprint.pprint(gb.groups)
 
-# počet skupin
+# zobrazení počtu skupin
 print()
 print("Number of groups:", len(gb))
