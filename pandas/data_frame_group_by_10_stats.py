b892b6a5139391560af61e8db5c2c46d22422405
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-01-07 08:26:18 +0100

    Basic statistic about data frame

--- pandas/data_frame_group_by_10_stats.py
+++ pandas/data_frame_group_by_10_stats.py
@@ -4,18 +4,19 @@
 import pandas
 import numpy as np
 
-# přečtení zdrojových dat
+# přečtení zdrojových dat s jejich konverzí do datového rámce
 df = pandas.read_csv("git_log.txt", sep="|")
 
 # datový rámec zobrazíme
 print(df)
 print()
 
-# rozdělení do skupin
+# rozdělení do skupin podle data, autora i podle obou sloupců
 gb1 = df.groupby(["Date"])
 gb2 = df.groupby(["Author"])
 gb3 = df.groupby(["Date", "Author"])
 
+# zobrazení velikosti záznamů rozdělených do skupin
 print(gb1.agg(np.size))
 print(gb2.agg(np.size))
 print(gb3.agg(np.size))
