3890e2ec4fdbe4239f88547694eba379a494c88b
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-04-02 09:22:15 +0200

    Fixed issue #519

--- gc_objgraph/objgraph4.py
+++ gc_objgraph/objgraph4.py
@@ -3,7 +3,7 @@
 import objgraph
 
 x = "Foo"
-y = [x, "bar", [x], (x, x), {"x":x}]
+y = [x, "bar", [x], (x, x), {"x": x}]
 
 # reference u složitějších datových struktur
 objgraph.show_refs([x, y], filename='objgraph4.png')
