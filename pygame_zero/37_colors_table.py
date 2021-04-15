6cf9a2143c7e79224eafb577b76f8de2252fdc95
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-04-15 07:53:00 +0200

    Added license header into 37_colors_table.py

--- pygame_zero/37_colors_table.py
+++ pygame_zero/37_colors_table.py
@@ -1,3 +1,15 @@
+#
+#  (C) Copyright 2020  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
 from pygame.colordict import THECOLORS
 
 for i, color in enumerate(sorted(THECOLORS)):
