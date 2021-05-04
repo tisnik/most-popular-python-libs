5ed2356e60cc27e666cff9f3c98ffc04f5d4ccf3
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-05-04 07:46:31 +0200

    Display image on terminal, using foreground character

---
+++ blessed/show_image_1.py
@@ -0,0 +1,18 @@
+from PIL import Image
+import blessed
+
+terminal = blessed.Terminal()
+
+terminal.number_of_colors = 1 << 24
+
+filename = "fruits.png"
+img = Image.open(filename)
+
+for j in range(0, img.height, 2):
+    for i in range(img.width):
+        red, green, blue = img.getpixel((i, j))
+        print(f"{terminal.color_rgb(red, green, blue)}█", end="")
+    print()
+
+print()
+print(f"{terminal.normal}DONE")
