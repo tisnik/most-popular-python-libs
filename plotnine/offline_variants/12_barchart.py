32e675e4d898bd8d6ab7812df81de03deb43a946
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-05-13 10:19:03 +0200

    Fixed issue #613

--- plotnine/offline_variants/12_barchart.py
+++ plotnine/offline_variants/12_barchart.py
@@ -1,6 +1,6 @@
 from plotnine.data import mpg
 from plotnine import ggplot, aes, geom_bar
 
-g=(ggplot(mpg) + aes(x="class") + geom_bar())
+g = (ggplot(mpg) + aes(x="class") + geom_bar())
 
 g.save("12.png")
