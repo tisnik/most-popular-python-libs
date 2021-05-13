daf92e1d7557de8923f79ec8eb2af2388ce4e508
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-05-13 10:25:04 +0200

    Fixed issue #619

--- plotnine/offline_variants/17_xyplot_more_variables_smooth.py
+++ plotnine/offline_variants/17_xyplot_more_variables_smooth.py
@@ -1,7 +1,7 @@
 from plotnine.data import mpg
 from plotnine import ggplot, aes, facet_grid, labs, geom_point, stat_smooth
 
-g=(ggplot(mpg)
+g = (ggplot(mpg)
       + facet_grid(facets="year~class")
       + aes(x="displ", y="hwy")
       + labs(
