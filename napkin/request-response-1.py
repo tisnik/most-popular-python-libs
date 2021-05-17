4cd6bb447d989d15ecde0f08558d7c880d824ac9
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-05-17 08:29:02 +0200

    Napkin request-response example #1

---
+++ napkin/request-response-1.py
@@ -0,0 +1,20 @@
+import napkin
+
+
+@napkin.seq_diagram()
+def request_response_1(c):
+    client = c.object('Client')
+    frontend = c.object('"Front end"')
+    backend = c.object('"Back end"')
+
+    with client:
+        with frontend.request("login='foo'", "password='bar'"):
+            with backend.try_login("'foo'", "'bar'"):
+                c.ret("login ok")
+            c.ret("login ok")
+
+    with client:
+        with frontend.request("login='foo'", "password='xyzzy'"):
+            with backend.try_login("'foo'", "'bar'"):
+                c.ret("login failed")
+            c.ret("login failed")
