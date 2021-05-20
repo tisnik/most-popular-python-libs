e00e977e88b93802251470cfe226324ec81ada4e
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-05-20 11:18:03 +0200

    Fixed issue #618

--- plotnine/offline_variants/16_xyplot_more_variables.py
+++ plotnine/offline_variants/16_xyplot_more_variables.py
@@ -2,12 +2,12 @@ from plotnine.data import mpg
 from plotnine import ggplot, aes, facet_grid, labs, geom_point, stat_smooth
 
 g = (ggplot(mpg)
-      + facet_grid(facets="year~class")
-      + aes(x="displ", y="hwy")
-      + labs(
-          x="Engine Size",
-          y="Miles per Gallon",
-          title="Miles per Gallon for Each Year and Vehicle Class")
-      + geom_point())
+     + facet_grid(facets="year~class")
+     + aes(x="displ", y="hwy")
+     + labs(
+         x="Engine Size",
+         y="Miles per Gallon",
+         title="Miles per Gallon for Each Year and Vehicle Class")
+     + geom_point())
 
 g.save("16.png")
