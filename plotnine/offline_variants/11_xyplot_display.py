89a553ebde6cd56fbb7d62bdf77da806ea04ed13
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-05-20 11:17:52 +0200

    Fixed issue #612

--- plotnine/offline_variants/11_xyplot_display.py
+++ plotnine/offline_variants/11_xyplot_display.py
@@ -2,8 +2,8 @@ from plotnine import ggplot, geom_point, aes, stat_smooth, facet_wrap
 from plotnine.data import mtcars
 
 g = (ggplot(mtcars, aes('wt', 'mpg', color='factor(gear)'))
-      + geom_point()
-      + stat_smooth(method='lm')
-      + facet_wrap('~gear'))
+     + geom_point()
+     + stat_smooth(method='lm')
+     + facet_wrap('~gear'))
 
 g.save("11.png")
