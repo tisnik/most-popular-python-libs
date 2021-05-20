458ef98d8c6e82c92244d91d5b9be62e46b7f3a1
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-05-20 11:52:53 +0200

    Fixed issue #607

--- plotnine/19_smoothing_span_0_5.py
+++ plotnine/19_smoothing_span_0_5.py
@@ -2,8 +2,8 @@ from plotnine.data import economics
 from plotnine import ggplot, aes, facet_grid, labs, geom_point, geom_smooth, xlab, ylab
 
 print(ggplot(economics)
-        + aes(x="date", y="uempmed")
-        + geom_point()
-        + geom_smooth(color="red", span=0.5)
-        + xlab("date (year)")
-        + ylab("unemploynment"))
+      + aes(x="date", y="uempmed")
+      + geom_point()
+      + geom_smooth(color="red", span=0.5)
+      + xlab("date (year)")
+      + ylab("unemploynment"))
