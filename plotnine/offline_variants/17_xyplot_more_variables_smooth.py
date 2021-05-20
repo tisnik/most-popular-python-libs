f71b36af79cd15fbd1337de3ec8d690b29238cce
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-05-20 11:18:22 +0200

    Fixed issue #620

--- plotnine/offline_variants/17_xyplot_more_variables_smooth.py
+++ plotnine/offline_variants/17_xyplot_more_variables_smooth.py
@@ -2,13 +2,13 @@ from plotnine.data import mpg
 from plotnine import ggplot, aes, facet_grid, labs, geom_point, stat_smooth
 
 g = (ggplot(mpg)
-      + facet_grid(facets="year~class")
-      + aes(x="displ", y="hwy")
-      + labs(
-          x="Engine Size",
-          y="Miles per Gallon",
-          title="Miles per Gallon for Each Year and Vehicle Class")
-      + geom_point()
-      + stat_smooth(method='lm'))
+     + facet_grid(facets="year~class")
+     + aes(x="displ", y="hwy")
+     + labs(
+         x="Engine Size",
+         y="Miles per Gallon",
+         title="Miles per Gallon for Each Year and Vehicle Class")
+     + geom_point()
+     + stat_smooth(method='lm'))
 
 g.save("17.png")
