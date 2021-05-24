5befb020002bda0c6f6317ec23f4eb9f86712a93
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-05-24 08:42:27 +0200

    Architecture diagram #5

---
+++ diagrams/onprem5.py
@@ -0,0 +1,28 @@
+from diagrams import Diagram
+from diagrams.onprem.queue import Kafka, ActiveMQ
+from diagrams.programming.language import Go, Rust
+from diagrams.aws.database import RDS
+
+with Diagram("OnPrem #5", show=True, direction="TB"):
+    # definice uzlu
+    consumer = Kafka("input stream")
+
+    db = RDS("storage")
+
+    # rozvetveni
+    workersA = [Go("worker #1"),
+                Go("worker #2"),
+                Go("worker #3")]
+
+    buffer = ActiveMQ("buffer")
+
+    # rozvetveni
+    workersB = [Rust("worker #1"),
+                Rust("worker #2"),
+                Rust("worker #3")]
+
+    producer = Kafka("output stream")
+
+    # propojeni uzlu grafu orientovanymi hranami
+    consumer >> workersA >> buffer >> workersB >> producer
+    db >> workersA
