adc0562c9277cf0d09b31b90924eb2f2893bf310
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-05-26 13:28:42 +0200

    Diagram with clusters, 3rd variant

---
+++ diagrams/clusters3.py
@@ -0,0 +1,33 @@
+from diagrams import Diagram
+from diagrams import Cluster
+from diagrams.onprem.queue import Kafka, ActiveMQ
+from diagrams.programming.language import Go, Rust
+from diagrams.aws.database import RDS
+
+with Diagram("Clusters #3", show=True, direction="LR"):
+    # definice clusteru
+    with Cluster("Input processor"):
+        # definice uzlu v clusteru
+        consumer = Kafka("input stream")
+
+        # rozvetveni
+        workersA = [Go("worker #1"),
+                    Go("worker #2"),
+                    Go("worker #3")]
+
+        db = RDS("storage")
+
+    # definice uzlu mimo cluster
+    buffer = ActiveMQ("buffer")
+
+    with Cluster("Output processor"):
+        # rozvetveni
+        workersB = [Rust("worker #1"),
+                    Rust("worker #2"),
+                    Rust("worker #3")]
+
+        producer = Kafka("output stream")
+
+    # propojeni uzlu grafu orientovanymi hranami
+    consumer >> workersA >> buffer >> workersB >> producer
+    db >> workersA
