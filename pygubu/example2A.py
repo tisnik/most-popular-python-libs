d443008c0224c4d0032dcc46a4866f572ce30de7
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-06-03 15:00:26 +0200

    Fixed issue #821

--- pygubu/example2A.py
+++ pygubu/example2A.py
@@ -1,5 +1,16 @@
 """Pygubu and Tkinter: main menu in main window (not working properly)."""
 
+#
+#  (C) Copyright 2020  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
 # example2A.py
 
 import tkinter as tk
