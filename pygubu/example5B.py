86e3eae6203bf53548b202a78bad7961d92844cd
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-06-03 15:01:05 +0200

    Fixed issue #826

--- pygubu/example5B.py
+++ pygubu/example5B.py
@@ -1,5 +1,17 @@
 """Pygubu and Tkinter: main menu in main window, callback functions, tearoff menu (working example)."""
 
+#
+#  (C) Copyright 2020  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
 # Example5.py
 
 import tkinter as tk
