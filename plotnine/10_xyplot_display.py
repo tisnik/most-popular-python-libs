fca798fa9b921b9b44709c73e998d31c00aedb8b
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-06-07 11:02:38 +0200

    Fixed issue #701

--- plotnine/10_xyplot_display.py
+++ plotnine/10_xyplot_display.py
@@ -10,7 +10,7 @@
 #      Pavel Tisnovsky
 #
 
-from plotnine import ggplot, geom_point, aes, stat_smooth, facet_wrap
+from plotnine import ggplot, geom_point, aes, stat_smooth
 from plotnine.data import mtcars
 
 print(ggplot(mtcars, aes('wt', 'mpg', color='factor(gear)'))
