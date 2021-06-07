961bc0dd9aaa75c5ac2f83461e16f1e5c565a827
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-06-07 10:46:34 +0200

    Fixed issue #842

--- edn/print_edn2.py
+++ edn/print_edn2.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python3
 
+#
+#  (C) Copyright 2021  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
 # Converts structured data from EDN format into JSON format.
 
 import sys
