f58f1fa6c0bb98cb302e40bb8b7aa116dc6e4c32
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-06-15 15:56:08 +0200

    Fixed issue #868

--- blessed/unicode_test.py
+++ blessed/unicode_test.py
@@ -1,3 +1,15 @@
+#
+#  (C) Copyright 2021  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
 # Based on famous UTF-8-demo.txt created by:
 # Markus Kuhn [ˈmaʳkʊs kuːn] <http://www.cl.cam.ac.uk/~mgk25/> — 2002-07-25
 
