13a2e9da6344d6b8701485c42851a280fcc0b321
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-06-17 13:10:07 +0200

    Fixed issue #899

--- pygame/pygame28.py
+++ pygame/pygame28.py
@@ -7,6 +7,17 @@
 #                   a operace pygame.transform.rotate()
 #                   + pygame.transform.rotozoom().
 
+#
+#  (C) Copyright 2020  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
 
 import pygame
 import sys
