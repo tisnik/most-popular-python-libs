afe9ec524e74574d973555e00258522412d0311f
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-06-17 13:10:07 +0200

    Fixed issue #881

--- pygame/pygame10.py
+++ pygame/pygame10.py
@@ -5,6 +5,17 @@
 
 # Příklad číslo 10: použití objektů typu Surface a metoda blit().
 
+#
+#  (C) Copyright 2020  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
 
 import pygame
 import sys
