de8ec126500f682fe137f5467f8d3fc8380c31a4
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-06-17 13:10:07 +0200

    Fixed issue #874

--- pygame/pygame03.py
+++ pygame/pygame03.py
@@ -6,6 +6,17 @@
 # Příklad číslo 3: další ze způsobů snížení zatížení
 #                  procesoru v hlavní smyčce událostí
 
+#
+#  (C) Copyright 2020  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
 
 import pygame
 import sys
