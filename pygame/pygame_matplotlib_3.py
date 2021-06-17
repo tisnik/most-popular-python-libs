3b288439c535eabc66b80b2aa27ffe63c9fd938c
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-06-17 13:10:07 +0200

    Fixed issue #902

--- pygame/pygame_matplotlib_3.py
+++ pygame/pygame_matplotlib_3.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python
 # vim: set fileencodings=utf-8
 
+#
+#  (C) Copyright 2020  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
 import matplotlib
 matplotlib.use("Agg")  # noqa: E402
 import matplotlib.pyplot as plt
