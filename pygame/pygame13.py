f078f8aefc8efa7c6534a7b4669551d118436e49
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-06-17 13:10:07 +0200

    Fixed issue #884

--- pygame/pygame13.py
+++ pygame/pygame13.py
@@ -6,6 +6,17 @@
 # Příklad číslo 13: konverze objektů typu Surface.
 #                   Zachování alfa kanálu.
 
+#
+#  (C) Copyright 2020  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
 
 import pygame
 import sys
