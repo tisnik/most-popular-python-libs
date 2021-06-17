72af75eacb15fd3d5a24d2f390e496d5fabeae82
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-06-17 13:10:07 +0200

    Fixed issue #897

--- pygame/pygame26.py
+++ pygame/pygame26.py
@@ -6,6 +6,17 @@
 # Příklad číslo 26: použití objektů typu Surface, metoda blit()
 #                   a operace pygame.transform.scale().
 
+#
+#  (C) Copyright 2020  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
 
 import pygame
 import sys
