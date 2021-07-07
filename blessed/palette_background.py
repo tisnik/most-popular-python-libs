1b961c24658c7519a1a163e2009caece21de9739
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-07-07 08:45:18 +0200

    Fixed issue #923

--- blessed/palette_background.py
+++ blessed/palette_background.py
@@ -1,3 +1,5 @@
+"""24-bit palette in terminal - specifying background color."""
+
 #
 #  (C) Copyright 2021  Pavel Tisnovsky
 #
@@ -12,6 +14,7 @@
 
 import blessed
 
+# initialize terminal
 terminal = blessed.Terminal()
 
 terminal.number_of_colors = 1 << 24
