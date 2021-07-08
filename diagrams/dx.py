6838054da4c7c158c97e2e26e4b14f73843b4f6e
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-07-08 07:58:52 +0200

    Fixed issue #935

--- diagrams/dx.py
+++ diagrams/dx.py
@@ -1,3 +1,15 @@
+#
+#  (C) Copyright 2021  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
 from diagrams import Cluster, Diagram
 from diagrams.aws.compute import ECS, EKS, Lambda
 from diagrams.aws.database import Redshift
