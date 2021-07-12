f2da40d517dd4273979e2bb014fca0bd623c6ff9
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-07-12 08:03:17 +0200

    Added comments into get_image.py

--- blessed/get_image.py
+++ blessed/get_image.py
@@ -15,13 +15,20 @@
 from PIL import Image
 import urllib.request
 
+# URL to the test image
 url = "https://homepages.cae.wisc.edu/~ece533/images/fruits.png"
 
 original_filename = "fruits_.png"
 resized_filename = "fruits.png"
 
+# try to retrieve the test image from given URL
 urllib.request.urlretrieve(url, original_filename)
 
+# read image from file
 img = Image.open(original_filename)
+
+# resize it
 resized = img.resize((128, 128), Image.BILINEAR)
+
+# and store under new name
 resized.save(resized_filename)
