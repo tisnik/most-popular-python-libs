8f92a2b0618793df1881bc0f36023cb480a3bf49
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-07-13 08:20:43 +0200

    Updated and added comments into onprem5.py

--- diagrams/onprem5.py
+++ diagrams/onprem5.py
@@ -15,24 +15,28 @@ from diagrams.onprem.queue import Kafka, ActiveMQ
 from diagrams.programming.language import Go, Rust
 from diagrams.aws.database import RDS
 
+# definice diagramu se specifikaci jeho zakladnich vlastnosti
 with Diagram("OnPrem #5", show=True, direction="TB"):
-    # definice uzlu
+    # definice uzlu - konzument
     consumer = Kafka("input stream")
 
+    # definice uzlu - databaze
     db = RDS("storage")
 
-    # rozvetveni
+    # rozvetveni - vetsi mnozstvi workeru
     workersA = [Go("worker #1"),
                 Go("worker #2"),
                 Go("worker #3")]
 
+    # buffer vlozeny mezi skupiny workeru
     buffer = ActiveMQ("buffer")
 
-    # rozvetveni
+    # rozvetveni - vetsi mnozstvi workeru
     workersB = [Rust("worker #1"),
                 Rust("worker #2"),
                 Rust("worker #3")]
 
+    # definice uzlu - producent
     producer = Kafka("output stream")
 
     # propojeni uzlu grafu orientovanymi hranami
