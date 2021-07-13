c8edeb899a4c776e3fe879142d5bc1c8eefed074
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-07-13 08:20:43 +0200

    Updated and added comments into dx.py

--- diagrams/dx.py
+++ diagrams/dx.py
@@ -16,25 +16,36 @@ from diagrams.aws.database import Redshift
 from diagrams.aws.integration import SQS
 from diagrams.aws.storage import S3
 
+# definice diagramu se specifikaci jeho zakladnich vlastnosti
 with Diagram("Event Processing", show=False):
     source = EKS("k8s source")
 
+    # definice clusteru
     with Cluster("Event Flows"):
+        # definice clusteru uvnitr clusteru
         with Cluster("Event Workers"):
             workers = [ECS("worker1"),
                        ECS("worker2"),
                        ECS("worker3")]
 
+        # definice uzlu
         queue = SQS("event queue")
 
+        # definice clusteru
         with Cluster("Processing"):
             handlers = [Lambda("proc1"),
                         Lambda("proc2"),
                         Lambda("proc3")]
 
+    # definice uzlu
     store = S3("events store")
     dw = Redshift("analytics")
 
+    # propojeni uzlu orientovanymi hranami
     source >> workers >> queue >> handlers
+
+    # dalsi propojeni uzlu orientovanymi hranami
     handlers >> store
+
+    # dalsi propojeni uzlu orientovanymi hranami
     handlers >> dw
