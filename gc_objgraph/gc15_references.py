2b611444ff664d816e44c0b8ddeba4ac796ac3f4
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-07-15 08:13:31 +0200

    Added license header into gc15_references.py

--- gc_objgraph/gc15_references.py
+++ gc_objgraph/gc15_references.py
@@ -1,5 +1,17 @@
 """Example that retrieves and prints reference counts."""
 
+#
+#  (C) Copyright 2021  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
 import gc
 import sys
 import pprint
