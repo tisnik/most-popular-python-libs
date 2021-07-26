57745da8e58e748ad1d999d428473798c81c3b9b
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-07-26 11:04:13 +0200

    Fixed typo

--- gc_objgraph/lsize2.py
+++ gc_objgraph/lsize2.py
@@ -8,5 +8,5 @@ for i in range(31):
     test_list.append(i)
 
 for i in range(31):
-    print(len(l), sys.getsizeof(test_list))
+    print(len(test_list), sys.getsizeof(test_list))
     del test_list[-1]
