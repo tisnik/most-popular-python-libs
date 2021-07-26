94cb3920c6e1052aa04ac74bc068daab41a7f094
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-07-26 11:02:36 +0200

    Added license

--- numpy/harmonic_fp32.py
+++ numpy/harmonic_fp32.py
@@ -1,5 +1,17 @@
 """Investigation of basic properties of float32 floating point numbers."""
 
+#
+#  (C) Copyright 2021  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
 import numpy as np
 
 h1 = np.float32(0)
