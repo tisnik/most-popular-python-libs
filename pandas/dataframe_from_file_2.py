40f4aef846a005d4487c815063579591a325f1f8
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-10-25 08:14:03 +0200

    Reading data frame from file

---
+++ pandas/dataframe_from_file_2.py
@@ -0,0 +1,22 @@
+"""Načtení obsahu datového rámce z binárního souboru se specifikací formátu."""
+
+import numpy as np
+import pandas as pd
+
+dt = np.dtype([
+    ("i", "u4"),
+    ("x", "f4")
+    ])
+
+np_data = np.fromfile("binary_df_1.bin", dtype=dt)
+print(np_data)
+print(np_data.ndim)
+print(np_data.dtype)
+
+df = pd.DataFrame(np_data)
+
+print(df)
+print()
+print(df.info())
+print()
+print(df.describe())
