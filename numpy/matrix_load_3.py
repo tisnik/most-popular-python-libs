2b9dad492333f4f736fa5a3905065982138baefd
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-11-01 07:36:01 +0100

    Load matrix from standard file, example #3

---
+++ numpy/matrix_load_3.py
@@ -0,0 +1,16 @@
+"""Přečtení obsahu matice ze standardního binárního souboru."""
+
+import numpy as np
+
+# načtení matice ze standardního binárního souboru
+m = np.load("matrix3.npy")
+
+# zobrazení obsahu matice
+print(m)
+print()
+
+# zobrazení dalších informací o matici
+print("Dimensios:", m.ndim)
+print("Data type:", m.dtype)
+print("Item size:", m.itemsize, "bytes")
+print("Array size:", m.size, "items")
