ce10e4e1ba30960a178133f40d8f6b872b97fba4
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-11-01 07:36:27 +0100

    Save matrix into standard binary file

---
+++ numpy/matrix_save_3.py
@@ -0,0 +1,16 @@
+"""Uložení obsahu matice do standardního binárního souboru."""
+
+import numpy as np
+
+# rozměry matice
+rows = 1000
+columns = 5
+
+# matice obsahující celočíselné hodnoty typu integer
+m = np.linspace(1, rows*columns, rows*columns, dtype="i").reshape(rows, columns)
+
+# tisk obsahu zkonstruované matice
+print(m)
+
+# uložení matice do souboru ve standardním formátu
+np.save("matrix3.npy", m, allow_pickle=False)
