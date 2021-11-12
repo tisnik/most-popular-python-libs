d7a078b14e23a35e1929569356fba14a1eff7e06
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-11-12 10:07:02 +0100

    Empty worksheet

---
+++ XlsxWriter/01_empty_worksheet.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+"""Vytvoření prázdného sešitu."""
+
+import xlsxwriter
+
+# vytvoření objektu reprezentujícího celý sešit
+workbook = xlsxwriter.Workbook('example01.xlsx')
+
+# vložení nového listu do sešitu
+worksheet = workbook.add_worksheet()
+
+# explicitní uzavření sešitu
+workbook.close()
