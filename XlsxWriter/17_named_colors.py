06e0d08d078387053825229a349ababdecd48ddf
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-11-12 10:07:02 +0100

    Named colors

---
+++ XlsxWriter/17_named_colors.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+"""Vytvoření sešitu s delší tabulkou, nastavení barvy textu."""
+
+import xlsxwriter
+
+# vytvoření objektu reprezentujícího celý sešit
+with xlsxwriter.Workbook('example17.xlsx') as workbook:
+    # vložení nového listu do sešitu
+    worksheet = workbook.add_worksheet()
+
+    # nastavení šířky sloupců
+    worksheet.set_column('A:A', 20)
+
+    # barvy buněk
+    colors = (
+            'black',
+            'blue',
+            'brown',
+            'cyan',
+            'gray',
+            'green',
+            'lime',
+            'magenta',
+            'navy',
+            'orange',
+            'pink',
+            'purple',
+            'red',
+            'silver',
+            'white',
+            'yellow'
+            )
+
+    # buňky s řetězci a různou barvou textu
+    for x, color in enumerate(colors):
+        style = workbook.add_format()
+        style.set_font_color(color)
+        worksheet.write_string(x, 0, color, style)
+
+    # sešit bude uzavřen automaticky
