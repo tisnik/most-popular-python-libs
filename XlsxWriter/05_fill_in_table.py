6dcdd7e0a8171c814408883cb8a5ddad5bb91f56
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-11-12 10:07:02 +0100

    Fill in table

---
+++ XlsxWriter/05_fill_in_table.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+"""Vytvoření sešitu s delší tabulkou."""
+
+import xlsxwriter
+
+# vytvoření objektu reprezentujícího celý sešit
+with xlsxwriter.Workbook('example05.xlsx') as workbook:
+    # vložení nového listu do sešitu
+    worksheet = workbook.add_worksheet()
+
+    # buňky s textem
+    worksheet.write('A1', 'x')
+    worksheet.write('B1', '1/x')
+
+    # buňky s numerickými hodnotami
+    for x in range(1, 21):
+        worksheet.write(x, 0, x)
+        worksheet.write(x, 1, 1.0/x)
+
+    # sešit bude uzavřen automaticky
