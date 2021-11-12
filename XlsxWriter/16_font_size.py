62acca123f6222abba250bae0068a5023452e31e
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-11-12 10:07:02 +0100

    Font size

---
+++ XlsxWriter/16_font_size.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+"""Vytvoření sešitu s delší tabulkou, změna velikosti fontu."""
+
+import xlsxwriter
+
+# vytvoření objektu reprezentujícího celý sešit
+with xlsxwriter.Workbook('example16.xlsx') as workbook:
+    # vložení nového listu do sešitu
+    worksheet = workbook.add_worksheet()
+
+    # nastavení šířky sloupců
+    worksheet.set_column('A:A', 20)
+
+    # buňky s numerickými hodnotami a různou velikostí textu
+    for x in range(1, 21):
+        style = workbook.add_format()
+        style.set_font_size(x)
+        worksheet.write_number(x, 0, x, style)
+
+    # sešit bude uzavřen automaticky
