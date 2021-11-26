eda5a44d3029f4460ba4151c6c4cf3df12ad4a15
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-11-26 17:50:39 +0100

    Merged already merged cells

---
+++ XlsxWriter/28_merged_cells_error.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+"""Pokus o vytvoření sešitu se sloučenými buňkami."""
+
+import xlsxwriter
+
+# vytvoření objektu reprezentujícího celý sešit
+with xlsxwriter.Workbook('example28.xlsx') as workbook:
+    # vložení nového listu do sešitu
+    worksheet = workbook.add_worksheet()
+
+    # první formát použitý pro zvýraznění sloučené buňky
+    format1 = workbook.add_format({
+        'bold': 1,
+        'border': 1,
+        'align': 'center',
+        'valign': 'vcenter',
+        'fg_color': '#ff8080'})
+
+    # druhý formát použitý pro zvýraznění sloučené buňky
+    format2 = workbook.add_format({
+        'bold': 1,
+        'border': 1,
+        'align': 'center',
+        'valign': 'vcenter',
+        'fg_color': '#80ff80'})
+
+    # sloučená buňka
+    worksheet.merge_range('B2:D2', 'Sloučená buňka', format1)
+
+    # sloučená buňka
+    worksheet.merge_range('B2:B4', 'Sloučená\nbuňka', format2)
+
+    # sešit bude uzavřen automaticky
