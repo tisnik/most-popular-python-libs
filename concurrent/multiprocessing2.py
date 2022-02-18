b33a6b719384e8a7012b2364a0a0238c218b55ef
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-02-18 17:24:07 +0100

    Multiprocessing #2

---
+++ concurrent/multiprocessing2.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+from multiprocessing import Process
+import time
+
+
+def worker(name):
+    print("hello", name)
+    time.sleep(5)
+    print("done", name)
+
+
+def main():
+    ps = []
+
+    for name in ("foo", "bar", "baz", "other"):
+        p = Process(target=worker, args=(name,))
+        p.start()
+        ps.append(p)
+
+    for p in ps:
+        p.join()
+
+
+if __name__ == '__main__':
+    print("Running main")
+    main()
