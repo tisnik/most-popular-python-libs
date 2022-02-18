2a05fd9d81ef0f06a9e75705787cb1818fdda5fb
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-02-18 17:24:07 +0100

    Multiprocessing #1

---
+++ concurrent/multiprocessing1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+from multiprocessing import Process
+
+
+def worker(name):
+    print("hello", name)
+
+
+def main():
+    p = Process(target=worker, args=("foo",))
+    p.start()
+    p.join()
+
+
+if __name__ == '__main__':
+    print("Running main")
+    main()
