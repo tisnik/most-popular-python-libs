38c78036c5d9cb7c5f6b4cbdaf31836697225f15
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-02-18 17:24:07 +0100

    Multithreading no join no daemon

---
+++ concurrent/multithreading_no_join_no_deamon.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import threading
+import time
+
+
+def worker():
+    threadName = threading.current_thread().name
+    delay = 1
+    n = 10
+    for counter in range(1, n+1):
+        time.sleep(delay)
+        print("{}: {}/{} - {}".format(threadName, counter, n, time.ctime(time.time())))
+
+
+# vytvoření a spuštění trojice vláken
+threading.Thread(target=worker).start()
+threading.Thread(target=worker).start()
+threading.Thread(target=worker).start()
+
+# automaticky se čeká na dokončení vláken
