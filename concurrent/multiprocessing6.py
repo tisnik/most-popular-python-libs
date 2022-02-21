10b7b04c70a092335e71db311665f1034a9ad27c
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-02-21 08:59:42 +0100

    6th example about multiprocessing

---
+++ concurrent/multiprocessing6.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+from multiprocessing import Process
+import time
+
+
+def worker(name, dictionary):
+    dictionary[name] = "ok"
+    print("hello", name)
+    time.sleep(5)
+    print("done", name)
+
+
+def main():
+    d = {}
+
+    for name in ("foo", "bar", "baz", "other"):
+        worker(name, d)
+
+    print(d)
+
+
+if __name__ == '__main__':
+    print("Running main")
+    main()
