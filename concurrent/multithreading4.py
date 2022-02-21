1106de8741325030dc9189bd4d8afce04b684648
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-02-21 09:05:04 +0100

    Reformatted by black

--- concurrent/multithreading4.py
+++ concurrent/multithreading4.py
@@ -1,27 +1,27 @@
 #!/usr/bin/env python3
- 
+
 """Multithreading."""
- 
+
 import threading
 import time
- 
-class X():
 
+
+class X:
     def __init__(self):
         print("X constructed")
 
     def __del__(self):
         print("X destructed")
 
- 
+
 def worker():
     print("thread started")
     time.sleep(10)
     print("thread finished")
- 
- 
+
+
 print("main started")
- 
+
 x = X()
 
 # vytvoření a spuštění trojice vláken
