12a21597c9429f1318ce3ec2c172f559bf600933
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-02-21 09:05:04 +0100

    Reformatted by black

--- concurrent/multithreading1.py
+++ concurrent/multithreading1.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python3
- 
+
 """Multithreading."""
- 
+
 import threading
 import time
- 
- 
+
+
 def worker():
     threadName = threading.current_thread().name
     delay = 1
     n = 10
-    for counter in range(1, n+1):
+    for counter in range(1, n + 1):
         time.sleep(delay)
         print("{}: {}/{} - {}".format(threadName, counter, n, time.ctime(time.time())))
- 
- 
+
+
 # vytvoření a spuštění trojice vláken
 threading.Thread(target=worker).start()
 threading.Thread(target=worker).start()
