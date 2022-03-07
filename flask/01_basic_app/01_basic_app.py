d6f12d035177dfa85f83834c4d115db0bd8d6e42
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-07 09:45:00 +0100

    Reformatted by black

--- flask/01_basic_app/01_basic_app.py
+++ flask/01_basic_app/01_basic_app.py
@@ -20,7 +20,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def hello_world():
     """Implement handler for the / endpoint."""
-    return 'Hello, World!\n'
+    return "Hello, World!\n"
