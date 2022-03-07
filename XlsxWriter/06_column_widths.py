e4e3a586da20ca45ed066841acd568d5f164dd44
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-07 09:44:59 +0100

    Reformatted by black

--- XlsxWriter/06_column_widths.py
+++ XlsxWriter/06_column_widths.py
@@ -6,22 +6,22 @@
 import xlsxwriter
 
 # vytvoření objektu reprezentujícího celý sešit
-with xlsxwriter.Workbook('example06.xlsx') as workbook:
+with xlsxwriter.Workbook("example06.xlsx") as workbook:
     # vložení nového listu do sešitu
     worksheet = workbook.add_worksheet()
 
     # nastavení šířky sloupců
-    worksheet.set_column('A:A', 8)
-    worksheet.set_column('B:B', 14)
-    worksheet.set_column('C:Z', 2)
+    worksheet.set_column("A:A", 8)
+    worksheet.set_column("B:B", 14)
+    worksheet.set_column("C:Z", 2)
 
     # buňky s textem
-    worksheet.write('A1', 'x')
-    worksheet.write('B1', '1/x')
+    worksheet.write("A1", "x")
+    worksheet.write("B1", "1/x")
 
     # buňky s numerickými hodnotami
     for x in range(1, 21):
         worksheet.write(x, 0, x)
-        worksheet.write(x, 1, 1.0/x)
+        worksheet.write(x, 1, 1.0 / x)
 
     # sešit bude uzavřen automaticky
