3ac6616e884d8cfc8dec0bd74636f2687f111000
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-07 09:44:59 +0100

    Reformatted by black

--- XlsxWriter/10_format_for_row.py
+++ XlsxWriter/10_format_for_row.py
@@ -6,30 +6,30 @@
 import xlsxwriter
 
 # vytvoření objektu reprezentujícího celý sešit
-with xlsxwriter.Workbook('example10.xlsx') as workbook:
+with xlsxwriter.Workbook("example10.xlsx") as workbook:
     # vložení nového listu do sešitu
     worksheet = workbook.add_worksheet()
 
     # nastavení šířky sloupců
-    worksheet.set_column('A:A', 8)
-    worksheet.set_column('B:B', 14)
-    worksheet.set_column('C:Z', 2)
+    worksheet.set_column("A:A", 8)
+    worksheet.set_column("B:B", 14)
+    worksheet.set_column("C:Z", 2)
 
     # definice nového stylu
     bold_style = workbook.add_format()
     bold_style.set_bold()
-    bold_style.set_font_color('blue')
+    bold_style.set_font_color("blue")
 
     # styl pro první řádek
     worksheet.set_row(0, 20, bold_style)
 
     # buňky s textem
-    worksheet.write('A1', 'x')
-    worksheet.write('B1', '1/x')
+    worksheet.write("A1", "x")
+    worksheet.write("B1", "1/x")
 
     # buňky s numerickými hodnotami
     for x in range(1, 21):
         worksheet.write(x, 0, x)
-        worksheet.write(x, 1, 1.0/x)
+        worksheet.write(x, 1, 1.0 / x)
 
     # sešit bude uzavřen automaticky
