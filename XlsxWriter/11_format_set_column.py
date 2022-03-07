f02ee3ed3382d7530c5a683490bc72931783c55e
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-07 09:44:59 +0100

    Reformatted by black

--- XlsxWriter/11_format_set_column.py
+++ XlsxWriter/11_format_set_column.py
@@ -6,34 +6,34 @@
 import xlsxwriter
 
 # vytvoření objektu reprezentujícího celý sešit
-with xlsxwriter.Workbook('example11.xlsx') as workbook:
+with xlsxwriter.Workbook("example11.xlsx") as workbook:
     # vložení nového listu do sešitu
     worksheet = workbook.add_worksheet()
 
     # definice nového stylu
     bold_style = workbook.add_format()
     bold_style.set_bold()
-    bold_style.set_font_color('blue')
+    bold_style.set_font_color("blue")
 
     # definice dalšího nového stylu
     red_style = workbook.add_format()
-    red_style.set_font_color('red')
+    red_style.set_font_color("red")
 
     # nastavení šířky sloupců a stylu
-    worksheet.set_column('A:A', 8, red_style)
-    worksheet.set_column('B:B', 14)
-    worksheet.set_column('C:Z', 2)
+    worksheet.set_column("A:A", 8, red_style)
+    worksheet.set_column("B:B", 14)
+    worksheet.set_column("C:Z", 2)
 
     # styl pro první řádek
     worksheet.set_row(0, 20, bold_style)
 
     # buňky s textem
-    worksheet.write('A1', 'x')
-    worksheet.write('B1', '1/x')
+    worksheet.write("A1", "x")
+    worksheet.write("B1", "1/x")
 
     # buňky s numerickými hodnotami
     for x in range(1, 21):
         worksheet.write(x, 0, x)
-        worksheet.write(x, 1, 1.0/x)
+        worksheet.write(x, 1, 1.0 / x)
 
     # sešit bude uzavřen automaticky
