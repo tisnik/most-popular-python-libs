18ff8479360dc1cd289bdb97e88ffa658644eb4e
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-07 09:44:59 +0100

    Reformatted by black

--- XlsxWriter/05_fill_in_table.py
+++ XlsxWriter/05_fill_in_table.py
@@ -6,17 +6,17 @@
 import xlsxwriter
 
 # vytvoření objektu reprezentujícího celý sešit
-with xlsxwriter.Workbook('example05.xlsx') as workbook:
+with xlsxwriter.Workbook("example05.xlsx") as workbook:
     # vložení nového listu do sešitu
     worksheet = workbook.add_worksheet()
 
     # buňky s textem
-    worksheet.write('A1', 'x')
-    worksheet.write('B1', '1/x')
+    worksheet.write("A1", "x")
+    worksheet.write("B1", "1/x")
 
     # buňky s numerickými hodnotami
     for x in range(1, 21):
         worksheet.write(x, 0, x)
-        worksheet.write(x, 1, 1.0/x)
+        worksheet.write(x, 1, 1.0 / x)
 
     # sešit bude uzavřen automaticky
