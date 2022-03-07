c56390b175af2144864168b0f3b22ca8888e9fa7
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-07 09:44:59 +0100

    Reformatted by black

--- XlsxWriter/30_merged_cells_addressing.py
+++ XlsxWriter/30_merged_cells_addressing.py
@@ -6,30 +6,36 @@
 import xlsxwriter
 
 # vytvoření objektu reprezentujícího celý sešit
-with xlsxwriter.Workbook('example30.xlsx') as workbook:
+with xlsxwriter.Workbook("example30.xlsx") as workbook:
     # vložení nového listu do sešitu
     worksheet = workbook.add_worksheet()
 
     # první formát použitý pro zvýraznění sloučené buňky
-    format1 = workbook.add_format({
-        'bold': 1,
-        'border': 1,
-        'align': 'center',
-        'valign': 'vcenter',
-        'fg_color': '#ff8080'})
+    format1 = workbook.add_format(
+        {
+            "bold": 1,
+            "border": 1,
+            "align": "center",
+            "valign": "vcenter",
+            "fg_color": "#ff8080",
+        }
+    )
 
     # druhý formát použitý pro zvýraznění sloučené buňky
-    format2 = workbook.add_format({
-        'bold': 1,
-        'border': 1,
-        'align': 'center',
-        'valign': 'vcenter',
-        'fg_color': '#80ff80'})
+    format2 = workbook.add_format(
+        {
+            "bold": 1,
+            "border": 1,
+            "align": "center",
+            "valign": "vcenter",
+            "fg_color": "#80ff80",
+        }
+    )
 
     # sloučená buňka
-    worksheet.merge_range(1, 2, 1, 4, 'Sloučená buňka', format1)
+    worksheet.merge_range(1, 2, 1, 4, "Sloučená buňka", format1)
 
     # sloučená buňka
-    worksheet.merge_range(1, 6, 4, 6, 'Sloučená\nbuňka', format2)
+    worksheet.merge_range(1, 6, 4, 6, "Sloučená\nbuňka", format2)
 
     # sešit bude uzavřen automaticky
