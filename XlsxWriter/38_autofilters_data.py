de21dd629493728550ab3992c5175269bbab4e5e
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-07 09:44:59 +0100

    Reformatted by black

--- XlsxWriter/38_autofilters_data.py
+++ XlsxWriter/38_autofilters_data.py
@@ -6,53 +6,53 @@
 import xlsxwriter
 
 # vytvoření objektu reprezentujícího celý sešit
-with xlsxwriter.Workbook('example38.xlsx') as workbook:
+with xlsxwriter.Workbook("example38.xlsx") as workbook:
     # vložení nového listu do sešitu
     worksheet = workbook.add_worksheet()
 
     # definice nového stylu
     bold_style = workbook.add_format()
     bold_style.set_bold()
-    bold_style.set_font_color('blue')
+    bold_style.set_font_color("blue")
 
     # definice dalšího nového stylu
     red_style = workbook.add_format()
-    red_style.set_font_color('red')
+    red_style.set_font_color("red")
 
     # definice formátu čísel
-    numeric_format = workbook.add_format({'num_format': '0.0000'})
+    numeric_format = workbook.add_format({"num_format": "0.0000"})
 
     # nastavení šířky sloupců a stylu
-    worksheet.set_column('A:A', 8)
-    worksheet.set_column('B:B', 14, numeric_format)
-    worksheet.set_column('C:Z', 2)
+    worksheet.set_column("A:A", 8)
+    worksheet.set_column("B:B", 14, numeric_format)
+    worksheet.set_column("C:Z", 2)
 
     # styl pro první řádek
     worksheet.set_row(0, 20, bold_style)
 
     # buňky s textem
-    worksheet.write_string('A1', 'x')
-    worksheet.write_string('B1', '1/x')
+    worksheet.write_string("A1", "x")
+    worksheet.write_string("B1", "1/x")
 
     # buňky s numerickými hodnotami
     for x in range(1, 21):
         worksheet.write_number(x, 0, x)
-        worksheet.write_number(x, 1, 1.0/x)
+        worksheet.write_number(x, 1, 1.0 / x)
 
     # nastavení automatického filtru pro dva sloupce
-    worksheet.autofilter('A1:B21')
+    worksheet.autofilter("A1:B21")
 
-    worksheet.filter_column('A', 'x > 10')
+    worksheet.filter_column("A", "x > 10")
 
     # formát s podmínkou
     conditional_format = {
-             'type':     'cell',
-             'criteria': '>=',
-             'value':    0.2,
-             'format':   red_style
-            }
+        "type": "cell",
+        "criteria": ">=",
+        "value": 0.2,
+        "format": red_style,
+    }
 
     # nastavení podmíněného formátu u druhého sloupce hodnot
-    worksheet.conditional_format('B2:B21', conditional_format)
+    worksheet.conditional_format("B2:B21", conditional_format)
 
     # sešit bude uzavřen automaticky
