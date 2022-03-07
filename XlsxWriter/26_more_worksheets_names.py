12962569357416f80b9d58647f7bc170c47e9d36
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-07 09:44:59 +0100

    Reformatted by black

--- XlsxWriter/26_more_worksheets_names.py
+++ XlsxWriter/26_more_worksheets_names.py
@@ -6,7 +6,7 @@
 import xlsxwriter
 
 # vytvoření objektu reprezentujícího celý sešit
-with xlsxwriter.Workbook('example26.xlsx') as workbook:
+with xlsxwriter.Workbook("example26.xlsx") as workbook:
     # vytvoření několika listů
     for name in ("První", "Druhý", "Třetí", "Čtvrtý", "První"):
         # vložení nového listu do sešitu
