6dad7ef0bbd5752e27c96cccd52379796b49882b
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-07 09:44:59 +0100

    Reformatted by black

--- XlsxWriter/08_more_styles.py
+++ XlsxWriter/08_more_styles.py
@@ -6,26 +6,25 @@
 import xlsxwriter
 
 # vytvoření objektu reprezentujícího celý sešit
-with xlsxwriter.Workbook('example08.xlsx') as workbook:
+with xlsxwriter.Workbook("example08.xlsx") as workbook:
     # vložení nového listu do sešitu
     worksheet = workbook.add_worksheet()
 
     # nastavení šířky sloupců
-    worksheet.set_column('A:A', 8)
-    worksheet.set_column('B:B', 14)
-    worksheet.set_column('C:Z', 2)
+    worksheet.set_column("A:A", 8)
+    worksheet.set_column("B:B", 14)
+    worksheet.set_column("C:Z", 2)
 
     # definice nového stylu
-    bold_style = workbook.add_format({'bold': True,
-                                      'font_color': 'blue'})
+    bold_style = workbook.add_format({"bold": True, "font_color": "blue"})
 
     # buňky s textem
-    worksheet.write('A1', 'x', bold_style)
-    worksheet.write('B1', '1/x', bold_style)
+    worksheet.write("A1", "x", bold_style)
+    worksheet.write("B1", "1/x", bold_style)
 
     # buňky s numerickými hodnotami
     for x in range(1, 21):
         worksheet.write(x, 0, x)
-        worksheet.write(x, 1, 1.0/x)
+        worksheet.write(x, 1, 1.0 / x)
 
     # sešit bude uzavřen automaticky
