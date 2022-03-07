3dccf78af070f19398ea72cde779379240fed846
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-07 09:44:59 +0100

    Reformatted by black

--- XlsxWriter/03_simple_data.py
+++ XlsxWriter/03_simple_data.py
@@ -6,16 +6,16 @@
 import xlsxwriter
 
 # vytvoření objektu reprezentujícího celý sešit
-with xlsxwriter.Workbook('example03.xlsx') as workbook:
+with xlsxwriter.Workbook("example03.xlsx") as workbook:
     # vložení nového listu do sešitu
     worksheet = workbook.add_worksheet()
 
     # buňka s textem
-    worksheet.write('A1', 'www.root.cz')
+    worksheet.write("A1", "www.root.cz")
 
     # buňky s numerickými hodnotami
-    worksheet.write('A2', 6)
-    worksheet.write('A3', 7)
-    worksheet.write('B3', 8)
+    worksheet.write("A2", 6)
+    worksheet.write("A3", 7)
+    worksheet.write("B3", 8)
 
     # sešit bude uzavřen automaticky
