e09d68482191f6ba584020e17c65d4ef46b34477
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-07 09:44:59 +0100

    Reformatted by black

--- XlsxWriter/29_merged_cells.py
+++ XlsxWriter/29_merged_cells.py
@@ -6,19 +6,22 @@
 import xlsxwriter
 
 # vytvoření objektu reprezentujícího celý sešit
-with xlsxwriter.Workbook('example29.xlsx') as workbook:
+with xlsxwriter.Workbook("example29.xlsx") as workbook:
     # vložení nového listu do sešitu
     worksheet = workbook.add_worksheet()
 
     # formát použitý pro zvýraznění sloučené buňky
-    format1 = workbook.add_format({
-        'bold': 1,
-        'border': 1,
-        'align': 'center',
-        'valign': 'vcenter',
-        'fg_color': '#ff8080'})
+    format1 = workbook.add_format(
+        {
+            "bold": 1,
+            "border": 1,
+            "align": "center",
+            "valign": "vcenter",
+            "fg_color": "#ff8080",
+        }
+    )
 
     # sloučená buňka
-    worksheet.merge_range('B2:D4', 'Sloučená buňka', format1)
+    worksheet.merge_range("B2:D4", "Sloučená buňka", format1)
 
     # sešit bude uzavřen automaticky
