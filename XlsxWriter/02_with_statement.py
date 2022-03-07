51bd8dece30625732a47fbbd3440164481c748c5
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-07 09:44:59 +0100

    Reformatted by black

--- XlsxWriter/02_with_statement.py
+++ XlsxWriter/02_with_statement.py
@@ -6,7 +6,7 @@
 import xlsxwriter
 
 # vytvoření objektu reprezentujícího celý sešit
-with xlsxwriter.Workbook('example02.xlsx') as workbook:
+with xlsxwriter.Workbook("example02.xlsx") as workbook:
     # vložení nového listu do sešitu
     worksheet = workbook.add_worksheet()
     # sešit bude uzavřen automaticky
