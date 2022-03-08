099d87a8a430d4846cd8fd84b1a155d700d66d6b
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-08 16:28:47 +0100

    Reformatted by black

--- napkin/client-server-1.py
+++ napkin/client-server-1.py
@@ -15,8 +15,8 @@ import napkin
 
 @napkin.seq_diagram()
 def client_server_1(c):
-    client = c.object('Client')
-    server = c.object('Server')
+    client = c.object("Client")
+    server = c.object("Server")
 
     with client:
         server.SYN()
