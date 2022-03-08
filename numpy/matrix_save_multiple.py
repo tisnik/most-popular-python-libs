bb1136cb330061afb919402fdbda79c0880dfa5d
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-08 16:28:47 +0100

    Reformatted by black

--- numpy/matrix_save_multiple.py
+++ numpy/matrix_save_multiple.py
@@ -15,9 +15,15 @@ rows3 = 1
 columns3 = 100
 
 # tři matice obsahující celočíselné hodnoty různých typů
-m1 = np.linspace(1, rows1*columns1, rows1*columns1, dtype="b").reshape(rows1, columns1)
-m2 = np.linspace(1, rows2*columns2, rows2*columns2, dtype="i").reshape(rows2, columns2)
-m3 = np.linspace(1, rows3*columns3, rows3*columns3, dtype="f").reshape(rows3, columns3)
+m1 = np.linspace(1, rows1 * columns1, rows1 * columns1, dtype="b").reshape(
+    rows1, columns1
+)
+m2 = np.linspace(1, rows2 * columns2, rows2 * columns2, dtype="i").reshape(
+    rows2, columns2
+)
+m3 = np.linspace(1, rows3 * columns3, rows3 * columns3, dtype="f").reshape(
+    rows3, columns3
+)
 
 # uložení matic do souboru ve standardním formátu
 np.savez("matrix.npz", m1, m2, m3)
