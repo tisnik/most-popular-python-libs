0858506e4d64e4bffc8e075cbe79b4b8fbead22d
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-08 16:28:47 +0100

    Reformatted by black

--- gc_objgraph/objgraph3.py
+++ gc_objgraph/objgraph3.py
@@ -6,4 +6,4 @@ x = "Foo"
 y = [x, "bar"]
 
 # zobrazení referencí na dva řetězce uložené v seznamu
-objgraph.show_refs([y], filename='objgraph3.png')
+objgraph.show_refs([y], filename="objgraph3.png")
