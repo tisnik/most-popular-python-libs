2a469ccc9eaae8aff508a0c3573b06419dce8322
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-08 16:28:47 +0100

    Reformatted by black

--- pandas/data_frame_group_by_11_kurzy.py
+++ pandas/data_frame_group_by_11_kurzy.py
@@ -7,7 +7,7 @@ import pandas
 df = pandas.read_csv("denni_kurz2.txt", sep="|", skiprows=0)
 
 # převod číselných hodnot
-df["kurz"] = pandas.to_numeric(df["kurz"].str.replace(',', '.'), errors='coerce')
+df["kurz"] = pandas.to_numeric(df["kurz"].str.replace(",", "."), errors="coerce")
 
 # datový rámec zobrazíme
 print(df)
