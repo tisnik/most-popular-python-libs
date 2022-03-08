9e72d85463f888f8ef57559fecb8e21f464c5717
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-08 16:28:47 +0100

    Reformatted by black

--- pandas/check_types_3.py
+++ pandas/check_types_3.py
@@ -37,7 +37,9 @@ def validate_item(schema, data):
 def pos(value):
     """Validate if the value is positive integer number."""
     if type(value) is not int or value <= 0:
-        raise Invalid("positive integer value expected, but got {v} instead".format(v=value))
+        raise Invalid(
+            "positive integer value expected, but got {v} instead".format(v=value)
+        )
 
 
 def nnat(value):
