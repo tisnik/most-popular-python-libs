56969bfa3c4440ec089487fc9307fcf32a71b2a7
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-08 16:28:47 +0100

    Reformatted by black

--- gc_objgraph/objgraph4.py
+++ gc_objgraph/objgraph4.py
@@ -6,4 +6,4 @@ x = "Foo"
 y = [x, "bar", [x], (x, x), {"x": x}]
 
 # reference u složitějších datových struktur
-objgraph.show_refs([x, y], filename='objgraph4.png')
+objgraph.show_refs([x, y], filename="objgraph4.png")
