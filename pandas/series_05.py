f6f62ebac31d4e9e21bf4a5a84a0054b6b3ef434
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-09 09:22:27 +0100

    Reformatted by black

--- pandas/series_05.py
+++ pandas/series_05.py
@@ -4,13 +4,13 @@
 import pandas
 
 input_data = {
-        "a": 1,
-        "b": 2,
-        "c": 3,
-        "d": 4,
-        "e": 5,
-        "f": 6,
-        }
+    "a": 1,
+    "b": 2,
+    "c": 3,
+    "d": 4,
+    "e": 5,
+    "f": 6,
+}
 
 print("Input data:")
 print(input_data)
