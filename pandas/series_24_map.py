8baf65c5dd63ec832d507d27bea2c25b3a36c145
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-09 09:22:27 +0100

    Reformatted by black

--- pandas/series_24_map.py
+++ pandas/series_24_map.py
@@ -5,15 +5,14 @@ import pandas
 import numpy as np
 
 # přečtení zdrojových dat
-s = pandas.Series(['cat', 'dog', np.nan, 'rabbit'])
+s = pandas.Series(["cat", "dog", np.nan, "rabbit"])
 
 # tisk obsahu původní datové řady
 print(s)
 print()
 
 # mapování hodnot
-s2 = s.map({'cat': 'kitten',
-            'dog': 'puppy'})
+s2 = s.map({"cat": "kitten", "dog": "puppy"})
 
 # tisk obsahu nové datové řady
 print(s2)
