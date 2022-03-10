6d45930953f640206dbef0f129706dc508a3bf5c
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-10 08:56:18 +0100

    Reformatted by black

--- plotnine/offline_variants/06_xyplot_show.py
+++ plotnine/offline_variants/06_xyplot_show.py
@@ -14,9 +14,11 @@ from plotnine.data import economics
 
 from plotnine import ggplot, aes, geom_line, labs
 
-g = ggplot(economics) + \
-        aes(x="date", y="uempmed") + \
-        geom_line() + \
-        labs(x="date", y="median duration of unemployment")
+g = (
+    ggplot(economics)
+    + aes(x="date", y="uempmed")
+    + geom_line()
+    + labs(x="date", y="median duration of unemployment")
+)
 
 g.save("06.png")
