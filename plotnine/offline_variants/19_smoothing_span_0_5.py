7b9329681c7e4db2a788efb61ff790bc7b68d936
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-10 08:56:19 +0100

    Reformatted by black

--- plotnine/offline_variants/19_smoothing_span_0_5.py
+++ plotnine/offline_variants/19_smoothing_span_0_5.py
@@ -13,11 +13,13 @@
 from plotnine.data import economics
 from plotnine import ggplot, aes, geom_point, geom_smooth, xlab, ylab
 
-g = (ggplot(economics)
-     + aes(x="date", y="uempmed")
-     + geom_point()
-     + geom_smooth(color="red", span=0.5)
-     + xlab("date (year)")
-     + ylab("unemploynment"))
+g = (
+    ggplot(economics)
+    + aes(x="date", y="uempmed")
+    + geom_point()
+    + geom_smooth(color="red", span=0.5)
+    + xlab("date (year)")
+    + ylab("unemploynment")
+)
 
 g.save("19.png")
