aaeab975dfdc89d5fa8ab8c4cd9f0e001a6fa5ad
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-10 08:56:19 +0100

    Reformatted by black

--- plotnine/offline_variants/21_lm.py
+++ plotnine/offline_variants/21_lm.py
@@ -13,11 +13,13 @@
 from plotnine.data import economics
 from plotnine import ggplot, aes, geom_point, geom_smooth, xlab, ylab
 
-g = (ggplot(economics)
-     + aes(x="date", y="uempmed")
-     + geom_point()
-     + geom_smooth(color="red", method="lm")
-     + xlab("date (year)")
-     + ylab("unemploynment"))
+g = (
+    ggplot(economics)
+    + aes(x="date", y="uempmed")
+    + geom_point()
+    + geom_smooth(color="red", method="lm")
+    + xlab("date (year)")
+    + ylab("unemploynment")
+)
 
 g.save("21.png")
