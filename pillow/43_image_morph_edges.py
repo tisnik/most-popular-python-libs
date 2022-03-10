52b04fbcb808c21ef7df007ca451fc92ffef5c2a
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-10 08:56:18 +0100

    Reformatted by black

--- pillow/43_image_morph_edges.py
+++ pillow/43_image_morph_edges.py
@@ -33,10 +33,10 @@ try:
     height = test_image.size[1]
 
     # načtení fontu
-    font = ImageFont.truetype('FreeMono.ttf', 100)
+    font = ImageFont.truetype("FreeMono.ttf", 100)
 
     # vykreslení jednoduchého textu
-    draw.text((70, height/2 - 50), "Pillow", font=font, fill=255)
+    draw.text((70, height / 2 - 50), "Pillow", font=font, fill=255)
 
     # vzorek pro LUT
     patterns = ["4:(000 000 000)->1"]
