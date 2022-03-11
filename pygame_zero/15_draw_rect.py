acdc4f962020349d0e4665fdebcd3faea1001f04
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-11 12:59:05 +0100

    Reformatted by black

--- pygame_zero/15_draw_rect.py
+++ pygame_zero/15_draw_rect.py
@@ -18,5 +18,5 @@ BORDER = 20
 
 def draw():
     screen.fill("darkgreen")
-    rect = Rect((BORDER, BORDER), (WIDTH-BORDER*2, HEIGHT-BORDER*2))
+    rect = Rect((BORDER, BORDER), (WIDTH - BORDER * 2, HEIGHT - BORDER * 2))
     screen.draw.rect(rect, (255, 255, 255))
