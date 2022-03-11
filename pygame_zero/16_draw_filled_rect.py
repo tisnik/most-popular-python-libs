11f3fe55b3d86ac32dbed5e6a4ad00ea398c9f6f
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-11 12:59:05 +0100

    Reformatted by black

--- pygame_zero/16_draw_filled_rect.py
+++ pygame_zero/16_draw_filled_rect.py
@@ -20,15 +20,15 @@ def draw():
     for j in range(0, 255, 16):
         for i in range(0, 255, 16):
             color = (0, i, j)
-            rect = Rect((20+i, 20+j), (14, 14))
+            rect = Rect((20 + i, 20 + j), (14, 14))
             screen.draw.filled_rect(rect, color)
 
     for j in range(0, 255, 16):
         color = (j, 0, 0)
-        rect = Rect((300, 20+j), (160, 14))
+        rect = Rect((300, 20 + j), (160, 14))
         screen.draw.filled_rect(rect, color)
 
     for i in range(0, 255, 16):
         color = (i, i, 0)
-        rect = Rect((20+i, 300), (14, 160))
+        rect = Rect((20 + i, 300), (14, 160))
         screen.draw.filled_rect(rect, color)
