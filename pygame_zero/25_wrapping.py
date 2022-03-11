1737fc20ba7f213a00f30b60e193449f5f6f1b08
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-11 12:59:05 +0100

    Reformatted by black

--- pygame_zero/25_wrapping.py
+++ pygame_zero/25_wrapping.py
@@ -19,16 +19,20 @@ BORDER = 60
 def draw():
     screen.fill("white")
 
-    message = ("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed"
-               "viverra vestibulum eros at pulvinar. Phasellus vitae mattis nibh. Cras et"
-               "turpis ac erat eleifend lobortis a at ante. Nulla tincidunt quis sapien"
-               "vitae ultricies. Curabitur suscipit bibendum lectus, at ornare elit finibus"
-               "vel. Nullam aliquet libero vitae mattis mattis. Praesent id libero tempor,"
-               "finibus metus a, fermentum velit.")
+    message = (
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed"
+        "viverra vestibulum eros at pulvinar. Phasellus vitae mattis nibh. Cras et"
+        "turpis ac erat eleifend lobortis a at ante. Nulla tincidunt quis sapien"
+        "vitae ultricies. Curabitur suscipit bibendum lectus, at ornare elit finibus"
+        "vel. Nullam aliquet libero vitae mattis mattis. Praesent id libero tempor,"
+        "finibus metus a, fermentum velit."
+    )
 
-    screen.draw.text(message,
-                     (BORDER, 100),
-                     width=WIDTH - BORDER*2,
-                     fontname="freesans",
-                     fontsize=20,
-                     color="navy")
+    screen.draw.text(
+        message,
+        (BORDER, 100),
+        width=WIDTH - BORDER * 2,
+        fontname="freesans",
+        fontsize=20,
+        color="navy",
+    )
