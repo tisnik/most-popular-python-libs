7bdff34cbec59e6cac045f9d14ee589fba222919
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-11 12:59:05 +0100

    Reformatted by black

--- pygame_zero/21_outline.py
+++ pygame_zero/21_outline.py
@@ -17,9 +17,11 @@ HEIGHT = 480
 def draw():
     screen.fill("white")
 
-    screen.draw.text("Pygame Zero",
-                     (140, HEIGHT/2-20),
-                     owidth=1.5,
-                     fontsize=40,
-                     ocolor="orange",
-                     color="black")
+    screen.draw.text(
+        "Pygame Zero",
+        (140, HEIGHT / 2 - 20),
+        owidth=1.5,
+        fontsize=40,
+        ocolor="orange",
+        color="black",
+    )
