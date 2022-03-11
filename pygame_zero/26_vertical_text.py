a6f5ff25049242a4989e52fad1a44fed93e1cf7a
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-11 12:59:05 +0100

    Reformatted by black

--- pygame_zero/26_vertical_text.py
+++ pygame_zero/26_vertical_text.py
@@ -17,9 +17,11 @@ HEIGHT = 480
 def draw():
     screen.fill("black")
 
-    screen.draw.text("Pygame Zero",
-                     (WIDTH/2-20, HEIGHT-60),
-                     angle=90,
-                     fontsize=80,
-                     color="orange",
-                     gcolor="red")
+    screen.draw.text(
+        "Pygame Zero",
+        (WIDTH / 2 - 20, HEIGHT - 60),
+        angle=90,
+        fontsize=80,
+        color="orange",
+        gcolor="red",
+    )
