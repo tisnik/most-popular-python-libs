9f245352059edb3a9b2616e0c07d9395acaca536
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-11 12:59:05 +0100

    Reformatted by black

--- pygame_zero/13_draw_lines.py
+++ pygame_zero/13_draw_lines.py
@@ -18,7 +18,11 @@ BORDER = 20
 
 def draw():
     screen.fill("darkgreen")
-    screen.draw.line((BORDER, BORDER), (WIDTH-BORDER, BORDER), (255, 255, 255))
-    screen.draw.line((WIDTH-BORDER, BORDER), (WIDTH-BORDER, HEIGHT-BORDER), (255, 255, 255))
-    screen.draw.line((BORDER, BORDER), (BORDER, HEIGHT-BORDER), (255, 255, 255))
-    screen.draw.line((BORDER, HEIGHT-BORDER), (WIDTH-BORDER, HEIGHT-BORDER), (255, 255, 255))
+    screen.draw.line((BORDER, BORDER), (WIDTH - BORDER, BORDER), (255, 255, 255))
+    screen.draw.line(
+        (WIDTH - BORDER, BORDER), (WIDTH - BORDER, HEIGHT - BORDER), (255, 255, 255)
+    )
+    screen.draw.line((BORDER, BORDER), (BORDER, HEIGHT - BORDER), (255, 255, 255))
+    screen.draw.line(
+        (BORDER, HEIGHT - BORDER), (WIDTH - BORDER, HEIGHT - BORDER), (255, 255, 255)
+    )
