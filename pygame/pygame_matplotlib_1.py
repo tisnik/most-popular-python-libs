8096c3b36b62a280134cada6ebf027b0878b56ad
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-11 12:59:05 +0100

    Reformatted by black

--- pygame/pygame_matplotlib_1.py
+++ pygame/pygame_matplotlib_1.py
@@ -14,6 +14,7 @@
 #
 
 import matplotlib
+
 matplotlib.use("Agg")  # noqa: E402
 import matplotlib.pyplot as plt
 import numpy as np
@@ -38,7 +39,7 @@ FRAMEBUFFER_DEVICE = "/dev/fb0"
 # Vytvoreni grafu
 def create_graph():
     fig = plt.figure()
-    x = np.linspace(0, 2*np.pi, 100)
+    x = np.linspace(0, 2 * np.pi, 100)
     y = np.sin(x)
     plt.plot(x, y)
     plt.xlabel("x")
