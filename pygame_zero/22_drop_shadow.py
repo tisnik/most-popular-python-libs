a9d1306ee8cf8bb3a64b1e4bac7968122fc4cada
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-11 12:59:05 +0100

    Reformatted by black

--- pygame_zero/22_drop_shadow.py
+++ pygame_zero/22_drop_shadow.py
@@ -17,9 +17,11 @@ HEIGHT = 480
 def draw():
     screen.fill("gray")
 
-    screen.draw.text("Pygame Zero",
-                     (140, HEIGHT/2-20),
-                     fontsize=40,
-                     shadow=(1, 1),
-                     scolor="#202020",
-                     color="white")
+    screen.draw.text(
+        "Pygame Zero",
+        (140, HEIGHT / 2 - 20),
+        fontsize=40,
+        shadow=(1, 1),
+        scolor="#202020",
+        color="white",
+    )
