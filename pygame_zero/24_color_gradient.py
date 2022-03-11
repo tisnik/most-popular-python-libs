b85c634ea0a4d0f69a15e341c95be253e22f18be
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-11 12:59:05 +0100

    Reformatted by black

--- pygame_zero/24_color_gradient.py
+++ pygame_zero/24_color_gradient.py
@@ -17,8 +17,6 @@ HEIGHT = 480
 def draw():
     screen.fill("black")
 
-    screen.draw.text("Pygame Zero",
-                     (60, HEIGHT/2-20),
-                     fontsize=80,
-                     color="orange",
-                     gcolor="red")
+    screen.draw.text(
+        "Pygame Zero", (60, HEIGHT / 2 - 20), fontsize=80, color="orange", gcolor="red"
+    )
