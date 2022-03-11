632a1da6aad0382510d73b45f6e36eef1215846f
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-11 12:59:05 +0100

    Reformatted by black

--- pygame_zero/17_draw_circle.py
+++ pygame_zero/17_draw_circle.py
@@ -18,6 +18,6 @@ BORDER = 20
 
 def draw():
     screen.fill("darkgreen")
-    center = WIDTH//2, WIDTH//2
-    radius = WIDTH//3
+    center = WIDTH // 2, WIDTH // 2
+    radius = WIDTH // 3
     screen.draw.circle(center, radius, (255, 255, 255))
