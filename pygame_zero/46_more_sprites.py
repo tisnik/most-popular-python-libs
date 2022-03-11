fcd3311f389c0c6d6b2e855298ac0314853cd0e4
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-11 12:59:05 +0100

    Reformatted by black

--- pygame_zero/46_more_sprites.py
+++ pygame_zero/46_more_sprites.py
@@ -31,14 +31,16 @@ def init_sprites():
 
 
 def a1(sprite):
-    sprite.x = WIDTH/2 + randrange(-X_SPREAD, X_SPREAD)
+    sprite.x = WIDTH / 2 + randrange(-X_SPREAD, X_SPREAD)
     sprite.y = 0 - randrange(Y_SPREAD)
-    animate(sprite,
-            x=WIDTH/2 + randrange(-X_SPREAD, X_SPREAD),
-            y=HEIGHT + randrange(Y_SPREAD),
-            on_finished=lambda: a1(sprite),
-            duration=randrange(2, 7),
-            tween="accelerate")
+    animate(
+        sprite,
+        x=WIDTH / 2 + randrange(-X_SPREAD, X_SPREAD),
+        y=HEIGHT + randrange(Y_SPREAD),
+        on_finished=lambda: a1(sprite),
+        duration=randrange(2, 7),
+        tween="accelerate",
+    )
 
 
 def draw():
