18c028ba945d128865a229f819813b901d83afea
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-11 12:59:05 +0100

    Reformatted by black

--- pygame/pygame25.py
+++ pygame/pygame25.py
@@ -38,7 +38,7 @@ clock = pygame.time.Clock()
 display = pygame.display.set_mode([WIDTH, HEIGHT])
 
 # Nastavení titulku okna
-pygame.display.set_caption('Pygame test #25')
+pygame.display.set_caption("Pygame test #25")
 
 # Konstanty s n-ticemi představujícími základní barvy
 BLACK = (0, 0, 0)
@@ -48,7 +48,7 @@ display.fill(BLACK)
 
 
 # Načtení obrázku, který se bude vykreslovat a transformovat
-image_surface = pygame.image.load(os.path.join('images', 'pygame.png'))
+image_surface = pygame.image.load(os.path.join("images", "pygame.png"))
 
 # Vytvoření zrcadlených obrázků
 horizontally_flipped = pygame.transform.flip(image_surface, True, False)
