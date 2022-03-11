47a96977ef3c153a1db0b420b3da5c5ed1877270
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-11 12:59:05 +0100

    Reformatted by black

--- pyflowchart/bubble_sort.py
+++ pyflowchart/bubble_sort.py
@@ -5,11 +5,11 @@ size = 40
 a = [random.randrange(0, 20) for i in range(size)]
 print(a)
 
-for i in range(size-1, 0, -1):
+for i in range(size - 1, 0, -1):
     print(i)
     for j in range(0, i):
-        if a[j] > a[j+1]:
-            a[j], a[j+1] = a[j+1], a[j]
+        if a[j] > a[j + 1]:
+            a[j], a[j + 1] = a[j + 1], a[j]
 
 print("Sorted")
 print(a)
