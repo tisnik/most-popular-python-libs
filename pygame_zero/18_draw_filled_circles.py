5c2f90316ea8d0d495998d35cd8719260cdb1d8a
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-11 12:59:05 +0100

    Reformatted by black

--- pygame_zero/18_draw_filled_circles.py
+++ pygame_zero/18_draw_filled_circles.py
@@ -22,15 +22,15 @@ def draw():
     for j in range(0, 255, 16):
         for i in range(0, 255, 16):
             color = (0, i, j)
-            pos = 20+i*1.5, 20+j*1.5
+            pos = 20 + i * 1.5, 20 + j * 1.5
             screen.draw.filled_circle(pos, radius, color)
 
     for j in range(0, 255, 16):
         color = (j, 0, 0)
-        pos = 420, 20+j*1.5
+        pos = 420, 20 + j * 1.5
         screen.draw.filled_circle(pos, radius, color)
 
     for i in range(0, 255, 16):
         color = (i, i, 0)
-        pos = 20+i*1.5, 420
+        pos = 20 + i * 1.5, 420
         screen.draw.filled_circle(pos, radius, color)
