4fd5e7a90f705672f0e59a3fc2104a32395e3e7f
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-14 08:14:43 +0100

    Reformatted by black

--- pyjq/13_get_parameters.py
+++ pyjq/13_get_parameters.py
@@ -23,5 +23,7 @@ from pprint import pprint
 with open("openapi.json") as fin:
     content = json.load(fin)
 
-    for parameters in pyjq.compile('.paths."/client/cluster/search".get.parameters').all(content):
+    for parameters in pyjq.compile(
+        '.paths."/client/cluster/search".get.parameters'
+    ).all(content):
         pprint(parameters)
