444e22953bb61647b9a6d98d406f6d589e0d5da8
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-14 08:14:43 +0100

    Reformatted by black

--- requests/08_post_method_with_payload.py
+++ requests/08_post_method_with_payload.py
@@ -18,11 +18,7 @@ import requests
 # adresa s testovaci REST API sluzbou
 URL = "https://httpbin.org/post"
 
-payload = {
-    "klic": "hodnota",
-    "answer": 42,
-    "question": None,
-    "correct": True}
+payload = {"klic": "hodnota", "answer": 42, "question": None, "correct": True}
 
 # poslani HTTP dotazu typu POST se specifikaci hodnot formulare
 response = requests.post(URL, data=payload)
