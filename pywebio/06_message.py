ebd6c786ef4d67fb74252a79d69e75b377f96b06
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-16 13:43:37 +0100

    Message types

---
+++ pywebio/06_message.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+import pywebio.output as out
+
+
+out.put_info("Info message")
+out.put_success("Success message")
+out.put_warning("Warning message")
+out.put_error("Error message")
