12ac92a6cad55a3a9ba39631c60621a13e88e6ae
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-16 13:43:37 +0100

    Loading widget

---
+++ pywebio/12_loading.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+import pywebio.output as out
+
+import time
+
+
+i = 0
+
+for color in ('primary', 'secondary', 'success', 'danger', 'warning', 'info', 'light', 'dark'):
+    with out.put_loading(shape='border', color=color):
+        time.sleep(1)
+        out.put_text(i)
+        i += 1
+
+out.put_text("Done")
