8dd2aafd140c4a90d028076ac2c8e3a7b8ae0c6e
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-16 13:43:37 +0100

    Progress bar widget

---
+++ pywebio/10_progress_bar.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+import pywebio.output as out
+import time
+
+
+out.put_processbar('bar');
+
+for i in range(1, 11):
+    out.set_processbar('bar', i / 10)
+    time.sleep(0.1)
