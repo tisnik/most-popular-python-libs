d5752f74ebfc1f9da1b1941eabc270146ce61cda
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-16 08:20:33 +0100

    Thread pool demo #3

---
+++ concurrent/thread_pool_3.py
@@ -0,0 +1,19 @@
+from concurrent.futures.thread import ThreadPoolExecutor
+import time
+
+
+def worker(threadName, delay, n):
+    for counter in range(1, n + 1):
+        time.sleep(delay)
+        print("{}: {}/{} - {}".format(threadName, counter, n, time.ctime(time.time())))
+    print("{}: DONE!".format(threadName))
+
+
+workers = 10
+
+with ThreadPoolExecutor(max_workers=3) as executor:
+    for w in range(workers):
+        executor.submit(worker, "Thread-{}".format(w + 1), 0.5 + w / 10.0, 10)
+
+
+print("Done!")
