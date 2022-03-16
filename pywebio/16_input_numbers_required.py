11c191f03fbe38b841703b38c82b6e60b9a5d3bc
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-16 13:43:37 +0100

    Grouped text areas for reading numbers, entry required

---
+++ pywebio/16_input_numbers_required.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+import pywebio.input as inp
+import pywebio.output as out
+
+
+# Body Mass Index calculator
+
+info = inp.input_group("Entry",[
+  inp.input("Mass (kg)", name="mass", type=inp.NUMBER, required=True),
+  inp.input("Height (cm)", name="height", type=inp.NUMBER, required=True)
+])
+
+mass = info["mass"]
+height = info["height"]
+
+# převod výšky na metry
+height = height / 100.0
+
+# výpočet (bez jakýchkoli kontrol)
+bmi = mass / (height * height)
+
+# výpis výsledku
+out.put_info("BMI = ", bmi)
