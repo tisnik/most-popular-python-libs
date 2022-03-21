65c51a8c79eb4cd94450bd383ed0cb940d6a6a19
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-21 14:27:58 +0100

    Aiohttp library, example #2

---
+++ concurrent/async_aiohttp_2.py
@@ -0,0 +1,46 @@
+import asyncio
+import aiohttp
+import time
+
+
+async def download(name, queue):
+    async with aiohttp.ClientSession() as session:
+        while not queue.empty():
+            url = await queue.get()
+            t1 = time.time()
+            print(f"Task named {name} getting URL: {url}")
+            async with session.get(url) as response:
+                t = await response.text()
+                t2 = time.time()
+                print(f"Task named {name} downloaded {len(t)} characters in {t2-t1} seconds")
+            print(f"Task named {name} finished")
+
+
+async def main():
+    queue = asyncio.Queue()
+
+    t1 = time.time()
+
+    for url in (
+        "http://www.root.cz",
+        "http://duckduckgo.com",
+        "http://seznam.com",
+        "https://www.root.cz/programovaci-jazyky/",
+        "https://www.root.cz/clanky/soubezne-a-paralelne-bezici-ulohy-naprogramovane-v-pythonu/",
+        "https://www.root.cz/clanky/pywebio-interaktivni-webove-dialogy-a-formulare-v-cistem-pythonu/",
+        "https://streamlit.io/",
+        "https://pglet.io/",
+        "https://www.root.cz/serialy/graficke-uzivatelske-rozhrani-v-pythonu/",
+        "https://github.com/"
+    ):
+        await queue.put(url)
+
+    await asyncio.gather(
+            asyncio.create_task(download(1, queue)),
+            asyncio.create_task(download(2, queue)),
+            asyncio.create_task(download(3, queue)))
+
+    t2 = time.time()
+    print(f"Total time: {t2-t1} seconds")
+
+asyncio.run(main())
