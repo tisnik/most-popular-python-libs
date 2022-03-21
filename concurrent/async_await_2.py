b98b386c8cdd4c6039fe1e54f860e229189ce4f4
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-21 14:27:58 +0100

    Async and await, example #2

---
+++ concurrent/async_await_2.py
@@ -0,0 +1,20 @@
+import asyncio
+import time
+
+
+async def task():
+    print("task started")
+    await asyncio.sleep(5)
+    print("task finished")
+
+
+async def main():
+    task1 = asyncio.create_task(task())
+    print("task created")
+
+    await task1
+
+    print("done")
+
+
+asyncio.run(main())
