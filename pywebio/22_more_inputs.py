1aa29613695807ddb58d104bd216909ab5fcbff8
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-29 07:52:48 +0200

    Pywebio example #22

---
+++ pywebio/22_more_inputs.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+import pywebio.input as inp
+import pywebio.output as out
+
+
+# vstupní údaje
+info = inp.input_group("Adresa",[
+  inp.input("Jméno", name="name"),
+  inp.input("Příjmení", name="surname"),
+  inp.input("Ulice", name="street"),
+  inp.input("ČP", name="conscription_number"),
+  inp.input("Město", name="city"),
+  inp.input("PSČ", name="postal_code")
+])
+
+# výpis výsledků
+out.put_info("Zakázka bude poslána na adresu")
+
+out.put_info(f"{info['name']} {info['surname']}\n{info['street']} {info['conscription_number']}\n{info['postal_code']} {info['city']}")
