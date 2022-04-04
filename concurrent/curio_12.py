226c2e3d1f12e149e29514a844f9feb88ed66a7c
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-04-04 08:29:24 +0200

    Curio example #12

---
+++ concurrent/curio_12.py
@@ -0,0 +1,27 @@
+import curio
+
+
+async def task(name, a, b):
+    print(f"{name} task started")
+
+    result = a * b
+    await curio.sleep(100)
+
+    print(f"{name} task finished")
+    return result
+
+
+async def main():
+    print("main started")
+    task1 = await curio.spawn(task, "1st", 2, 3)
+    task2 = await curio.spawn(task, "2nd", 4, 5)
+    task3 = await curio.spawn(task, "3rd", 6, 7)
+
+    print(await task1.join())
+    print(await task2.join())
+    print(await task3.join())
+
+    print("done")
+
+
+curio.run(main())
