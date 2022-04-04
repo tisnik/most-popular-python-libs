d0e44c42dc8edf04d2e2378d6b111f1258c1e506
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-04-04 08:29:24 +0200

    Curio example #14

---
+++ concurrent/curio_14.py
@@ -0,0 +1,33 @@
+import curio
+
+
+async def task(name, a, b):
+    try:
+        print(f"{name} task started")
+
+        result = a * b
+        await curio.sleep(100)
+
+        print(f"{name} task finished")
+        return result
+
+    except curio.CancelledError:
+        print("Timeouting...")
+        raise
+
+
+async def main():
+    print("main started")
+    task1 = await curio.spawn(task, "1st", 2, 3)
+
+    try:
+        result = await curio.timeout_after(1, task1.join)
+        print(result)
+    except curio.TaskTimeout as e:
+        print("Timeout!")
+        await task1.cancel()
+
+    print("done")
+
+
+curio.run(main())
