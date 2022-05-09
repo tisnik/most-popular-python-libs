c6915658d4c923a49041f42b69ba6a9f5e1d443a
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-05-09 08:51:49 +0200

    Trio example #7

---
+++ concurrent/trio_07.py
@@ -0,0 +1,24 @@
+import trio
+
+
+async def task(name, n, s):
+    print(f"{name} task started")
+
+    for i in range(n):
+        print(f"{name} {i+1}/{n}")
+        await trio.sleep(s)
+
+    raise Exception(name)
+    print(f"{name} task finished")
+
+
+async def main():
+    print("main started")
+    async with trio.open_nursery() as nursery:
+        nursery.start_soon(task, "1st", 10, 0.3)
+        nursery.start_soon(task, "2nd", 10, 0.3)
+        nursery.start_soon(task, "3rd", 10, 0.3)
+    print("done")
+
+
+trio.run(main)
