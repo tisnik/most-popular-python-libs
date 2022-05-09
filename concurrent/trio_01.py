7f113aa18cdf14eba0b0a96cdef0dd161b76e514
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-05-09 08:51:49 +0200

    Trio example #1

---
+++ concurrent/trio_01.py
@@ -0,0 +1,16 @@
+import trio
+
+
+async def task():
+    print("task started")
+    await trio.sleep(5)
+    print("task finished")
+
+
+def main():
+    print("main started")
+    trio.run(task)
+    print("done")
+
+
+main()
