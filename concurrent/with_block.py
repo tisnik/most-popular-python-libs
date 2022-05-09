38f0f10c923ebf19d7050c70359ea4045a53bc9d
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-05-09 08:51:49 +0200

    With block + context manager

---
+++ concurrent/with_block.py
@@ -0,0 +1,19 @@
+class Context():
+    def __init__(self):
+        print("Context: init")
+
+    def __enter__(self):
+        print("Context: enter")
+        return "foo"
+
+    def __exit__(self, type, value, traceback):
+        print("Context: exit", type, value, traceback)
+
+
+print("Before with block")
+
+with Context() as c:
+    print("Inside with block")
+    print(c)
+
+print("After with block")
