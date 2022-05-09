a41f499f020b8021bdc6421a5d4c879218cae5af
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-05-09 08:51:49 +0200

    Async with block + async context manager

---
+++ concurrent/async_with_block.py
@@ -0,0 +1,31 @@
+import trio
+
+
+class AsyncContext():
+    def __init__(self):
+        print("Context: init")
+
+    def __aenter__(self):
+        print("Context: aenter")
+        return trio.sleep(2)
+
+    def __aexit__(self, type, value, traceback):
+        print("Context: aexit", type, value, traceback)
+        return self
+
+    def __await__(self):
+        print("Context: await")
+        return None
+
+
+async def main():
+    print("Before with block")
+
+    async with AsyncContext() as c:
+        print("Inside with block")
+        print(c)
+
+    print("After with block")
+
+
+trio.run(main)
