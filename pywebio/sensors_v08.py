d73e11e35fe8edc9112672f6a5cac482a9c09637
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-05-11 08:33:55 +0200

    'Sensors' web app, version #8

---
+++ pywebio/sensors_v08.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+from pywebio import *
+from pywebio.input import *
+from pywebio.output import *
+from functools import partial
+
+from minio import Minio, ResponseError
+
+
+bucket_name = "sensors"
+minio_address = "localhost:9000"
+minio_access_key = "tester"
+minio_secret_key = "tester01"
+
+client = Minio(
+    minio_address,
+    minio_access_key,
+    minio_secret_key,
+    secure=False
+)
+
+
+def show_data(sensor):
+    popup(f"Data ze senzoru {sensor}", [
+        put_text(f"Data ze senzoru {sensor}"),
+    ])
+
+
+def show_graph(sensor):
+    popup(f"Graf pro senzor {sensor}", [
+        put_text(f"Graf pro senzor {sensor}"),
+    ])
+
+
+def main():
+    put_info("Výsledky měření senzorů")
+    found = client.bucket_exists(bucket_name)
+
+    if not found:
+        put_error("Data nelze přečíst - chybné připojení k Miniu")
+
+    objects = client.list_objects(bucket_name, recursive=False)
+
+    table = [['Senzor', 'Datum', 'Graf', 'Info']]
+
+    for obj in objects:
+        row = [obj.object_name,
+               obj.last_modified,
+               put_button("Graf", onclick=partial(show_graph, sensor=obj.object_name)),
+               put_button("Data", onclick=partial(show_data, sensor=obj.object_name))]
+        table.append(row)
+
+    put_table(table)
+
+
+start_server(main, port=8080, debug=True)
