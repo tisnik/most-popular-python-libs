389f460d92acf7e38e0ded756749b2df8af43948
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-05-17 07:44:19 +0200

    Trio example #19

---
+++ concurrent/trio_19_no_consumer_no_wait.py
@@ -0,0 +1,17 @@
+import trio
+
+
+async def producer(send_channel):
+    for i in range(1, 1000):
+        message = f"message {i}"
+        print(f"Producer: {message}")
+        send_channel.send_nowait(message)
+
+
+async def main():
+    async with trio.open_nursery() as nursery:
+        send_channel, receive_channel = trio.open_memory_channel(10)
+        nursery.start_soon(producer, send_channel)
+
+
+trio.run(main)
