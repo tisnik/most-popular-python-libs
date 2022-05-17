474cc765178e5b626805f2b546d2fa231e9772fb
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-05-17 07:44:19 +0200

    Trio example #20

---
+++ concurrent/trio_20_no_consumer_fail_after.py
@@ -0,0 +1,18 @@
+import trio
+
+
+async def producer(send_channel):
+    for i in range(1, 1000):
+        message = f"message {i}"
+        print(f"Producer: {message}")
+        with trio.fail_after(1):
+            await send_channel.send(message)
+
+
+async def main():
+    async with trio.open_nursery() as nursery:
+        send_channel, receive_channel = trio.open_memory_channel(10)
+        nursery.start_soon(producer, send_channel)
+
+
+trio.run(main)
