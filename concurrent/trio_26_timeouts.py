f3dc8f234e70cdb8b172465492e298d5662ba2ef
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-05-17 07:44:19 +0200

    Trio example #26

---
+++ concurrent/trio_26_timeouts.py
@@ -0,0 +1,35 @@
+import trio
+
+
+num_producers = 5
+num_consumers = 20
+consumer_timeout = 2
+producer_timeout = 2
+
+
+async def producer(id, send_channel):
+    for i in range(1, 10):
+        message = f"message {i}"
+        print(f"Producer #{id}: {message}")
+        with trio.fail_after(producer_timeout):
+            await send_channel.send(message)
+
+
+async def consumer(id, receive_channel):
+    while True:
+        with trio.fail_after(1):
+            value = await receive_channel.receive()
+            print(f"Consumer #{id}: received{value!r}")
+            await trio.sleep(1)
+
+
+async def main():
+    async with trio.open_nursery() as nursery:
+        send_channel, receive_channel = trio.open_memory_channel(0)
+        for id in range(num_producers):
+            nursery.start_soon(producer, id, send_channel)
+        for id in range(num_consumers):
+            nursery.start_soon(consumer, id, receive_channel)
+
+
+trio.run(main)
