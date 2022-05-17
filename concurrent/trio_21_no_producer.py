76588681d6cf02809c9cebfe224349ab6558f935
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-05-17 07:44:19 +0200

    Trio example #21

---
+++ concurrent/trio_21_no_producer.py
@@ -0,0 +1,16 @@
+import trio
+
+
+async def consumer(receive_channel):
+    async for value in receive_channel:
+        print(f"Consumer: received{value!r}")
+        await trio.sleep(1)
+
+
+async def main():
+    async with trio.open_nursery() as nursery:
+        send_channel, receive_channel = trio.open_memory_channel(0)
+        nursery.start_soon(consumer, receive_channel)
+
+
+trio.run(main)
