0231c31a93474e268f91060d0e85fbf7c92bf0a0
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-05-17 07:44:19 +0200

    Trio example #25

---
+++ concurrent/trio_25_multiple_prod_cons.py
@@ -0,0 +1,30 @@
+import trio
+
+
+num_producers = 5
+num_consumers = 20
+
+
+async def producer(id, send_channel):
+    for i in range(1, 10):
+        message = f"message {i}"
+        print(f"Producer #{id}: {message}")
+        await send_channel.send(message)
+
+
+async def consumer(id, receive_channel):
+    async for value in receive_channel:
+        print(f"Consumer #{id}: received{value!r}")
+        await trio.sleep(1)
+
+
+async def main():
+    async with trio.open_nursery() as nursery:
+        send_channel, receive_channel = trio.open_memory_channel(0)
+        for id in range(num_producers):
+            nursery.start_soon(producer, id, send_channel)
+        for id in range(num_consumers):
+            nursery.start_soon(consumer, id, receive_channel)
+
+
+trio.run(main)
