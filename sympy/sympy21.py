7d9723e130142a3505db25fb39eca5cca8eed905
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-05-23 07:56:28 +0200

    SymPy example #21

---
+++ sympy/sympy21.py
@@ -0,0 +1,13 @@
+from sympy import solve, symbols, pprint
+
+a, b, c, d, e, x, y = symbols('a,b,c,d,e,x,y')
+
+f = a*x**2 + b*x + c*y**2 + d*y + e
+
+pprint(f)
+
+solution = solve(f, x)
+pprint(solution)
+
+solution = solve(f, y)
+pprint(solution)
