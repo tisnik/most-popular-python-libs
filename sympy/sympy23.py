23f715e39b974cfd8b59dad35fc73cf92b4ced62
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-05-23 07:56:28 +0200

    SymPy example #23

---
+++ sympy/sympy23.py
@@ -0,0 +1,13 @@
+from sympy import *
+
+a, b, c, d, e, x, y = symbols('a,b,c,d,e,x,y')
+
+f = a*sin(x**2) / b*cos(y**2) + c*sqrt(x+y*d) + e
+
+pprint(f)
+
+diff1 = diff(f, x)
+pprint(diff1)
+
+diff2 = diff(f, y)
+pprint(diff2)
