b49f3dbf5512ad5742f83974a1813d17a38dae49
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-05-23 07:56:28 +0200

    SymPy example #26

---
+++ sympy/sympy26.py
@@ -0,0 +1,12 @@
+from sympy import solve, symbols, pprint, init_printing
+
+init_printing(use_unicode=True)
+
+a, b, c, x = symbols('α,β,Γ,x')
+
+f = a*x**2 + b*x + c
+
+pprint(f)
+
+solution = solve(f, x)
+pprint(solution)
