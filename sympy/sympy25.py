773d3dce63b0570f6295b180998cd52825cba439
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-05-23 07:56:28 +0200

    SymPy example #25

---
+++ sympy/sympy25.py
@@ -0,0 +1,13 @@
+from sympy import integrate, symbols, pprint
+
+a, b, c, d, e, x, y = symbols('a,b,c,d,e,x,y')
+
+f = a*x**2 + b*x + c*y**2 + d*y + e
+
+pprint(f)
+
+solution = integrate(f, x)
+pprint(solution)
+
+solution = integrate(f, y)
+pprint(solution)
