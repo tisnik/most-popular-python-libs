d0c563861e8ef871b07b3dc917e228cf1dfc7159
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-05-23 07:56:29 +0200

    SymPy example #32

---
+++ sympy/sympy32.py
@@ -0,0 +1,12 @@
+from sympy import solveset, symbols, pprint, init_printing, cos
+
+init_printing(use_unicode=True)
+
+x = symbols('x')
+
+f = cos(x)
+
+pprint(f)
+
+solution = solveset(f, x)
+pprint(solution)
