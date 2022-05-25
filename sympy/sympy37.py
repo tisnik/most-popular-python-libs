d167dfb19f4707ede52273cd9ce143dbc5b65358
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-05-25 08:56:17 +0200

    SymPy example #37

---
+++ sympy/sympy37.py
@@ -0,0 +1,16 @@
+from sympy import solve, solveset, symbols, pprint, init_printing
+
+init_printing(use_unicode=True)
+
+x = symbols('x')
+
+f1 = x**2 < 10
+f2 = x**3 > -10
+
+pprint(f1)
+pprint(f2)
+
+print()
+
+solution = solve((f1, f2), x)
+pprint(solution)
