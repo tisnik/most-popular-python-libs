9f6e60b20a4ca2dc4d95cd1c6ae2908349042903
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-05-25 08:56:17 +0200

    SymPy example #35

---
+++ sympy/sympy35.py
@@ -0,0 +1,19 @@
+from sympy import solve, solveset, symbols, pprint, init_printing, sin
+
+init_printing(use_unicode=True)
+
+x = symbols('x')
+
+f = sin(x) > 10
+
+pprint(f)
+
+print()
+
+solution = solve(f, x)
+pprint(solution)
+
+print()
+
+solution = solveset(f, x)
+pprint(solution)
