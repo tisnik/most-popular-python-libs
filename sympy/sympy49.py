a5962e321a9b558140bfe9986b5beaee6ce9cf49
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-05-30 09:51:50 +0200

    SymPy example #49

---
+++ sympy/sympy49.py
@@ -0,0 +1,15 @@
+from sympy import symbols, pprint, init_printing
+from sympy import Interval, maximum, S
+
+init_printing(use_unicode=True)
+
+x = symbols('x')
+
+f = x**3 - 2*x**2
+
+pprint(f)
+
+print()
+
+print(maximum(f, x, Interval(-2, 2)))
+print(maximum(f, x, Interval(-1, 1)))
