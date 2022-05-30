64fe62550dce44859f935515d1b6430eeb5c3c2b
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-05-30 09:51:50 +0200

    SymPy example #46

---
+++ sympy/sympy46.py
@@ -0,0 +1,12 @@
+from sympy import singularities, symbols, pprint, sin
+  
+a, b, c, d, e, x = symbols('a,b,c,d,e,x')
+
+f = 1/(sin(x)-x**2)
+
+pprint(f)
+
+print()
+
+s = singularities(f, x)
+pprint(s)
