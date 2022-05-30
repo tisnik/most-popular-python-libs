7ee18369c8765e652abd9dae839ad736f4be6eba
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-05-30 09:51:50 +0200

    SymPy example #40

---
+++ sympy/sympy40.py
@@ -0,0 +1,27 @@
+from sympy import diff, symbols, pprint
+
+x, y, z, w = symbols('x, y, z, w')
+
+f = 4*x**2 + 3*y**2 + 1/z
+
+pprint(f)
+
+print()
+
+diff1 = diff(f, x)
+pprint(diff1)
+
+print()
+
+diff2 = diff(f, y)
+pprint(diff2)
+
+print()
+
+diff3 = diff(f, z)
+pprint(diff3)
+
+print()
+
+diff4 = diff(f, w)
+pprint(diff4)
