7c0db068baf8ee32031713abcf24d3db009c58b9
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-05-30 09:51:50 +0200

    SymPy example #48

---
+++ sympy/sympy48.py
@@ -0,0 +1,15 @@
+from sympy import symbols, pprint, init_printing
+from sympy import Interval, minimum, S
+
+init_printing(use_unicode=True)
+
+x = symbols('x')
+
+f = x**3 - 2*x**2
+
+pprint(f)
+
+print()
+
+print(minimum(f, x, Interval(-2, 2)))
+print(minimum(f, x, Interval(-1, 1)))
