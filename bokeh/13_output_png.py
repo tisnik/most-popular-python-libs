172cc276a9216eb2af3a7e71fd343e82fbd5abe3
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-06-06 07:53:16 +0200

    Bokeh example #13

---
+++ bokeh/13_output_png.py
@@ -0,0 +1,38 @@
+# naimportujeme vybrané funkce z knihovny `bokeh.plotting`
+from bokeh.plotting import figure, show
+
+# export grafu
+from bokeh.io import export_png
+
+# taktéž budeme potřebovat některé funkce z knihovny `numpy`
+import numpy as np
+
+# vykreslení průběhu funkce sin
+
+# počet bodů na spirále
+points = 150
+
+# úhel v polárním grafu
+theta = np.linspace(0.01, 8*np.pi, points)
+
+# koeficient spirály
+k = 0.15
+
+# funkce: vzdálenost od středu
+radius = np.exp(k*theta)
+
+y1 = radius*np.sin(theta)
+
+y2 = radius*np.cos(theta)
+
+# plocha pro graf
+p = figure(title="Spiral", x_axis_label='x', y_axis_label='y')
+
+# vykreslení průběhu
+p.line(y1, y2, line_width=2, color="blue")
+
+# vykreslení grafu do plochy webové stránky
+show(p)
+
+# export grafu
+export_png(p, filename="plot1.png")
