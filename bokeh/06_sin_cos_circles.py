95029df5b987fe6e7fbc65e7e6da147c6e6779bd
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-06-06 07:53:16 +0200

    Bokeh example #6

---
+++ bokeh/06_sin_cos_circles.py
@@ -0,0 +1,27 @@
+# naimportujeme vybrané funkce z knihovny `bokeh.plotting`
+
+from bokeh.plotting import figure, show
+
+# taktéž budeme potřebovat některé funkce z knihovny `numpy`
+import numpy as np
+
+# vykreslení průběhu funkce sin
+
+# hodnoty na x-ové ose
+x = np.linspace(0, 2 * np.pi, 100)
+
+# hodnoty na y-ové ose
+y1 = np.sin(x)
+
+# hodnoty na y-ové ose
+y2 = np.cos(x)
+
+# plocha pro graf
+p = figure(title="sin(x) a cos(x)", x_axis_label='x', y_axis_label='sin(x) a cos(x)')
+
+# vykreslení průběhu
+p.square_dot(x, y1, legend_label="sin(x)", line_width=2, color="blue")
+p.circle(x, y2, legend_label="cos(x)", line_width=2, color="red")
+
+# vykreslení grafu do plochy webové stránky
+show(p)
