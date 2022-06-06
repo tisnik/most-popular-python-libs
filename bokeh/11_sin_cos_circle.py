eecfcf44375d71c9eeb68f5fb7baacb73ee1359b
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-06-06 07:53:16 +0200

    Bokeh example #11

---
+++ bokeh/11_sin_cos_circle.py
@@ -0,0 +1,24 @@
+# naimportujeme vybrané funkce z knihovny `bokeh.plotting`
+
+from bokeh.plotting import figure, show
+
+# taktéž budeme potřebovat některé funkce z knihovny `numpy`
+import numpy as np
+
+# vykreslení průběhu funkce sin
+
+# hodnoty parametrické křivky
+t = np.linspace(0, 2 * np.pi, 100)
+
+y1 = np.sin(t)
+
+y2 = np.cos(t)
+
+# plocha pro graf
+p = figure(title="Circle", x_axis_label='x', y_axis_label='y')
+
+# vykreslení průběhu
+p.line(y1, y2, line_width=2, color="blue")
+
+# vykreslení grafu do plochy webové stránky
+show(p)
