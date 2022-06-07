ca420e2e4efc5344858871456f79ce1f8d9e27f0
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-06-07 09:31:10 +0200

    Reformatted by Black

--- pywebio/08_put_markdown.py
+++ pywebio/08_put_markdown.py
@@ -4,7 +4,7 @@
 import pywebio.output as out
 
 
-markdown = '''
+markdown = """
 Markdown rendering
 ==================
 
@@ -17,6 +17,6 @@ nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
 Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
 fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
 culpa qui officia deserunt mollit anim id est laborum.
-'''
+"""
 
 out.put_markdown(markdown)
