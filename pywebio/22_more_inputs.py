cf53259e2a951fc79fd1c8f1e011f35a25312538
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-06-07 09:31:10 +0200

    Reformatted by Black

--- pywebio/22_more_inputs.py
+++ pywebio/22_more_inputs.py
@@ -6,16 +6,21 @@ import pywebio.output as out
 
 
 # vstupní údaje
-info = inp.input_group("Adresa",[
-  inp.input("Jméno", name="name"),
-  inp.input("Příjmení", name="surname"),
-  inp.input("Ulice", name="street"),
-  inp.input("ČP", name="conscription_number"),
-  inp.input("Město", name="city"),
-  inp.input("PSČ", name="postal_code")
-])
+info = inp.input_group(
+    "Adresa",
+    [
+        inp.input("Jméno", name="name"),
+        inp.input("Příjmení", name="surname"),
+        inp.input("Ulice", name="street"),
+        inp.input("ČP", name="conscription_number"),
+        inp.input("Město", name="city"),
+        inp.input("PSČ", name="postal_code"),
+    ],
+)
 
 # výpis výsledků
 out.put_info("Zakázka bude poslána na adresu")
 
-out.put_info(f"{info['name']} {info['surname']}\n{info['street']} {info['conscription_number']}\n{info['postal_code']} {info['city']}")
+out.put_info(
+    f"{info['name']} {info['surname']}\n{info['street']} {info['conscription_number']}\n{info['postal_code']} {info['city']}"
+)
