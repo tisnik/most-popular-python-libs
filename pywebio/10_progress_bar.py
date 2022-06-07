dbbb7def12dc3498be58789aaa8539cde634bc31
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-06-07 09:31:10 +0200

    Reformatted by Black

--- pywebio/10_progress_bar.py
+++ pywebio/10_progress_bar.py
@@ -5,8 +5,8 @@ import pywebio.output as out
 import time
 
 
-out.put_processbar('bar');
+out.put_processbar("bar")
 
 for i in range(1, 11):
-    out.set_processbar('bar', i / 10)
+    out.set_processbar("bar", i / 10)
     time.sleep(0.1)
