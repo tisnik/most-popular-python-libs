a9cb4873d1e103d9479e4a746042de5329a49c69
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-06-07 09:31:10 +0200

    Reformatted by Black

--- pywebio/23_input_validators.py
+++ pywebio/23_input_validators.py
@@ -6,16 +6,21 @@ import pywebio.output as out
 
 
 # vstupní údaje
-info = inp.input_group("Adresa",[
-  inp.input("Jméno", name="name", type=inp.TEXT),
-  inp.input("Příjmení", name="surname", type=inp.TEXT),
-  inp.input("Ulice", name="street", type=inp.TEXT),
-  inp.input("ČP", name="conscription_number", type=inp.NUMBER),
-  inp.input("Město", name="city", type=inp.TEXT),
-  inp.input("PSČ", name="postal_code", type=inp.NUMBER)
-])
+info = inp.input_group(
+    "Adresa",
+    [
+        inp.input("Jméno", name="name", type=inp.TEXT),
+        inp.input("Příjmení", name="surname", type=inp.TEXT),
+        inp.input("Ulice", name="street", type=inp.TEXT),
+        inp.input("ČP", name="conscription_number", type=inp.NUMBER),
+        inp.input("Město", name="city", type=inp.TEXT),
+        inp.input("PSČ", name="postal_code", type=inp.NUMBER),
+    ],
+)
 
 # výpis výsledků
 out.put_info("Zakázka bude poslána na adresu")
 
-out.put_info(f"{info['name']} {info['surname']}\n{info['street']} {info['conscription_number']}\n{info['postal_code']} {info['city']}")
+out.put_info(
+    f"{info['name']} {info['surname']}\n{info['street']} {info['conscription_number']}\n{info['postal_code']} {info['city']}"
+)
