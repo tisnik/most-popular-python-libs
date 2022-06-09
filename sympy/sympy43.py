116e9e71080a84e422d53dbff5cf5683d599f09d
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-06-09 08:47:06 +0200

    Reformatted by Black

--- sympy/sympy43.py
+++ sympy/sympy43.py
@@ -1,8 +1,8 @@
 from sympy import diff, symbols, pprint
-  
-a, b, c, d, e, x = symbols('a,b,c,d,e,x')
 
-f = a*x**3 + b*x**2 + c*x + d + e/x
+a, b, c, d, e, x = symbols("a,b,c,d,e,x")
+
+f = a * x ** 3 + b * x ** 2 + c * x + d + e / x
 
 pprint(f)
 
