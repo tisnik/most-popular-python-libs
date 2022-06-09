233706a46f46ca9f6bb1fc229c19a7ca3d6f0931
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-06-09 08:47:05 +0200

    Reformatted by Black

--- sympy/sympy20.py
+++ sympy/sympy20.py
@@ -2,9 +2,9 @@ from sympy import solve, symbols, pprint, init_printing
 
 init_printing(use_unicode=False)
 
-a, b, c, x = symbols('a,b,c,x')
+a, b, c, x = symbols("a,b,c,x")
 
-f = a*x**2 + b*x + c
+f = a * x ** 2 + b * x + c
 
 pprint(f)
 
