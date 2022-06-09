2d175b3697ff9752e6652be65f63d15e335bad03
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-06-09 08:47:06 +0200

    Reformatted by Black

--- sympy/sympy38.py
+++ sympy/sympy38.py
@@ -1,8 +1,8 @@
 from sympy import diff, symbols, pprint
-  
-x = symbols('x')
 
-f = 5*x**3 + 4*x**2 + 3*x + 4 + 1/x
+x = symbols("x")
+
+f = 5 * x ** 3 + 4 * x ** 2 + 3 * x + 4 + 1 / x
 
 pprint(f)
 
