46c3b7456dbf8db4a3ef2d13ef076e37eab523dd
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-06-09 08:47:05 +0200

    Reformatted by Black

--- sympy/sympy10.py
+++ sympy/sympy10.py
@@ -1,5 +1,5 @@
 import sympy as sp
 
-x,y,z = sp.symbols('x,y,z')
-expression = 2*x + 3*y + 3*x + y + 1/z + 3/z
+x, y, z = sp.symbols("x,y,z")
+expression = 2 * x + 3 * y + 3 * x + y + 1 / z + 3 / z
 sp.pprint(expression)
