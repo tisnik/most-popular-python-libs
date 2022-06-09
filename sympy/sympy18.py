5bbf36e6b98fd94451092e479d9f5eff0b639166
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-06-09 08:47:05 +0200

    Reformatted by Black

--- sympy/sympy18.py
+++ sympy/sympy18.py
@@ -1,5 +1,5 @@
 import sympy as sp
 
-x,y,z = sp.symbols('x,y,z')
-expression = sp.expand((x-y) * (x+y) * (x+z) * (y-z))
+x, y, z = sp.symbols("x,y,z")
+expression = sp.expand((x - y) * (x + y) * (x + z) * (y - z))
 sp.pprint(expression)
