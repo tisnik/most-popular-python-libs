7d45c23d0efde9396f9cb12652c183d70b7055c2
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-06-09 08:47:06 +0200

    Reformatted by Black

--- sympy/sympy37.py
+++ sympy/sympy37.py
@@ -2,10 +2,10 @@ from sympy import solve, solveset, symbols, pprint, init_printing
 
 init_printing(use_unicode=True)
 
-x = symbols('x')
+x = symbols("x")
 
-f1 = x**2 < 10
-f2 = x**3 > -10
+f1 = x ** 2 < 10
+f2 = x ** 3 > -10
 
 pprint(f1)
 pprint(f2)
