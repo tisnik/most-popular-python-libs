2b817e7808722d4dffdfedfdd8abfa558c40f3ed
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-06-09 08:47:06 +0200

    Reformatted by Black

--- sympy/sympy47.py
+++ sympy/sympy47.py
@@ -1,8 +1,8 @@
 from sympy import singularities, symbols, pprint
-  
-a, b, c, d, e, x = symbols('a,b,c,d,e,x')
 
-f = 1/(x**3 - x**2 + x)
+a, b, c, d, e, x = symbols("a,b,c,d,e,x")
+
+f = 1 / (x ** 3 - x ** 2 + x)
 
 pprint(f)
 
