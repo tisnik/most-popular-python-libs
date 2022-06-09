1c4cfa48b09a00bee1cc99d026493448204c8cff
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-06-09 08:47:06 +0200

    Reformatted by Black

--- sympy/sympy26.py
+++ sympy/sympy26.py
@@ -2,9 +2,9 @@ from sympy import solve, symbols, pprint, init_printing
 
 init_printing(use_unicode=True)
 
-a, b, c, x = symbols('α,β,Γ,x')
+a, b, c, x = symbols("α,β,Γ,x")
 
-f = a*x**2 + b*x + c
+f = a * x ** 2 + b * x + c
 
 pprint(f)
 
