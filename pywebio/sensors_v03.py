8cca039f20718e4930b457b60d96ccfe7e940fc5
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-06-09 08:47:05 +0200

    Reformatted by Black

--- pywebio/sensors_v03.py
+++ pywebio/sensors_v03.py
@@ -10,12 +10,7 @@ minio_secret_key = "tester01"
 
 
 def main():
-    client = Minio(
-        minio_address,
-        minio_access_key,
-        minio_secret_key,
-        secure=False
-    )
+    client = Minio(minio_address, minio_access_key, minio_secret_key, secure=False)
 
     found = client.bucket_exists(bucket_name)
     print("Bucket found:", found)
@@ -23,8 +18,14 @@ def main():
     objects = client.list_objects(bucket_name, recursive=False)
 
     for obj in objects:
-        print(obj.bucket_name, obj.object_name, obj.last_modified,
-              obj.etag, obj.size, obj.content_type)
+        print(
+            obj.bucket_name,
+            obj.object_name,
+            obj.last_modified,
+            obj.etag,
+            obj.size,
+            obj.content_type,
+        )
         try:
             response = client.get_object(bucket_name, obj.object_name)
             data = response.read().decode()
