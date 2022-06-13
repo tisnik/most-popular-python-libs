999304c21356b2f19624274ec28547f2c989f3cf
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-06-13 08:25:41 +0200

    Bokeh example #33

---
+++ bokeh/33_select_color_size_alpha.py
@@ -0,0 +1,59 @@
+# naimportujeme vybrané funkce z knihovny `bokeh.plotting` i dalších knihoven
+from bokeh.plotting import figure, show
+from bokeh.layouts import column
+from bokeh.models import ColorPicker, Slider
+
+
+# taktéž budeme potřebovat některé funkce z knihovny `numpy`
+import numpy as np
+
+
+# funkce pro výpočet dalšího bodu Lorenzova atraktoru
+def lorenz(x, y, z, s=10, r=28, b=2.667):
+    x_dot = s * (y - x)
+    y_dot = r * x - y - x * z
+    z_dot = x * y - b * z
+    return x_dot, y_dot, z_dot
+
+
+# krok (změna času)
+dt = 0.01
+
+# celkový počet vypočtených bodů na Lorenzově atraktoru
+n = 10000
+
+# prozatím prázdné pole připravené pro výpočet
+x = np.zeros((n,))
+y = np.zeros((n,))
+z = np.zeros((n,))
+
+# počáteční hodnoty
+x[0], y[0], z[0] = (0.0, 1.0, 1.05)
+
+# vlastní výpočet atraktoru
+for i in range(n - 1):
+    x_dot, y_dot, z_dot = lorenz(x[i], y[i], z[i])
+    x[i + 1] = x[i] + x_dot * dt
+    y[i + 1] = y[i] + y_dot * dt
+    z[i + 1] = z[i] + z_dot * dt
+
+# plocha pro graf
+p = figure(title="Lorenz attractor", x_axis_label="x", y_axis_label="z", width=500, height=500)
+
+# vykreslení průběhu
+scatter = p.scatter(x, z, size=1, color="blue", alpha=0.5)
+
+# výběr barvy
+picker = ColorPicker(title="Color")
+picker.js_link("color", scatter.glyph, "line_color")
+
+# posuvník pro změnu průblednosti
+slider_alpha = Slider(start=0, end=1, value=0.5, step=.01, title="Alpha")
+slider_alpha.js_link("value", scatter.glyph, "line_alpha")
+
+# posuvník pro změnu velikosti
+slider_size = Slider(start=0, end=10, value=1, step=.1, title="Splatter size")
+slider_size.js_link("value", scatter.glyph, "size")
+
+# vykreslení grafu do plochy webové stránky
+show(column(p, picker, slider_alpha, slider_size))
