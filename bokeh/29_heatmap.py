48ca4cf63f8ce17853258aa5a1aabd100b6f39f3
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-06-13 08:25:41 +0200

    Bokeh example #29

---
+++ bokeh/29_heatmap.py
@@ -0,0 +1,27 @@
+# naimportujeme vybrané funkce z knihovny `bokeh.plotting`
+from bokeh.plotting import figure, show
+
+factors = ["a", "b", "c"]
+x = ["a"]*3 + ["b"]*3 + ["c"]*3
+y = factors*3
+
+print(factors)
+print(x)
+print(y)
+
+# barvy jednotlivých čtverečků
+colors = [
+    "#0B486B", "#79BD9A", "#CFF09E",
+    "#79BD9A", "#0B486B", "#79BD9A",
+    "#CFF09E", "#79BD9A", "#0B486B"
+]
+
+# plocha pro graf
+p = figure(title="Categorical Heatmap", tools="hover",
+        toolbar_location=None, x_range=factors, y_range=factors)
+
+# vykreslení heatmapy
+p.rect(x, y, color=colors, width=1, height=1)
+
+# vykreslení grafu do plochy webové stránky
+show(p)
