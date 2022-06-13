610ee6ac5dbaf4ba9effef2c8beb7dded371285c
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-06-13 08:25:41 +0200

    Bokeh example #28

---
+++ bokeh/28_colors.py
@@ -0,0 +1,51 @@
+# naimportujeme vybrané funkce z knihovny `bokeh.plotting`
+
+from bokeh.plotting import figure, show
+
+# taktéž budeme potřebovat některé funkce z knihovny `numpy`
+import numpy as np
+
+
+# funkce pro výpočet dalšího bodu Lorenzova atraktoru
+def lorenz(x, y, z, s=10, r=28, b=2.667):
+    x_dot = s * (y - x)
+    y_dot = r * x - y - x * z
+    z_dot = x * y - b * z
+    return x_dot, y_dot, z_dot
+
+
+# krok (změna času)
+dt = 0.01
+
+# celkový počet vypočtených bodů na Lorenzově atraktoru
+n = 100000
+
+# gradient od 0 do 255
+l = np.linspace(0, 255, num=n)
+
+# barvová paleta
+colors = ["#%02x%02x%02x" % (255-int(value), 0, int(value)) for value in l]
+
+# prozatím prázdné pole připravené pro výpočet
+x = np.zeros((n,))
+y = np.zeros((n,))
+z = np.zeros((n,))
+
+# počáteční hodnoty
+x[0], y[0], z[0] = (0.0, 1.0, 1.05)
+
+# vlastní výpočet atraktoru
+for i in range(n - 1):
+    x_dot, y_dot, z_dot = lorenz(x[i], y[i], z[i])
+    x[i + 1] = x[i] + x_dot * dt
+    y[i + 1] = y[i] + y_dot * dt
+    z[i + 1] = z[i] + z_dot * dt
+
+# plocha pro graf
+p = figure(title="Lorenz attractor", x_axis_label="x", y_axis_label="y")
+
+# vykreslení průběhu
+p.dot(x, y, line_color=colors, line_alpha=0.2)
+
+# vykreslení grafu do plochy webové stránky
+show(p)
