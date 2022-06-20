fb6f604c285aa9f44a8435b2480a763952cfd487
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-06-20 16:51:19 +0200

    Sympy example #66

---
+++ sympy/sympy66.py
@@ -0,0 +1,13 @@
+import sympy as sp
+
+a1 = sp.Array(range(16))
+sp.pprint(a1)
+print(a1.rank())
+print(a1.shape)
+
+print()
+
+a2 = a1.reshape(2,2,2,2)
+sp.pprint(a2)
+print(a2.rank())
+print(a2.shape)
