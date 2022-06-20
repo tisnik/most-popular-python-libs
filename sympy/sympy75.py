d497c382e2eaf1caf9b9305746a9e149f3c5f757
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-06-20 16:51:19 +0200

    Sympy example #75

---
+++ sympy/sympy75.py
@@ -0,0 +1,15 @@
+import sympy as sp
+
+a = sp.MatrixSymbol("A", 3, 4)
+b = sp.MatrixSymbol("B", 4, 3)
+
+sp.pprint(sp.Matrix(a))
+
+print()
+
+sp.pprint(sp.Matrix(b))
+
+print()
+
+c = a*b
+sp.pprint(sp.Matrix(c))
