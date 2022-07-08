61bc7fe05978796aa549a40a332d49594db6975e
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-07-08 08:33:55 +0200

    Bokeh example #39

---
+++ bokeh/39_data_source_color.py
@@ -0,0 +1,28 @@
+# naimportujeme vybrané funkce z knihovny `bokeh.plotting`
+from bokeh.io import output_file, show
+from bokeh.plotting import figure
+from bokeh.models import ColumnDataSource
+from bokeh.palettes import Spectral10
+
+# jména na X-ové ose
+languages = ["Python", "C++", "C#", "Visual Basic", "JavaScript", "SQL", "Assembly", "Java", "C", "Swift"]
+
+# hodnoty na Y-ové ose
+ratings = [12.20, 9.63, 6.12, 5.42, 2.09, 1.94, 1.85, 10.47, 11.91, 1.55]
+
+# definice zdroje dat
+source = ColumnDataSource(data=dict(languages=languages, ratings=ratings, color=Spectral10))
+
+# plocha pro graf
+p = figure(x_range=languages, height=250, title="TIOBE index",
+           toolbar_location=None, tools="")
+
+# vykreslení průběhu hodnot
+p.vbar(x='languages', top='ratings', color='color', source=source, width=0.9)
+
+# styl vykreslení
+p.xgrid.grid_line_color = None
+p.y_range.start = 0
+
+# vykreslení grafu do plochy webové stránky
+show(p)
