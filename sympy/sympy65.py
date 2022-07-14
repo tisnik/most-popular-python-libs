8c683e35519a834d9e7c22ba04fe3d66a08af426
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-07-14 08:10:26 +0200

    Reformatted by Black

--- sympy/sympy65.py
+++ sympy/sympy65.py
@@ -5,28 +5,27 @@ sp.pprint(a1)
 
 print()
 
-a2 = a1.reshape(2,8)
+a2 = a1.reshape(2, 8)
 sp.pprint(a2)
 
 print()
 
-a3 = a1.reshape(4,4)
+a3 = a1.reshape(4, 4)
 sp.pprint(a3)
 
 print()
 
-a4 = a1.reshape(4,2,2)
+a4 = a1.reshape(4, 2, 2)
 sp.pprint(a4)
 
 print()
 
-a5 = a1.reshape(2,4,2)
+a5 = a1.reshape(2, 4, 2)
 sp.pprint(a5)
 
 print()
 
-a6 = a1.reshape(2,2,4)
+a6 = a1.reshape(2, 2, 4)
 sp.pprint(a6)
 
 print()
-
