d78958debb5281bfb60634ce9599a21a6f2e970f
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-07-14 08:10:27 +0200

    Reformatted by Black

--- sympy/sympy71.py
+++ sympy/sympy71.py
@@ -1,13 +1,13 @@
 import sympy as sp
 
 a1 = sp.ImmutableSparseNDimArray(range(12))
-a2 = a1.reshape(3,4)
+a2 = a1.reshape(3, 4)
 
 sp.pprint(a2)
 
 print()
 
-a3 = 2*a2
+a3 = 2 * a2
 sp.pprint(a3)
 
 print()
