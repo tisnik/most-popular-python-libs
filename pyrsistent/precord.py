e4c38d2dea19410e2413d165b495934c0d6b8e3a
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-07-18 07:37:46 +0200

    Persistent vector

---
+++ pyrsistent/precord.py
@@ -0,0 +1,15 @@
+from pyrsistent import PRecord, field
+
+class XYRecord(PRecord):
+    x = field()
+    y = field()
+
+
+record1 = XYRecord(x=1)
+print(record1)
+
+record2 = record1.set("y", 42)
+print(record2)
+
+record3 = record2.set("z", 0)
+print(record3)
