85a5f90fe1e64a29fce3ff9f45824c13ab3cf1f0
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-07-18 07:37:46 +0200

    Freeze operation for vector, example #2

---
+++ pyrsistent/freeze_vectors_02.py
@@ -0,0 +1,12 @@
+from pyrsistent import freeze
+
+vector1 = freeze([1, "foo", [1, 2, 3], None])
+print(vector1)
+print(type(vector1))
+
+vector2 = vector1.append("Five!")
+print(vector1)
+print(type(vector1))
+
+print(vector2)
+print(type(vector2))
