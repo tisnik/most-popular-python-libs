37a6de6bc534d3eae632374d2d0ff48cb32f8d41
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-07-18 07:37:45 +0200

    Persistent map, example #5

---
+++ pyrsistent/maps05.py
@@ -0,0 +1,19 @@
+from pyrsistent import m
+
+map1 = m(first=1, second=2, third=3)
+print("Original map")
+print(map1)
+
+map2 = map1.set("fourth", 4)
+print("\nAfter set")
+print(map1)
+print(map2)
+
+map3 = map1.discard("first")
+print("\nAfter remove")
+print(map1)
+print(map2)
+print(map3)
+
+map1.discard("nonexistentA")
+map1.remove("nonexistentB")
