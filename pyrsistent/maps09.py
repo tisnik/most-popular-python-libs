9fafec7cdffd9b833c77f69cdf918c4a6a08add4
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-07-18 07:37:45 +0200

    Persistent map, example #9

---
+++ pyrsistent/maps09.py
@@ -0,0 +1,16 @@
+from pyrsistent import pmap
+
+map1 = pmap({1:"first", 2:"second", 3:"third"})
+
+vector1 = map1.items()
+
+print("Items:")
+print(vector1)
+print(type(vector1))
+
+vector2 = map1.values()
+
+print("\nValues:")
+print(vector2)
+print(type(vector2))
+
