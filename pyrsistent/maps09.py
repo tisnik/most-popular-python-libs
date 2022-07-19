8c1b414bb33661af13877a709dc7dbd0cf2016b4
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-07-19 11:26:55 +0200

    Reformatted by Black

--- pyrsistent/maps09.py
+++ pyrsistent/maps09.py
@@ -1,6 +1,6 @@
 from pyrsistent import pmap
 
-map1 = pmap({1:"first", 2:"second", 3:"third"})
+map1 = pmap({1: "first", 2: "second", 3: "third"})
 
 vector1 = map1.items()
 
@@ -13,4 +13,3 @@ vector2 = map1.values()
 print("\nValues:")
 print(vector2)
 print(type(vector2))
-
