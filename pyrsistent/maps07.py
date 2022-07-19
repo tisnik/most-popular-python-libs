68b8c3e375971039cd954349fdfbc7a43ed66376
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-07-19 11:26:55 +0200

    Reformatted by Black

--- pyrsistent/maps07.py
+++ pyrsistent/maps07.py
@@ -1,7 +1,7 @@
 from pyrsistent import pmap
 
-map1 = pmap({1:"first", 2:"second", 3:"third"})
-map2 = pmap({3:"3rd", 4:"4th"})
+map1 = pmap({1: "first", 2: "second", 3: "third"})
+map2 = pmap({3: "3rd", 4: "4th"})
 
 print(map1)
 print(map2)
