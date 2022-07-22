58a539cfbae82cd2b23aeef9848489a4e0f6233b
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-07-22 10:26:43 +0200

    Added (c) header

--- numpy/matrix_load_3.py
+++ numpy/matrix_load_3.py
@@ -1,5 +1,18 @@
 """Přečtení obsahu matice ze standardního binárního souboru."""
 
+#
+#  (C) Copyright 2021  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
+
 import numpy as np
 
 # načtení matice ze standardního binárního souboru
