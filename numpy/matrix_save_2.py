59f9614ba1478f20adf8b95056276b593485515c
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-07-22 10:26:43 +0200

    Added (c) header

--- numpy/matrix_save_2.py
+++ numpy/matrix_save_2.py
@@ -1,5 +1,18 @@
 """Uložení obsahu matice do standardního binárního souboru."""
 
+#
+#  (C) Copyright 2021  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
+
 import numpy as np
 
 m = np.linspace(1, 12, 12, dtype="f").reshape(3, 4)
