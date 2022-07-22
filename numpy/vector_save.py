9bdcd7ed647f7af6b2f0386cdbada37a1447654f
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-07-22 10:26:43 +0200

    Added (c) header

--- numpy/vector_save.py
+++ numpy/vector_save.py
@@ -1,5 +1,18 @@
 """Uložení obsahu vektoru do standardního binárního souboru."""
 
+#
+#  (C) Copyright 2021  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
+
 import numpy as np
 
 # vektor obsahující hodnoty s plovoucí řádovou čárkou
