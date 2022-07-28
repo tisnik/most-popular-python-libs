2422ce626bc48f818f7f9aab8853f194fe0696df
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-07-28 07:36:20 +0200

    Added (c) header + shebang

--- pyrsistent/vectors09.py
+++ pyrsistent/vectors09.py
@@ -1,3 +1,17 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+#
+#  (C) Copyright 2022  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+
 from pyrsistent import v
 
 vector1 = v(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
