23a601f4709e16c2d9f42149f51992b518d31b8a
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-07-28 07:36:19 +0200

    Added (c) header + shebang

--- pyrsistent/freeze_vectors_03.py
+++ pyrsistent/freeze_vectors_03.py
@@ -1,3 +1,17 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+#
+#  (C) Copyright 2022  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+
 from pyrsistent import freeze, v
 
 vector1 = freeze([1, "foo", v(1, [2, 3]), None])
