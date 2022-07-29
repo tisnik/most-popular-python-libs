deaa50fc25e1781137c2f7d3885f5b36180d9f9e
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-07-29 07:47:56 +0200

    Added (c) header

--- pywebio/32_fm_synth.py
+++ pywebio/32_fm_synth.py
@@ -2,6 +2,18 @@
 
 """Texture rendering based on FM synthesis."""
 
+#
+#  (C) Copyright 2021  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
 import pywebio.input as inp
 import pywebio.output as out
 
