6dbdb1cadd134fa7fdd398040d5233242f6feef6
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-08-01 07:55:28 +0200

    Traverse AST, 2nd version

---
+++ ast/traverse_expression_2.py
@@ -0,0 +1,12 @@
+import ast
+
+class Visitor(ast.NodeVisitor):
+    def visit(self, node):
+        print(node)
+        self.generic_visit(node)
+
+
+tree = ast.parse("1+2*3")
+
+visitor = Visitor()
+visitor.visit(tree)
