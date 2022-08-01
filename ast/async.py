4044f26515c3c72b4aaf9ca5f8102a86ddbbbcf6
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-08-01 07:55:28 +0200

    Code to be tokenized and parsed

---
+++ ast/async.py
@@ -0,0 +1,24 @@
+import trio
+
+
+async def producer(send_channel):
+    for i in range(1, 10):
+        message = f"message {i}"
+        print(f"Producer: {message}")
+        await send_channel.send(message)
+
+
+async def consumer(receive_channel):
+    async for value in receive_channel:
+        print(f"Consumer: received{value!r}")
+        await trio.sleep(1)
+
+
+async def main():
+    async with trio.open_nursery() as nursery:
+        send_channel, receive_channel = trio.open_memory_channel(0)
+        nursery.start_soon(producer, send_channel)
+        nursery.start_soon(consumer, receive_channel)
+
+
+trio.run(main)
