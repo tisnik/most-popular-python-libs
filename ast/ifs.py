58c6a4e12d94d0efd49fb9371e55d7b19b0aaff9
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-08-03 08:38:36 +0200

    Removed unused/renamed code

--- ast/ifs.py
+++
@@ -1,4 +0,0 @@
-if True:
-    if False:
-        if False and True:
-            print("impossible")
--- ast/tokenize_ifs.py
+++
@@ -1,6 +0,0 @@
-import tokenize
-
-with tokenize.open("ifs.py") as fin:
-    token_generator = tokenize.generate_tokens(fin.readline)
-    for token in token_generator:
-        print(token)
