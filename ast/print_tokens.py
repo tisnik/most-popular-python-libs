3ebafcf7e482c4cfce9267d276e8933e93995c0d
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-08-05 08:41:48 +0200

    Reformatted by Black

--- ast/print_tokens.py
+++ ast/print_tokens.py
@@ -1,6 +1,6 @@
 import token
 
 for name, value in vars(token).items():
-    if not name.startswith('_'):
+    if not name.startswith("_"):
         if isinstance(value, int):
             print("{:20s} {}".format(name, value))
