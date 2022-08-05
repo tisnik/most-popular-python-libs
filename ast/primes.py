b1f7ebb043945ae1803101731352ad333d639da6
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-08-05 08:41:48 +0200

    Reformatted by Black

--- ast/primes.py
+++ ast/primes.py
@@ -3,9 +3,9 @@
 def primes2(limit):
     """Výpočet seznamu prvočísel až do zadaného limitu."""
     is_prime = [False] * 2 + [True] * (limit - 1)
-    for n in range(int(limit**0.5 + 1.5)):  # stop at ``sqrt(limit)``
+    for n in range(int(limit ** 0.5 + 1.5)):  # stop at ``sqrt(limit)``
         if is_prime[n]:
-            for i in range(n*n, limit+1, n):
+            for i in range(n * n, limit + 1, n):
                 is_prime[i] = False
     return [i for i, prime in enumerate(is_prime) if prime]
 
