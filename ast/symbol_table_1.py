eb79ee6acacdd0e99a0e61e18a1d9df2e937204e
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-08-17 07:41:10 +0200

    Displaying symbol table

---
+++ ast/symbol_table_1.py
@@ -0,0 +1,28 @@
+#
+#  (C) Copyright 2022  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
+import symtable
+
+
+t = symtable.symtable("a+b*c", "<string>", "eval")
+
+print("Symbol table:", t)
+print("Type:", t.get_type())
+print("Has children:", t.has_children())
+print("Identifiers:", t.get_identifiers())
+
+symbols = t.get_symbols()
+
+print("\nList of symbols:")
+
+for symbol in symbols:
+    print(symbol.get_name())
