c0caa1a3580658f8db87ef3ca17d713f25ce3c89
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-08-17 07:41:10 +0200

    Bytecode decompilation

---
+++ ast/decompile_1.py
@@ -0,0 +1,42 @@
+#
+#  (C) Copyright 2022  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
+import ast
+import dis
+
+
+class Visitor(ast.NodeVisitor):
+    def __init__(self):
+        self.nest_level = 1
+
+    def visit(self, node):
+        indent = " " * self.nest_level * 2
+        print(indent, node)
+        self.nest_level += 1
+        self.generic_visit(node)
+        self.nest_level -= 1
+
+
+tree = ast.parse("print(1+2*(1-3/4)+5)", mode="exec")
+
+visitor = Visitor()
+visitor.visit(tree)
+
+print("Compiling")
+
+compiled = compile(tree, filename="<ast>", mode="exec")
+
+print("Decompiling")
+
+dis.dis(compiled)
+
+print("Done")
