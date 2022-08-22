927b3416acd939b2c9c23ddb122542fa55d8774f
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-08-22 08:45:59 +0200

    Disassembling bytecode, example #5

---
+++ bytecode/dis_5.py
@@ -0,0 +1,44 @@
+#
+# Modul s nekolika jednoduchymi funkcemi
+# pro otestovani zakladnich vlastnosti bajtkodu jazyka Python
+# prekladu programove struktury typu if-then-else.
+#
+ 
+def prikaz1(x):
+    if x:
+        return 10
+    else:
+        return 20
+ 
+def prikaz2(x, y):
+    if x:
+        if y:
+            return 10
+        else:
+            return 20
+    else:
+        return 30
+ 
+#
+# Vse je nutne otestovat.
+#
+def main():
+    print(prikaz1(True))
+    print(prikaz1(False))
+    print(prikaz2(True, True))
+    print(prikaz2(True, False))
+    print(prikaz2(False, True))
+    print(prikaz2(False, False))
+ 
+def disassemble():
+    from dis import dis
+ 
+    print("\nprikaz1:")
+    dis(prikaz1)
+ 
+    print("\nprikaz2:")
+    dis(prikaz2)
+ 
+main()
+ 
+disassemble()
