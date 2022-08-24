b479c0811b4a3781f4b98832e0804803a292d306
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-08-24 08:35:19 +0200

    Reformatted

--- bytecode/dis_6.py
+++ bytecode/dis_6.py
@@ -18,28 +18,28 @@
 # zakladnich vlastnosti bajtkodu jazyka Lua
 # prekladu programove smycky typu while.
 #
- 
+
 def loop(x):
     while x > 0:
         x = x - 1
     return x
- 
- 
+
+
 #
 # Vse je nutne otestovat.
 #
 def main():
     print(loop(10))
- 
+
 #
 # Vypsani bajkkodu testovane funkce
 #
 def disassemble():
     from dis import dis
- 
+
     print("\nloop:")
     dis(loop)
- 
+
 main()
- 
+
 disassemble()
