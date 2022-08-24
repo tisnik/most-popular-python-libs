07ef3890ec95872c371e6e1cee73db20b03f5ee3
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-08-24 08:40:25 +0200

    Reformatted by black

--- bytecode/dis_9.py
+++ bytecode/dis_9.py
@@ -20,17 +20,21 @@
 # (presneji receno jeji emulace)
 #
 
+
 def loop(x):
     while True:
         x = x - 1
-        if x <= 0: break
+        if x <= 0:
+            break
     return x
+
 
 #
 # Vse je nutne otestovat.
 #
 def main():
     print(loop(10))
+
 
 #
 # Vypsani bajkkodu testovane funkce
@@ -40,6 +44,7 @@ def disassemble():
 
     print("\nloop:")
     dis(loop)
+
 
 main()
 
