3e362ab24dfeaab31c7d9936066bc3dec9ad3c43
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-08-24 08:35:19 +0200

    Reformatted

--- bytecode/dis_1.py
+++ bytecode/dis_1.py
@@ -18,47 +18,47 @@
 # pro otestovani zakladnich vlastnosti bajtkodu jazyka Python
 # prekladu aritmetickych vyrazu.
 #
- 
+
 def vyraz1(x, y):
     result = x + y
     return result
- 
- 
+
+
 def vyraz2(x, y):
     result = x - y
     return result
- 
- 
+
+
 def vyraz3(x, y):
     result = x * y
     return result
- 
- 
+
+
 def vyraz4(x, y):
     result = x / y
     return result
- 
- 
+
+
 def vyraz5(x, y):
     result = x % y
     return result
- 
- 
+
+
 def vyraz6(x, y, z):
     result = x + y * z
     return result
- 
- 
+
+
 def vyraz7(x, y, z, w):
     result = x + y * z + w
     return result
- 
- 
+
+
 def vyraz8(x, y, z, w):
     result = 2 * (x + y) * (z + w) * ((x + z) / (y + w))
     return result
- 
- 
+
+
 #
 # Vse je nutne otestovat.
 #
@@ -71,34 +71,34 @@ def main():
     print(vyraz6(4, 3, 2))
     print(vyraz7(4, 3, 2, 1))
     print(vyraz8(4, 3, 2, 1))
- 
+
 def disassemble():
     from dis import dis
- 
+
     print("\nvyraz1:")
     dis(vyraz1)
- 
+
     print("\nvyraz2:")
     dis(vyraz2)
- 
+
     print("\nvyraz3:")
     dis(vyraz3)
- 
+
     print("\nvyraz4:")
     dis(vyraz4)
- 
+
     print("\nvyraz5:")
     dis(vyraz5)
- 
+
     print("\nvyraz6:")
     dis(vyraz6)
- 
+
     print("\nvyraz7:")
     dis(vyraz7)
- 
+
     print("\nvyraz8:")
     dis(vyraz8)
- 
+
 main()
- 
+
 disassemble()
