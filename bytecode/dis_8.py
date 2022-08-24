812067e9b8bc7d97d5b246611cff492699cef9cb
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-08-24 08:35:19 +0200

    Reformatted

--- bytecode/dis_8.py
+++ bytecode/dis_8.py
@@ -19,31 +19,31 @@
 # prekladu programove smycky typu while pri
 # vypoctu rady prvocisel.
 #
- 
+
 def primeNumbers(min, max):
     i = min
-    while(i <= max):       # projit vsechny hodnoty od min do max
+    while i <= max:        # projit vsechny hodnoty od min do max
         j = 2
-        while(j < i):      # (lze optimalizovat a zkratit smycku!)
+        while j < i:       # (lze optimalizovat a zkratit smycku!)
             if i % j == 0: # je mozne celociselne delit?
                 break      # - pak ovsem nejde o prvocislo
             j = j + 1      # vyzkouset dalsiho kandidata na celociselne deleni
-        if (j == i):       # pokud jsme dosli az k cislu i
+        if j == i:         # pokud jsme dosli az k cislu i
             print(i)       # nedoslo nikdy k celociselnemu deleni
         i = i + 1          # dalsi hodnota v posloupnosti
- 
+
 #
 # Vse je nutne otestovat.
 #
 def main():
     primeNumbers(2, 1000)
- 
+
 def disassemble():
     from dis import dis
- 
+
     print("\nprimeNumbers:")
     dis(primeNumbers)
- 
+
 main()
- 
+
 disassemble()
