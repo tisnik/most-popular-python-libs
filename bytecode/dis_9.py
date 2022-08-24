168c62147bfdb1799b16de1701ffde1bfd94166e
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-08-24 08:35:19 +0200

    Reformatted

--- bytecode/dis_9.py
+++ bytecode/dis_9.py
@@ -19,28 +19,28 @@
 # prekladu programove smycky typu do-while.
 # (presneji receno jeji emulace)
 #
- 
+
 def loop(x):
     while True:
         x = x - 1
         if x <= 0: break
     return x
- 
+
 #
 # Vse je nutne otestovat.
 #
 def main():
     print(loop(10))
- 
+
 #
 # Vypsani bajkkodu testovane funkce
 #
 def disassemble():
     from dis import dis
- 
+
     print("\nloop:")
     dis(loop)
- 
+
 main()
- 
+
 disassemble()
