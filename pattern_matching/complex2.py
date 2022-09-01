b591d66901b22ebee5c7cc7c5a3ae203ca838e67
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-09-01 07:18:02 +0200

    Added shebang and (c) header

--- pattern_matching/complex2.py
+++ pattern_matching/complex2.py
@@ -1,3 +1,18 @@
+#!/usr/bin/python
+# vim: set fileencoding=utf-8
+
+#
+#  (C) Copyright 2022  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
 def test_number(value):
     match value:
         case (0, 0):
