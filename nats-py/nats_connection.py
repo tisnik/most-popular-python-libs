44e668e420e35c18763ab13400bca1efd4aaeda9
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-09-20 09:27:24 +0200

    Reformatted by Black

--- nats-py/nats_connection.py
+++ nats-py/nats_connection.py
@@ -8,7 +8,9 @@ NATS_PASSWORD = "--password--"
 
 async def main():
     print(f"Connecting to NATS at address {URL}")
-    nats_connection = await nats.connect(URL, user=NATS_USERNAME, password=NATS_PASSWORD)
+    nats_connection = await nats.connect(
+        URL, user=NATS_USERNAME, password=NATS_PASSWORD
+    )
     print(f"Connected...{nats_connection}")
 
     print("Closing connection")
@@ -16,5 +18,5 @@ async def main():
     print("Connection closed")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     asyncio.run(main())
