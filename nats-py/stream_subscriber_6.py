ff83c33b29f342bc1ec38c945eba1ca302b051f5
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-09-20 09:27:24 +0200

    Reformatted by Black

--- nats-py/stream_subscriber_6.py
+++ nats-py/stream_subscriber_6.py
@@ -12,19 +12,28 @@ async def on_message(message):
 
 async def main():
     print(f"Connecting to NATS at address {URL}")
-    nats_connection = await nats.connect(URL, user=NATS_USERNAME, password=NATS_PASSWORD)
+    nats_connection = await nats.connect(
+        URL, user=NATS_USERNAME, password=NATS_PASSWORD
+    )
     print("Connected...")
 
     print("Retrieving JetStream object")
     jet_stream = nats_connection.jetstream()
     print(f"Retrieved {jet_stream}")
 
-    configuration = nats.js.api.ConsumerConfig(deliver_policy = nats.js.api.DeliverPolicy.BY_START_SEQUENCE,
-                                               opt_start_seq = 1)
+    configuration = nats.js.api.ConsumerConfig(
+        deliver_policy=nats.js.api.DeliverPolicy.BY_START_SEQUENCE, opt_start_seq=1
+    )
 
     print("Waiting for messages")
     try:
-        sub = await jet_stream.subscribe(None, stream="users-add-operations", cb=on_message, manual_ack=True, config=configuration)
+        sub = await jet_stream.subscribe(
+            None,
+            stream="users-add-operations",
+            cb=on_message,
+            manual_ack=True,
+            config=configuration,
+        )
         while True:
             await sub.next_msg(timeout=1)
     except nats.errors.TimeoutError:
@@ -35,5 +44,5 @@ async def main():
     print("Connection closed")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     asyncio.run(main())
