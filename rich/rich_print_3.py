0e5f724252280243ce46baea6f764f7a6ec1115c
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-11-15 07:54:15 +0100

    Rich print() function, import variant #3

---
+++ rich/rich_print_3.py
@@ -0,0 +1,14 @@
+from rich import print as rprint
+
+a_list = [1, 2, 3, 4, 5]
+rprint(a_list)
+
+a_tuple = (1, 2, 3, 4, 5)
+rprint(a_tuple)
+
+a_dict = {'list': a_list, 'tuple': a_tuple, 'nil': None}
+rprint(a_dict)
+
+other_list = [True, False, None]
+rprint(other_list)
+
