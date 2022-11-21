1630d656b664740eeddf62a115e28267adc08daf
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-11-21 08:53:00 +0100

    Columns with panels, example #2

---
+++ rich/columns_panels_2.py
@@ -0,0 +1,15 @@
+from rich.console import Console
+from rich.columns import Columns
+from rich.panel import Panel
+
+
+console = Console()
+
+panel1 = Panel("RGB\n\n[red]Red\n[green]Green\n[blue]Blue")
+panel2 = Panel("CMYK\n\n[cyan]Cyan\n[magenta]Magenta\n[yellow]Yellow\n[black]blacK")
+panel3 = Panel("YUV")
+panel4 = Panel("HSL\n\nHue\nSaturation\nLightness")
+panel5 = Panel("HSV\n\nHue\nSaturation\nValue")
+
+columns = Columns([panel1, panel2, panel3, panel4, panel5])
+console.print(columns)
