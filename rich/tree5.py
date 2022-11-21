d79c952be974e7363f40b8ab87bbec20300be0e6
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-11-21 08:53:00 +0100

    Tree rendering, example #5

---
+++ rich/tree5.py
@@ -0,0 +1,29 @@
+from rich.tree import Tree
+from rich import print
+
+
+tree = Tree("Barvové modely")
+rgb = tree.add("RGB")
+cmyk = tree.add("CMYK")
+yuv = tree.add("YUV")
+hsl = tree.add("HSL")
+hsv = tree.add("HSV")
+
+rgb.add("barva [red]Red")
+rgb.add("barva [green]Green")
+rgb.add("barva [blue]Blue")
+
+cmyk.add("barva [cyan]Cyan")
+cmyk.add("barva [magenta]Magenta")
+cmyk.add("barva [yellow]Yellow")
+cmyk.add("barva [black]blacK")
+
+hsl.add("Hue")
+hsl.add("Saturation")
+hsl.add("Lightness")
+
+hsv.add("Hue")
+hsv.add("Saturation")
+hsv.add("Value")
+
+print(tree)
