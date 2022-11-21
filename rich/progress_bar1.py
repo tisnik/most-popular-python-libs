83cec826b2aa0dd1bda2875ce3350ec0f339c832
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-11-21 08:53:00 +0100

    Progress bar, example #1

---
+++ rich/progress_bar1.py
@@ -0,0 +1,10 @@
+from rich.progress import track
+from time import sleep
+
+
+def process_data():
+    sleep(0.02)
+
+
+for _ in track(range(100), description='[green]Processing data'):
+    process_data()
