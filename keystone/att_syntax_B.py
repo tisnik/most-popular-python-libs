802e185588ea4eda9d2438ed9cf89ffcfaea1b5a
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-12-08 08:59:01 +0100

    AT&T syntax, correct variant

---
+++ keystone/att_syntax_B.py
@@ -0,0 +1,15 @@
+from keystone import *
+
+CODE = """
+    MOV $64, %AX
+    INC %AX
+    MOV %AX, %BX
+"""
+ 
+try:
+    ks = Ks(KS_ARCH_X86, KS_MODE_16)
+    ks.syntax = KS_OPT_SYNTAX_ATT
+    encoding, count = ks.asm(CODE)
+    print("%s = %s (number of statements: %u)" %(CODE, encoding, count))
+except KsError as e:
+    print("ERROR: %s" %e)
