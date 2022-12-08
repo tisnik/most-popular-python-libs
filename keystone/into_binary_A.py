5c5441d7f1a0653c3d3aff80ccfa57cece941949
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-12-08 08:59:01 +0100

    Assembling into binary

---
+++ keystone/into_binary_A.py
@@ -0,0 +1,20 @@
+from keystone import *
+
+CODE = """
+OUTER_LOOP:
+    MOV EBX, 10
+INNER_LOOP:
+    MOV EAX, 100
+    DEC EAX
+    JNZ INNER_LOOP
+    DEC EBX
+    JNZ OUTER_LOOP
+"""
+ 
+try:
+    ks = Ks(KS_ARCH_X86, KS_MODE_64)
+    encoding, count = ks.asm(CODE)
+    with open("loops.bin", "wb") as fout:
+        fout.write(bytes(encoding))
+except KsError as e:
+    print("ERROR: %s" %e)
