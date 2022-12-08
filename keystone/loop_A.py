c2aa907c75c422586bd01d626d8b4c85a7f06a76
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-12-08 08:59:01 +0100

    Simple loops

---
+++ keystone/loop_A.py
@@ -0,0 +1,15 @@
+from keystone import *
+
+CODE = """
+LOOP:
+    MOV EAX, 100
+    DEC EAX
+    JNZ LOOP
+"""
+ 
+try:
+    ks = Ks(KS_ARCH_X86, KS_MODE_64)
+    encoding, count = ks.asm(CODE)
+    print("%s = %s (number of statements: %u)" %(CODE, encoding, count))
+except KsError as e:
+    print("ERROR: %s" %e)
