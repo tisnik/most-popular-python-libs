8706c455931c34f611f67453102b35983d511803
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-12-08 09:35:23 +0100

    Assembler into binary

---
+++ keystone/into_binary_C.py
@@ -0,0 +1,14 @@
+from keystone import *
+
+try:
+    with open("hello_world_2.asm", "r") as fin:
+        code = fin.read()
+
+    print(code)
+
+    ks = Ks(KS_ARCH_X86, KS_MODE_64)
+    encoding, count = ks.asm(code)
+    with open("hello_2.bin", "wb") as fout:
+        fout.write(bytes(encoding))
+except KsError as e:
+    print("ERROR: %s" %e)
