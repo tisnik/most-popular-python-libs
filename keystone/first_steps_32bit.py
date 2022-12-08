96632b5ecf414ad993a89a36e4832586c0959578
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-12-08 08:59:01 +0100

    32bit x86 code

---
+++ keystone/first_steps_32bit.py
@@ -0,0 +1,14 @@
+from keystone import *
+
+CODE = """
+    MOV AX, 100
+    INC AX
+    MOV BX, AX
+"""
+ 
+try:
+    ks = Ks(KS_ARCH_X86, KS_MODE_32)
+    encoding, count = ks.asm(CODE)
+    print("%s = %s (number of statements: %u)" %(CODE, encoding, count))
+except KsError as e:
+    print("ERROR: %s" %e)
