a7313f25e1498ff4ae82d31222c2605fd34eb5f0
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-12-09 08:51:07 +0100

    Reformatted by Black

--- keystone/disasm.py
+++ keystone/disasm.py
@@ -6,4 +6,4 @@ with open("loops.bin", "rb") as fin:
 
 md = Cs(CS_ARCH_X86, CS_MODE_64)
 for i in md.disasm(code, 0x0000):
-    print("0x%x:\t%s\t%s" %(i.address, i.mnemonic, i.op_str))
+    print("0x%x:\t%s\t%s" % (i.address, i.mnemonic, i.op_str))
