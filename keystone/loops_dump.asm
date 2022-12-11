3c32b627f298cf3230733f5180ca21a1e72921fc
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-12-11 13:48:03 +0100

    Generated file

--- keystone/loops_dump.asm
+++ keystone/loops_dump.asm
@@ -8,6 +8,6 @@ Disassembly of section .data:
    0:	bb 0a 00 00 00       	mov    ebx,0xa
    5:	b8 64 00 00 00       	mov    eax,0x64
    a:	ff c8                	dec    eax
-   c:	75 f7                	jne    0x5
+   c:	75 fc                	jne    0xa
    e:	ff cb                	dec    ebx
-  10:	75 ee                	jne    0x0
+  10:	75 f3                	jne    0x5
