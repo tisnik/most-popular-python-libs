3e9db82b8187597696358f68f376b5f96ec057ae
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-12-21 15:54:28 +0100

    Dumped binary

---
+++ keystone/att_syntax_dump.asm
@@ -0,0 +1,10 @@
+
+att_syntax.bin:     file format binary
+
+
+Disassembly of section .data:
+
+0000000000000000 <.data>:
+   0:	66 b8 64 00          	mov    ax,0x64
+   4:	66 ff c0             	inc    ax
+   7:	66 89 c3             	mov    bx,ax
