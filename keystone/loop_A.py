6ebab9136a4e0897536a5480cb0221da08cdcfd1
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-12-22 13:48:06 +0100

    Added comments

--- keystone/loop_A.py
+++ keystone/loop_A.py
@@ -12,6 +12,7 @@
 
 from keystone import *
 
+# instrukce, které se mají assemblerem přeložit
 CODE = """
     MOV EAX, 100
 LOOP:
@@ -20,8 +21,13 @@ LOOP:
 """
 
 try:
+    # inicializace assembleru se specifikací architektury a popř. i režimu
     ks = Ks(KS_ARCH_X86, KS_MODE_64)
+
+    # vlastní překlad (assembling)
     encoding, count = ks.asm(CODE)
+
+    # tisk výsledku činnosti assembleru
     print("%s = %s (number of statements: %u)" % (CODE, encoding, count))
 except KsError as e:
     print("ERROR: %s" % e)
