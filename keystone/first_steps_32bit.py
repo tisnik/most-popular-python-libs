cbf622de4dc1284c53ea13694541fa053d7c9726
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-12-27 08:55:33 +0100

    Explicit imports

--- keystone/first_steps_32bit.py
+++ keystone/first_steps_32bit.py
@@ -10,7 +10,8 @@
 #      Pavel Tisnovsky
 #
 
-from keystone import *
+# import všech symbolů použitých ve skriptu
+from keystone import Ks, KS_ARCH_X86, KS_MODE_32, KsError
 
 # instrukce, které se mají assemblerem přeložit
 CODE = """
