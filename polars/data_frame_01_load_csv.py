fa46739ad9a7009a75a05366be9949c9ca3576fe
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-01-09 07:52:54 +0100

    Loading CSV, basic variant

---
+++ polars/data_frame_01_load_csv.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+import polars
+
+# přečtení zdrojových dat
+df = polars.read_csv("hall_of_fame.csv")
+
+# zobrazíme datový rámec
+print(df)
+print()
+
+# podrobnější informace o datovém rámci
+print("Column types")
+print(df.dtypes)
+print()
