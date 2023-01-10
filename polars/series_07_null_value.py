72e07f684075b1c7a7e939f7666110a6da2ef729
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-01-10 08:28:57 +0100

    Null values support

---
+++ polars/series_07_null_value.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+import polars
+
+# vytvoření datové řady
+s = polars.Series("sloupec", [1, 2, None, 3, 4])
+
+# zobrazíme datovou řadu
+print(s)
+print()
+
+# podrobnější informace o datové řadě
+print("Column type")
+print(s.dtype)
+print()
