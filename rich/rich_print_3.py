7dddf401c43eefe0004eb751c2f6f93dd088c204
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-01-12 11:10:49 +0100

    Reformatted by Black

--- rich/rich_print_3.py
+++ rich/rich_print_3.py
@@ -6,9 +6,8 @@ rprint(a_list)
 a_tuple = (1, 2, 3, 4, 5)
 rprint(a_tuple)
 
-a_dict = {'list': a_list, 'tuple': a_tuple, 'nil': None}
+a_dict = {"list": a_list, "tuple": a_tuple, "nil": None}
 rprint(a_dict)
 
 other_list = [True, False, None]
 rprint(other_list)
-
