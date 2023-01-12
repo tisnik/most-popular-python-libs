c20015863e2a0661e09353577b4990e96c8e7198
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-01-12 11:10:49 +0100

    Reformatted by Black

--- rich/standard_print.py
+++ rich/standard_print.py
@@ -4,9 +4,8 @@ print(a_list)
 a_tuple = (1, 2, 3, 4, 5)
 print(a_tuple)
 
-a_dict = {'list': a_list, 'tuple': a_tuple, 'nil': None}
+a_dict = {"list": a_list, "tuple": a_tuple, "nil": None}
 print(a_dict)
 
 other_list = [True, False, None]
 print(other_list)
-
