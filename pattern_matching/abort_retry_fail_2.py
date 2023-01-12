6e833ff524b61e9695d1722b7c7646089dcc4272
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-01-12 11:10:49 +0100

    Reformatted by Black

--- pattern_matching/abort_retry_fail_2.py
+++ pattern_matching/abort_retry_fail_2.py
@@ -19,11 +19,7 @@ print("Not ready reading drive A")
 def abort_retry_fail():
     response = input("Abort, Retry, Fail? ")
 
-    commands = {
-            "a": "Abort",
-            "r": "Retry",
-            "f": "Fail"
-            }
+    commands = {"a": "Abort", "r": "Retry", "f": "Fail"}
 
     return commands.get(response, "Wrong response")
 
