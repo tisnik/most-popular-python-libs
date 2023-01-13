b64044a80b67a65475740c187aeaa4f7f3179f2a
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-01-13 08:00:49 +0100

    Data frame formatting, without shape

---
+++ polars/data_frame_16_formatting_B.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+import polars
+
+# přečtení zdrojových dat
+df = polars.read_csv("hall_of_fame.csv")
+
+# maximální počet zobrazených řádků
+polars.Config.set_tbl_rows(100)
+
+# nezobrazovat tvar (shape) datových rámců
+polars.Config.set_tbl_hide_dataframe_shape(True)
+
+formattings = (
+        "ASCII_FULL",
+        "ASCII_FULL_CONDENSED",
+        "ASCII_NO_BORDERS",
+        "ASCII_BORDERS_ONLY",
+        "ASCII_BORDERS_ONLY_CONDENSED",
+        "ASCII_HORIZONTAL_ONLY",
+        "ASCII_MARKDOWN",
+        "UTF8_FULL",
+        "UTF8_FULL_CONDENSED",
+        "UTF8_NO_BORDERS",
+        "UTF8_BORDERS_ONLY",
+        "UTF8_HORIZONTAL_ONLY",
+        "NOTHING",
+        )
+
+for formatting in formattings:
+    print()
+    print(formatting)
+    print()
+
+    polars.Config.set_tbl_formatting(formatting)
+
+    # zobrazíme datový rámec
+    print(df)
