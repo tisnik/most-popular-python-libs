b1eb3da94db07167fbeacfc21e70d375feed7bf8
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-01-13 08:00:49 +0100

    Reversing data frame

---
+++ polars/data_frame_18_sort_reverse.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+import polars
+
+# přečtení zdrojových dat
+df = polars.read_csv("hall_of_fame.csv")
+
+# maximální počet zobrazených řádků
+polars.Config.set_tbl_rows(100)
+
+# seřadit a vytvořit nový datový rámec
+df = df.sort("Winner").reverse()
+
+# zobrazíme datový rámec
+print(df)
