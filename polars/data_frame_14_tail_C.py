fed1e98f9d561336eb59829d7d17a0b9a2c826dc
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-01-16 08:07:42 +0100

    Tail and maximal limit

---
+++ polars/data_frame_14_tail_C.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+import polars
+
+# přečtení zdrojových dat
+df = polars.read_csv("hall_of_fame.csv")
+
+# maximální počet zobrazených řádků
+polars.Config.set_tbl_rows(100)
+
+# zobrazíme datový rámec
+print(df.tail(30))
