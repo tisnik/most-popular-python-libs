a0e255d8f37531cc19c07c2855d61a7f105008e2
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-01-16 08:07:10 +0100

    Updated comments

--- polars/data_frame_19_unique.py
+++ polars/data_frame_19_unique.py
@@ -9,7 +9,7 @@ df = polars.read_csv("hall_of_fame.csv")
 # maximální počet zobrazených řádků
 polars.Config.set_tbl_rows(100)
 
-# seřadit a vytvořit nový datový rámec
+# vyfiltrovat a vytvořit nový datový rámec
 df = df.unique(subset="Winner")
 
 # zobrazíme datový rámec
