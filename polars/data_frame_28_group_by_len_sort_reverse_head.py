b0e502b4d98c279081a219cb6854a757290933d6
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-01-16 08:06:41 +0100

    Dtto, but pipeline continues with head()

---
+++ polars/data_frame_28_group_by_len_sort_reverse_head.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+import polars
+
+# přečtení zdrojových dat
+df = polars.read_csv("hall_of_fame.csv")
+
+# maximální počet zobrazených řádků
+polars.Config.set_tbl_rows(100)
+
+# seskupení podle názvu jazyka
+df = df.groupby("Winner", maintain_order=True).agg([polars.col("Year").len()]). \
+        sort("Year"). \
+        reverse(). \
+        head(5)
+
+# zobrazíme datový rámec
+print(df)
