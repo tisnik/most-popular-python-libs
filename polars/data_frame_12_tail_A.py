fb52eee3ce340b5f39c971b72ebc144a9b1b4889
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-01-16 08:07:23 +0100

    Tail basic usage

---
+++ polars/data_frame_12_tail_A.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+import polars
+
+# přečtení zdrojových dat
+df = polars.read_csv("hall_of_fame.csv")
+
+# zobrazíme datový rámec, resp.jeho patičku
+print(df.tail())
