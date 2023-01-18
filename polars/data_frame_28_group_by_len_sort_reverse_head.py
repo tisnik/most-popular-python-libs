3ebb633d682262ad4898bde15b2276d9706dd6df
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-01-18 07:42:26 +0100

    Formatted by Black

--- polars/data_frame_28_group_by_len_sort_reverse_head.py
+++ polars/data_frame_28_group_by_len_sort_reverse_head.py
@@ -22,10 +22,13 @@ df = polars.read_csv("hall_of_fame.csv")
 polars.Config.set_tbl_rows(100)
 
 # seskupení podle názvu jazyka
-df = df.groupby("Winner", maintain_order=True).agg([polars.col("Year").len().alias("Zvítězil")]). \
-        sort("Zvítězil"). \
-        reverse(). \
-        head(5)
+df = (
+    df.groupby("Winner", maintain_order=True)
+    .agg([polars.col("Year").len().alias("Zvítězil")])
+    .sort("Zvítězil")
+    .reverse()
+    .head(5)
+)
 
 # zobrazíme datový rámec
 print(df)
