1c534ada76e649fcb99a19f733540e35c427e7cc
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-01-18 07:41:49 +0100

    Formatted by Black

--- polars/data_frame_15_formatting_A.py
+++ polars/data_frame_15_formatting_A.py
@@ -22,20 +22,20 @@ df = polars.read_csv("hall_of_fame.csv")
 polars.Config.set_tbl_rows(100)
 
 formattings = (
-        "ASCII_FULL",
-        "ASCII_FULL_CONDENSED",
-        "ASCII_NO_BORDERS",
-        "ASCII_BORDERS_ONLY",
-        "ASCII_BORDERS_ONLY_CONDENSED",
-        "ASCII_HORIZONTAL_ONLY",
-        "ASCII_MARKDOWN",
-        "UTF8_FULL",
-        "UTF8_FULL_CONDENSED",
-        "UTF8_NO_BORDERS",
-        "UTF8_BORDERS_ONLY",
-        "UTF8_HORIZONTAL_ONLY",
-        "NOTHING",
-        )
+    "ASCII_FULL",
+    "ASCII_FULL_CONDENSED",
+    "ASCII_NO_BORDERS",
+    "ASCII_BORDERS_ONLY",
+    "ASCII_BORDERS_ONLY_CONDENSED",
+    "ASCII_HORIZONTAL_ONLY",
+    "ASCII_MARKDOWN",
+    "UTF8_FULL",
+    "UTF8_FULL_CONDENSED",
+    "UTF8_NO_BORDERS",
+    "UTF8_BORDERS_ONLY",
+    "UTF8_HORIZONTAL_ONLY",
+    "NOTHING",
+)
 
 for formatting in formattings:
     print()
