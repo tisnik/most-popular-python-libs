129cb8c3cfec40ea4b4414dc9d834fb9f569881c
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-01-19 09:07:07 +0100

    Pyglet example #1: empty window creation on screen

---
+++ pyglet/01_empty_window.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# vim: set fileencoding=utf-8
+
+# ---------------------------------------------------------------------
+#
+# Demonstrační příklady využívající knihovnu Pyglet
+#
+# Příklad číslo 1: vytvoření a zobrazení prázdného okna
+#
+# ---------------------------------------------------------------------
+
+
+# všechny třídy a funkce jsou obsaženy v jediném modulu nazvaném pyglet
+import pyglet
+
+# vytvoření okna
+window = pyglet.window.Window(width=640, height=480, caption="Pyglet library")
+
+# spuštění smyčky pro zpracování událostí
+pyglet.app.run()
