2c29b442ff975ed2098598213412ecb6a9567bb9
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-01-19 09:07:07 +0100

    Pyglet example #2: empty window app as a script

---
+++ pyglet/02_empty_window_script.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8
+
+# ---------------------------------------------------------------------
+#
+# Demonstrační příklady využívající knihovnu Pyglet
+#
+# Příklad číslo 2: vytvoření a zobrazení prázdného okna, skript lze spustit
+#
+# ---------------------------------------------------------------------
+
+
+# všechny třídy a funkce jsou obsaženy v jediném modulu nazvaném pyglet
+import pyglet
+
+# vytvoření okna
+window = pyglet.window.Window(width=640, height=480, caption="Pyglet library")
+
+# spuštění smyčky pro zpracování událostí
+pyglet.app.run()
