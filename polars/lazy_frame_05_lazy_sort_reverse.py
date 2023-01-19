084d6701a83ce85c5c29f872fb0a50f90756e0ed
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-01-19 12:47:45 +0100

    Lazy frame example #5: lazy sort+reverse operations

---
+++ polars/lazy_frame_05_lazy_sort_reverse.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+import polars
+
+# líné přečtení zdrojových dat
+df = polars.scan_csv("hall_of_fame.csv")
+
+# zobrazíme líně načtený datový rámec
+print(df)
+print()
+
+# aplikace operace na líný datový rámec
+df2 = df.sort("Winner").reverse()
+
+# převod na běžný datový rámec
+df3 = df2.collect()
+
+# zobrazíme druhý líny datový rámec
+print(df2)
+print()
+
+# zobrazíme běžný (výsledný) datový rámec
+print(df3)
+print()
+print(df3.columns)
+print(df3.dtypes)
