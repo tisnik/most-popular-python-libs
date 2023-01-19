4783a8800d9435c0f09df0e0c132d9a9cb99ba1d
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-01-19 09:07:07 +0100

    Pyglet example #7: OpenGL code to draw the scene

---
+++ pyglet/07_opengl2.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8
+
+# ---------------------------------------------------------------------
+#
+# Demonstrační příklady využívající knihovnu Pyglet
+#
+# Příklad číslo 7: ukázka použití základních funkcí OpenGL
+#                  další možnosti použití textového návěští
+#
+# ---------------------------------------------------------------------
+
+
+# všechny třídy a funkce jsou obsaženy v jediném modulu nazvaném pyglet
+import pyglet
+
+# druhý import s funkcemi převzatými z OpenGL
+from pyglet.gl import *
+
+
+# vytvoření okna
+window = pyglet.window.Window(width=640, height=480, caption="Pyglet+OpenGL")
+
+
+@window.event
+def on_draw():
+    """Obsluha události - překreslení obsahu okna."""
+    glClear(GL_COLOR_BUFFER_BIT)
+    glLoadIdentity()
+    glBegin(GL_TRIANGLES)
+    glVertex2f(0, 0)
+    glVertex2f(window.width, 0)
+    glVertex2f(window.width, window.height)
+    glEnd()
+
+
+# spuštění smyčky pro zpracování událostí
+pyglet.app.run()
