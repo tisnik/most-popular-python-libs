175b7bc1c5b1885e1bf5588d2cf8e90a90b12f6d
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-01-19 09:07:07 +0100

    Pyglet example #4: text label

---
+++ pyglet/04_label.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8
+
+# ---------------------------------------------------------------------
+#
+# Demonstrační příklady využívající knihovnu Pyglet
+#
+# Příklad číslo 4: použití textového návěští
+#
+# ---------------------------------------------------------------------
+
+
+# všechny třídy a funkce jsou obsaženy v jediném modulu nazvaném pyglet
+import pyglet
+
+# vytvoření okna
+window = pyglet.window.Window(width=640, height=480, caption="Pyglet library")
+
+# textové návěští, které se má zobrazit v oknu
+label = pyglet.text.Label(
+    "Pyglet library",
+    font_name="Terminus",
+    font_size=36,
+    x=window.width // 2,
+    y=window.height // 2,
+)
+
+
+@window.event
+def on_draw():
+    """Obsluha události - překreslení obsahu okna."""
+    window.clear()
+    label.draw()
+
+
+# spuštění smyčky pro zpracování událostí
+pyglet.app.run()
