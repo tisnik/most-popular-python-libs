3da1813deec2ee5a8cbe309f379f18e3c6fb390b
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-01-19 09:07:07 +0100

    Pyglet example #3: on_draw event handling

---
+++ pyglet/03_on_draw_event.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8
+
+# ---------------------------------------------------------------------
+#
+# Demonstrační příklady využívající knihovnu Pyglet
+#
+# Příklad číslo 3: obsluha události - překreslení obsahu okna
+#
+# ---------------------------------------------------------------------
+
+
+# všechny třídy a funkce jsou obsaženy v jediném modulu nazvaném pyglet
+import pyglet
+
+# vytvoření okna
+window = pyglet.window.Window(width=640, height=480, caption="Pyglet library")
+
+
+@window.event
+def on_draw():
+    """Obsluha události - překreslení obsahu okna."""
+    window.clear()
+
+
+# spuštění smyčky pro zpracování událostí
+pyglet.app.run()
