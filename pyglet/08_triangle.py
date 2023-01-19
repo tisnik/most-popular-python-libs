210fb2bf10f24878e566859dc4b5677a2a7a5588
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-01-19 09:07:07 +0100

    Pyglet example #8: colorized triangle (Gouraud shading)

---
+++ pyglet/08_triangle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8
+
+# ---------------------------------------------------------------------
+#
+# Demonstrační příklady využívající knihovnu Pyglet
+#
+# Příklad číslo 8: ukázka použití základních funkcí OpenGL
+#                  vykreslení trojúhelníku vyplněného RGB gradientem
+#
+# ---------------------------------------------------------------------
+
+# všechny třídy a funkce jsou obsaženy v jediném modulu nazvaném pyglet
+import pyglet
+
+# druhý import s funkcemi převzatými z OpenGL
+from pyglet.gl import *
+
+# vytvoření okna
+window = pyglet.window.Window(width=640, height=480, caption="Pyglet+OpenGL")
+
+
+@window.event
+def on_draw():
+    """Obsluha události - překreslení obsahu okna."""
+    glClear(GL_COLOR_BUFFER_BIT)
+    glLoadIdentity()
+    glBegin(GL_TRIANGLES)
+    glColor3f(1, 0, 0)
+    glVertex2f(window.width / 2, 0)
+    glColor3f(0, 1, 0)
+    glVertex2f(0, window.height)
+    glColor3f(0, 0, 1)
+    glVertex2f(window.width, window.height)
+    glEnd()
+
+
+# spuštění smyčky pro zpracování událostí
+pyglet.app.run()
