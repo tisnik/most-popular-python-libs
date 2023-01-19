814d062c4b627363776b266def5707f45d53d01c
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-01-19 12:47:45 +0100

    Lazy frame example #6: describe plan for lazy operations

---
+++ polars/lazy_frame_06_describe_plan.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+import polars
+
+# líné přečtení zdrojových dat
+df = polars.scan_csv("hall_of_fame.csv")
+
+# zobrazíme plán pro líně načtený datový rámec
+print(df.describe_plan())
+print()
+
+# aplikace operace na líný datový rámec
+df2 = df.sort("Winner").reverse()
+
+# převod na běžný datový rámec
+df3 = df2.collect()
+
+# zobrazíme plán pro druhý líny datový rámec
+print(df2.describe_plan())
+print()
+
+# zobrazíme běžný (výsledný) datový rámec
+print(df3)
+print()
+print(df3.columns)
+print(df3.dtypes)
