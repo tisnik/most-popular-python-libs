39e124747aa22a0149a40db4c0046a6e865672d3
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-01-23 08:12:59 +0100

    Vertical concatenation of data frames

---
+++ polars/data_frame_concat_vertically.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+import polars
+
+# maximální počet zobrazených řádků
+polars.Config.set_tbl_rows(100)
+
+# přečtení zdrojových dat
+df1 = polars.read_csv("tiobeC.tsv", sep="\t")
+df2 = polars.read_csv("tiobeD.tsv", sep="\t")
+
+# datové rámce zobrazíme
+print(df1)
+print()
+print(df2)
+print()
+
+# spojení obou datových rámců
+concatenated = polars.concat([df1, df2], how="vertical")
+
+# výpis výsledku
+print(concatenated)
