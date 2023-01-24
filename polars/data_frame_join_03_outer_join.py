e4f4f802d05f291ecb64e6f8bc8733ab63f2f414
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-01-24 08:03:37 +0100

    Outer join of two data frames

---
+++ polars/data_frame_join_03_outer_join.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+import polars
+
+# maximální počet zobrazených řádků
+polars.Config.set_tbl_rows(100)
+
+# přečtení zdrojových dat
+df1 = polars.read_csv("tiobeA.tsv", sep="\t")
+df2 = polars.read_csv("tiobeB.tsv", sep="\t")
+
+# datové rámce zobrazíme
+print(df1)
+print()
+print(df2)
+print()
+
+# spojení obou datových rámců
+merged = df1.join(df2, on="Language", how="outer")
+
+# výpis výsledku
+print(merged)
