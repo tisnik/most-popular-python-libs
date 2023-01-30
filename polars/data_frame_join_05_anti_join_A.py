03ce0dd6e45b431bb2114d99c46a62474f6c0ca5
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-01-30 14:29:53 +0100

    Anti join, example #1

---
+++ polars/data_frame_join_05_anti_join_A.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+#
+#  (C) Copyright 2023  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
+import polars
+
+# maximální počet zobrazených řádků
+polars.Config.set_tbl_rows(100)
+
+# přečtení zdrojových dat
+df1 = polars.read_csv("tiobeA.tsv", sep="\t")
+df2 = polars.read_csv("tiobeB.tsv", sep="\t")
+
+# datové rámce zobrazíme
+print(df1)
+print()
+print(df2)
+print()
+
+# spojení obou datových rámců
+merged = df1.join(df2, on="Language", how="anti")
+
+# výpis výsledku
+print(merged)
