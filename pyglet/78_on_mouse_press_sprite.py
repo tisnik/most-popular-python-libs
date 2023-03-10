5a5ccefaca60b6aa2f817c7ed7e603592652f125
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-03-10 07:36:34 +0100

    Added license header

--- pyglet/78_on_mouse_press_sprite.py
+++ pyglet/78_on_mouse_press_sprite.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python
 
+#
+#  (C) Copyright 2017  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
 import pyglet
 
 
