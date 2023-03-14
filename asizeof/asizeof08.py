1d4ee8568d96cc0df66996607bbb436bb9a941c1
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-03-14 09:21:48 +0100

    Asizeof usage: example #8

---
+++ asizeof/asizeof08.py
@@ -0,0 +1,26 @@
+from pympler import asizeof
+
+
+def print_sizeof(value):
+    typename = "{:8}".format(type(value).__name__)
+    print(asizeof.asizeof(value), "\t", typename, "\t", value)
+
+
+def foo():
+    pass
+
+
+def bar(x, y):
+    return x+y
+
+
+def baz(x=0, y=1):
+    print(x)
+    print(y)
+    return x+y
+
+
+print_sizeof(print)
+print_sizeof(foo)
+print_sizeof(bar)
+print_sizeof(baz)
