a258d7b3a2d08c81e7597b4f5a42fd462217a564
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-05-17 07:55:30 +0200

    Added license header

--- memory_profiler/app5.py
+++ memory_profiler/app5.py
@@ -1,13 +1,28 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+#
+#  (C) Copyright 2023  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
 from time import sleep
 
 
 @profile
 def foo_construct():
     l = []
-    for i in range(100000):
+    for i in range(1000000):
         l.append(i)
 
-    for i in range(100000, 0, -1):
+    for i in range(1000000, 0, -1):
         del l[i-1]
 
 
