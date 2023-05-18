2f131fd88b69e800056c9311afcc4dca2d439bef
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-05-18 08:40:02 +0200

    Added license

--- pandas/check_types_1.py
+++ pandas/check_types_1.py
@@ -3,6 +3,18 @@
 
 """Reading CSV file that contains column with integer values (some are missing)."""
 
+#
+#  (C) Copyright 2021  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
 import pandas
 from voluptuous import Schema
 
