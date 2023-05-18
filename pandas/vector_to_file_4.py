b577ad5382b39f94f24bcaeaea953502263a4267
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-05-18 08:40:02 +0200

    Added license

--- pandas/vector_to_file_4.py
+++ pandas/vector_to_file_4.py
@@ -1,5 +1,17 @@
 """Uložení obsahu vektoru do binárního souboru."""
 
+#
+#  (C) Copyright 2021  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
 import numpy as np
 
 # vektor obsahující celočíselné 16bitové hodnoty (half integer)
