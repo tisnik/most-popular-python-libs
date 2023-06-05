d2e4891526a27ca0296bcc7d1d1de88d3458c53e
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-06-05 07:50:09 +0200

    Passing arrays to C functions: example #3

---
+++ cffi/array_printer_3/call_via_cffi.py
@@ -0,0 +1,23 @@
+import pathlib
+
+from cffi import FFI
+
+ffi = FFI()
+
+def load_header(filename):
+    directory = pathlib.Path().absolute()
+    header = directory / filename
+    with open(header) as fin:
+        return fin.read()
+
+
+def load_library(library_name):
+    return ffi.dlopen(library_name)
+
+
+ffi.cdef(load_header("array_printer.h"))
+
+array = ffi.new("float[10]", (1.1, 2.2, 3.3, 4.4, 5.5))
+
+printer = load_library("libaprinter.so")
+printer.print_array(array, len(array))
