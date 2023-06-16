0f71ac50abb3df54e181a050ba0925fca831151c
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-06-16 08:43:00 +0200

    Callable data type + higher order functions

---
+++ mypy/callable3.py
@@ -0,0 +1,20 @@
+from typing import Callable
+
+
+def printIsPositive(condition:Callable[[float], bool]) -> None:
+    if condition(5):
+        print("Positive")
+    else:
+        print("Negative")
+
+
+def positiveFloat(x:float) -> bool:
+    return x > 0.0
+
+
+def positiveInt(x:int) -> bool:
+    return x > 0
+
+
+printIsPositive(positiveFloat)
+printIsPositive(positiveInt)
