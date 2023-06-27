4b3fc2ee6fcc49fb433be52bc59a0e86194292da
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-06-27 10:46:31 +0200

    List and Sequence types

---
+++ mypy/list_sequence.py
@@ -0,0 +1,15 @@
+from typing import List, Sequence
+
+l1: List[bool] = [True, False]
+
+l1.append(True)
+l1.append(False)
+
+print(l1)
+
+l2: Sequence[bool] = [True, False]
+
+l2.append(True)
+l2.append(False)
+
+print(l2)
