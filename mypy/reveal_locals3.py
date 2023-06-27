60da295e8382a85bc8ae2adec1b84dc425862499
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-06-27 08:23:59 +0200

    Reveal locals: 3rd example

---
+++ mypy/reveal_locals3.py
@@ -0,0 +1,21 @@
+from typing import Callable
+
+
+def printIsPositive(condition:Callable[[int], bool]) -> None:
+    reveal_locals()
+    if condition(5):
+        print("Positive")
+    else:
+        print("Negative")
+
+
+def positiveFloat(x:float) -> bool:
+    return x > 0.0
+
+
+def positiveInt(x:int) -> bool:
+    return x > 0
+
+
+printIsPositive(positiveFloat)
+printIsPositive(positiveInt)
