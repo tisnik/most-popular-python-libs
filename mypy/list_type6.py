7ae5a0de556d09ee4dc9aa6e19af0155bb7dbc82
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-07-11 07:20:44 +0200

    Added license hearer

--- mypy/list_type6.py
+++ mypy/list_type6.py
@@ -1,3 +1,18 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+#
+#  (C) Copyright 2023  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
 from typing import List
 
 l: List[bool] = [True, False, 42]
