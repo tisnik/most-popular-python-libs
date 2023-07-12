870615d893ecaefb5bda278fe911e5ecc060afa4
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-07-12 08:03:40 +0200

    Added license hearer

--- mypy/reveal_type4.py
+++ mypy/reveal_type4.py
@@ -1,7 +1,27 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+#
+#  (C) Copyright 2023  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
 d = {}
+
+reveal_type(d)
 
 d["foo"] = 1
 d["bar"] = 3.14
+
+reveal_type(d)
+
 d[10] = 10
 d[42] = "answer"
 
