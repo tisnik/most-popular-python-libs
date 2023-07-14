fe853f366a0d8a1cfa680a1fa3ac40cb6bb556a4
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-07-14 07:43:54 +0200

    Change script to run examples

--- cffi/array_printer_5/call_via_cffi.sh
+++ cffi/array_printer_5/call_via_cffi.sh
@@ -1,2 +1,3 @@
 export LD_LIBRARY_PATH=.
 python3 call_via_cffi.py
+python3 call_via_cffi2.py
--- cffi/array_printer_6/call_via_cffi.sh
+++ cffi/array_printer_6/call_via_cffi.sh
@@ -1,2 +1,6 @@
 export LD_LIBRARY_PATH=.
 python3 call_via_cffi.py
+
+echo ""
+
+python3 call_via_cffi2.py
