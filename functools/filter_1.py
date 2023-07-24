913b0992fcfd4fe653b27e0464088968e2b3a4a3
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-07-24 07:51:26 +0200

    How to use filter function, 1st example

---
+++ functools/filter_1.py
@@ -0,0 +1,8 @@
+message = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"
+words = message.split()
+
+filtered = filter(lambda word: len(word) > 4, words)
+print(list(filtered))
+
+filtered = filter(lambda word: len(word) <= 4, words)
+print(list(filtered))
