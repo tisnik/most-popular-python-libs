99d0720309ed41e3cf87b3e57582f0f44c6a9620
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-07-24 07:51:26 +0200

    Operator getter, variant with type specification

---
+++ functools/get_operator_types.py
@@ -0,0 +1,35 @@
+from typing import Callable
+
+
+def get_operator(symbol: str) -> Callable[[int, int], int]:
+    operators = {
+            "+": add,
+            "*": mul,
+    }
+    return operators[symbol]
+
+
+def calc(operator: Callable[[int, int], int], x: int, y: int) -> int:
+    return operator(x, y)
+
+
+def add(x: int, y: int) -> int:
+    return x + y
+
+
+def mul(x: int, y: int) -> int:
+    return x * y
+
+
+def less_than(x: int, y: int) -> bool:
+    return x < y
+
+
+z = calc(get_operator("+"), 10, 20)
+print(z)
+
+z = calc(get_operator("*"), 10, 20)
+print(z)
+
+z = calc(less_than, 10, 20)
+print(z)
