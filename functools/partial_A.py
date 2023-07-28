e20075c9f07e45e53f63f38ba5b5962bd4bb2e0a
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-07-28 09:00:25 +0200

    Currying with partial: example #10

---
+++ functools/partial_A.py
@@ -0,0 +1,32 @@
+from functools import partial, reduce
+
+
+def mul(x=1, y=1, z=1, w=1):
+    return x * y * z * w
+
+
+def displayInfo(name, obj):
+    print(name)
+    print(obj.func)
+    print(obj.args)
+    print(obj.keywords)
+    print()
+
+
+#f1 = mul
+#displayInfo(f1())
+
+f2 = partial(mul, x=2)
+displayInfo("f2", f2)
+
+f3 = partial(mul, y=2)
+displayInfo("f3", f3)
+
+f4 = partial(mul, y=2, z=2)
+displayInfo("f4", f4)
+
+f5 = partial(mul, x=2, y=2, z=2)
+displayInfo("f5", f5)
+
+f6 = partial(mul, x=2, y=2, z=2, w=2)
+displayInfo("f6", f6)
