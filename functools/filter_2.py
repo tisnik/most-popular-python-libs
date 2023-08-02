e8f2b59a10b4c62a565edbde1df154b3d42b66a6
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-08-02 08:23:18 +0200

    Added license header

--- functools/filter_2.py
+++ functools/filter_2.py
@@ -1,3 +1,15 @@
+#
+#  (C) Copyright 2023  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
 def odd(value):
     return value % 2 == 1
 
