08a7cf809dab455717d2e7d992b8e8b4d7c76894
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-08-04 08:13:17 +0200

    partialmethod, example #1

---
+++ functools/partial_method_1.py
@@ -0,0 +1,13 @@
+class Foo:
+    def __init__(self):
+        self._enabled = False
+
+    def set_enabled(self, state):
+        self._enabled = state
+
+    def __str__(self):
+        return "Foo that is " + ("enabled" if self._enabled else "disabled")
+
+
+foo = Foo()
+print(foo)
