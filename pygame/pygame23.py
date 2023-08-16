f239f3e81397a9e3e4e944a362bf5b1078aeb141
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-08-16 07:50:05 +0200

    Proper import of used symbols

--- pygame/pygame23.py
+++ pygame/pygame23.py
@@ -22,7 +22,7 @@ import pygame
 import sys
 
 # Nutno importovat kvůli konstantám QUIT atd.
-from pygame.locals import *
+from pygame.locals import QUIT, K_ESCAPE, KEYUP, KEYDOWN
 
 # Velikost okna aplikace
 WIDTH = 320
