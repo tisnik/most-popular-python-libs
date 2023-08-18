e8820c5dc31ba4f6de8459c6e41e0e8918e95393
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-08-18 09:20:17 +0200

    Function lfilter and regexp as predicate

---
+++ funcy/filter_5.py
@@ -0,0 +1,14 @@
+from funcy import lfilter
+
+
+message = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"
+words = message.split()
+
+filtered = lfilter("et", words)
+print(filtered)
+
+filtered = lfilter("a", words)
+print(filtered)
+
+filtered = lfilter("o", words)
+print(filtered)
