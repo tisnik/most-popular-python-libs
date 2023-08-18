63ddb5448f251f7747c20d855800b83aa2def37e
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-08-18 09:20:17 +0200

    Standard function map

---
+++ funcy/map_1.py
@@ -0,0 +1,7 @@
+message = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"
+words = message.split()
+
+lengths = map(len, words)
+
+print(lengths)
+print(list(lengths))
