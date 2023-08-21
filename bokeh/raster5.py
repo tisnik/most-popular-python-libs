cf4083c2825f172781768210e91f65299ac4192c
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-08-21 09:04:29 +0200

    Removed unused import

--- bokeh/raster5.py
+++ bokeh/raster5.py
@@ -14,7 +14,7 @@
 #
 
 # naimportujeme vybrané funkce z knihovny `bokeh.plotting`
-from bokeh.plotting import figure, output_file, show
+from bokeh.plotting import figure, show
 
 # taktéž budeme potřebovat některé funkce z knihovny `numpy`
 import numpy as np
