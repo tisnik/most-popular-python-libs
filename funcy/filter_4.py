1db9133492458d05c7bc7dde07008e6bc9e2a568
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-08-23 09:11:51 +0200

    More examples

--- funcy/filter_4.py
+++ funcy/filter_4.py
@@ -1,9 +1,20 @@
-from funcy import filter
+from funcy import filter, lfilter
 
 data = range(0, 11)
+
+print("filter")
 
 filtered = filter(lambda value : value %2 == 1, data)
 print(list(filtered))
 
 filtered = filter(lambda value : value %2 == 0, data)
 print(list(filtered))
+
+print()
+print("lfilter")
+
+filtered = lfilter(lambda value : value %2 == 1, data)
+print(filtered)
+
+filtered = lfilter(lambda value : value %2 == 0, data)
+print(filtered)
