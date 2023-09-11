7a53a41b9a462d650a5be3865c73b6218690e260
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-09-11 07:40:42 +0200

    Count function usage, 1st example

---
+++ funcy/count_5.py
@@ -0,0 +1,19 @@
+#
+#  (C) Copyright 2023  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
+from funcy import count, take
+
+values = count(start=10)
+
+slice = take(20, values)
+
+print(list(slice))
