9f142b40f6daa997abeb3de0d704978e43320744
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-09-18 10:04:19 +0200

    Added license header

--- funcy/map_2.py
+++ funcy/map_2.py
@@ -1,3 +1,15 @@
+#
+#  (C) Copyright 2023  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
 from funcy import map
 
 message = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"
