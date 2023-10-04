1153a4bd9d957f2c5170f72dc8bb3fd7723796db
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-10-04 13:14:32 +0200

    Fixed imports

--- pyglet/10_points.py
+++ pyglet/10_points.py
@@ -26,7 +26,9 @@
 import pyglet
 
 # druhý import s funkcemi převzatými z OpenGL
-from pyglet.gl import *
+from pyglet.gl import glClear, glLoadIdentity, glBegin, glEnd, glVertex2f, glColor3f
+from pyglet.gl import glEnable, glDisable, glPointSize
+from pyglet.gl import GL_COLOR_BUFFER_BIT, GL_POINTS, GL_POINT_SMOOTH
 
 # vytvoření okna
 window = pyglet.window.Window(width=450, height=350, caption="Pyglet+OpenGL")
