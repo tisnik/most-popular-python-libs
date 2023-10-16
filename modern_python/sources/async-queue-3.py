64a6d810898c3b9b4a2f10e4e9dbc1694f14999d
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-10-16 07:55:29 +0200

    Queue for asynchronous communication, example #3

---
+++ modern_python/sources/async-queue-3.py
@@ -0,0 +1,26 @@
+import asyncio
+
+
+async def task(name, queue):
+    while not queue.empty():
+        param = await queue.get()
+        print(f"Task named {name} started with parameter {param}")
+        await asyncio.sleep(5)
+        print(f"{name} task finished")
+
+
+async def main():
+    queue = asyncio.Queue()
+
+    for i in range(20):
+        await queue.put(i)
+
+    await asyncio.gather(
+        asyncio.create_task(task(1, queue)),
+        asyncio.create_task(task(2, queue)),
+        asyncio.create_task(task(3, queue)),
+        asyncio.create_task(task(4, queue)),
+    )
+
+
+asyncio.run(main())
