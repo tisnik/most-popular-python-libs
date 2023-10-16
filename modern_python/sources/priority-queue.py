70f6005e248722c7ead19992f3aeb13a78249cb5
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-10-16 07:55:29 +0200

    Priority queue

---
+++ modern_python/sources/priority-queue.py
@@ -0,0 +1,17 @@
+import queue
+import random
+
+q = queue.PriorityQueue(40)
+
+for item in range(30):
+    print("Size", q.qsize())
+    print("Empty?", q.empty())
+    print("Full?", q.full())
+
+    value = random.randint(1, 20)
+    print(value)
+    q.put("prvek # {:2d}".format(value))
+
+
+while not q.empty():
+    print("Read item:", q.get())
