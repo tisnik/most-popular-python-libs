e1c52610820ef85e23c0a7c00285c49c94543998
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-10-16 07:55:29 +0200

    Async and await in Python, example #3

---
+++ modern_python/sources/async-await-3.py
@@ -0,0 +1,23 @@
+import asyncio
+
+
+async def task(name):
+    print(f"{name} task started")
+    await asyncio.sleep(5)
+    print(f"{name} task finished")
+
+
+async def main():
+    task1 = asyncio.create_task(task("first"))
+    print("first task created")
+
+    task2 = asyncio.create_task(task("second"))
+    print("second task created")
+
+    await task1
+    await task2
+
+    print("done")
+
+
+asyncio.run(main())
