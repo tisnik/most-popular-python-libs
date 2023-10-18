e4b81be5729699dc71e23b29ef6b9a69433d16ea
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-10-18 07:59:09 +0200

    Walrus operator, example #4

---
+++ modern_python/sources/walrus-operator-4.py
@@ -0,0 +1,12 @@
+values = (1, 2, 3, 4, 5)
+
+count = len(values)
+summ = sum(values)
+
+result = {
+        "count": count,
+        "sum": summ,
+        "mean": summ/count
+        }
+
+print(result)
