044c7a97e4c76d92387ffcce2ed83a9c5b064563
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-10-20 15:35:50 +0200

    Canvas usage, example #2

---
+++ modern_python/sources/x/transcrypt-canvas2.js
@@ -0,0 +1,30 @@
+// Transcrypt'ed from Python, 2023-10-20 14:52:22
+import {AssertionError, AttributeError, BaseException, DeprecationWarning, Exception, IndexError, IterableError, KeyError, NotImplementedError, RuntimeWarning, StopIteration, UserWarning, ValueError, Warning, __JsIterator__, __PyIterator__, __Terminal__, __add__, __and__, __call__, __class__, __envir__, __eq__, __floordiv__, __ge__, __get__, __getcm__, __getitem__, __getslice__, __getsm__, __gt__, __i__, __iadd__, __iand__, __idiv__, __ijsmod__, __ilshift__, __imatmul__, __imod__, __imul__, __in__, __init__, __ior__, __ipow__, __irshift__, __isub__, __ixor__, __jsUsePyNext__, __jsmod__, __k__, __kwargtrans__, __le__, __lshift__, __lt__, __matmul__, __mergefields__, __mergekwargtrans__, __mod__, __mul__, __ne__, __neg__, __nest__, __or__, __pow__, __pragma__, __pyUseJsNext__, __rshift__, __setitem__, __setproperty__, __setslice__, __sort__, __specialattrib__, __sub__, __super__, __t__, __terminal__, __truediv__, __withblock__, __xor__, abs, all, any, assert, bool, bytearray, bytes, callable, chr, copy, deepcopy, delattr, dict, dir, divmod, enumerate, filter, float, getattr, hasattr, input, int, isinstance, issubclass, len, list, map, max, min, object, ord, pow, print, property, py_TypeError, py_iter, py_metatype, py_next, py_reversed, py_typeof, range, repr, round, set, setattr, sorted, str, sum, tuple, zip} from './org.transcrypt.__runtime__.js';
+var __name__ = '__main__';
+export var canvas = document.getElementById ('canvas');
+export var context = canvas.getContext ('2d');
+context.fillStyle = 'darkblue';
+context.beginPath ();
+context.moveTo (75, 50);
+context.lineTo (100, 75);
+context.lineTo (100, 25);
+context.fill ();
+context.fillStyle = 'darkred';
+context.beginPath ();
+context.moveTo (225, 50);
+context.lineTo (200, 75);
+context.lineTo (200, 25);
+context.fill ();
+context.strokeStyle = 'darkgreen';
+context.lineWidth = 10;
+context.beginPath ();
+context.moveTo (75, 125);
+context.quadraticCurveTo (25, 125, 25, 162.5);
+context.quadraticCurveTo (25, 200, 50, 200);
+context.quadraticCurveTo (50, 220, 30, 225);
+context.quadraticCurveTo (60, 220, 65, 200);
+context.quadraticCurveTo (125, 200, 125, 162.5);
+context.quadraticCurveTo (125, 125, 75, 125);
+context.stroke ();
+
+//# sourceMappingURL=canvas2.map
---
+++ modern_python/sources/x/transcrypt-canvas2.py
@@ -0,0 +1,28 @@
+canvas = document.getElementById('canvas')
+context = canvas.getContext('2d')
+
+context.fillStyle = 'darkblue'
+context.beginPath();
+context.moveTo(75, 50);
+context.lineTo(100, 75);
+context.lineTo(100, 25);
+context.fill();
+
+context.fillStyle = 'darkred'
+context.beginPath();
+context.moveTo(225, 50);
+context.lineTo(200, 75);
+context.lineTo(200, 25);
+context.fill();
+
+context.strokeStyle = 'darkgreen'
+context.lineWidth = 10;
+context.beginPath();
+context.moveTo(75, 125);
+context.quadraticCurveTo(25, 125, 25, 162.5);
+context.quadraticCurveTo(25, 200, 50, 200);
+context.quadraticCurveTo(50, 220, 30, 225);
+context.quadraticCurveTo(60, 220, 65, 200);
+context.quadraticCurveTo(125, 200, 125, 162.5);
+context.quadraticCurveTo(125, 125, 75, 125);
+context.stroke();
