fe76dc35629274cbdf92706baf0b88479fbb48f0
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-10-23 17:57:33 +0200

    Removed whitespace

--- transcrypt/hello.py
+++ transcrypt/hello.py
@@ -9,27 +9,27 @@ class SolarSystem:
         ('Jupiter', 'stormy', 71492),
         ('Saturn', 'ringed', 60268),
         ('Uranus', 'cold', 25559),
-        ('Neptune', 'very cold', 24766) 
+        ('Neptune', 'very cold', 24766)
     ))]
-    
+
     lines = (
         '{} is a {} planet',
         'The radius of {} is {} km',
         '{} is planet nr. {} counting from the sun'
     )
-    
+
     def __init__ (self):
         self.lineIndex = 0
-    
+
     def greet (self):
         self.planet = self.planets [int (Math.random () * len (self.planets))]
         document.getElementById ('greet') .innerHTML = 'Hello {}'.format (self.planet [0])
         self.explain ()
-        
+
     def explain (self):
         document.getElementById ('explain').innerHTML = (
             self.lines [self.lineIndex] .format (self.planet [0], self.planet [self.lineIndex + 1])
         )
         self.lineIndex = (self.lineIndex + 1) % 3
-        
+
 solarSystem = SolarSystem ()
