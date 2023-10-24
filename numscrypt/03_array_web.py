579205222c1c515f8996e6e9efa057fea218f52c
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-10-24 15:09:55 +0200

    Array construction, 3rd example

---
+++ numscrypt/03_array_web.py
@@ -0,0 +1,21 @@
+# Knihovna Numpy a Numscrypt
+#
+# Konstrukce jednorozměrného pole konstruktorem numpy.array()
+#
+# vytvoření pole se čtyřmi prvky
+
+# import hlavního balíčku knihovny Numpy
+if __envir__.executor_name == __envir__.transpiler_name:
+    import numscrypt as np
+
+# import pro CPython
+__pragma__ ('skip')
+import numpy as np
+__pragma__ ('noskip')
+
+
+# konstrukce pole
+a = np.array([1, 2, 3, 4])
+
+# tisk obsahu pole na standardní výstup
+print(a)
