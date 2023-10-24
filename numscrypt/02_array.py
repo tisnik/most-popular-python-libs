f167eb5428ef5a3e054fba2f9dc931c18ef0c3dd
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-10-24 16:15:26 +0200

    Fixed

--- numscrypt/02_array.py
+++ numscrypt/02_array.py
@@ -6,15 +6,15 @@
 
 # import hlavního balíčku knihovny Numpy
 if __envir__.executor_name == __envir__.transpiler_name:
-    import numscrypt
+    import numscrypt as np
 
 # import pro CPython
 __pragma__ ('skip')
-import numpy
+import numpy as np
 __pragma__ ('noskip')
 
 # konstrukce pole
-a = numpy.array([1, 2, 3, 4])
+a = np.array([1, 2, 3, 4])
 
 # tisk obsahu pole na standardní výstup
 print(a)
