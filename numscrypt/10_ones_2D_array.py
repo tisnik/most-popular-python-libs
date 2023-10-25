92272a7274b9b9a011d066a52205c02a21df8f30
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-10-25 13:13:05 +0200

    Numscrypt example #10

---
+++ numscrypt/10_ones_2D_array.py
@@ -0,0 +1,20 @@
+# Knihovna Numpy
+#
+# Příklady použití konstruktoru numpy.ones
+#
+# matice se třemi řádky a čtyřmi sloupci
+
+# import hlavního balíčku knihovny Numpy
+if __envir__.executor_name == __envir__.transpiler_name:
+    import numscrypt as np
+
+# import pro CPython
+__pragma__ ('skip')
+import numpy as np
+__pragma__ ('noskip')
+
+# konstrukce pole
+a = np.ones((3, 4))
+
+# tisk obsahu pole na standardní výstup
+print(a)
