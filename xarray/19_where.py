7ed7c2d8b350ae1e8a5b605c311c8bda81f2c21c
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-10-26 18:24:48 +0200

    Where selector

---
+++ xarray/19_where.py
@@ -0,0 +1,25 @@
+import numpy as np
+import xarray as xr
+
+temperatures = -10 + 40*np.random.rand(5, 5)
+
+xcoords = np.linspace(10, 100, 5)
+ycoords = np.linspace(-100, -10, 5)
+
+array = xr.DataArray(temperatures,
+                     name="Temperature measurement",
+                     dims=("x", "y"),
+                     coords={"x":xcoords, "y":ycoords},
+                     attrs=dict(
+                         units = "centigrees",
+                         description ="Local temperature values measured in grid",
+                         measured_by = {"name": "ThermometerBot",
+                              "vendor": "BIY",
+                              "version": (1, 0, 0)}
+                         ))
+
+
+print(array)
+print()
+
+print(xr.where(array <= 0, "freezing", "above 0°C"))
