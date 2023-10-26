dfc3ae8e8ace9182b0470055f84b1fb5ac18bd82
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-10-26 18:24:20 +0200

    Indexing by coordinates

---
+++ xarray/15_get_by_coordinate.py
@@ -0,0 +1,34 @@
+import numpy as np
+import xarray as xr
+
+temperatures = np.arange(0, 300).reshape((10, 10, 3))
+
+xcoords = np.linspace(10, 100, 10)
+ycoords = np.linspace(-100, -10, 10)
+
+times = ["2023-10-01", "2023-10-02", "2023-10-03"]
+
+array = xr.DataArray(temperatures,
+                     name="Temperature measurement",
+                     dims=("x", "y", "time"),
+                     coords={"x":xcoords, "y":ycoords, "time":times},
+                     attrs=dict(
+                         units = "centigrees",
+                         description ="Local temperature values measured in grid",
+                         measured_by = {"name": "ThermometerBot",
+                              "vendor": "BIY",
+                              "version": (1, 0, 0)}
+                         ))
+
+
+print(array)
+print()
+
+print(array[0])
+print()
+
+print(array[0][2])
+print()
+
+print(array[:,2])
+print()
