17208d86c90c80886391295126c4df7a44065cb9
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-10-26 14:11:33 +0200

    xarray example #10

---
+++ xarray/10_multiple_dimensions.py
@@ -0,0 +1,27 @@
+import numpy as np
+import xarray as xr
+
+temperatures = -10 + 40*np.random.rand(2, 2, 3)
+
+longitudes = [[-99.83, -99.32], [-99.79, -99.23]]
+latitudes = [[42.25, 42.21], [42.63, 42.59]]
+times = ["2023-10-01", "2023-10-02", "2023-10-03"]
+
+array = xr.DataArray(temperatures,
+                     name="Temperature measurement",
+                     dims=("x", "y", "time"),
+                     coords=dict(
+                        lon=(["x", "y"], longitudes),
+                        lat=(["x", "y"], latitudes),
+                        time=times,
+                     ),
+                     attrs=dict(
+                         units = "centigrees",
+                         description ="Local temperature values measured in grid",
+                         measured_by = {"name": "ThermometerBot",
+                              "vendor": "BIY",
+                              "version": (1, 0, 0)}
+                         ))
+
+
+print(array)
