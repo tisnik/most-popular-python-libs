29879a6ae44426ce66cf95f999959b88eabe163e
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-10-27 14:29:58 +0200

    Dataset

---
+++ xarray/21_dataset.py
@@ -0,0 +1,14 @@
+import numpy as np
+import xarray as xr
+
+temperatures = -10 + 40*np.random.rand(10, 10)
+wind = 100*np.random.rand(20, 20)
+
+xcoords = np.linspace(10, 100, 10)
+ycoords = np.linspace(-100, -10, 10)
+
+dataset = xr.Dataset({"temperatures": (["x", "y"], temperatures),
+                      "wind": (["x", "y"], wind)},
+                       coords={"x":xcoords, "y":ycoords})
+
+print(dataset)
