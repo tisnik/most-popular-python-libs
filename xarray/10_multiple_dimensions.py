c6edd228ae2e1b3f80426d2e06cf70e4d289915b
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-10-30 07:48:58 +0100

    Use dict literal

--- xarray/10_multiple_dimensions.py
+++ xarray/10_multiple_dimensions.py
@@ -10,18 +10,18 @@ times = ["2023-10-01", "2023-10-02", "2023-10-03"]
 array = xr.DataArray(temperatures,
                      name="Temperature measurement",
                      dims=("x", "y", "time"),
-                     coords=dict(
-                        lon=(["x", "y"], longitudes),
-                        lat=(["x", "y"], latitudes),
-                        time=times,
-                     ),
-                     attrs=dict(
-                         units = "centigrees",
-                         description ="Local temperature values measured in grid",
-                         measured_by = {"name": "ThermometerBot",
+                     coords={
+                        "lon": (["x", "y"], longitudes),
+                        "lat": (["x", "y"], latitudes),
+                        "time": times,
+                     },
+                     attrs={
+                         "units": "centigrees",
+                         "description": "Local temperature values measured in grid",
+                         "measured_by": {"name": "ThermometerBot",
                               "vendor": "BIY",
                               "version": (1, 0, 0)}
-                         ))
+                         })
 
 
 print(array)
