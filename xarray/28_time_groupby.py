d72c7114cbf65bffb63bd86606840eeffeb8ebde
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-10-30 07:48:58 +0100

    Group by operation, example #1

---
+++ xarray/28_time_groupby.py
@@ -0,0 +1,25 @@
+import numpy as np
+import xarray as xr
+
+measured_at = np.arange(
+        np.datetime64("2023-01-01"),
+        np.datetime64("2024-01-01"),
+        np.timedelta64(1, "D")).astype('datetime64[ns]')
+
+temperatures = 10 + 20 * np.random.rand(365)
+
+array = xr.DataArray(temperatures,
+                     name="Temperature measurement",
+                     dims=("time",),
+                     coords={"time":measured_at},
+                     attrs={
+                         "units": "centigrees",
+                         "description": "Local temperature values measured in grid #1",
+                         "measured_by": {"name": "ThermometerBot",
+                              "vendor": "BIY",
+                              "version": (1, 0, 0)}
+                         })
+
+print(array.groupby("time.season"))
+print()
+print()
