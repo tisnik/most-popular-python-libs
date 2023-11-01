51c5bf1cda28451da8eeae7d950f115eafa6a789
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-11-01 07:35:27 +0100

    Selection from 3D array

---
+++ xarray/39_select_3D.py
@@ -0,0 +1,28 @@
+import numpy as np
+import xarray as xr
+
+temperatures = np.arange(0, 300).reshape((10, 10, 3))
+
+xcoords = np.linspace(10, 100, 10)
+ycoords = np.linspace(-100, -10, 10)
+
+times = ["2023-10-01", "2023-10-02", "2023-10-03"]
+
+array = xr.DataArray(temperatures,
+                     name="Temperature measurement",
+                     dims=("x", "y", "time"),
+                     coords={"x":xcoords, "y":ycoords, "time":times},
+                     attrs={
+                         "units": "centigrees",
+                         "description": "Local temperature values measured in grid",
+                         "measured_by": {"name": "ThermometerBot",
+                              "vendor": "BIY",
+                              "version": (1, 0, 0)}
+                         })
+
+
+print(array)
+print()
+
+print(array.sel(time="2023-10-02").sel(x=50).sel(y=-50))
+print()
