7d45fe70d0f3695abe76ffd66341b2dc8cb94ff9
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-11-09 07:10:57 +0100

    Fixed import

--- pyglet/17_3d_projection.py
+++ pyglet/17_3d_projection.py
@@ -13,8 +13,11 @@
 #
 
 import pyglet
-from pyglet.gl import *
-from pyglet.window import key
+from pyglet.gl import glClear, glLoadIdentity, glColor3f, glVertex3f, glBegin, glEnd
+from pyglet.gl import glClearColor, glPolygonMode, glDisable
+from pyglet.gl import glViewport, glMatrixMode, glOrtho, gluLookAt
+from pyglet.gl import GL_LINE, GL_COLOR_BUFFER_BIT, GL_MODELVIEW
+from pyglet.gl import GL_FRONT, GL_BACK, GL_CULL_FACE, GL_PROJECTION, GL_QUADS, GL_TRIANGLES
 
 window = pyglet.window.Window(width=500, height=500, caption="Pyglet+OpenGL")
 
