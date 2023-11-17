9dd105b52c919215c2325de7d8dd7a8f99769392
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-11-17 09:13:52 +0100

    Update test script

--- mypyc/test_native.sh
+++ mypyc/test_native.sh
@@ -3,6 +3,22 @@ sizes="16 24 32 48 64 96 128 192 256 384 512 768 1024 1536 2048 3072 4096"
 OUTFILE="native.times"
 PREFIX="native"
 
+gcc mandelbrot.c -o mandelbrot
+
+rm $OUTFILE
+
+for size in $sizes
+do
+    echo $size
+    echo -n "$size " >> $OUTFILE
+    /usr/bin/time --output $OUTFILE --append --format "%e %M" ./mandelbrot $size $size 255 > "${PREFIX}_${size}_${size}.ppm"
+done
+
+OUTFILE="native_optim.times"
+PREFIX="native_optim"
+
+gcc -Ofast mandelbrot.c -o mandelbrot
+
 rm $OUTFILE
 
 for size in $sizes
