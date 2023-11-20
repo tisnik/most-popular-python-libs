5a0e14098f53c0e4896520f3f57e85e3e017be6a
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-11-20 08:14:47 +0100

    Scripts to start benchmarks

---
+++ numba/mandelbrot-v2/test_numba_var_iter.sh
@@ -0,0 +1,13 @@
+iterations="1 10 100 1000 10000 100000"
+
+OUTFILE="numba2.times"
+PREFIX="numba2"
+
+rm $OUTFILE
+
+for iter in $iterations
+do
+    echo $iter
+    echo -n "$iter " >> $OUTFILE
+    /usr/bin/time --output $OUTFILE --append --format "%e %M" python3 mandelbrot_python.py 256 256 $iter 255 > "${PREFIX}_${iter}.ppm"
+done
