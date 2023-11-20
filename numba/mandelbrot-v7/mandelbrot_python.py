30f8af9941aefc25facdcfafd1bd24647b860e57
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-11-20 08:15:42 +0100

    Better solution

---
+++ numba/mandelbrot-v7/mandelbrot_python.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8
+
+#
+#  (C) Copyright 2018  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
+import palette_mandmap
+from sys import argv, exit
+
+import numpy as np
+from numba import jit, prange
+from numba.types import UniTuple, int64
+
+
+@jit(nopython=True)
+def calc_pixel(maxiter, cx, cy):
+    zx = 0.0
+    zy = 0.0
+    i = 0
+    while i < maxiter:
+        zx2 = zx * zx
+        zy2 = zy * zy
+        if zx2 + zy2 > 4.0:
+            break
+        zy = 2.0 * zx * zy + cy
+        zx = zx2 - zy2 + cx
+        i += 1
+    return i
+
+
+@jit((int64, int64, int64, UniTuple(UniTuple(int64, 3), 256)), nopython=True, parallel=True)
+def calc_mandelbrot(width, height, maxiter, palette):
+    iters = np.empty((height, width), dtype=np.uint8)
+
+    # calc part
+    for y in prange(0, height):
+        cy = -1.5 + 3.0*y/height
+        cx = -2.0
+        for x in range(0, width):
+            i = calc_pixel(maxiter, cx, cy)
+            iters[y][x] = i
+
+            cx += 3.0/width
+        cy += 3.0/height
+
+    # image export part
+    print("P3")
+    print(width)
+    print(height)
+    print("255")
+    for y in range(0, height):
+        for x in range(0, width):
+            i = iters[y][x]
+            r = palette[i % 256][0]
+            g = palette[i % 256][1]
+            b = palette[i % 256][2]
+            print(r)
+            print(g)
+            print(b)
+
+
+if __name__ == "__main__":
+    if len(argv) < 4:
+        width = 512
+        height = 512
+        maxiter = 255
+    else:
+        width = int(argv[1])
+        height = int(argv[2])
+        maxiter = int(argv[3])
+    calc_mandelbrot(width, height, maxiter, palette_mandmap.palette)
+    # calc_mandelbrot.parallel_diagnostics(level=4)
---
+++ numba/mandelbrot-v7/test_linear_scale.sh
@@ -0,0 +1,13 @@
+width="2048"
+
+OUTFILE="numba4_linear_scale.times"
+PREFIX="numba4_linear_scale"
+
+rm $OUTFILE
+
+for height in $(seq 0 100 3500)
+do
+    echo "${width} x ${height}"
+    echo -n "${height} " >> $OUTFILE
+    /usr/bin/time --output $OUTFILE --append --format "%e %M" python3 mandelbrot_python.py $width $height 255 > "${PREFIX}_${width}_${height}.ppm"
+done
