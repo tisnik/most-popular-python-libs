003072f559b65c404483c677d3191575069a7517
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-11-24 16:54:11 +0100

    Grayscale images

---
+++ sklearn/06_grayscale_images.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+# budeme provádět vykreslování de facto standardní knihovnou Matplotlib
+import matplotlib.pyplot as plt
+
+# import funkce, která vrátí obrázky pro práci
+from sklearn.datasets import load_digits
+
+# načtení obrázků, s nimiž budeme dále pracovat
+digits_data = load_digits()
+
+# zjištění základních informací o obrázcích
+print(digits_data.data.shape)
+
+# vykreslení a uložení prvních deseti obrázků
+for i in range(0, 10):
+    plt.matshow(digits_data.images[i])
+    # převod na stupně šedi
+    plt.gray()
+
+    plt.savefig(f"Grayscale image #{i}.png")
+
+    # vykreslení na obrazovku
+    plt.show()
+
+# finito
