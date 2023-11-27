bef286717f2816468877f91376d86e11005277a7
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-11-27 12:40:10 +0100

    Reformatted

--- sklearn/07_multiplot.py
+++ sklearn/07_multiplot.py
@@ -9,14 +9,14 @@ from sklearn.datasets import load_digits
 # načtení obrázků, s nimiž budeme dále pracovat
 digits_data = load_digits()
 
-# vytvoření seznamu, které použijeme 
+# vytvoření seznamu, které použijeme
 images = list(zip(digits_data.target, digits_data.images))
 
 # vykreslení tréninkových číslic
 for i, (label, image) in enumerate(images[:15]):
-	plt.subplot(3, 5, i+1)
-	plt.imshow(image, cmap=plt.cm.gray_r) #, interpolation='nearest')
-	plt.title(f"číslice {label}")
+    plt.subplot(3, 5, i+1)
+    plt.imshow(image, cmap=plt.cm.gray_r) #, interpolation='nearest')
+    plt.title(f"číslice {label}")
 
 # uložení a vykreslení výsledku
 plt.savefig("training_set.png")
