8f9c00e38ed6f221c8d21fbe7177c5452d41bcc6
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-12-01 17:45:09 +0100

    Display clusters, 3rd variant

---
+++ sklearn/21_other_settings.py
@@ -0,0 +1,40 @@
+# budeme provádět vykreslování de facto standardní knihovnou Matplotlib
+import matplotlib.pyplot as plt
+
+from sklearn.cluster import KMeans
+from sklearn.datasets import make_blobs
+
+# testovací data
+n_samples = 10000
+
+# počet oblastí, kam se budou data sdružovat
+n_components = 6
+
+samples, labels = make_blobs(
+    n_samples=n_samples, centers=n_components, cluster_std=2.00, random_state=0
+)
+
+samples = samples[:, ::-1]
+
+plt.figure(1)
+colors = ["#4444cc", "#44bb44", "#cc4444", "#cccc44", "#44cccc", "#cc44cc"]
+
+# clustering
+kmeans = KMeans(n_clusters=6, random_state=0, n_init="auto").fit(samples)
+
+#print(kmeans.labels_)
+#print(kmeans.cluster_centers_)
+
+# vykreslení bodů s jejich přiřazením ke clusteru
+for i, color in enumerate(colors):
+    selector = kmeans.labels_ == i
+    plt.scatter(samples[selector, 0], samples[selector, 1], c=color, marker=".", s=1)
+
+plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], c="red", s=50)
+plt.title("K-Means++")
+
+# uložení grafu do souboru
+plt.savefig(f"k_means_4.png")
+
+# vykreslení na obrazovku
+plt.show()
