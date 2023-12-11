7de63fdd0e679969826b000b8b5468d361561d1a
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-12-11 07:51:45 +0100

    K-means speed measurement, benchmark #1

---
+++ sklearn/43_speed_measurements.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+from time import perf_counter
+import matplotlib.pyplot as plt
+import numpy as np
+
+from sklearn.cluster import KMeans
+from sklearn.datasets import make_blobs
+
+
+def run_k_means(n_samples):
+    # počet oblastí, kam se budou data sdružovat
+    n_components = 8
+
+    samples, _ = make_blobs(
+        n_samples=n_samples, n_features=3, centers=n_components,
+        cluster_std=0.80, random_state=0
+    )
+
+    # clustering
+    kmeans = KMeans(n_clusters=n_components, random_state=0, n_init="auto").fit(samples)
+
+
+x = []
+y = []
+
+for i in range(10000, 1000000, 10000):
+    started = perf_counter()
+    run_k_means(i)
+    finished = perf_counter()
+    duration = finished - started
+    x.append(i)
+    y.append(duration)
+    print(i, duration)
+
+plt.figure(1)
+plt.plot(x, y, marker="o")
+plt.savefig("benchmark1.png")
+plt.show()
