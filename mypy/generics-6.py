34dc057c62538a52724a719d27b3b57b9e69e132
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-12-15 09:42:29 +0100

    Reveal types

---
+++ mypy/generics-6.py
@@ -0,0 +1,14 @@
+from typing import TypeVar, Tuple
+
+T = TypeVar('T')
+
+
+def pair(first: T, second: T) -> Tuple[T, T]:
+    x = (first, second)
+    return x
+
+
+reveal_type(pair(1, 2))
+reveal_type(pair(0, "B"))
+reveal_type(pair("A", 42))
+reveal_type(pair("A", "B"))
