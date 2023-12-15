38bc9cad30f395908f24227042952f65c2f732ae
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-12-15 09:42:29 +0100

    Two distinct types, compatible variant

---
+++ mypy/generics-4.py
@@ -0,0 +1,15 @@
+from typing import TypeVar, Tuple
+
+T = TypeVar('T')
+U = TypeVar('U')
+
+
+def pair(first: T, second: U) -> Tuple[T, U]:
+    x = (first, second)
+    return x
+
+
+reveal_type(pair(1, 2))
+reveal_type(pair(0, "B"))
+reveal_type(pair("A", 42))
+reveal_type(pair("A", "B"))
