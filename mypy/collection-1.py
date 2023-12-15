0284ec6aca383b9c96bdc3f910251c4fd8b4d4e4
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-12-15 09:42:29 +0100

    Generic collections, compatible variant

---
+++ mypy/collection-1.py
@@ -0,0 +1,22 @@
+from typing import TypeVar, Generic
+from typing import List
+
+
+T = TypeVar('T')
+
+
+class Collection(Generic[T]):
+    def __init__(self) -> None:
+        self.collection : List[T] = []
+
+    def append(self, item: T) -> None:
+        self.collection.append(item)
+
+    def get_all(self) -> List[T]:
+        return self.collection
+
+
+c = Collection[int]()
+c.append(1)
+c.append(2)
+print(c.get_all())
