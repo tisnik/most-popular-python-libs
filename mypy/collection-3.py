bd8460a30318a372633f24b1e2a90205a5fbaf35
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-12-15 09:42:29 +0100

    Generic collections, variant for Python 3.12

---
+++ mypy/collection-3.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Collection[T]():
+    def __init__(self) -> None:
+        self.collection : List[T] = []
+
+    def append(self, item: T) -> None:
+        self.collection.append(item)
+
+    def get_all(self) -> List[T]:
+        return self.collection
+
+
+c = Collection[int]()
+c.append(1)
+c.append("foo")
