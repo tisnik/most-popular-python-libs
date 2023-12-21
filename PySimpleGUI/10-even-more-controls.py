95e3ccc01b701f649ef2ecf1e2c2d514bb4aa271
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-12-21 18:16:12 +0100

    Even more controls

---
+++ PySimpleGUI/10-even-more-controls.py
@@ -0,0 +1,39 @@
+import PySimpleGUI as sg
+
+# ovládací prvky, které se mají zobrazit v okně
+layout = [
+    [
+        sg.Text("Name", size=(8, 0)),
+        sg.InputText(key="name")
+    ],
+    [
+        sg.Text("Surname", size=(8, 0)),
+        sg.InputText(key="surname")
+    ],
+    [
+        sg.Text("Role", size=(8, 0)),
+        sg.Combo(["Administrator", "Maintainer", "Guest"], default_value="Guest", readonly=True, key="role")
+    ],
+    [
+        sg.Text("Register e-mail", size=(8, 0)),
+        sg.Checkbox("", default=True, key="register e-mail")
+    ],
+    [
+        sg.Text("Color theme", size=(8, 0)),
+        sg.Radio("Light", "THEME", default=False, key="light_theme"),
+        sg.Radio("Dark", "THEME", default=True, key="dark_theme"),
+    ],
+    [
+        sg.Submit()
+    ],
+]
+
+# vytvoření okna s ovládacími prvky
+window = sg.Window("Window #9", layout)
+
+# přečtení jediné události
+event, values = window.read()
+print("Event: ", event, "    Values: ", values)
+
+# po přečtení události okno zavřeme
+window.close()
