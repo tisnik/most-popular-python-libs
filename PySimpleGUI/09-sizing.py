fe8b89aaf2772ef8d560e6dc95207b7a20e4aae6
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-12-21 18:02:55 +0100

    Window sizing

--- PySimpleGUI/09-sizing.py
+++ PySimpleGUI/09-sizing.py
@@ -2,13 +2,13 @@ import PySimpleGUI as sg
 
 # ovládací prvky, které se mají zobrazit v okně
 layout = [
-    [sg.Text("Name"), sg.InputText(key="name")],
-    [sg.Text("Surname"), sg.InputText(key="surname")],
+    [sg.Text("Name", size=(8, 0)), sg.InputText(key="name")],
+    [sg.Text("Surname", size=(8, 0)), sg.InputText(key="surname")],
     [sg.Submit()],
 ]
 
 # vytvoření okna s ovládacími prvky
-window = sg.Window("Window #8", layout)
+window = sg.Window("Window #9", layout)
 
 # přečtení jediné události
 event, values = window.read()
