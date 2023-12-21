c18b9b4c6a17b8b7bf975b89fe5dfe250cb16f6c
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-12-21 18:02:28 +0100

    One shot window

---
+++ PySimpleGUI/08-one-shot-window.py
@@ -0,0 +1,18 @@
+import PySimpleGUI as sg
+
+# ovládací prvky, které se mají zobrazit v okně
+layout = [
+    [sg.Text("Name"), sg.InputText(key="name")],
+    [sg.Text("Surname"), sg.InputText(key="surname")],
+    [sg.Submit()],
+]
+
+# vytvoření okna s ovládacími prvky
+window = sg.Window("Window #6", layout)
+
+# přečtení jediné události
+event, values = window.read()
+print("Event: ", event, "    Values: ", values)
+
+# po přečtení události okno zavřeme
+window.close()
