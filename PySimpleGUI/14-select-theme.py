193d873ec2ebf0331548913c7bc1ae3856cd5ad2
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-12-22 13:26:49 +0100

    Theme selection

---
+++ PySimpleGUI/14-select-theme.py
@@ -0,0 +1,44 @@
+import PySimpleGUI as sg
+
+themes = sorted(sg.list_of_look_and_feel_values())
+
+
+# vytvoření okna s ovládacími prvky
+def main_window(theme=None):
+    selected_theme = theme or themes[0]
+
+    # ovládací prvky, které se mají zobrazit v okně
+    layout = [
+        [
+            sg.Text("Theme"),
+            sg.Combo(themes, default_value=selected_theme, readonly=True, key="theme"),
+        ],
+        [
+            sg.Button("Change"),
+            sg.Cancel("Exit")
+        ],
+    ]
+
+    sg.theme(selected_theme)
+    return sg.Window("Window #4", layout)
+
+
+window = main_window()
+
+# obsluha smyčky událostí (event loop)
+while True:
+    # přečtení události
+    event, values = window.read()
+
+    # reakce na událost "uzavření okna"
+    if event in {sg.WIN_CLOSED, "Exit"}:
+        break
+
+    # reakce na výběr tématu
+    if event == "Change":
+        selected_theme = values["theme"]
+        window.close()
+        window = main_window(selected_theme)
+
+# po výskoku ze smyčky událostí aplikaci ukončíme
+window.close()
