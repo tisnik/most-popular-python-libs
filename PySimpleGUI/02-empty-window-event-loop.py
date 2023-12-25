c18caffef08fbf8c64004ee2a32214d3c1902869
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-12-25 10:12:30 +0100

    Empty window with event loop

---
+++ PySimpleGUI/02-empty-window-event-loop.py
@@ -0,0 +1,19 @@
+import PySimpleGUI as sg
+
+# ovládací prvky, které se mají zobrazit v okně
+layout = [[]]
+
+# vytvoření okna s ovládacími prvky
+window = sg.Window("Window #1", layout, size=(320, 240))
+
+# obsluha smyčky událostí (event loop)
+while True:
+    # přečtení události
+    event, values = window.read()
+
+    # reakce na událost "uzavření okna"
+    if event == sg.WIN_CLOSED:
+        break
+
+# po výskoku ze smyčky událostí aplikaci ukončíme
+window.close()
