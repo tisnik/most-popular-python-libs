6bb63474106a2fe7ab3604148cbe7da286a64866
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-12-27 09:03:01 +0100

    Monkeytype example #1

---
+++ monkeytype/adder/adder.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+def add(a, b):
+    return a+b
+
+def inc(a):
+    return a+1
---
+++ monkeytype/test_adder_1.py
@@ -0,0 +1,4 @@
+from adder.adder import add, inc
+
+print(add(1, 2))
+print(inc(41))
