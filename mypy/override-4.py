9ff73457a75441ce661f2c8a3f083f77fe9a663d
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-12-27 09:01:21 +0100

    Override example with added types

---
+++ mypy/override-4.py
@@ -0,0 +1,10 @@
+from typing_extensions import override
+
+class Fruit:
+    def eat(self) -> None:
+        pass
+
+class Apple(Fruit):
+    @override
+    def eat_apple(self) -> None:
+        pass
