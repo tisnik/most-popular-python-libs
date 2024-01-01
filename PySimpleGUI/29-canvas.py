436539eacf0a12f25f74315cbd54a520d1856013
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-01 10:22:50 +0100

    Canvas on window

---
+++ PySimpleGUI/29-canvas.py
@@ -0,0 +1,29 @@
+import PySimpleGUI as sg
+
+# ovládací prvky, které se mají zobrazit v okně
+left_column = [
+    [sg.Button("Exit", size=(8, 0))],
+]
+
+right_column = [
+    [
+        sg.Canvas(),
+    ],
+]
+
+layout = [
+    [
+        sg.Frame("Commands", left_column),
+        sg.Frame("Canvas", right_column),
+    ],
+]
+
+# vytvoření okna s ovládacími prvky
+window = sg.Window("Window #29", layout, use_custom_titlebar=False)
+
+# přečtení jediné události
+event, values = window.read()
+print("Event: ", event, "    Values: ", values)
+
+# po přečtení události okno zavřeme
+window.close()
