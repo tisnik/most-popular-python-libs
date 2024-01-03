5e30b338f061c2a187f8ae7cc137de030e206a85
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-03 08:43:29 +0100

    Function that call other function with tuple, with type info

---
+++ monkeytype/tuple_patched.py
@@ -0,0 +1,17 @@
+from typing import Tuple
+
+def print_tuple(x: Tuple[int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int]) -> None:
+    print(x)
+
+def use_tuple() -> None:
+    x = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+         1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+         1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+         1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+         1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+         1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+         1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+         1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+         1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+         1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    print_tuple(x)
