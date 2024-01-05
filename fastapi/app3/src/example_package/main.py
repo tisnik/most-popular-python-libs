8e1550f5c00099432d43dc61c1239a3b5330198e
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-05 09:01:46 +0100

    Reformatted by Black

--- fastapi/app3/src/example_package/main.py
+++ fastapi/app3/src/example_package/main.py
@@ -2,17 +2,21 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.post("/create")
 async def create_operation(text: str):
     return {"created": text}
+
 
 @app.get("/")
 async def read_operation():
     return {"list": None}
 
+
 @app.put("/update/{id}")
 async def update_operation(id: int, text: str = ""):
     pass
+
 
 @app.delete("/delete/{id}")
 async def delete_operation(id: int):
