2c2c9bb51a529c4e42f29941ae4b80602c66ae94
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-05 09:02:33 +0100

    Reformatted by Black

--- xarray/05_xarray_metadata.py
+++ xarray/05_xarray_metadata.py
@@ -19,7 +19,5 @@ import xarray as xr
 xcoords = np.linspace(10, 100, 10)
 ycoords = np.linspace(-100, -10, 10)
 
-array = xr.DataArray(np.identity(10),
-                     dims=("x", "y"),
-                     coords={"x":xcoords, "y":ycoords})
+array = xr.DataArray(np.identity(10), dims=("x", "y"), coords={"x": xcoords, "y": ycoords})
 print(array)
