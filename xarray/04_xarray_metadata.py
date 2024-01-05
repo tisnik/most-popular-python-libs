8d9b9fb517d77862f0790045c1de14a2b1eb6163
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-05 09:02:33 +0100

    Reformatted by Black

--- xarray/04_xarray_metadata.py
+++ xarray/04_xarray_metadata.py
@@ -16,7 +16,7 @@
 import numpy as np
 import xarray as xr
 
-array = xr.DataArray(np.identity(10),
-                     dims=("x", "y"),
-                     coords={"x":[10, 20, 30, 40, 50, 60, 70, 80, 90, 100]})
+array = xr.DataArray(
+    np.identity(10), dims=("x", "y"), coords={"x": [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]}
+)
 print(array)
