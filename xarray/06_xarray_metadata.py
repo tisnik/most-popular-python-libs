2e85730493dea30b3d0971e60659f6af3fa8b419
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-05 09:02:33 +0100

    Reformatted by Black

--- xarray/06_xarray_metadata.py
+++ xarray/06_xarray_metadata.py
@@ -16,14 +16,16 @@
 import numpy as np
 import xarray as xr
 
-temperatures = -10 + 40*np.random.rand(10, 10)
+temperatures = -10 + 40 * np.random.rand(10, 10)
 
 xcoords = np.linspace(10, 100, 10)
 ycoords = np.linspace(-100, -10, 10)
 
-array = xr.DataArray(temperatures,
-                     name="Temperature measurement",
-                     dims=("x", "y"),
-                     coords={"x":xcoords, "y":ycoords})
+array = xr.DataArray(
+    temperatures,
+    name="Temperature measurement",
+    dims=("x", "y"),
+    coords={"x": xcoords, "y": ycoords},
+)
 
 print(array)
