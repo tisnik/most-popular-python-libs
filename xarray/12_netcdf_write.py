da2e4ea3a73f8472b532ecca6737602b1a9de210
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-05 09:02:33 +0100

    Reformatted by Black

--- xarray/12_netcdf_write.py
+++ xarray/12_netcdf_write.py
@@ -16,22 +16,22 @@
 import numpy as np
 import xarray as xr
 
-temperatures = -10 + 40*np.random.rand(10, 10)
+temperatures = -10 + 40 * np.random.rand(10, 10)
 
 xcoords = np.linspace(10, 100, 10)
 ycoords = np.linspace(-100, -10, 10)
 
-array = xr.DataArray(temperatures,
-                     name="Temperature measurement",
-                     dims=("x", "y"),
-                     coords={"x":xcoords, "y":ycoords},
-                     attrs={
-                         "units": "centigrees",
-                         "description": "Local temperature values measured in grid",
-                         "measured_by": {"name": "ThermometerBot",
-                              "vendor": "BIY",
-                              "version": (1, 0, 0)}
-                         })
+array = xr.DataArray(
+    temperatures,
+    name="Temperature measurement",
+    dims=("x", "y"),
+    coords={"x": xcoords, "y": ycoords},
+    attrs={
+        "units": "centigrees",
+        "description": "Local temperature values measured in grid",
+        "measured_by": {"name": "ThermometerBot", "vendor": "BIY", "version": (1, 0, 0)},
+    },
+)
 
 
 print(array)
