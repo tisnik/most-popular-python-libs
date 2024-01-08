748c0228568421d27cdc688ebfe4b25fcef9b967
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-08 09:08:24 +0100

    Reformatted by Black

--- xarray/18_condition.py
+++ xarray/18_condition.py
@@ -16,22 +16,22 @@
 import numpy as np
 import xarray as xr
 
-temperatures = -10 + 40*np.random.rand(5, 5)
+temperatures = -10 + 40 * np.random.rand(5, 5)
 
 xcoords = np.linspace(10, 100, 5)
 ycoords = np.linspace(-100, -10, 5)
 
-array = xr.DataArray(temperatures,
-                     name="Temperature measurement",
-                     dims=("x", "y"),
-                     coords={"x":xcoords, "y":ycoords},
-                     attrs={
-                         "units": "centigrees",
-                         "description": "Local temperature values measured in grid",
-                         "measured_by": {"name": "ThermometerBot",
-                              "vendor": "BIY",
-                              "version": (1, 0, 0)}
-                         })
+array = xr.DataArray(
+    temperatures,
+    name="Temperature measurement",
+    dims=("x", "y"),
+    coords={"x": xcoords, "y": ycoords},
+    attrs={
+        "units": "centigrees",
+        "description": "Local temperature values measured in grid",
+        "measured_by": {"name": "ThermometerBot", "vendor": "BIY", "version": (1, 0, 0)},
+    },
+)
 
 
 print(array)
