bb59f3d58ed5e6e70ccef52006ebf975c07b4a5f
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-08 09:09:08 +0100

    Memory allocation details

---
+++ array/alloc_details.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+from sys import getsizeof
+from pympler.asizeof import asizeof
+from array import array
+
+# prázdná struktura
+a1 = array("B")
+
+last_size = asizeof(a1)
+last_index = 0
+
+# naplnit daty
+for i in range(1000):
+    a1.append(i%100)
+    size = asizeof(a1)
+    if size != last_size:
+        print(f"{i:5}  {size:5}  {size-last_size:+5}   {last_index:4}-{i:<4}")
+        last_index = i
+        last_size = size
