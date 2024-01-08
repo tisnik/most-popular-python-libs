6e0978409baad30e09f683d5ba13c83f73d61050
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-08 09:09:39 +0100

    Reformatted by Black

--- xarray/23_math_coords.py
+++ xarray/23_math_coords.py
@@ -17,7 +17,7 @@ import numpy as np
 import xarray as xr
 
 temperatures1 = np.arange(100).reshape(10, 10)
-temperatures2 = np.array([100]*100).reshape(10, 10)
+temperatures2 = np.array([100] * 100).reshape(10, 10)
 
 xcoords1 = np.linspace(1, 10, 10)
 ycoords1 = np.linspace(1, 10, 10)
@@ -35,30 +35,30 @@ print("x:", xcoords2)
 print("y:", ycoords2)
 print()
 
-array1 = xr.DataArray(temperatures1,
-                      name="Temperature measurement #1",
-                      dims=("x", "y"),
-                      coords={"x":xcoords1, "y":ycoords1},
-                      attrs={
-                          "units": "centigrees",
-                          "description": "Local temperature values measured in grid #1",
-                          "measured_by": {"name": "ThermometerBot",
-                               "vendor": "BIY",
-                               "version": (1, 0, 0)}
-                          })
+array1 = xr.DataArray(
+    temperatures1,
+    name="Temperature measurement #1",
+    dims=("x", "y"),
+    coords={"x": xcoords1, "y": ycoords1},
+    attrs={
+        "units": "centigrees",
+        "description": "Local temperature values measured in grid #1",
+        "measured_by": {"name": "ThermometerBot", "vendor": "BIY", "version": (1, 0, 0)},
+    },
+)
 
 
-array2 = xr.DataArray(temperatures2,
-                      name="Temperature measurement #2",
-                      dims=("x", "y"),
-                      coords={"x":xcoords2, "y":ycoords2},
-                      attrs={
-                          "units": "centigrees",
-                          "description": "Local temperature values measured in grid #2",
-                          "measured_by": {"name": "ThermometerBot",
-                               "vendor": "BIY",
-                               "version": (1, 0, 0)}
-                          })
+array2 = xr.DataArray(
+    temperatures2,
+    name="Temperature measurement #2",
+    dims=("x", "y"),
+    coords={"x": xcoords2, "y": ycoords2},
+    attrs={
+        "units": "centigrees",
+        "description": "Local temperature values measured in grid #2",
+        "measured_by": {"name": "ThermometerBot", "vendor": "BIY", "version": (1, 0, 0)},
+    },
+)
 
 
 print("Array #1:")
