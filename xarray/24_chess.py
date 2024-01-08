ff4fdf1c27813295000e62904269f5335c6823db
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-08 09:09:39 +0100

    Reformatted by Black

--- xarray/24_chess.py
+++ xarray/24_chess.py
@@ -16,7 +16,7 @@
 import numpy as np
 import xarray as xr
 
-chessboard = np.array([" "]*64).reshape(8, 8)
+chessboard = np.array([" "] * 64).reshape(8, 8)
 chessboard[0, 0] = "♚"
 chessboard[1, 5] = "♔"
 chessboard[2, 5] = "♙"
@@ -25,16 +25,20 @@ chessboard[3, 4] = "♜"
 files = ["a", "b", "c", "d", "e", "f", "g", "h"]
 ranks = np.linspace(1, 8, 8, dtype=np.int8)
 
-array = xr.DataArray(chessboard,
-                     name="Saavedra position",
-                     dims=("files", "ranks"),
-                     coords={"files":files, "ranks":ranks})
+array = xr.DataArray(
+    chessboard,
+    name="Saavedra position",
+    dims=("files", "ranks"),
+    coords={"files": files, "ranks": ranks},
+)
 
 array.attrs["units"] = "chess pieces"
-array.attrs["description"] ="White to move and win",
-array.attrs["metadata"] = {"played by": "Fernando Saavedra",
-                           "winner": "white",
-                           "see also": "https://www.youtube.com/watch?v=Mg2OOsQPURs",}
+array.attrs["description"] = ("White to move and win",)
+array.attrs["metadata"] = {
+    "played by": "Fernando Saavedra",
+    "winner": "white",
+    "see also": "https://www.youtube.com/watch?v=Mg2OOsQPURs",
+}
 
 print(array)
 print()
@@ -44,4 +48,3 @@ print()
 
 print(array.sel(ranks=6))
 print()
-
