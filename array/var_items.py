12a63712b96c3095a4ad1a02ec9c816fb3047627
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-08 09:08:59 +0100

    Var. number of items

---
+++ array/var_items.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+from pympler.asizeof import asizeof
+from array import array
+
+# prázdné struktury
+l1 = []
+a1 = array("B")
+a2 = array("H")
+a3 = array("I")
+a4 = array("L")
+
+# naplnit daty
+for i in range(200):
+    l1.append(i)
+    a1.append(i%100)
+    a2.append(i%100)
+    a3.append(i)
+    a4.append(i)
+
+    # výpis skutečných velikostí
+    print(i, asizeof(l1), asizeof(a1), asizeof(a2), asizeof(a3), asizeof(a4))
