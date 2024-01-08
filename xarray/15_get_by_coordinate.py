774d61b6caf3aed61d368ebcfe9170e7c172972e
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-08 09:08:24 +0100

    Reformatted by Black

--- xarray/15_get_by_coordinate.py
+++ xarray/15_get_by_coordinate.py
@@ -23,17 +23,17 @@ ycoords = np.linspace(-100, -10, 10)
 
 times = ["2023-10-01", "2023-10-02", "2023-10-03"]
 
-array = xr.DataArray(temperatures,
-                     name="Temperature measurement",
-                     dims=("x", "y", "time"),
-                     coords={"x":xcoords, "y":ycoords, "time":times},
-                     attrs={
-                         "units": "centigrees",
-                         "description": "Local temperature values measured in grid",
-                         "measured_by": {"name": "ThermometerBot",
-                              "vendor": "BIY",
-                              "version": (1, 0, 0)}
-                         })
+array = xr.DataArray(
+    temperatures,
+    name="Temperature measurement",
+    dims=("x", "y", "time"),
+    coords={"x": xcoords, "y": ycoords, "time": times},
+    attrs={
+        "units": "centigrees",
+        "description": "Local temperature values measured in grid",
+        "measured_by": {"name": "ThermometerBot", "vendor": "BIY", "version": (1, 0, 0)},
+    },
+)
 
 
 print(array)
@@ -45,5 +45,5 @@ print()
 print(array[0][2])
 print()
 
-print(array[:,2])
+print(array[:, 2])
 print()
