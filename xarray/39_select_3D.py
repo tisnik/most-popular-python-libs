9ae52e20ec80a0d913558c6d84229a053df06023
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-09 09:06:55 +0100

    Reformatted by Black

--- xarray/39_select_3D.py
+++ xarray/39_select_3D.py
@@ -23,17 +23,17 @@ ycoords = np.linspace(-100, -10, 10)
 
 times = ["2023-10-01", "2023-10-02", "2023-10-03"]
 
-array = xr.DataArray(temperatures,
-                     name="Temperature measurement",
-                     dims=("x", "y", "time"),
-                     coords={"x":xcoords, "y":ycoords, "time":times},
-                     attrs={
-                         "units": "centigrees",
-                         "description": "Local temperature values measured in grid",
-                         "measured_by": {"name": "ThermometerBot",
-                              "vendor": "BIY",
-                              "version": (1, 0, 0)}
-                         })
+array = xr.DataArray(
+    temperatures,
+    name="Temperature measurement",
+    dims=("x", "y", "time"),
+    coords={"x": xcoords, "y": ycoords, "time": times},
+    attrs={
+        "units": "centigrees",
+        "description": "Local temperature values measured in grid",
+        "measured_by": {"name": "ThermometerBot", "vendor": "BIY", "version": (1, 0, 0)},
+    },
+)
 
 
 print(array)
