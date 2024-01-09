89b3f5df4703b8cc2a1f8c7c5b18cf68f5bfc45b
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-09 09:06:55 +0100

    Reformatted by Black

--- xarray/33_time_group_by_mean.py
+++ xarray/33_time_group_by_mean.py
@@ -17,23 +17,22 @@ import numpy as np
 import xarray as xr
 
 measured_at = np.arange(
-        np.datetime64("2023-01-01"),
-        np.datetime64("2024-01-01"),
-        np.timedelta64(1, "D")).astype('datetime64[ns]')
+    np.datetime64("2023-01-01"), np.datetime64("2024-01-01"), np.timedelta64(1, "D")
+).astype("datetime64[ns]")
 
 temperatures = 10 + 20 * np.random.rand(365)
 
-array = xr.DataArray(temperatures,
-                     name="Temperature measurement",
-                     dims=("time",),
-                     coords={"time":measured_at},
-                     attrs={
-                         "units": "centigrees",
-                         "description": "Local temperature values measured in time serie #1",
-                         "measured_by": {"name": "ThermometerBot",
-                              "vendor": "BIY",
-                              "version": (1, 0, 0)}
-                         })
+array = xr.DataArray(
+    temperatures,
+    name="Temperature measurement",
+    dims=("time",),
+    coords={"time": measured_at},
+    attrs={
+        "units": "centigrees",
+        "description": "Local temperature values measured in time serie #1",
+        "measured_by": {"name": "ThermometerBot", "vendor": "BIY", "version": (1, 0, 0)},
+    },
+)
 
 grouped = array.groupby("time.month")
 
