34bfb16bac05b23f839a8d37f697373510de68a2
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-09 09:06:55 +0100

    Reformatted by Black

--- xarray/34_select_isel.py
+++ xarray/34_select_isel.py
@@ -20,10 +20,7 @@ values = np.arange(0, 10)
 
 xcoords = np.linspace(20, 2, 10)
 
-array = xr.DataArray(values,
-                     name="Temperature measurement",
-                     dims=("x",),
-                     coords={"x":xcoords})
+array = xr.DataArray(values, name="Temperature measurement", dims=("x",), coords={"x": xcoords})
 
 
 print(array)
