4df1219b02f16d91a2c9da64fbd7341000ebdc40
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-12 08:08:17 +0100

    Prime numbers generator, 4th variant of tests

---
+++ mutmut/primes4/main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""Vstupní bod do testované aplikace."""
+
+from primes import find_primes
+
+
+if __name__ == '__main__':
+    # pouze se ujistíme, že lze spustit funkci primes2
+    print(find_primes(1000))
