13d002c249ade95772ae89e2ed177ea044425103
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-16 08:25:00 +0100

    Properly sorted imports

--- capstone/disasm_x86_64_offset_C.py
+++ capstone/disasm_x86_64_offset_C.py
@@ -11,7 +11,7 @@
 #
 
 # import všech symbolů použitých ve skriptu
-from capstone import Cs, CS_ARCH_X86, CS_MODE_64
+from capstone import CS_ARCH_X86, CS_MODE_64, Cs
 
 # pozice v binárním souboru, od níž jsou instrukce uloženy
 OFFSET = 1000
