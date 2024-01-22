ea45f998dcdd7b1da919808c6585de51d62e8ee5
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-22 08:29:59 +0100

    PyScript project + scripts to run on web pages

---
+++ micropython-wasm/hello.py
@@ -0,0 +1,6 @@
+import sys
+
+for i in range(10):
+    print("Hello MicroPython")
+
+print(sys.version)
---
+++ micropython-wasm/hello2.py
@@ -0,0 +1,7 @@
+import sys
+from pyscript import document
+
+for i in range(10):
+    document.write("Hello Python<br>")
+
+document.write(sys.version)
