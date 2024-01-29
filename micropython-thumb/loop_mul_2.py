153e608b8dd1c8517709a3041894a3524d4c3b05
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-29 08:27:21 +0100

    Multiply called in a loop (benchmark #2)

---
+++ micropython-thumb/loop_mul_2.py
@@ -0,0 +1,14 @@
+def loop_mul():
+    x = 10
+    for i in range(100):
+        for j in range(100):
+            x*=1
+    return x
+
+
+import utime
+t1 = utime.ticks_us()
+loop_mul()
+t2 = utime.ticks_us()
+print(utime.ticks_diff(t2, t1))
+
