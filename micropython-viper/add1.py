085554e62e092616774876d13b2afc8471444867
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-02-02 08:30:59 +0100

    Add function compiled into bytecode

---
+++ micropython-viper/add1.py
@@ -0,0 +1,14 @@
+def add(x, y):
+    return x+y
+
+
+import utime
+
+t1 = utime.ticks_us()
+x = 0
+for i in range(10000):
+    x = add(x, 10)
+
+print(x)
+t2 = utime.ticks_us()
+print(utime.ticks_diff(t2, t1))
