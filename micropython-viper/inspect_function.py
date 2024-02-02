ff74592e639ed6b2c5cffdb38d4dc0d0bd4d6aa2
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-02-02 08:30:44 +0100

    Inspect functions compiled by Viper

---
+++ micropython-viper/inspect_function.py
@@ -0,0 +1,27 @@
+import array
+
+import machine
+
+
+def inspect(f):
+    baddr = bytes(array.array("O", [f]))
+    addr = int.from_bytes(baddr, "little")
+    print("function object at: 0x%08x" % addr)
+    print("number of args:     %u" % machine.mem32[addr + 8])
+    code_addr = machine.mem32[addr + 12]
+    print("machine code at:    0x%08x" % code_addr)
+    previous = -1
+    size = 0
+    while True:
+        current = machine.mem8[code_addr + size]
+        size += 1
+        if current == 0xbd and previous == 0xfe:
+            break
+        previous = current
+    print(f"machine code size:  {size} bytes")
+    print("-- code --")
+    for i in range(size):
+        print(f"{machine.mem8[code_addr + i]:02x}", end=" ")
+        if i % 16 == 15:
+            print()
+    print("\n----------")
