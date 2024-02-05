db498d8fc2d6357a7d2931e0b48f1082e983acca
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-02-05 08:44:41 +0100

    Nested loops compiled into bytecode

---
+++ micropython-viper/loop1.py
@@ -0,0 +1,14 @@
+def loop():
+    x = 0
+    for i in range(1000):
+        for j in range(1000):
+            x+=1
+    print(x)
+
+
+import utime
+
+t1 = utime.ticks_us()
+loop()
+t2 = utime.ticks_us()
+print(utime.ticks_diff(t2, t1))
