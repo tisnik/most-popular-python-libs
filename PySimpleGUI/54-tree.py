ae6843f438e9400abe7d95c1f9b223cd4f915068
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-02-08 09:53:49 +0100

    Tree widget

---
+++ PySimpleGUI/54-tree.py
@@ -0,0 +1,33 @@
+import PySimpleGUI as sg
+
+data = sg.TreeData()
+
+data.Insert("", "A", "A", [])
+data.Insert("", "B", "B", [])
+data.Insert("", "C", "D", [])
+data.Insert("A", "A1", "A1", [])
+data.Insert("A", "A2", "A2", [])
+data.Insert("A", "A3", "A3", [])
+data.Insert("B", "B1", "B1", [])
+data.Insert("B", "B2", "B2", [])
+data.Insert("B", "B3", "B3", [])
+data.Insert("C", "C1", "C1", [])
+data.Insert("C", "C2", "C2", [])
+data.Insert("C", "C3", "C3", [])
+
+
+# ovládací prvky, které se mají zobrazit v okně
+layout = [
+    [sg.Tree(data=data, headings=[""])],
+    [sg.Push(), sg.Submit(), sg.Push()],
+]
+
+# vytvoření okna s ovládacími prvky
+window = sg.Window("Window #54", layout)
+
+# přečtení jediné události
+event, values = window.read()
+print("Event: ", event, "    Values: ", values)
+
+# po přečtení události okno zavřeme
+window.close()
