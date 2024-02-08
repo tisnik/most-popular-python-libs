c4087acd7382726170b2215c754df5ee462d5b00
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-02-08 09:53:42 +0100

    Table widget

---
+++ PySimpleGUI/53-table.py
@@ -0,0 +1,27 @@
+import PySimpleGUI as sg
+
+data = [
+        ["Jan 2024", "Jan 2023", "Language", "Ratings", "Change"],
+        [1, 1, "Python", "13.97%", "-2.39%"],
+        [2, 2, "C", "11.44%", "-4.81%"],
+        [3, 3, "C++", "9.96%", "-2.95%"],
+        [4, 4, "Java", "7.87%", "-4.34%"],
+        [5, 5, "C#", "7.16%", "+1.43%"],
+        [6, 7, "JavaScript", "2.77%", "-0.11%"],
+        ]
+
+# ovládací prvky, které se mají zobrazit v okně
+layout = [
+    [sg.Table(data)],
+    [sg.Push(), sg.Submit(), sg.Push()],
+]
+
+# vytvoření okna s ovládacími prvky
+window = sg.Window("Window #53", layout)
+
+# přečtení jediné události
+event, values = window.read()
+print("Event: ", event, "    Values: ", values)
+
+# po přečtení události okno zavřeme
+window.close()
