dc9d30e10179b87eb3a2fd0a8a92621774a70395
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-02-14 08:25:02 +0100

    Added license header

--- micropython-thumb/add_five.py
+++ micropython-thumb/add_five.py
@@ -1,3 +1,15 @@
+#
+#  (C) Copyright 2024  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
 @micropython.asm_thumb
 def add_four(r0, r1, r2, r3, r4):
     add(r0, r0, r1)
