9b686296b687deaee801ebcb721246316e5a022b
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-02-19 08:15:04 +0100

    Partition by operation, 1st example

---
+++ toolz/partitionby1.py
@@ -0,0 +1,8 @@
+from random import randrange
+from toolz.recipes import partitionby
+
+values = [randrange(1, 11, 1) for _ in range(30)]
+print(values)
+
+values2 = partitionby(lambda x: x < 5, values)
+print(list(values2))
