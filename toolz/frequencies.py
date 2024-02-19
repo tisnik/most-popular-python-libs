fb3c6e2064dc053b952921b66ddfbd5829a93341
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-02-19 08:15:04 +0100

    Item frequencies

---
+++ toolz/frequencies.py
@@ -0,0 +1,8 @@
+from random import randrange
+from toolz.itertoolz import frequencies, unique
+
+values = [randrange(1, 11, 1) for _ in range(30)]
+print(frequencies(values))
+
+values2 = unique(values)
+print(frequencies(values2))
