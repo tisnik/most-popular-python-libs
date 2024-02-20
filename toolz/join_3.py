037bce7fa5c79b6e328dcf77a6079f190bfdaa3f
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-02-20 08:36:38 +0100

    Join two sequences, outer join

---
+++ toolz/join_3.py
@@ -0,0 +1,18 @@
+from toolz.itertoolz import join
+
+names = [
+    (0, "Alice"),
+    (1, "Bob"),
+    (2, "Charlie"),
+    (3, "Zdenek"),
+]
+
+phones = [
+    ("Alice",   "555-1234"),
+    ("Bob",     "555-5678"),
+    ("Charlie", "555-9999"),
+    ("Martin",  "000-0000"),
+]
+
+for item in join(1, names, 0, phones): 
+    print(item[0][0], item[0][1], item[1][1])
