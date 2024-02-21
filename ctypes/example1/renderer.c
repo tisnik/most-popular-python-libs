79ac7865bf25e962a149d534b83e6bdf8a529ef1
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-02-21 12:56:10 +0100

    Reformatted

--- ctypes/example1/renderer.c
+++ ctypes/example1/renderer.c
@@ -1,13 +1,13 @@
-void render_test_rgb_image(unsigned int width, unsigned int height, unsigned char *pixels, unsigned char green)
-{
+void render_test_rgb_image(unsigned int width, unsigned int height,
+                           unsigned char *pixels, unsigned char green) {
     unsigned int i, j;
     unsigned char *p = pixels;
 
-    for (j=0; j<height; j++) {
-        for (i=0; i<width; i++) {
-            *p++=i;
-            *p++=green;
-            *p++=j;
+    for (j = 0; j < height; j++) {
+        for (i = 0; i < width; i++) {
+            *p++ = i;
+            *p++ = green;
+            *p++ = j;
             p++;
         }
     }
