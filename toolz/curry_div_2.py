898588f29b9f9ce47af54470fbed4c1901b9255d
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-03-04 08:57:54 +0100

    Curried division

---
+++ toolz/curry_div_2.py
@@ -0,0 +1,21 @@
+from toolz import curry, take, iterate
+
+
+def inc(x):
+    return x+1
+
+
+@curry
+def divisible_by(y, x):
+    return x % y == 0
+
+
+even = divisible_by(2)
+
+
+sequence = iterate(inc, 0)
+evens = filter(even, sequence)
+
+sequence = take(10, evens)
+print(list(sequence))
+
