310f9c503fe17ba82481a2a1f29dbd3c5d972bff
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-03-04 08:57:09 +0100

    Head operation defined using currying

---
+++ toolz/head_3.py
@@ -0,0 +1,12 @@
+from toolz import curry
+from toolz.itertoolz import take
+
+
+sequence = range(1, 1000)
+
+curried = curry(take)
+
+head = curried(10)
+
+first10 = head(sequence)
+print(list(first10))
