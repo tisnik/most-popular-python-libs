de6de1a18484d6dcee056abd6d0a3d03f0b915d1
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-03-05 09:16:36 +0100

    Curried version of map function

---
+++ toolz/curried_map.py
@@ -0,0 +1,9 @@
+from toolz.curried import map
+
+message = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"
+words = message.split()
+
+lengths = map(len)
+
+print(list(lengths(words)))
+
