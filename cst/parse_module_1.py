099c9ecbc18892dbfeefc648d67004b782697c49
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-03-05 09:15:59 +0100

    Parsing and dumping whole module (made from one expression), 1st example

---
+++ cst/parse_module_1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# vim: set fileencoding=utf-8
+
+from libcst import parse_module
+from libcst.tool import dump
+
+
+expression = "1 + 2"
+
+parsed = parse_module(expression)
+print("Parsed:")
+print(parsed)
+
+print()
+
+dumped = dump(parsed)
+print("Dumped:")
+print(dumped)
+
+print()
