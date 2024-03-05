937bb0900b9b2b23159270735504eed0ba4c25e5
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-03-05 09:16:46 +0100

    Normal version of map function

---
+++ toolz/un_curried_map.py
@@ -0,0 +1,7 @@
+message = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"
+words = message.split()
+
+lengths = map(len)
+
+print(list(lengths(words)))
+
