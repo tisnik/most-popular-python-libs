65d29b148e7f6c1c8ca0f568d988bb417819fd60
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-03-05 09:15:59 +0100

    Code traversing, skip some node type(s)

---
+++ cst/traverse_code_4.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+# vim: set fileencoding=utf-8
+
+from libcst import parse_module, CSTVisitor
+from libcst import BinaryOperation, Integer, Add, Multiply
+from libcst import SimpleWhitespace
+from libcst.tool import dump
+
+
+class Visitor(CSTVisitor):
+    def __init__(self):
+        self.nest_level = 0
+
+    def on_visit(self, node):
+        indent = " " * self.nest_level * 2
+        info = node.__class__.__name__
+
+        if isinstance(node, SimpleWhitespace):
+            return True
+
+        if isinstance(node, BinaryOperation):
+            info = "Binary operation: " + node.operator.__class__.__name__
+
+        if isinstance(node, Integer):
+            info = node.value
+
+        if isinstance(node, Add):
+            info = "+"
+
+        if isinstance(node, Multiply):
+            info = "*"
+
+        print(indent, info)
+
+        self.nest_level += 1
+        return True
+
+    def on_leave(self, node):
+        self.nest_level -= 1
+
+
+constant = "1 + 2 * 3"
+
+parsed = parse_module(constant)
+visitor = Visitor()
+parsed.visit(visitor)
