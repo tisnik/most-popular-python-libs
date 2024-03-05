bd608a3c4173341374f94b942f17b69c3b891f89
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-03-05 09:45:19 +0100

    Currying, example #2

---
+++ toolz/curry2.py
@@ -0,0 +1,13 @@
+from toolz import curry
+
+
+def add3(x, y, z):
+    return x + y + z
+
+
+curried = curry(add3)
+
+print(curried)
+print(curried(1))
+print(curried(1)(2))      # pozor na umístění závorek!
+print(curried(1)(2)(3))   # pozor na umístění závorek!
