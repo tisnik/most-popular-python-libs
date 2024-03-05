e57bb11ffdde2294a5dd2f784dfedf0fec1e96f0
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-03-05 09:15:59 +0100

    Code traversing, print tree

---
+++ cst/traverse_code_2.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# vim: set fileencoding=utf-8
+
+from libcst import parse_module, CSTVisitor
+from libcst.tool import dump
+
+
+class Visitor(CSTVisitor):
+    def __init__(self):
+        self.nest_level = 0
+
+    def on_visit(self, node):
+        indent = " " * self.nest_level * 2
+        print(indent, node.__class__.__name__)
+        self.nest_level += 1
+        return True
+
+    def on_leave(self, node):
+        self.nest_level -= 1
+
+
+constant = "1 + 2 * 3"
+
+parsed = parse_module(constant)
+visitor = Visitor()
+parsed.visit(visitor)
