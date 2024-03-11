00258a963739ca44c7545161ce8797ab18f457d8
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-03-11 08:43:12 +0100

    Decoding tokens back to text

---
+++ tiktoken/tokens_to_text.py
@@ -0,0 +1,23 @@
+import tiktoken
+
+encoder = tiktoken.get_encoding("cl100k_base")
+
+tokens = [9906, 1917]
+text = encoder.decode(tokens)
+print(text)
+
+tokens = [9906, 11, 1917, 0]
+text = encoder.decode(tokens)
+print(text)
+
+tokens = [9906, 11, 1917, 0, 0, 0, 0]
+text = encoder.decode(tokens)
+print(text)
+
+tokens = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+text = encoder.decode(tokens)
+print(text)
+
+tokens = [1000, 1001, 1002, 1003, 1004, 1005, 1006, 1007, 1008, 1009, 1010]
+text = encoder.decode(tokens)
+print(text)
