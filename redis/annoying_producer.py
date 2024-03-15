7009546e11c2efb2421b89a8b881c13e37b6b652
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-03-15 08:36:53 +0100

    Simple repeating producer

---
+++ redis/annoying_producer.py
@@ -0,0 +1,16 @@
+import time
+import redis
+
+
+redis_client = redis.StrictRedis(
+    host="localhost",
+    port="6379",
+    decode_responses=True,
+)
+
+key = "bar"
+sleep_time = 0.18
+
+while True:
+    redis_client.set(key, "that's me")
+    time.sleep(sleep_time)
