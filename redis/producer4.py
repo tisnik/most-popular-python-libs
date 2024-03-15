7a785101fc7f314bf243ab9c241f5e8d7f333462
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-03-15 08:36:53 +0100

    GET+SET combo

---
+++ redis/producer4.py
@@ -0,0 +1,22 @@
+import time
+import redis
+
+
+redis_client = redis.StrictRedis(
+    host="localhost",
+    port="6379",
+    decode_responses=True,
+)
+
+key = "bar"
+sleep_time = 0.1
+
+while True:
+    v = redis_client.get(key) or ""
+    v += "^"
+    redis_client.set(key, v)
+    # time.sleep(1.4)
+    v += "$"
+    redis_client.set(key, v)
+    time.sleep(sleep_time)
+    print(v)
