005dafd857f9f8a9aca26949ce115732adedc9e1
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-03-15 08:36:53 +0100

    Consumer checking retrieved value

---
+++ redis/consumer1.py
@@ -0,0 +1,27 @@
+import redis
+import time
+
+
+redis_client = redis.StrictRedis(
+    host="localhost",
+    port="6379",
+    decode_responses=True,
+)
+
+key = "foo"
+expected_value = "^$"
+sleep_time = 0.01
+
+start_time = time.perf_counter()
+
+errors = 0
+
+while True:
+    value = redis_client.get(key)
+    if value != expected_value:
+        errors += 1
+        now = time.perf_counter()
+        duration = now - start_time
+        frequency = errors / duration
+        print(f"{value:3} {errors:3}  {duration:6.3}  {frequency:4.3}")
+    time.sleep(sleep_time)
