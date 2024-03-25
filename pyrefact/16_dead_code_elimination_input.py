0fe78b76d16ca9508d1abec9e41e56c8cef806f0
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-03-25 07:25:19 +0100

    Dead code in sources, original version

---
+++ pyrefact/16_dead_code_elimination_input.py
@@ -0,0 +1,24 @@
+import math
+import sys
+
+def foo(x):
+    import os
+    y = False
+    z = False
+    w = []
+
+    if x > 0:
+        y = True
+
+    if 0:
+        z = True
+
+    if x >= 1:
+        return 10
+
+    return 0
+
+
+print(foo(0))
+print(foo(1))
+print(foo(2))
