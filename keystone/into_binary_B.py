9c990d85e47b19f2c1fee66f3505d23322f175db
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-04-01 09:54:44 +0200

    Re-organized imports

--- keystone/into_binary_B.py
+++ keystone/into_binary_B.py
@@ -11,8 +11,7 @@
 #
 
 # import všech symbolů použitých ve skriptu
-from keystone import Ks, KS_ARCH_X86, KS_MODE_64, KsError
-
+from keystone import KS_ARCH_X86, KS_MODE_64, Ks, KsError
 
 try:
     # načtení kódu v assembleru ze souboru
