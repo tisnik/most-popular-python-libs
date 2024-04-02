49ef45ba66ccaace24850ec845b43cb01b3851a0
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-04-02 08:45:00 +0200

    Sorted and formatted imports

--- plotnine/15_histogram.py
+++ plotnine/15_histogram.py
@@ -10,7 +10,7 @@
 #      Pavel Tisnovsky
 #
 
+from plotnine import aes, geom_bar, ggplot, stat_bin
 from plotnine.data import economics
-from plotnine import ggplot, aes, geom_bar, stat_bin
 
 print(ggplot(economics) + aes(x="uempmed") + stat_bin(bins=20) + geom_bar())
