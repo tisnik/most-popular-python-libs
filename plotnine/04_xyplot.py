c42936f021898d221626fdce1a65ed131c0c8cbb
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-04-02 08:44:59 +0200

    Sorted and formatted imports

--- plotnine/04_xyplot.py
+++ plotnine/04_xyplot.py
@@ -10,8 +10,7 @@
 #      Pavel Tisnovsky
 #
 
+from plotnine import aes, geom_line, ggplot
 from plotnine.data import economics
-
-from plotnine import ggplot, aes, geom_line
 
 ggplot(economics) + aes(x="date", y="pop") + geom_line()
