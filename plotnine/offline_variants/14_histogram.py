ef7f2e54c9dd4d1f3ba87c624010ceb1c7dba86a
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-04-02 08:45:00 +0200

    Sorted and formatted imports

--- plotnine/offline_variants/14_histogram.py
+++ plotnine/offline_variants/14_histogram.py
@@ -10,8 +10,8 @@
 #      Pavel Tisnovsky
 #
 
+from plotnine import aes, geom_bar, ggplot, stat_bin
 from plotnine.data import mtcars
-from plotnine import ggplot, aes, geom_bar, stat_bin
 
 g = ggplot(mtcars) + aes(x="hp") + stat_bin(bins=12) + geom_bar()
 
