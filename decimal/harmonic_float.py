1328998fa2fb6f3014fbe9933f834a7530e2b2f7
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-04-09 12:22:56 +0200

    Harmonic value: decimal

---
+++ decimal/harmonic_float.py
@@ -0,0 +1,16 @@
+n = 1
+h1 = 0.0
+h2 = 0.0
+
+while True:
+        h2 = h1 + 1.0 / n;
+
+        if n % 1000000 == 0:
+            print(n, h1, h2, h2 - h1)
+
+        if h1 == h2:
+            break
+        h1 = h2
+        n += 1
+
+print(h1, h2, n)
