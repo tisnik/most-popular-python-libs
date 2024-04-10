47937ced4ccbb229ae71957f5b7434d485c2552d
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-04-10 08:03:59 +0200

    Decimal values and traps, example #3

---
+++ decimal/traps_C.py
@@ -0,0 +1,13 @@
+from decimal import Decimal, getcontext, DivisionByZero
+
+c = getcontext()
+c.traps[DivisionByZero] = False
+
+for trap, state in c.traps.items():
+    print(trap, state)
+
+x = Decimal(1)
+y = Decimal(0)
+z = x/y
+
+print(z)
