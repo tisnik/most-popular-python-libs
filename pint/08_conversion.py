959a0e5d8b7e44f7c688ca38866dc4b92fc8c1d6
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-04-15 08:48:41 +0200

    Better units

--- pint/08_conversion.py
+++ pint/08_conversion.py
@@ -2,9 +2,9 @@ from pint import UnitRegistry
 
 ureg = UnitRegistry()
 
-t1 = 1800 * ureg.hour
+t1 = 1800 * ureg.second
 t2 = t1.to(ureg.minute)
-t3 = t1.to(ureg.second)
+t3 = t1.to(ureg.hour)
 
 print(t1, t1.dimensionality)
 print(t2, t2.dimensionality)
