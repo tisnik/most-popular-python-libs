640b9f0ac9f4165aaf4796680c95afcd47648456
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-04-15 08:49:20 +0200

    Reduced units

---
+++ pint/22_reduced_units.py
@@ -0,0 +1,15 @@
+from pint import UnitRegistry
+
+ureg = UnitRegistry()
+
+l = 1.5 * ureg.meter
+s = l * l
+v = s * l
+d = v / (10 * ureg.hectare)
+
+print(l)
+print(s)
+print(v)
+print(d)
+print(d.to_reduced_units())
+print(d.to(ureg.millimeter))
