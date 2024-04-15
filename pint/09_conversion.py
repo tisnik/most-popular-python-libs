0efd82772b16c97dbd00f2635ac47003cd48b3d8
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-04-15 08:49:02 +0200

    Use lightyear as distance unit too

--- pint/09_conversion.py
+++ pint/09_conversion.py
@@ -6,8 +6,10 @@ s1 = 1000 * ureg.meter
 s2 = s1.to(ureg.millimetre)
 s3 = s1.to(ureg.nautical_mile)
 s4 = s1.to(ureg.inch)
+s5 = s1.to(ureg.ly)
 
 print(s1, s1.dimensionality)
 print(s2, s2.dimensionality)
 print(s3, s3.dimensionality)
 print(s4, s4.dimensionality)
+print(s5, s5.dimensionality)
