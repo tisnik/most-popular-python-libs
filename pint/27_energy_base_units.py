17be8589c4c7f19fe4734463f6a058ef0bcee249
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-04-15 08:49:56 +0200

    Energy base units

---
+++ pint/27_energy_base_units.py
@@ -0,0 +1,15 @@
+from pint import UnitRegistry
+
+ureg = UnitRegistry()
+
+f1 = 10 * ureg.newton
+f2 = 20 * ureg.newton
+f3 = f1 + f2
+l = 100 * ureg.meter
+e = f3 * l
+
+print(f1)
+print(f2)
+print(f3)
+print(e.to(ureg.joule))
+print(e.to_base_units())
