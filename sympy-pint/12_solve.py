a4908b857b08c3790f72e791c1c3a2bf094746cf
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-04-15 12:32:03 +0200

    Solving equation, one unit everywhere

---
+++ sympy-pint/12_solve.py
@@ -0,0 +1,16 @@
+from sympy import pprint, solve, Symbol
+from sympy.physics.units import meter, second
+
+a = Symbol("a") * meter
+b = Symbol("b") * meter
+c = Symbol("c") * meter
+
+x = Symbol("x")
+
+f = a * x ** 2 + b * x + c
+
+pprint(f)
+print()
+
+solution = solve(f, x)
+pprint(solution)
