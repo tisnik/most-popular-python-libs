a3b527f219c3b49fe97e77e05fbc80d70ce00f7b
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-04-22 09:46:29 +0200

    Function with type hints

---
+++ astropy/20_type_hints.py
@@ -0,0 +1,10 @@
+from astropy import units as u
+
+def velocity(s: u.Quantity[u.meter], t: u.Quantity[u.second]) -> u.Quantity[u.meter/u.second]:
+    return s/t
+
+s = 100 * u.meter
+t = 20 * u.second
+v = velocity(s, t)
+
+print(v)
