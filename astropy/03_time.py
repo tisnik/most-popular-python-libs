723836ef3e915d09c1f7ac8e01a6754914361514
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-04-22 09:46:28 +0200

    Operations with time units

---
+++ astropy/03_time.py
@@ -0,0 +1,14 @@
+from astropy import units as u
+
+t1 = 10 * u.second
+t2 = 1 * u.minute
+t3 = 1 * u.second
+
+print(t1)
+print(t2)
+print(t1*t1)
+print(1/t1)
+print(t1+t2)
+print(t1-t2)
+print(t2/t1)
+print(t1/t3)
