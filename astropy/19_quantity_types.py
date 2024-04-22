a3dd3537e2ba3afce61aa935b968c54b43897bcc
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-04-22 09:46:29 +0200

    Quantity types are real types (second example)

---
+++ astropy/19_quantity_types.py
@@ -0,0 +1,9 @@
+from astropy import units as u
+
+length = u.Quantity(1, u.meter)
+time = u.Quantity(2, u.second)
+velocity = u.Quantity(1/5, u.meter/u.second)
+
+print(length)
+print(time)
+print(velocity)
