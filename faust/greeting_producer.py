f6b53f52be8ece03ebe771793a7b83d0b3e9f39d
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-04-23 08:48:42 +0200

    Greeting producer (standard one)

---
+++ faust/greeting_producer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from kafka import KafkaProducer
+from time import sleep
+
+
+server = "localhost:9092"
+topic = "greetings"
+
+print("Connecting to Kafka")
+producer = KafkaProducer(
+    bootstrap_servers=[server],
+    value_serializer=lambda x: x.encode("utf-8")
+)
+print("Connected to Kafka")
+
+for i in range(1000):
+    print(i)
+    message = f"Greeting #{i}"
+    producer.send(topic, value=message)
+    sleep(1)
