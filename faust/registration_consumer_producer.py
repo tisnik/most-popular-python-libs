9a4f10e8bc734dd83ea9959cc1c3e1f1ee528873
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-04-23 18:25:54 +0200

    Registration producer + consumer

---
+++ faust/registration_consumer_producer.py
@@ -0,0 +1,42 @@
+import faust
+
+app = faust.App(
+    "registrations",
+    broker="kafka://localhost:9092",
+)
+
+
+class Registration(faust.Record):
+    name: str
+    surname: str
+    id: int
+
+
+registrations_topic = app.topic("registrations", key_type=str, value_type=Registration)
+
+@app.timer(interval=5.0)
+async def example_sender(app):
+    await registrations_topic.send(
+        value=Registration("Eliška", "Najbrtová", 4))
+    await registrations_topic.send(
+        value=Registration("Jenny", "Suk", 3))
+    await registrations_topic.send(
+        value=Registration("Anička", "Šafářová", 0))
+    await registrations_topic.send(
+        value=Registration("Sváťa", "Pulec", 3))
+    await registrations_topic.send(
+        value=Registration("Blažej", "Motyčka", 8))
+    await registrations_topic.send(
+        value=Registration("Eda", "Wasserfall", 0))
+    await registrations_topic.send(
+        value=Registration("Přemysl", "Hájek", 10))
+
+
+@app.agent(registrations_topic)
+async def register(registrations):
+    async for registration in registrations:
+        print(f"Registration: {registration}")
+
+
+if __name__ == "__main__":
+    app.main()
