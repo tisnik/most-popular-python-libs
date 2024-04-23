c5c1b9623421bee07310a436cb7c3a800c64eaae
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-04-23 08:48:42 +0200

    Greeting + worker consumer (based on raw codec)

---
+++ faust/greeting_worker_consumer_raw.py
@@ -0,0 +1,24 @@
+import faust
+
+app = faust.App(
+    "hello-world",
+    broker="kafka://localhost:9092",
+    value_serializer="raw",
+)
+
+greetings_topic = app.topic("greetings")
+real_work_topic = app.topic("real_work")
+
+@app.agent(greetings_topic)
+async def greet(greetings):
+    async for greeting in greetings:
+        print(f"Greeter: {greeting}")
+
+@app.agent(real_work_topic)
+async def worker(works):
+    async for work in works:
+        print(f"Worker: {work}")
+
+
+if __name__ == "__main__":
+    app.main()
