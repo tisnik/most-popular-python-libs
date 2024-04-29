1a409a3e75a291334df6cbc62dc379ad590ffd4d
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-04-29 09:15:53 +0200

    JSON loading into nested structure

---
+++ containers/box_08.py
@@ -0,0 +1,13 @@
+from json import load
+from box import Box
+
+with open("openapi.json") as fin:
+    j = load(fin)
+
+print(j)
+print()
+
+b = Box(j)
+
+print(b["info"]["license"]["name"])
+print(b.info.license.name)
