411eeb2eae31e8d5ba5711193d287eca1fac9e41
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-05-01 09:23:04 +0200

    Reading all values stored under given key

---
+++ containers/multidict_10.py
@@ -0,0 +1,17 @@
+from multidict import MultiDict
+
+d = MultiDict({str(x): x*2 for x in range(10) if x%3 != 0})
+
+for i in range(10):
+    d.add(str(i), "foo")
+    d.add(str(i), "bar")
+
+print(d)
+print()
+
+keys = sorted(d.keys())
+print(keys)
+print()
+
+for key in keys:
+    print(key, d.getall(key))
