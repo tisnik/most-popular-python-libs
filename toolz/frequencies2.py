d6e3a4f29aa5874ff22affd94cdeec1a5f8b38e1
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-05-06 11:39:17 +0200

    Removed unused import

--- toolz/frequencies2.py
+++ toolz/frequencies2.py
@@ -1,6 +1,5 @@
-from random import randrange
 
-from toolz.itertoolz import frequencies, unique
+from toolz.itertoolz import frequencies
 
 values = ("Karel", "Ivan", "Petr", "Pavel", "Jan", "Petr", "Ivan", "Ivan")
 print(frequencies(values))
