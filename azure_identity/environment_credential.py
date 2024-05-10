92e7663271048ef99ee7d7fc7491068a82fb64df
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-05-10 09:56:42 +0200

    Azure identity example #4

---
+++ azure_identity/environment_credential.py
@@ -0,0 +1,30 @@
+"""Example how to use EnvironmentCredential to gain access to Azure OpenAI."""
+
+from azure.identity import EnvironmentCredential
+from openai import AzureOpenAI
+
+# following environment variables should be set
+# AZURE_TENANT_ID: ID of the service principal's tenant. Also called its 'directory' ID.
+# AZURE_CLIENT_ID: the service principal's client ID
+# AZURE_CLIENT_SECRET: one of the service principal's client secrets
+credential = EnvironmentCredential()
+
+token = credential.get_token("https://cognitiveservices.azure.com/.default")
+
+client = AzureOpenAI(
+    api_version="2024-02-15-preview",
+    azure_endpoint="https://ols-test.openai.azure.com/",
+    azure_ad_token = token.token,
+)
+
+response = client.chat.completions.create(
+    model="gpt-35-turbo-0125", # model = "deployment_name".
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "Does Azure OpenAI support customer managed keys?"},
+        {"role": "assistant", "content": "Yes, customer managed keys are supported by Azure OpenAI."},
+        {"role": "user", "content": "Do other Azure AI services support this too?"}
+    ]
+)
+
+print(response.choices[0].message.content)
