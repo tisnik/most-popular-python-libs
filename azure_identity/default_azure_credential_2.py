7881dbdd78c12763ce42765dcdf630a1190c9df1
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-05-10 09:56:39 +0200

    Azure identity example #3

---
+++ azure_identity/default_azure_credential_2.py
@@ -0,0 +1,26 @@
+"""Example how to use DefaultAzureCredential to gain access to Azure OpenAI."""
+
+from azure.identity import DefaultAzureCredential
+from openai import AzureOpenAI
+
+# user needs to be logged in via "az" CLI tool
+token_credential = DefaultAzureCredential()
+token = token_credential.get_token('https://cognitiveservices.azure.com/.default')
+
+client = AzureOpenAI(
+    api_version="2024-02-15-preview",
+    azure_endpoint="https://ols-test.openai.azure.com/",
+    azure_ad_token = token.token,
+)
+
+response = client.chat.completions.create(
+    model="gpt-35-turbo-0125", # model = "deployment_name".
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "Does Azure OpenAI support customer managed keys?"},
+        {"role": "assistant", "content": "Yes, customer managed keys are supported by Azure OpenAI."},
+        {"role": "user", "content": "Do other Azure AI services support this too?"}
+    ]
+)
+
+print(response.choices[0].message.content)
