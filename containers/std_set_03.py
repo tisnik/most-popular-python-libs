1b52162dd0c2eb2cc073c1c2a6a7323b2a7821b3
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-05-13 09:33:56 +0200

    Added license header

--- containers/std_set_03.py
+++ containers/std_set_03.py
@@ -1,3 +1,15 @@
+#
+#  (C) Copyright 2024  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
 engineers = {"John", "Jane", "Jack", "Janice"}
 programmers = {"Jack", "Sam", "Susan", "Janice"}
 managers = {"Jane", "Jack", "Susan", "Zack"}
