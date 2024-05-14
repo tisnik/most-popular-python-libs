afdd65a9d8f3703b2caab4a49f5355f3158766df
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-05-14 10:00:45 +0200

    Added license header

--- containers/std_list_06.py
+++ containers/std_list_06.py
@@ -1,3 +1,15 @@
+#
+#  (C) Copyright 2024  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
 seznam = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 seznam2 = [item * 2 for item in seznam]
