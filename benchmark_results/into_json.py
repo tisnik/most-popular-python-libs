d8d4c7556eaf3d110aada9a5f344315257b71893
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-05-20 09:00:38 +0200

    Benchmark results conversion into JSON file

---
+++ benchmark_results/into_json.py
@@ -0,0 +1,19 @@
+"""Export benchmark results into JSON prepared for further processing."""
+
+import csv
+import json
+
+import input_files
+
+results = {}
+
+for input_file in input_files.input_files:
+    name = input_file.replace(".times", "")
+    with open(input_file) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=' ')
+        results[name] = []
+        for row in csv_reader:
+            results[name].append(row)
+
+with open("results.json", "w") as fout:
+    json.dump(results, fout, indent=4)
