c7eff5f5bc5308dbf302b507ac087de5fa289acc
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-05-20 09:00:30 +0200

    Benchmark results conversion into HTML tables

---
+++ benchmark_results/into_html_tables.py
@@ -0,0 +1,17 @@
+"""Writes benchmark results into HTML tables."""
+
+import csv
+
+import input_files
+
+for input_file in input_files.input_files:
+    output_file = input_file.replace(".times", ".html")
+    print(output_file)
+    with open(input_file) as csv_file:
+        with open(output_file, "w") as html_file:
+            html_file.write("<table>\n")
+            html_file.write(f"<tr><th>Rozlišení</th><th>Čas [s]</th><th>Paměť [kB]</th></tr>\n")
+            csv_reader = csv.reader(csv_file, delimiter=' ')
+            for row in csv_reader:
+                html_file.write(f"<tr><td>{row[0]}&times;{row[0]}</td><td>{row[1]}</td><td>{row[2]}</td></tr>\n")
+            html_file.write("</table>\n")
