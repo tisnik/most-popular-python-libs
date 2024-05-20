d57a6aeb2db5b559b13eb7f12009ac7ee2be673f
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-05-20 08:59:59 +0200

    Input files with benchmark results

---
+++ benchmark_results/input_files.py
@@ -0,0 +1,20 @@
+"""List of files containing benchmark results."""
+
+input_files = (
+    "cython_basic.times",
+    "cython_optim.times",
+    "cython_types.times",
+    "mypyc_no_type_hints.times",
+    "mypyc_with_type_hints.times",
+    "native_optim.times",
+    "native.times",
+    "nuitka.times",
+    "numba2.times",
+    "numba3.times",
+    "numba4.times",
+    "python_3_10.times",
+    "python_3_11.times",
+    "python_3_12.times",
+    "python_3_8.times",
+    "python_3_9.times",
+)
