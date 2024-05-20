6ee21b45b643bfa501dcd0e42a5e190300fd800c
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-05-20 13:45:37 +0200

    Script to plot benchmarks results

--- benchmark_results/benchmark_results_plot.py
+++ benchmark_results/benchmark_results_plot.py
@@ -2,10 +2,6 @@
 # coding: utf-8
 
 # ### Set of input files with benchmark results to be processed
-
-# In[2]:
-
-
 input_files = (
     "native.times",
     "native_optim.times",
@@ -18,19 +14,16 @@ input_files = (
     "mypyc_with_type_hints.times",
     "numba2.times",
     "numba3.times",
-    "numba4.times")
-
-
-# In[3]:
+    "numba4.times",
+    "nuitka.times",
+    "cython_basic.times",
+    "cython_types.times",
+    "cython_optim.times")
 
 
 import pandas as pd
 
 # ### Helper functions to read benchmark results
-
-# In[4]:
-
-
 def read_benchmark_result(filename):
     return pd.read_csv(filename, sep=" ", header=0, names=("size", "time", "memory"))
 
@@ -43,10 +36,6 @@ def read_all_results(input_files):
 
 
 # ### Combine all results into one DataFrame
-
-# In[5]:
-
-
 r = read_all_results(input_files)
 
 results = pd.DataFrame()
@@ -63,85 +52,94 @@ results
 
 
 # ### Set plot size
-
-# In[6]:
-
-
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
 mpl.rcParams['figure.dpi'] = 150
+mpl.rcParams["figure.figsize"] = (12,4.8)
 xticks=df.index
+
+# color palette to be used
+colormap=["#a00000", "#a0a000",                                   # native
+          "#00a000", "#00b030", "#00c060", "#00d090", "#00e0a0",  # Python interpreters
+          "#ff0000", "#ff8000",                                   # mypyc
+          "#0000ff", "#0060ff", "#00c0ff",                        # Numba
+          "#ff8080",                                              # Nuitka
+          "#808080", "#a0a0a0", "#c0c0c0"                         # Cython
+     ]
 
 
 # ### Plot results
 
-# In[12]:
-
-
+# Startup times
 results[0:5].plot(kind='bar', stacked=False, width=0.9, title="Startup time",
-                  color=["#a00000", "#a0a000",
-                         "#00a000", "#00b030", "#00c060", "#00d090", "#00e0a0",
-                         "#ff0000", "#ff8000",
-                         "#0000ff", "#0060ff", "#00c0ff"])
+                  color=colormap)
+plt.legend(loc=(1.04, 0))
+plt.tight_layout()
 plt.savefig("1st.png")
 plt.show()
 
 
-# In[9]:
-
-
+# Computation with some startup time influence
 results[5:10].plot(kind='bar', stacked=False, width=0.9,
                    title="Computation with startup time influence",
-                   color=["#a00000", "#a0a000",
-                         "#00a000", "#00b030", "#00c060", "#00d090", "#00e0a0",
-                         "#ff0000", "#ff8000",
-                         "#0000ff", "#0060ff", "#00c0ff"])
+                   color=colormap)
+plt.legend(loc=(1.04, 0))
+plt.tight_layout()
 plt.savefig("Startup time and computation.png")
 plt.show()
 
 
-# In[10]:
-
-
+# Just the computation, w/o startup time
 results[10:].plot(kind='bar', stacked=False, width=0.9, title="Extensive computation",
-                  color=["#a00000", "#a0a000",
-                         "#00a000", "#00b030", "#00c060", "#00d090", "#00e0a0",
-                         "#ff0000", "#ff8000",
-                         "#0000ff", "#0060ff", "#00c0ff"])
+                   color=colormap)
+plt.tight_layout()
 plt.savefig("Extensive computation.png")
 plt.show()
 
 
-# In[43]:
-
-
+# Approximation, including startup time
 results.plot(title="Approximation, incl. startup time",
-            color=["#a00000", "#a0a000",
-                         "#00c000", "#00c030", "#00c060", "#00c090", "#00c0a0",
-                         "#ff0000", "#ff8000",
-                         "#0000ff", "#0060ff", "#00c0ff"])
+                   color=colormap)
+plt.tight_layout()
 plt.savefig("Approximation computation.png")
 plt.show()
 
 
-# In[102]:
-
-
+# Numba/CPython thresholds
 results[8:12].plot(title="Numba/CPython thresholds", color=["#a00000", "#a0a000",
                          "#00a000", "#00b030", "#00c060", "#00d090", "#00e0a0",
                          "#ff0000", "#ff8000",
                          "#0000ff", "#0060ff", "#00c0ff"])
+plt.tight_layout()
 plt.savefig("Numba CPython thresholds.png")
 plt.show()
 
 
-# In[104]:
-
-
+# Python interpreters only
 #results[0:5].plot(kind='bar', stacked=False, width=0.9, title="Startup time")
 results[11:][["python 3 8", "python 3 9" , "python 3 10", "python 3 11", "python 3 12"]].plot(
-        kind="bar", title="Python interpreters only")
+        kind="bar", title="Python interpreters only", color=colormap[2:])
+plt.tight_layout()
 plt.savefig("Python interpreters only.png")
 plt.show()
 
+
+colormap=["#a00000", "#a0a000",                                   # native
+          "#ff0000", "#ff8000",                                   # mypyc
+          "#8080ff",                                              # Nuitka
+          "#808080", "#a0a0a0", "#c0c0c0"                         # Cython
+     ]
+# Compiled code only
+compiled = results[11:][["native", "native optim", "mypyc no type hints", "mypyc with type hints",
+                    "nuitka", "cython basic", "cython types", "cython optim"]]
+compiled.plot(
+        kind="bar", color=colormap, title="Compiled code only")
+plt.tight_layout()
+plt.savefig("Compiled code only.png")
+plt.show()
+
+compiled.plot(title="Approximation, compiled", color=colormap)
+plt.tight_layout()
+plt.savefig("Approximation computation compiled.png")
+plt.show()
