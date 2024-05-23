3932e9568e1fef11654aa9e538227f3bd31a0b1e
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-05-23 08:21:31 +0200

    Use double quotes, not single ones

--- benchmark_results/into_dict.py
+++ benchmark_results/into_dict.py
@@ -10,7 +10,7 @@ results = {}
 for input_file in input_files.input_files:
     name = input_file.replace(".times", "")
     with open(input_file) as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=' ')
+        csv_reader = csv.reader(csv_file, delimiter=" ")
         results[name] = []
         for row in csv_reader:
             results[name].append(row)
