af69aca61a9d6e98a75e28e4b9161860f75e2210
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-06-01 15:26:01 +0200

    Statement lambda, 1st example

---
+++ coconut/statement-lambda-1.py
@@ -0,0 +1,12 @@
+# Compiled Coconut: -----------------------------------------------------------
+
+message = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"  #1 (line in Coconut source)
+words = message.split()  #2 (line in Coconut source)
+
+def _coconut_lambda_0(word):  #4 (line in Coconut source)
+    return len(word) > 4  #4 (line in Coconut source)
+
+filtered = filter((_coconut_lambda_0), words)  #4 (line in Coconut source)
+
+for word in filtered:  #6 (line in Coconut source)
+    print(word)  #7 (line in Coconut source)
