3a6a5a4110af7b88e7047388e6b172e69f755db8
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-06-01 15:26:01 +0200

    Lambda using to calculate factorial, example #2

---
+++ coconut/lambda-factorial-2.py
@@ -0,0 +1,10 @@
+# Compiled Coconut: -----------------------------------------------------------
+
+@_coconut_tco  #1 (line in Coconut source)
+def factorial(n):  #1 (line in Coconut source)
+    return _coconut_tail_call(reduce, lambda a, b: a * b, range(1, n + 1), 1)  #2 (line in Coconut source)
+
+
+
+for n in range(0, 11):  #5 (line in Coconut source)
+    print(n, factorial(n))  #6 (line in Coconut source)
