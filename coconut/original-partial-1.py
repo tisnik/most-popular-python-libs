9128828fc0380bc080ed5b406bdb07024ffee7fb
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-06-02 09:49:59 +0200

    Partial function with two parameters written in plain Python

---
+++ coconut/original-partial-1.py
@@ -0,0 +1,28 @@
+#
+#  (C) Copyright 2023  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
+from functools import partial
+
+
+def mul(x, y):
+    return x * y
+
+
+print(mul(6, 7))
+
+print()
+
+doubler = partial(mul, 2)
+
+
+for i in range(11):
+    print(i, doubler(i))
