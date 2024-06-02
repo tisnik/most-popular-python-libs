a135895213a92477d0989ab016608b0feb484c00
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-06-02 09:49:59 +0200

    Factorial computation written in plain Python

---
+++ coconut/original-factorial-1.py
@@ -0,0 +1,21 @@
+#
+#  (C) Copyright 2023  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
+from functools import reduce
+
+
+def factorial(n):
+    return reduce(lambda a, b: a*b, range(1, n+1), 1)
+
+
+for n in range(0, 11):
+    print(n, factorial(n))
