ac377ca05a25beee58f492c20930009b50c5c2d8
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-06-03 09:03:18 +0200

    Partial function with four parameters written in plain Python

---
+++ coconut/original-partial-3.py
@@ -0,0 +1,45 @@
+#
+#  (C) Copyright 2023  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
+from functools import partial
+
+
+def mul(x, y, z, w):
+    return x * y * z * w
+
+
+f1 = mul
+print(f1)
+
+f2 = partial(f1, 2)
+print(f2)
+
+f3 = partial(f2, 3)
+print(f3)
+
+f4 = partial(f3, 4)
+print(f4)
+
+f5 = partial(f4, 5)
+print(f5)
+
+f6 = partial(f5, 6)
+print(f6)
+
+print()
+
+print(f1(2, 3, 4, 5))
+print(f2(3, 4, 5))
+print(f3(4, 5))
+print(f4(5))
+print(f5())
+print(f6())
