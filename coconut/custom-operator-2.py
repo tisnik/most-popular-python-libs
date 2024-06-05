63e2b5cc1d9e1f84699da79f877d6fa0c7d7b7aa
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-06-05 08:02:21 +0200

    Custom binary operator defined

---
+++ coconut/custom-operator-2.py
@@ -0,0 +1,9 @@
+# Compiled Coconut: -----------------------------------------------------------
+
+
+def _coconut_op_U2190(x, y):  #3 (line in Coconut source)
+    return x << y  #3 (line in Coconut source)
+
+
+for i in range(0, 10):  #5 (line in Coconut source)
+    print((_coconut_op_U2190)(1, i))  #6 (line in Coconut source)
