06c9703ca99a48aad7b80a6cb0e1961d3b96cd6d
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-06-07 08:22:45 +0200

    Multiplots

--- data-science/Data science s využitím Pythonu.py
+++ data-science/Data science s využitím Pythonu.py
@@ -3248,6 +3248,105 @@ plt.contour(X, Y, Z)
 plt.show()
 
 
+# In[1]:
+
+
+# Lorenz attractor
+
+# import všech potřebných knihoven - Numpy a Matplotlibu
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def lorenz(x, y, z, s=10, r=28, b=2.667):
+    """Výpočet dalšího bodu Lorenzova atraktoru."""
+    x_dot = s * (y - x)
+    y_dot = r * x - y - x * z
+    z_dot = x * y - b * z
+    return x_dot, y_dot, z_dot
+
+
+# krok (změna času)
+dt = 0.01
+
+# celkový počet vypočtených bodů na Lorenzově atraktoru
+n = 10000
+
+# prozatím prázdné pole připravené pro výpočet
+x = np.zeros((n,))
+y = np.zeros((n,))
+z = np.zeros((n,))
+
+# počáteční hodnoty
+x[0], y[0], z[0] = (0.0, 1.0, 1.05)
+
+# vlastní výpočet atraktoru
+for i in range(n - 1):
+    x_dot, y_dot, z_dot = lorenz(x[i], y[i], z[i])
+    x[i + 1] = x[i] + x_dot * dt
+    y[i + 1] = y[i] + y_dot * dt
+    z[i + 1] = z[i] + z_dot * dt
+
+# konstrukce 3D grafu
+fig = plt.figure(figsize=(8, 6))
+ax = fig.add_subplot(projection="3d")
+
+# změna velikosti komponent v grafu.
+plt.tight_layout()
+
+# vykreslení grafu
+ax.plot(x, y, z)
+
+# uložení grafu
+plt.savefig("lorenz_1.png")
+
+# zobrazení grafu
+plt.show()
+
+# grafy s více pohledy na atraktor
+ch_3d = np.stack((x, y, z))
+lim_xyz = [(np.min(ch_3d[ii]), np.max(ch_3d[ii])) for ii in range(3)]
+
+fig2 = plt.figure("3D Coordinates", figsize=(8, 6))
+plt.subplot(2, 2, 1)
+plt.plot(y, x, linewidth=0.75)
+plt.grid()
+plt.xlabel("X")
+plt.ylabel("Y")
+plt.xlim(lim_xyz[1])
+plt.ylim(lim_xyz[0])
+
+plt.subplot(2, 2, 2)
+plt.plot(y, z, linewidth=0.75)
+plt.grid()
+plt.xlabel("Z")
+plt.ylabel("Y")
+plt.xlim(lim_xyz[1])
+plt.ylim(lim_xyz[2])
+
+plt.subplot(2, 2, 3)
+plt.plot(z, x, linewidth=0.75)
+plt.grid()
+plt.xlabel("X")
+plt.ylabel("Z")
+plt.xlim(lim_xyz[2])
+plt.ylim(lim_xyz[0])
+
+ax = fig2.add_subplot(2, 2, 4, projection="3d")
+ax.plot(x, y, z, linewidth=0.7)
+ax.set_xlabel("X")
+ax.set_ylabel("Y")
+ax.set_zlabel("Z")
+
+plt.tight_layout()
+
+# uložení grafu
+plt.savefig("lorenz_2.png")
+
+# zobrazení grafu
+plt.show()
+
+
 # ## Scipy
 
 # ## Pandas
