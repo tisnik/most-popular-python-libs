52ee5eaf6bdcde578634758fe39e463f592b1e9a
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-06-07 08:20:18 +0200

    Backward pipeline, that is None-aware

---
+++ coconut/pipeline-backward-none-aware.py
@@ -0,0 +1,5 @@
+# Compiled Coconut: -----------------------------------------------------------
+
+(lambda _coconut_x: None if _coconut_x is None else (print)(_coconut_x))(None)  #1 (line in Coconut source)
+
+(lambda _coconut_x: None if _coconut_x is None else (print)(_coconut_x))(((lambda _coconut_x: None if _coconut_x is None else (abs)(_coconut_x))(None)))  #3 (line in Coconut source)
