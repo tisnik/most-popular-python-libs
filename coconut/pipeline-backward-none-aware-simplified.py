fb121b288b1cacf75e56221a228dcf4e373b74e9
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-06-09 08:30:31 +0200

    Simplified version of Coconut code transpiled to Python

---
+++ coconut/pipeline-backward-none-aware-simplified.py
@@ -0,0 +1,5 @@
+# Simplified version of Coconut transpiled into Python
+
+(lambda _coconut_x: None if _coconut_x is None else print(_coconut_x))(None)
+
+(lambda _coconut_x: None if _coconut_x is None else print(_coconut_x))(((lambda _coconut_x: None if _coconut_x is None else abs(_coconut_x))(None)))
