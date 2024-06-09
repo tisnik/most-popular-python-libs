9f070b5e0f1f361e0a7d1546fa5b70b1efe57ec3
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-06-09 08:30:31 +0200

    Simplified version of Coconut code transpiled to Python

---
+++ coconut/pipeline-multiple-arguments-2-simplified.py
@@ -0,0 +1,10 @@
+# Simplified version of Coconut transpiled into Python
+
+def swap(x, y):
+    return y, x
+
+
+
+print(*(1, 2))
+print(((_coconut_minus))(*(1, 2)))
+print(((_coconut_minus))(*swap(*(1, 2))))
