cd347c31d3d22115382eef4e3d7571f96fe989ce
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-06-12 08:43:04 +0200

    Operator |> don't support passing multiple arguments

---
+++ coconut/pipeline-multiple-arguments-incorrect-simplified.py
@@ -0,0 +1,13 @@
+# Simplified version of Coconut transpiled into Python
+
+def swap(x, y):
+    return y, x
+
+
+
+def sub(x, y):
+    return x - y
+
+
+
+print(sub(swap((1, 2))))
---
+++ coconut/pipeline-multiple-arguments-incorrect.py
@@ -0,0 +1,13 @@
+# Compiled Coconut: -----------------------------------------------------------
+
+def swap(x, y):  #1 (line in Coconut source)
+    return y, x  #2 (line in Coconut source)
+
+
+
+def sub(x, y):  #5 (line in Coconut source)
+    return x - y  #6 (line in Coconut source)
+
+
+
+(print)((sub)((swap)((1, 2))))  #9 (line in Coconut source)
