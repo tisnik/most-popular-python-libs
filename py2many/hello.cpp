19309c61bed4cd96210d28ce15a630750ffacf6d
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-06-15 09:03:08 +0200

    Standard print() function call

---
+++ py2many/hello.cpp
@@ -0,0 +1,3 @@
+#include <iostream> // NOLINT(build/include_order)
+std::cout << std::string{"Hello, world!"};
+std::cout << std::endl;
---
+++ py2many/hello.go
@@ -0,0 +1,8 @@
+package main
+
+import (
+"fmt")
+
+
+
+fmt.Printf("%v\n","Hello, world!");
