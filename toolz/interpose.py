b04bfdba0f53db62149f8df7d31a52d33b7b32c5
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-06-17 08:44:30 +0200

    Use double quotes, not single ones

--- toolz/interpose.py
+++ toolz/interpose.py
@@ -12,7 +12,7 @@
 
 from toolz.itertoolz import interpose
 
-values = [chr(i) for i in range(ord('A'), ord('Z')+1)]
+values = [chr(i) for i in range(ord("A"), ord("Z")+1)]
 
 print(values)
 print("".join(interpose("*", values)))
