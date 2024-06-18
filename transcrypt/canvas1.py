d8ea57b191d73b74dadf77c4fd101e5772d5069f
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-06-18 08:36:23 +0200

    Use double quotes, not single ones

--- transcrypt/canvas1.py
+++ transcrypt/canvas1.py
@@ -10,12 +10,12 @@
 #      Pavel Tisnovsky
 #
 
-canvas = document.getElementById('canvas')
-context = canvas.getContext('2d')
+canvas = document.getElementById("canvas")
+context = canvas.getContext("2d")
 
-context.font = '60pt Arial'
-context.fillStyle = 'darkblue'
-context.strokeStyle = 'navyblue'
+context.font = "60pt Arial"
+context.fillStyle = "darkblue"
+context.strokeStyle = "navyblue"
 
-context.fillText('Hello Canvas', canvas.width / 2 - 210, canvas.height / 2 + 15)
-context.strokeText('Hello Canvas', canvas.width / 2 - 210, canvas.height / 2 + 15)
+context.fillText("Hello Canvas", canvas.width / 2 - 210, canvas.height / 2 + 15)
+context.strokeText("Hello Canvas", canvas.width / 2 - 210, canvas.height / 2 + 15)
