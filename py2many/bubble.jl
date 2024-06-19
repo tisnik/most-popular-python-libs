6268357b660b26f3a7d838b1f2d0e68f659ff68b
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-06-19 09:46:09 +0200

    Reformatted

--- py2many/bubble.jl
+++ py2many/bubble.jl
@@ -1,12 +1,12 @@
 function bubble_sort{T0}(size::T0)
-a = [randrange(random, 0, 10000) for i in 0:size - 1]
-for i in 0 - 1:-1:size - 1
-for j in 0:i - 1
-if a[j] > a[j + 1]
-a[j], a[j + 1] = (a[j + 1], a[j])
-end
-end
-end
-println(join([a], " "));
+    a = [randrange(random, 0, 10000) for i in 0:size - 1]
+    for i in 0 - 1:-1:size - 1
+    for j in 0:i - 1
+    if a[j] > a[j + 1]
+    a[j], a[j + 1] = (a[j + 1], a[j])
+    end
+    end
+    end
+    println(join([a], " "));
 end
 
