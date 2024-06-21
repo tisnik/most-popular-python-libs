b3f8504815e7e62d603e9f10eb96d5bb68e9262d
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-06-21 08:02:12 +0200

    Added license header

--- ast/sprites.py
+++ ast/sprites.py
@@ -2,6 +2,18 @@
 # vim: set fileencoding=utf-8
 
 # Demonstrační příklady využívající knihovnu Pygame
+#
+#  (C) Copyright 2022  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
 
 # Příklad číslo 22: použití spritů, pohyblivý sprite
 
