a02f643c6eb18d6ceaf0a27869f89588821340fa
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-06-22 09:01:25 +0200

    Pattern matching, example #6

---
+++ coconut/pattern-matching-F.py
@@ -0,0 +1,17 @@
+# Compiled Coconut: -----------------------------------------------------------
+
+def say_hello(name):  #1 (line in Coconut source)
+    _coconut_case_match_to_0 = name  #2 (line in Coconut source)
+    _coconut_case_match_check_0 = False  #2 (line in Coconut source)
+    _coconut_match_set_name_x = _coconut_sentinel  #2 (line in Coconut source)
+    _coconut_match_set_name_x = _coconut_case_match_to_0  #2 (line in Coconut source)
+    _coconut_case_match_check_0 = True  #2 (line in Coconut source)
+    if _coconut_case_match_check_0:  #2 (line in Coconut source)
+        if _coconut_match_set_name_x is not _coconut_sentinel:  #2 (line in Coconut source)
+            x = _coconut_match_set_name_x  #2 (line in Coconut source)
+    if _coconut_case_match_check_0:  #2 (line in Coconut source)
+        print("Hello {_coconut_format_0}!".format(_coconut_format_0=(x)))  #4 (line in Coconut source)
+
+
+
+say_hello("Marvin")  #7 (line in Coconut source)
