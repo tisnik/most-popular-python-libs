120a9228e1052c9899060e6007a182f4b5e9735e
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-06-26 09:00:30 +0200

    Added license header

--- numscrypt/10_ones_2D_array.py
+++ numscrypt/10_ones_2D_array.py
@@ -4,6 +4,18 @@
 #
 # matice se třemi řádky a čtyřmi sloupci
 
+#
+#  (C) Copyright 2024  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
 # import hlavního balíčku knihovny Numpy
 if __envir__.executor_name == __envir__.transpiler_name:
     import numscrypt as np
