62d07bf5d0f76bee3b198509dc443ba133487397
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-06-28 09:00:27 +0200

    String pattern match and | operator

---
+++ coconut/pattern-matching-H-2.py
@@ -0,0 +1,52 @@
+# Compiled Coconut: -----------------------------------------------------------
+
+def say_hello(about_me):  #1 (line in Coconut source)
+    _coconut_case_match_to_0 = about_me  #2 (line in Coconut source)
+    _coconut_case_match_check_0 = False  #2 (line in Coconut source)
+    _coconut_case_match_check_0 = True  #2 (line in Coconut source)
+    if _coconut_case_match_check_0:  #2 (line in Coconut source)
+        _coconut_case_match_check_0 = False  #2 (line in Coconut source)
+        if not _coconut_case_match_check_0:  #2 (line in Coconut source)
+            _coconut_match_set_name_name = _coconut_sentinel  #2 (line in Coconut source)
+            if (_coconut.isinstance(_coconut_case_match_to_0, _coconut.str)) and (_coconut.len(_coconut_case_match_to_0) >= 12) and (_coconut_case_match_to_0.startswith("My name is ")) and (_coconut_case_match_to_0.endswith(".")):  #2 (line in Coconut source)
+                _coconut_match_temp_0 = _coconut_case_match_to_0[11:-1]  #2 (line in Coconut source)
+                _coconut_match_set_name_name = _coconut_match_temp_0  #2 (line in Coconut source)
+                _coconut_case_match_check_0 = True  #2 (line in Coconut source)
+            if _coconut_case_match_check_0:  #2 (line in Coconut source)
+                if _coconut_match_set_name_name is not _coconut_sentinel:  #2 (line in Coconut source)
+                    name = _coconut_match_set_name_name  #2 (line in Coconut source)
+
+        if not _coconut_case_match_check_0:  #2 (line in Coconut source)
+            _coconut_match_set_name_name = _coconut_sentinel  #2 (line in Coconut source)
+            if (_coconut.isinstance(_coconut_case_match_to_0, _coconut.str)) and (_coconut.len(_coconut_case_match_to_0) >= 13) and (_coconut_case_match_to_0.startswith("His name is ")) and (_coconut_case_match_to_0.endswith(".")):  #2 (line in Coconut source)
+                _coconut_match_temp_1 = _coconut_case_match_to_0[12:-1]  #2 (line in Coconut source)
+                _coconut_match_set_name_name = _coconut_match_temp_1  #2 (line in Coconut source)
+                _coconut_case_match_check_0 = True  #2 (line in Coconut source)
+            if _coconut_case_match_check_0:  #2 (line in Coconut source)
+                if _coconut_match_set_name_name is not _coconut_sentinel:  #2 (line in Coconut source)
+                    name = _coconut_match_set_name_name  #2 (line in Coconut source)
+
+        if not _coconut_case_match_check_0:  #2 (line in Coconut source)
+            _coconut_match_set_name_name = _coconut_sentinel  #2 (line in Coconut source)
+            if (_coconut.isinstance(_coconut_case_match_to_0, _coconut.str)) and (_coconut.len(_coconut_case_match_to_0) >= 13) and (_coconut_case_match_to_0.startswith("He is named ")) and (_coconut_case_match_to_0.endswith(".")):  #2 (line in Coconut source)
+                _coconut_match_temp_2 = _coconut_case_match_to_0[12:-1]  #2 (line in Coconut source)
+                _coconut_match_set_name_name = _coconut_match_temp_2  #2 (line in Coconut source)
+                _coconut_case_match_check_0 = True  #2 (line in Coconut source)
+            if _coconut_case_match_check_0:  #2 (line in Coconut source)
+                if _coconut_match_set_name_name is not _coconut_sentinel:  #2 (line in Coconut source)
+                    name = _coconut_match_set_name_name  #2 (line in Coconut source)
+
+
+    if _coconut_case_match_check_0:  #2 (line in Coconut source)
+        print("Hello {_coconut_format_0}!".format(_coconut_format_0=(name)))  #4 (line in Coconut source)
+    if not _coconut_case_match_check_0:  #5 (line in Coconut source)
+        _coconut_case_match_check_0 = True  #5 (line in Coconut source)
+        if _coconut_case_match_check_0:  #5 (line in Coconut source)
+            print("Sorry, I don't know your name")  #6 (line in Coconut source)
+
+
+
+say_hello("My name is Marvin.")  #9 (line in Coconut source)
+say_hello("His name is Marvin.")  #10 (line in Coconut source)
+say_hello("He is named Marvin.")  #11 (line in Coconut source)
+say_hello("Its name is Marvin.")  #12 (line in Coconut source)
