46debfb3a302e1cccc82b4b2dda6daa87b569420
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-07-01 08:09:37 +0200

    Link to source

--- asizeof/asizeof03.py
+++ asizeof/asizeof03.py
@@ -12,6 +12,12 @@
 #  Contributors:
 #      Pavel Tisnovsky
 #
+#
+# link to source:
+# https://tisnik.github.io/most-popular-python-libs/asizeof/asizeof03.html
+# 
+# link to source in literate programming format:
+# https://tisnik.github.io/most-popular-python-libs/asizeof/asizeof03.html
 
 from pympler import asizeof
 
