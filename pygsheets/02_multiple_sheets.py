18978ca2b166f3193ce15dfa817f8ce0e86270fa
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-07-02 08:51:45 +0200

    Accessing multiple sheets

---
+++ pygsheets/02_multiple_sheets.py
@@ -0,0 +1,27 @@
+import pygsheets
+
+# inicializace klienta
+client = pygsheets.authorize(service_file="clanek_secret.json")
+
+# vytvoreni spreadsheetu
+spreadsheet = client.create("Druha tabulka")
+
+# informace nutne pro pristup ke spreadsheetu
+print("ID", spreadsheet.id)
+print("Title", spreadsheet.title)
+print("URL", spreadsheet.url)
+
+# nasdileni s dalsimi uzivateli
+spreadsheet.share("pavel.tisnovsky@gmail.com", role="writer", type="anyone")
+
+# vytvoreni dalsich listu
+spreadsheet.add_worksheet(title="Druhy")
+spreadsheet.add_worksheet(title="Treti")
+
+# pristup k prvnimu listu
+wks = spreadsheet.sheet1
+
+# uprava bunky ve spreadsheetu
+wks.update_value("A1", "Adresa")
+wks.update_value("A2", "www.root.cz")
+
