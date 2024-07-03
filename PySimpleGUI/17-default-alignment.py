f5e4354ce27bb01f813afd72b191e891ac739efa
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-07-03 08:47:15 +0200

    Added link to source + source in literate programming format

--- PySimpleGUI/17-default-alignment.py
+++ PySimpleGUI/17-default-alignment.py
@@ -9,6 +9,12 @@
 #  Contributors:
 #      Pavel Tisnovsky
 #
+#
+# link to source:
+# https://github.com/tisnik/most-popular-python-libs/blob/master/PySimpleGUI/17-default-alignment.py
+# 
+# link to source in literate programming format:
+# https://tisnik.github.io/most-popular-python-libs/PySimpleGUI/17-default-alignment.html
 
 import PySimpleGUI as sg
 
