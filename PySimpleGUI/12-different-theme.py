735bd1444338e65f1fa3258be682433b5ca984b6
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-07-03 08:47:15 +0200

    Added link to source + source in literate programming format

--- PySimpleGUI/12-different-theme.py
+++ PySimpleGUI/12-different-theme.py
@@ -9,6 +9,12 @@
 #  Contributors:
 #      Pavel Tisnovsky
 #
+#
+# link to source:
+# https://github.com/tisnik/most-popular-python-libs/blob/master/PySimpleGUI/12-different-theme.py
+# 
+# link to source in literate programming format:
+# https://tisnik.github.io/most-popular-python-libs/PySimpleGUI/12-different-theme.html
 
 import PySimpleGUI as sg
 
