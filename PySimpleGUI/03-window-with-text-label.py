368c516bd8fefde129682e89ad9f5bf7ff35d2d9
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-07-03 08:47:15 +0200

    Added link to source + source in literate programming format

--- PySimpleGUI/03-window-with-text-label.py
+++ PySimpleGUI/03-window-with-text-label.py
@@ -9,6 +9,12 @@
 #  Contributors:
 #      Pavel Tisnovsky
 #
+#
+# link to source:
+# https://github.com/tisnik/most-popular-python-libs/blob/master/PySimpleGUI/03-window-with-text-label.py
+# 
+# link to source in literate programming format:
+# https://tisnik.github.io/most-popular-python-libs/PySimpleGUI/03-window-with-text-label.html
 
 import PySimpleGUI as sg
 
