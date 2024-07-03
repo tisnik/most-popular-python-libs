5c65ee915f43b94796393286c48f17efe0691663
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-07-03 08:47:14 +0200

    Added link to source + source in literate programming format

--- PySimpleGUI/01-empty-window.py
+++ PySimpleGUI/01-empty-window.py
@@ -9,6 +9,12 @@
 #  Contributors:
 #      Pavel Tisnovsky
 #
+#
+# link to source:
+# https://github.com/tisnik/most-popular-python-libs/blob/master/PySimpleGUI/01-empty-window.py
+# 
+# link to source in literate programming format:
+# https://tisnik.github.io/most-popular-python-libs/PySimpleGUI/01-empty-window.html
 
 import PySimpleGUI as sg
 
