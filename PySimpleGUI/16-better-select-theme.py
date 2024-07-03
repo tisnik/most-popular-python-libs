1194218956b5c1947a52d130db467241620b04c5
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-07-03 08:47:15 +0200

    Added link to source + source in literate programming format

--- PySimpleGUI/16-better-select-theme.py
+++ PySimpleGUI/16-better-select-theme.py
@@ -9,6 +9,12 @@
 #  Contributors:
 #      Pavel Tisnovsky
 #
+#
+# link to source:
+# https://github.com/tisnik/most-popular-python-libs/blob/master/PySimpleGUI/16-better-select-theme.py
+# 
+# link to source in literate programming format:
+# https://tisnik.github.io/most-popular-python-libs/PySimpleGUI/16-better-select-theme.html
 
 import PySimpleGUI as sg
 
