171352c7cf0356871ba19a169fb823dacb321565
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-07-03 08:47:15 +0200

    Added link to source + source in literate programming format

--- PySimpleGUI/10-sizing.py
+++ PySimpleGUI/10-sizing.py
@@ -9,6 +9,12 @@
 #  Contributors:
 #      Pavel Tisnovsky
 #
+#
+# link to source:
+# https://github.com/tisnik/most-popular-python-libs/blob/master/PySimpleGUI/10-sizing.py
+# 
+# link to source in literate programming format:
+# https://tisnik.github.io/most-popular-python-libs/PySimpleGUI/10-sizing.html
 
 import PySimpleGUI as sg
 
