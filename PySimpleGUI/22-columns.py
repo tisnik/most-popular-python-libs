5cbe7552888096a52e697b67788acf011fa33a40
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-07-04 08:28:46 +0200

    Added link to sources and sources in literate programming format.

--- PySimpleGUI/22-columns.py
+++ PySimpleGUI/22-columns.py
@@ -9,6 +9,12 @@
 #  Contributors:
 #      Pavel Tisnovsky
 #
+#
+# link to source:
+# https://github.com/tisnik/most-popular-python-libs/blob/master/PySimpleGUI/22-columns.py
+# 
+# link to source in literate programming format:
+# https://tisnik.github.io/most-popular-python-libs/PySimpleGUI/22-columns.html
 
 import PySimpleGUI as sg
 
