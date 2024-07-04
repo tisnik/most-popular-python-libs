cc848d99137c8d38f6362fdd9b6104dfcf8d1ef2
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-07-04 08:28:46 +0200

    Added link to sources and sources in literate programming format.

--- PySimpleGUI/26-status-bar.py
+++ PySimpleGUI/26-status-bar.py
@@ -9,6 +9,12 @@
 #  Contributors:
 #      Pavel Tisnovsky
 #
+#
+# link to source:
+# https://github.com/tisnik/most-popular-python-libs/blob/master/PySimpleGUI/26-status-bar.py
+# 
+# link to source in literate programming format:
+# https://tisnik.github.io/most-popular-python-libs/PySimpleGUI/26-status-bar.html
 
 import PySimpleGUI as sg
 
