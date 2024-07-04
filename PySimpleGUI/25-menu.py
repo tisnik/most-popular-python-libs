95e08b2a7fee1db0516184f5ca1c920278be9ad0
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-07-04 08:28:46 +0200

    Added link to sources and sources in literate programming format.

--- PySimpleGUI/25-menu.py
+++ PySimpleGUI/25-menu.py
@@ -9,6 +9,12 @@
 #  Contributors:
 #      Pavel Tisnovsky
 #
+#
+# link to source:
+# https://github.com/tisnik/most-popular-python-libs/blob/master/PySimpleGUI/25-menu.py
+# 
+# link to source in literate programming format:
+# https://tisnik.github.io/most-popular-python-libs/PySimpleGUI/25-menu.html
 
 import PySimpleGUI as sg
 
