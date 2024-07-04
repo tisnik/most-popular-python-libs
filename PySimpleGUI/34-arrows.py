336123f693d8f2f4dca1890ed3256f7782aaa62a
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-07-04 08:28:46 +0200

    Added link to sources and sources in literate programming format.

--- PySimpleGUI/34-arrows.py
+++ PySimpleGUI/34-arrows.py
@@ -9,6 +9,12 @@
 #  Contributors:
 #      Pavel Tisnovsky
 #
+#
+# link to source:
+# https://github.com/tisnik/most-popular-python-libs/blob/master/PySimpleGUI/34-arrows.py
+# 
+# link to source in literate programming format:
+# https://tisnik.github.io/most-popular-python-libs/PySimpleGUI/34-arrows.html
 
 import tkinter
 
