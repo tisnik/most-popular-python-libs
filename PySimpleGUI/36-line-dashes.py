52402c9af18f2e936abbd42e24a664b7280d6771
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-07-04 08:28:46 +0200

    Added link to sources and sources in literate programming format.

--- PySimpleGUI/36-line-dashes.py
+++ PySimpleGUI/36-line-dashes.py
@@ -9,6 +9,12 @@
 #  Contributors:
 #      Pavel Tisnovsky
 #
+#
+# link to source:
+# https://github.com/tisnik/most-popular-python-libs/blob/master/PySimpleGUI/36-line-dashes.py
+# 
+# link to source in literate programming format:
+# https://tisnik.github.io/most-popular-python-libs/PySimpleGUI/36-line-dashes.html
 
 
 import PySimpleGUI as sg
