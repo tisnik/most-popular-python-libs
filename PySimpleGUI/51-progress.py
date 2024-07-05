c95fe6f37c5c5b2cd85377eac152582fbe896a0b
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-07-05 19:22:32 +0200

    Added link to sources and sources in literate programming format.

--- PySimpleGUI/51-progress.py
+++ PySimpleGUI/51-progress.py
@@ -9,6 +9,12 @@
 #  Contributors:
 #      Pavel Tisnovsky
 #
+#
+# link to source:
+# https://github.com/tisnik/most-popular-python-libs/blob/master/PySimpleGUI/51-progress.py
+# 
+# link to source in literate programming format:
+# https://tisnik.github.io/most-popular-python-libs/PySimpleGUI/51-progress.html
 
 import PySimpleGUI as sg
 
