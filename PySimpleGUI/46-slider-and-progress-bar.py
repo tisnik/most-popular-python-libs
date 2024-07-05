db10f0badc0ebff488935374a0b11d9c37a65dd8
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-07-05 19:22:32 +0200

    Added link to sources and sources in literate programming format.

--- PySimpleGUI/46-slider-and-progress-bar.py
+++ PySimpleGUI/46-slider-and-progress-bar.py
@@ -9,6 +9,12 @@
 #  Contributors:
 #      Pavel Tisnovsky
 #
+#
+# link to source:
+# https://github.com/tisnik/most-popular-python-libs/blob/master/PySimpleGUI/46-slider-and-progress-bar.py
+# 
+# link to source in literate programming format:
+# https://tisnik.github.io/most-popular-python-libs/PySimpleGUI/46-slider-and-progress-bar.html
 
 import PySimpleGUI as sg
 
