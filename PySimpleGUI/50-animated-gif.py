3f62b79cb9ca210edc5899133f098ce7a88f845f
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-07-05 19:22:32 +0200

    Added link to sources and sources in literate programming format.

--- PySimpleGUI/50-animated-gif.py
+++ PySimpleGUI/50-animated-gif.py
@@ -9,6 +9,12 @@
 #  Contributors:
 #      Pavel Tisnovsky
 #
+#
+# link to source:
+# https://github.com/tisnik/most-popular-python-libs/blob/master/PySimpleGUI/50-animated-gif.py
+# 
+# link to source in literate programming format:
+# https://tisnik.github.io/most-popular-python-libs/PySimpleGUI/50-animated-gif.html
 
 import PySimpleGUI as sg
 
