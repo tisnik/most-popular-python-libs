fa21d9af35c88572560765b7aefdf84688e3d364
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-07-05 19:22:32 +0200

    Added link to sources and sources in literate programming format.

--- PySimpleGUI/40-named-spinbox.py
+++ PySimpleGUI/40-named-spinbox.py
@@ -9,6 +9,12 @@
 #  Contributors:
 #      Pavel Tisnovsky
 #
+#
+# link to source:
+# https://github.com/tisnik/most-popular-python-libs/blob/master/PySimpleGUI/40-named-spinbox.py
+# 
+# link to source in literate programming format:
+# https://tisnik.github.io/most-popular-python-libs/PySimpleGUI/40-named-spinbox.html
 
 import PySimpleGUI as sg
 
