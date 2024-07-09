04ec65e65778867ca8650917f716f6eafb8f2d6f
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-07-09 08:37:51 +0200

    Added license header

--- sympy-pint/14_solve.py
+++ sympy-pint/14_solve.py
@@ -1,3 +1,15 @@
+#
+#  (C) Copyright 2024  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
 from pint import UnitRegistry
 from sympy import init_printing, pprint, solve, solveset, symbols
 from sympy.physics.units import meter
