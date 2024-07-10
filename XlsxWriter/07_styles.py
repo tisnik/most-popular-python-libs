444be42eed00753b717db7c20a7f57346a7b521b
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-07-10 09:53:05 +0200

    Link to sources

--- XlsxWriter/07_styles.py
+++ XlsxWriter/07_styles.py
@@ -12,6 +12,12 @@
 #  Contributors:
 #      Pavel Tisnovsky
 #
+#
+# link to source:
+# https://github.com/tisnik/most-popular-python-libs/blob/master/PySimpleGUI/07_styles.py
+# 
+# link to source in literate programming format:
+# https://tisnik.github.io/most-popular-python-libs/PySimpleGUI/07_styles.html
 
 """Vytvoření sešitu s delší tabulkou, nastavení stylů vybraných buněk."""
 
