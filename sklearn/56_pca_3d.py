35928af0c148375d0ee7ec7e66c6098b28b4df4b
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-07-10 09:51:18 +0200

    3D PCA and Iris dataset

---
+++ sklearn/56_pca_3d.py
@@ -0,0 +1,26 @@
+from sklearn.datasets import load_iris
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+
+iris = load_iris()
+
+data = iris.data
+
+reduced = PCA(n_components=3).fit_transform(data)
+
+fig = plt.figure(1)
+ax = fig.add_subplot(111, projection="3d", elev=-150, azim=110)
+
+reduced = PCA(n_components=3).fit_transform(iris.data)
+ax.scatter(
+    reduced[:, 0],
+    reduced[:, 1],
+    reduced[:, 2],
+    c=iris.target,
+    s=40,
+)
+
+plt.tight_layout()
+plt.savefig("56.png")
+plt.show()
+
