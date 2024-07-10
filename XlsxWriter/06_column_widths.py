c58a17517c5bfa694137f1dbdbdfc602a32b0cee
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-07-10 09:53:02 +0200

    Link to sources

--- XlsxWriter/06_column_widths.py
+++ XlsxWriter/06_column_widths.py
@@ -12,6 +12,12 @@
 #  Contributors:
 #      Pavel Tisnovsky
 #
+#
+# link to source:
+# https://github.com/tisnik/most-popular-python-libs/blob/master/PySimpleGUI/06_column_widths.py
+# 
+# link to source in literate programming format:
+# https://tisnik.github.io/most-popular-python-libs/PySimpleGUI/06_column_widths.html
 
 """Vytvoření sešitu s delší tabulkou, nastavení šířky sloupců."""
 
