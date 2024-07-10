7b27ca5eace7eb043022c7050be8a20136b74a48
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-07-10 09:51:18 +0200

    Scatter plot of Iris dataset

---
+++ sklearn/51_iris_scatter_plot_2.py
@@ -0,0 +1,15 @@
+from sklearn.datasets import load_iris
+import matplotlib.pyplot as plt
+
+iris = load_iris()
+
+data = iris.data
+
+_, ax = plt.subplots()
+scatter = ax.scatter(data[:, 0], data[:, 1], c=iris.target)
+ax.set(xlabel=iris.feature_names[0], ylabel=iris.feature_names[1])
+ax.legend(scatter.legend_elements()[0], iris.target_names, loc="upper right", title="Classes")
+
+plt.show()
+
+
