ae3cb811c969a63289213bdb1f2ab7186ada1711
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-07-11 08:48:09 +0200

    Links to source

--- XlsxWriter/10_format_for_row.py
+++ XlsxWriter/10_format_for_row.py
@@ -12,6 +12,12 @@
 #  Contributors:
 #      Pavel Tisnovsky
 #
+#
+# link to source:
+# https://github.com/tisnik/most-popular-python-libs/blob/master/PySimpleGUI/10_format_for_row.py
+# 
+# link to source in literate programming format:
+# https://tisnik.github.io/most-popular-python-libs/PySimpleGUI/10_format_for_row.html
 
 """Vytvoření sešitu s delší tabulkou, nastavení stylu řádků."""
 
