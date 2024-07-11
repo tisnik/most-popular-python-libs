b0b2e434069ceec37d1f4adc7d3c3545de7e3b35
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-07-11 08:48:12 +0200

    Links to source

--- XlsxWriter/11_format_set_column.py
+++ XlsxWriter/11_format_set_column.py
@@ -12,6 +12,12 @@
 #  Contributors:
 #      Pavel Tisnovsky
 #
+#
+# link to source:
+# https://github.com/tisnik/most-popular-python-libs/blob/master/PySimpleGUI/11_format_set_column.py
+# 
+# link to source in literate programming format:
+# https://tisnik.github.io/most-popular-python-libs/PySimpleGUI/11_format_set_column.html
 
 """Vytvoření sešitu s delší tabulkou, nastavení stylu sloupců."""
 
