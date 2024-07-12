b6549ca12669854465b91afed253fede7cbf4dc5
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-07-12 08:43:14 +0200

    Added links to source

--- XlsxWriter/16_font_size.py
+++ XlsxWriter/16_font_size.py
@@ -12,6 +12,12 @@
 #  Contributors:
 #      Pavel Tisnovsky
 #
+#
+# link to source:
+# https://github.com/tisnik/most-popular-python-libs/blob/master/PySimpleGUI/16_font_size.py
+# 
+# link to source in literate programming format:
+# https://tisnik.github.io/most-popular-python-libs/PySimpleGUI/16_font_size.html
 
 """Vytvoření sešitu s delší tabulkou, změna velikosti fontu."""
 
