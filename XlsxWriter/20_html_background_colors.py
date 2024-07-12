769b1b06da4416d884d631a60f6ce38061ad140a
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-07-12 08:43:14 +0200

    Added links to source

--- XlsxWriter/20_html_background_colors.py
+++ XlsxWriter/20_html_background_colors.py
@@ -12,6 +12,12 @@
 #  Contributors:
 #      Pavel Tisnovsky
 #
+#
+# link to source:
+# https://github.com/tisnik/most-popular-python-libs/blob/master/PySimpleGUI/20_html_background_colors.py
+# 
+# link to source in literate programming format:
+# https://tisnik.github.io/most-popular-python-libs/PySimpleGUI/20_html_background_colors.html
 
 """Vytvoření sešitu s delší tabulkou, nastavení barvy pozadí."""
 
