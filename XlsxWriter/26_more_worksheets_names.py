76d2b71a1bf7fda87d7bd32e10192d1f5eb3e13b
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-07-12 08:43:14 +0200

    Added links to source

--- XlsxWriter/26_more_worksheets_names.py
+++ XlsxWriter/26_more_worksheets_names.py
@@ -12,6 +12,12 @@
 #  Contributors:
 #      Pavel Tisnovsky
 #
+#
+# link to source:
+# https://github.com/tisnik/most-popular-python-libs/blob/master/PySimpleGUI/26_more_worksheets_names.py
+# 
+# link to source in literate programming format:
+# https://tisnik.github.io/most-popular-python-libs/PySimpleGUI/26_more_worksheets_names.html
 
 """Vytvoření sešitu s několika listy."""
 
