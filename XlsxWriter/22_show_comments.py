8589eea5b3a00f1dc6e99cc672db646689b52ec1
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-07-12 08:43:14 +0200

    Added links to source

--- XlsxWriter/22_show_comments.py
+++ XlsxWriter/22_show_comments.py
@@ -12,6 +12,12 @@
 #  Contributors:
 #      Pavel Tisnovsky
 #
+#
+# link to source:
+# https://github.com/tisnik/most-popular-python-libs/blob/master/PySimpleGUI/22_show_comments.py
+# 
+# link to source in literate programming format:
+# https://tisnik.github.io/most-popular-python-libs/PySimpleGUI/22_show_comments.html
 
 """Tabulka s komentáři přirazenými k vybraným buňkám."""
 
