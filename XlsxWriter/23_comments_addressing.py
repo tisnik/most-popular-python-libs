537a7bccd9037d91d81a9740532439f012162bb4
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-07-12 08:43:14 +0200

    Added links to source

--- XlsxWriter/23_comments_addressing.py
+++ XlsxWriter/23_comments_addressing.py
@@ -12,6 +12,12 @@
 #  Contributors:
 #      Pavel Tisnovsky
 #
+#
+# link to source:
+# https://github.com/tisnik/most-popular-python-libs/blob/master/PySimpleGUI/23_comments_addressing.py
+# 
+# link to source in literate programming format:
+# https://tisnik.github.io/most-popular-python-libs/PySimpleGUI/23_comments_addressing.html
 
 """Tabulka s komentáři přiřazenými k vybraným buňkám."""
 
