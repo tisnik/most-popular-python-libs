7887b9e402818f55f082ba59179128e878a7d0e7
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-07-12 08:43:14 +0200

    Added links to source

--- XlsxWriter/19_named_bg_colors.py
+++ XlsxWriter/19_named_bg_colors.py
@@ -12,6 +12,12 @@
 #  Contributors:
 #      Pavel Tisnovsky
 #
+#
+# link to source:
+# https://github.com/tisnik/most-popular-python-libs/blob/master/PySimpleGUI/19_named_bg_colors.py
+# 
+# link to source in literate programming format:
+# https://tisnik.github.io/most-popular-python-libs/PySimpleGUI/19_named_bg_colors.html
 
 """Vytvoření sešitu s delší tabulkou, nastavení barvy pozadí."""
 
