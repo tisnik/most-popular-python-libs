b5aff943a60684624aa0814c24e5b6269943c7ee
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-07-15 09:40:15 +0200

    Added links to source

--- XlsxWriter/38_autofilters_data.py
+++ XlsxWriter/38_autofilters_data.py
@@ -12,6 +12,12 @@
 #  Contributors:
 #      Pavel Tisnovsky
 #
+#
+# link to source:
+# https://github.com/tisnik/most-popular-python-libs/blob/master/PySimpleGUI/38_autofilters_data.py
+# 
+# link to source in literate programming format:
+# https://tisnik.github.io/most-popular-python-libs/PySimpleGUI/38_autofilters_data.html
 
 """Automatické filtry."""
 
