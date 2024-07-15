7e5ebe7d3e1997733e8c221254895803520798b3
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-07-15 09:40:15 +0200

    Added links to source

--- XlsxWriter/35_conditional_formatting_average.py
+++ XlsxWriter/35_conditional_formatting_average.py
@@ -12,6 +12,12 @@
 #  Contributors:
 #      Pavel Tisnovsky
 #
+#
+# link to source:
+# https://github.com/tisnik/most-popular-python-libs/blob/master/PySimpleGUI/35_conditional_formatting_average.py
+# 
+# link to source in literate programming format:
+# https://tisnik.github.io/most-popular-python-libs/PySimpleGUI/35_conditional_formatting_average.html
 
 """Podmíněné formátování, hodnoty pod průměrem."""
 
