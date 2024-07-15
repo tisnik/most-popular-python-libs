ef5e224172b03e7094981aa39d81f10c0a255a82
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-07-15 09:40:15 +0200

    Added links to source

--- XlsxWriter/41_validator.py
+++ XlsxWriter/41_validator.py
@@ -12,6 +12,12 @@
 #  Contributors:
 #      Pavel Tisnovsky
 #
+#
+# link to source:
+# https://github.com/tisnik/most-popular-python-libs/blob/master/PySimpleGUI/41_validator.py
+# 
+# link to source in literate programming format:
+# https://tisnik.github.io/most-popular-python-libs/PySimpleGUI/41_validator.html
 
 """Jednoduchý validátor."""
 
