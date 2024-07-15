eee0ca5c9fc22d76732611ad515af94f0556c8ff
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-07-15 09:40:15 +0200

    Added links to source

--- XlsxWriter/34_conditional_formatting_not_between.py
+++ XlsxWriter/34_conditional_formatting_not_between.py
@@ -12,6 +12,12 @@
 #  Contributors:
 #      Pavel Tisnovsky
 #
+#
+# link to source:
+# https://github.com/tisnik/most-popular-python-libs/blob/master/PySimpleGUI/34_conditional_formatting_not_between.py
+# 
+# link to source in literate programming format:
+# https://tisnik.github.io/most-popular-python-libs/PySimpleGUI/34_conditional_formatting_not_between.html
 
 """Podmíněné formátování, hodnoty mimo zadané meze."""
 
