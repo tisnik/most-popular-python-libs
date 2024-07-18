c3f71d490e386e096e7e1fc51d5e9509812c6ef1
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-07-18 09:45:57 +0200

    Simpler range loop w/o start

--- py2many/bubble.py
+++ py2many/bubble.py
@@ -2,7 +2,7 @@ def bubble_sort(size):
     a = [random.randrange(0, 10000) for i in range(size)]
 
     for i in range(size - 1, 0, -1):
-        for j in range(0, i):
+        for j in range(i):
             if a[j] > a[j + 1]:
                 a[j], a[j + 1] = a[j + 1], a[j]
 
