e86968ac0f2e1cc16a0e48bcd3ed602621714257
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-07-22 10:01:03 +0200

    Added comments

--- sklearn/46_iris_dataset.py
+++ sklearn/46_iris_dataset.py
@@ -1,5 +1,13 @@
 from sklearn.datasets import load_iris
 
+# nacteni datove sady
 iris = load_iris()
 
+# jakeho typu je vlastne datova sada?
+print(type(iris))
+
+print("-" * 100)
+
+# dostupne atributy a metody
 print(dir(iris))
+
