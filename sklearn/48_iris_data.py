721ebda3efd5c74a13783e3e8d86713c05775692
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-07-23 10:24:13 +0200

    Added comments

--- sklearn/48_iris_data.py
+++ sklearn/48_iris_data.py
@@ -1,15 +1,24 @@
 from sklearn.datasets import load_iris
 
+# nacteni datove sady
 iris = load_iris()
 
 print(dir(iris))
 
 print("-" * 100)
 
-print("Feature names:")
-print(iris["feature_names"])
-
-print("-" * 100)
+# precteni dat z datove sady
+# urcenych pro trenink, validaci atd.
+data = iris["data"]
 
 print("Feature data:")
-print(iris["data"])
+print(data)
+print("-" * 100)
+
+# typ a "tvar" n-dimenzionalniho pole
+print("Data type:")
+print(type(data))
+print()
+
+print("Data shape:")
+print(data.shape)
