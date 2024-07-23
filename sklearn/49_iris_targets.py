44a27500ff199fd09250669f06e0a2541223f4f5
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-07-23 10:24:16 +0200

    Added comments

--- sklearn/49_iris_targets.py
+++ sklearn/49_iris_targets.py
@@ -1,16 +1,26 @@
 from sklearn.datasets import load_iris
 
+# nacteni datove sady
 iris = load_iris()
 
 print(dir(iris))
 
 print("-" * 100)
 
+# jmena atributu
+print("Feature names:")
+print(iris["feature_names"])
+
+print("-" * 100)
+
+# vazba mezi numerickou hodnotou a lidskym vyjadrenim hodnoty
+# atributu
 print("Target names:")
 print(iris["target_names"])
 
 print("-" * 100)
 
+# druhy rostlin z datove sady v numericke podobe
 print("Targets:")
 print(iris["target"])
 
