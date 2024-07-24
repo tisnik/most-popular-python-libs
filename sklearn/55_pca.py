a8c1c89c3ab2de22d698920594346b35bbf318bc
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-07-24 17:36:48 +0200

    Added comments

--- sklearn/55_pca.py
+++ sklearn/55_pca.py
@@ -3,29 +3,43 @@ import matplotlib.pyplot as plt
 from sklearn.datasets import load_iris
 from sklearn.decomposition import PCA
 
+# nacteni datove sady
 iris = load_iris()
 
+# precteni dat z datove sady
+# urcenych pro trenink, validaci atd.
 data = iris.data
 
+# Principal component analysis
 reduced = PCA(n_components=3).fit_transform(data)
 
+# vykresleni vysledku do matice s diagramy
 fig, axes = plt.subplots(nrows=2, ncols=2)
 fig.set_figheight(15)
 fig.set_figwidth(15)
 
+# graf v matici
 ax = axes[0][0]
 scatter = ax.scatter(reduced[:, 0], reduced[:, 1], c=iris.target)
 
+# graf v matici
 ax = axes[1][0]
 scatter = ax.scatter(reduced[:, 0], reduced[:, 2], c=iris.target)
 
+# graf v matici
 ax = axes[1][1]
 scatter = ax.scatter(reduced[:, 1], reduced[:, 2], c=iris.target)
 
+# prazdne misto v matici
 ax = axes[0][1]
 fig.delaxes(ax)
 
+# zbavit se prazdneho mista okolo bunek mrizky
 plt.tight_layout()
+
+# ulozeni diagramu do souboru
 plt.savefig("55.png")
+
+# zobrazeni diagramu
 plt.show()
 
