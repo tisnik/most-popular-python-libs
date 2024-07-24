6200cc229713405a39facc9eb3fe221445ef2f0f
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-07-24 17:36:48 +0200

    Changing parameters for classification

---
+++ sklearn/61_changed_model_parameters.py
@@ -0,0 +1,42 @@
+from sklearn.datasets import load_iris
+from sklearn.neighbors import KNeighborsClassifier
+
+# nacteni datove sady
+iris = load_iris()
+
+# X je matice (feature matrix)
+X = iris.data
+
+# y je vektor (response vector)
+y = iris.target
+
+# konstrukce klasifikatoru
+# (s hyperparametrem)
+classifier = KNeighborsClassifier(n_neighbors=5)
+
+# trening modelu (se vsemi dostupnymi daty)
+classifier.fit(X, y)
+
+# parametry jedne rostliny
+unknown = [[3, 5, 4, 2]]
+print(unknown)
+
+# predikce modelu pro jednu sadu dat
+prediction = classifier.predict(unknown)
+print(prediction)
+
+# model predikuje hodnoty 0-3, ty si prevedeme na druhy rostlin
+print(iris.target_names[prediction])
+
+print()
+
+# parametry vice rostlin
+unknown = [[3, 5, 4, 2], [5, 4, 3, 2]]
+print(unknown)
+
+# predikce modelu pro vice sad dat
+predictions = classifier.predict(unknown)
+print(predictions)
+
+# model predikuje hodnoty 0-3, ty si prevedeme na druhy rostlin
+print(iris.target_names[predictions])
