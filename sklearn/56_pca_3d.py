976856ac1b7aab8eaedd44a830417e47cd581691
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-07-24 17:36:48 +0200

    Added comments

--- sklearn/56_pca_3d.py
+++ sklearn/56_pca_3d.py
@@ -3,16 +3,20 @@ import matplotlib.pyplot as plt
 from sklearn.datasets import load_iris
 from sklearn.decomposition import PCA
 
+# nacteni datove sady
 iris = load_iris()
 
+# precteni dat z datove sady
+# urcenych pro trenink, validaci atd.
 data = iris.data
 
+# Principal component analysis
 reduced = PCA(n_components=3).fit_transform(data)
 
+# vykresleni korelacniho grafu ve 3D
 fig = plt.figure(1)
 ax = fig.add_subplot(111, projection="3d", elev=-150, azim=110)
 
-reduced = PCA(n_components=3).fit_transform(iris.data)
 ax.scatter(
     reduced[:, 0],
     reduced[:, 1],
@@ -21,7 +25,12 @@ ax.scatter(
     s=40,
 )
 
-plt.tight_layout()
-plt.savefig("56.png")
-plt.show()
 
+# zbavit se prazdneho mista okolo bunek mrizky
+plt.tight_layout()
+
+# ulozeni grafu do souboru
+plt.savefig("56.png")
+
+# zobrazeni grafu
+plt.show()
