67205dcf1b500de9dedd4d9178471444a278a366
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-07-24 17:36:48 +0200

    Added comments

--- sklearn/50_iris_scatter_plot_1.py
+++ sklearn/50_iris_scatter_plot_1.py
@@ -2,12 +2,23 @@ import matplotlib.pyplot as plt
 
 from sklearn.datasets import load_iris
 
+# nacteni datove sady
 iris = load_iris()
 
+# precteni dat z datove sady
+# urcenych pro trenink, validaci atd.
 data = iris.data
 
+# vykresleni korelacniho diagramu pro dvojici vybranych atributu
 plt.scatter(data[:, 0], data[:, 1], c=iris.target)
 plt.title("Classes")
+
+# popisky os
 plt.xlabel(iris.feature_names[0])
 plt.ylabel(iris.feature_names[1])
+
+# ulozeni diagramu do souboru
+plt.savefig("50.png")
+
+# zobrazeni diagramu
 plt.show()
