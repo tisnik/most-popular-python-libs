7335d6c198aeabb591b03b5340bd95e9b1627461
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-07-24 17:36:48 +0200

    Added comments

--- sklearn/54_iris_histograms.py
+++ sklearn/54_iris_histograms.py
@@ -2,21 +2,32 @@ import matplotlib.pyplot as plt
 
 from sklearn.datasets import load_iris
 
+# nacteni datove sady
 iris = load_iris()
 
+# precteni dat z datove sady
+# urcenych pro trenink, validaci atd.
 data = iris.data
 
+# vykresleni sady histogramu do mrizky
 fig, axes = plt.subplots(nrows=2, ncols=2)
 fig.set_figheight(15)
 fig.set_figwidth(15)
 
+# vykresleni jednotlivych histogramu do mrizky
 for i in range(4):
     ax = axes[i//2][i%2]
+    # modifikace zpusobu vypoctu a zobrazeni histogrami
     ax.hist(data[:, i], bins=50, histtype="step")
     ax.set(xlabel=iris.feature_names[i])
 
 
+# zbavit se prazdneho mista okolo bunek mrizky
 plt.tight_layout()
-plt.savefig("53.png")
+
+# ulozeni diagramu do souboru
+plt.savefig("54.png")
+
+# zobrazeni diagramu
 plt.show()
 
