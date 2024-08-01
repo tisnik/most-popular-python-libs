bd7aa5d6bff45985e0d282cacd20590f650d727a
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-08-01 09:34:36 +0200

    Removed trailing whitespaces

--- PySimpleGUI/08-events-cancel.py
+++ PySimpleGUI/08-events-cancel.py
@@ -12,7 +12,7 @@
 #
 # link to source:
 # https://github.com/tisnik/most-popular-python-libs/blob/master/PySimpleGUI/08-events-cancel.py
-# 
+#
 # link to source in literate programming format:
 # https://tisnik.github.io/most-popular-python-libs/PySimpleGUI/08-events-cancel.html
 
