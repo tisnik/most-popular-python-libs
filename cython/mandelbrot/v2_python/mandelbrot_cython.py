a71bb429fb03fd751f1693f03188be2ecc7a02ed
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-08-01 09:32:23 +0200

    Simpler range loop

--- cython/mandelbrot/v2_python/mandelbrot_cython.py
+++ cython/mandelbrot/v2_python/mandelbrot_cython.py
@@ -90,9 +90,9 @@ def calc_mandelbrot(width: cython.int, height: cython.int, maxiter: cython.int, 
     print("255")
 
     cy = -1.5
-    for y in range(0, height):
+    for y in range(height):
         cx = -2.0
-        for x in range(0, width):
+        for x in range(width):
             zx = 0.0
             zy = 0.0
             i = 0
