c9146447e7a1e002e6d69ff462af93516914c272
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-08-01 09:34:36 +0200

    Removed trailing whitespaces

--- PySimpleGUI/01-empty-window.py
+++ PySimpleGUI/01-empty-window.py
@@ -12,7 +12,7 @@
 #
 # link to source:
 # https://github.com/tisnik/most-popular-python-libs/blob/master/PySimpleGUI/01-empty-window.py
-# 
+#
 # link to source in literate programming format:
 # https://tisnik.github.io/most-popular-python-libs/PySimpleGUI/01-empty-window.html
 
