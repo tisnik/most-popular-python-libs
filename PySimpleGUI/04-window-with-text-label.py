bc58c6f5a453282b8c0737a4181b9bd4cfbf6e06
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-08-01 09:34:36 +0200

    Removed trailing whitespaces

--- PySimpleGUI/04-window-with-text-label.py
+++ PySimpleGUI/04-window-with-text-label.py
@@ -12,7 +12,7 @@
 #
 # link to source:
 # https://github.com/tisnik/most-popular-python-libs/blob/master/PySimpleGUI/04-window-with-text-label.py
-# 
+#
 # link to source in literate programming format:
 # https://tisnik.github.io/most-popular-python-libs/PySimpleGUI/04-window-with-text-label.html
 
