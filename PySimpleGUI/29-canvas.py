7b617c5b8351858bb6e88cf03bcc1902dd9dfb2f
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-08-02 09:24:26 +0200

    Removed trailing whitespaces

--- PySimpleGUI/29-canvas.py
+++ PySimpleGUI/29-canvas.py
@@ -12,7 +12,7 @@
 #
 # link to source:
 # https://github.com/tisnik/most-popular-python-libs/blob/master/PySimpleGUI/29-canvas.py
-# 
+#
 # link to source in literate programming format:
 # https://tisnik.github.io/most-popular-python-libs/PySimpleGUI/29-canvas.html
 
