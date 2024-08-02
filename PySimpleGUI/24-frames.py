aa524d9f9ede0c39d70c99800475e0c217475835
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-08-02 09:24:26 +0200

    Removed trailing whitespaces

--- PySimpleGUI/24-frames.py
+++ PySimpleGUI/24-frames.py
@@ -12,7 +12,7 @@
 #
 # link to source:
 # https://github.com/tisnik/most-popular-python-libs/blob/master/PySimpleGUI/24-frames.py
-# 
+#
 # link to source in literate programming format:
 # https://tisnik.github.io/most-popular-python-libs/PySimpleGUI/24-frames.html
 
