afdf1122b44ec0a7036c8003647bec1215b72729
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-08-02 09:24:26 +0200

    Removed trailing whitespaces

--- PySimpleGUI/26-status-bar.py
+++ PySimpleGUI/26-status-bar.py
@@ -12,7 +12,7 @@
 #
 # link to source:
 # https://github.com/tisnik/most-popular-python-libs/blob/master/PySimpleGUI/26-status-bar.py
-# 
+#
 # link to source in literate programming format:
 # https://tisnik.github.io/most-popular-python-libs/PySimpleGUI/26-status-bar.html
 
