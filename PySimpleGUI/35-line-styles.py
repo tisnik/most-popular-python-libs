fe58ae8c775a030ebb19770b897ff5100dd680d6
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-08-02 09:24:26 +0200

    Removed trailing whitespaces

--- PySimpleGUI/35-line-styles.py
+++ PySimpleGUI/35-line-styles.py
@@ -12,7 +12,7 @@
 #
 # link to source:
 # https://github.com/tisnik/most-popular-python-libs/blob/master/PySimpleGUI/35-line-styles.py
-# 
+#
 # link to source in literate programming format:
 # https://tisnik.github.io/most-popular-python-libs/PySimpleGUI/35-line-styles.html
 
