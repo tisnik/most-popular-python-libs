bd0cd7e2b1628c8c3094e0fd5e48f4dc0ad93c41
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-08-02 09:24:26 +0200

    Removed trailing whitespaces

--- PySimpleGUI/36-line-dashes.py
+++ PySimpleGUI/36-line-dashes.py
@@ -12,7 +12,7 @@
 #
 # link to source:
 # https://github.com/tisnik/most-popular-python-libs/blob/master/PySimpleGUI/36-line-dashes.py
-# 
+#
 # link to source in literate programming format:
 # https://tisnik.github.io/most-popular-python-libs/PySimpleGUI/36-line-dashes.html
 
