1a6b76380b9b8492950d04b3e341152020917615
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-08-02 09:24:26 +0200

    Removed trailing whitespaces

--- PySimpleGUI/30-canvas-size-background.py
+++ PySimpleGUI/30-canvas-size-background.py
@@ -12,7 +12,7 @@
 #
 # link to source:
 # https://github.com/tisnik/most-popular-python-libs/blob/master/PySimpleGUI/30-canvas-size-background.py
-# 
+#
 # link to source in literate programming format:
 # https://tisnik.github.io/most-popular-python-libs/PySimpleGUI/30-canvas-size-background.html
 
