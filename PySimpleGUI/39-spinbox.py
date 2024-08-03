97c3984534fa1336e91b50cb150e696611e7d704
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-08-03 09:45:02 +0200

    Removed trailing whitespaces

--- PySimpleGUI/39-spinbox.py
+++ PySimpleGUI/39-spinbox.py
@@ -12,7 +12,7 @@
 #
 # link to source:
 # https://github.com/tisnik/most-popular-python-libs/blob/master/PySimpleGUI/39-spinbox.py
-# 
+#
 # link to source in literate programming format:
 # https://tisnik.github.io/most-popular-python-libs/PySimpleGUI/39-spinbox.html
 
