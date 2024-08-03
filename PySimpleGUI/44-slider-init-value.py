9aaa9ef6a10f9c17c2529c57263d6aa35e94f835
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-08-03 09:45:19 +0200

    Removed trailing whitespaces

--- PySimpleGUI/44-slider-init-value.py
+++ PySimpleGUI/44-slider-init-value.py
@@ -12,7 +12,7 @@
 #
 # link to source:
 # https://github.com/tisnik/most-popular-python-libs/blob/master/PySimpleGUI/44-slider-init-value.py
-# 
+#
 # link to source in literate programming format:
 # https://tisnik.github.io/most-popular-python-libs/PySimpleGUI/44-slider-init-value.html
 
