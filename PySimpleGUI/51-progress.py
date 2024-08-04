b60626b211ab63baa5377cfbf577a79b78d22125
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-08-04 08:57:48 +0200

    Removed trailing whitespaces

--- PySimpleGUI/51-progress.py
+++ PySimpleGUI/51-progress.py
@@ -12,7 +12,7 @@
 #
 # link to source:
 # https://github.com/tisnik/most-popular-python-libs/blob/master/PySimpleGUI/51-progress.py
-# 
+#
 # link to source in literate programming format:
 # https://tisnik.github.io/most-popular-python-libs/PySimpleGUI/51-progress.html
 
