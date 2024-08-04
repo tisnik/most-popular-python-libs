44cb3dcd2d420ab4bca2cdf15ad8bd57df247d1b
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-08-04 08:57:48 +0200

    Removed trailing whitespaces

--- XlsxWriter/05_fill_in_table.py
+++ XlsxWriter/05_fill_in_table.py
@@ -15,7 +15,7 @@
 #
 # link to source:
 # https://github.com/tisnik/most-popular-python-libs/blob/master/PySimpleGUI/05_fill_in_table.py
-# 
+#
 # link to source in literate programming format:
 # https://tisnik.github.io/most-popular-python-libs/PySimpleGUI/05_fill_in_table.html
 
