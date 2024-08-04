73f4b4b77ad5dfd428a41c4606dd2589a1805863
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-08-04 08:57:48 +0200

    Removed trailing whitespaces

--- XlsxWriter/15_alternative_numeric_format.py
+++ XlsxWriter/15_alternative_numeric_format.py
@@ -15,7 +15,7 @@
 #
 # link to source:
 # https://github.com/tisnik/most-popular-python-libs/blob/master/PySimpleGUI/15_alternative_numeric_format.py
-# 
+#
 # link to source in literate programming format:
 # https://tisnik.github.io/most-popular-python-libs/PySimpleGUI/15_alternative_numeric_format.html
 
