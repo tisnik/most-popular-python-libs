12447368d9f098acbaeae3f046aac8d62b73f399
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-08-04 08:57:48 +0200

    Removed trailing whitespaces

--- XlsxWriter/11_format_set_column.py
+++ XlsxWriter/11_format_set_column.py
@@ -15,7 +15,7 @@
 #
 # link to source:
 # https://github.com/tisnik/most-popular-python-libs/blob/master/PySimpleGUI/11_format_set_column.py
-# 
+#
 # link to source in literate programming format:
 # https://tisnik.github.io/most-popular-python-libs/PySimpleGUI/11_format_set_column.html
 
