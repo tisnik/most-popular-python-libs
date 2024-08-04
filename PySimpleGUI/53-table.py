4a730b79c291512facac5540d97d82ef8284630c
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-08-04 08:57:48 +0200

    Removed trailing whitespaces

--- PySimpleGUI/53-table.py
+++ PySimpleGUI/53-table.py
@@ -12,7 +12,7 @@
 #
 # link to source:
 # https://github.com/tisnik/most-popular-python-libs/blob/master/PySimpleGUI/53-table.py
-# 
+#
 # link to source in literate programming format:
 # https://tisnik.github.io/most-popular-python-libs/PySimpleGUI/53-table.html
 
