cc580ca3f1cdd74a696913b8f81b90321921f226
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-08-04 08:57:48 +0200

    Removed trailing whitespaces

--- PySimpleGUI/54-tree.py
+++ PySimpleGUI/54-tree.py
@@ -12,7 +12,7 @@
 #
 # link to source:
 # https://github.com/tisnik/most-popular-python-libs/blob/master/PySimpleGUI/54-tree.py
-# 
+#
 # link to source in literate programming format:
 # https://tisnik.github.io/most-popular-python-libs/PySimpleGUI/54-tree.html
 
