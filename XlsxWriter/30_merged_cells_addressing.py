b5ea49dd76a51897ea00ad026400d410c0f66744
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-08-05 09:12:20 +0200

    Removed trailing whitespaces

--- XlsxWriter/30_merged_cells_addressing.py
+++ XlsxWriter/30_merged_cells_addressing.py
@@ -15,7 +15,7 @@
 #
 # link to source:
 # https://github.com/tisnik/most-popular-python-libs/blob/master/PySimpleGUI/30_merged_cells_addressing.py
-# 
+#
 # link to source in literate programming format:
 # https://tisnik.github.io/most-popular-python-libs/PySimpleGUI/30_merged_cells_addressing.html
 
