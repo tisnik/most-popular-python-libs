c3ebf54db388fb05885011c89fabc17b81e011ad
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-08-05 09:12:20 +0200

    Removed trailing whitespaces

--- XlsxWriter/33_conditional_formatting_between.py
+++ XlsxWriter/33_conditional_formatting_between.py
@@ -15,7 +15,7 @@
 #
 # link to source:
 # https://github.com/tisnik/most-popular-python-libs/blob/master/PySimpleGUI/33_conditional_formatting_between.py
-# 
+#
 # link to source in literate programming format:
 # https://tisnik.github.io/most-popular-python-libs/PySimpleGUI/33_conditional_formatting_between.html
 
