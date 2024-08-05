f7db07809fbd175afa1b446c812036efd20e1d07
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-08-05 09:12:20 +0200

    Removed trailing whitespaces

--- XlsxWriter/17_named_colors.py
+++ XlsxWriter/17_named_colors.py
@@ -15,7 +15,7 @@
 #
 # link to source:
 # https://github.com/tisnik/most-popular-python-libs/blob/master/PySimpleGUI/17_named_colors.py
-# 
+#
 # link to source in literate programming format:
 # https://tisnik.github.io/most-popular-python-libs/PySimpleGUI/17_named_colors.html
 
