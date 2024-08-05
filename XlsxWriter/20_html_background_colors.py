c4eecca8a7d74ae08cb9a50f03ce9b61f387f440
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-08-05 09:12:20 +0200

    Removed trailing whitespaces

--- XlsxWriter/20_html_background_colors.py
+++ XlsxWriter/20_html_background_colors.py
@@ -15,7 +15,7 @@
 #
 # link to source:
 # https://github.com/tisnik/most-popular-python-libs/blob/master/PySimpleGUI/20_html_background_colors.py
-# 
+#
 # link to source in literate programming format:
 # https://tisnik.github.io/most-popular-python-libs/PySimpleGUI/20_html_background_colors.html
 
