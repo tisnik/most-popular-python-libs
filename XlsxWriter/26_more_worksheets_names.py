c074cf876bea7347966b5644816980c516868e2b
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-08-05 09:12:20 +0200

    Removed trailing whitespaces

--- XlsxWriter/26_more_worksheets_names.py
+++ XlsxWriter/26_more_worksheets_names.py
@@ -15,7 +15,7 @@
 #
 # link to source:
 # https://github.com/tisnik/most-popular-python-libs/blob/master/PySimpleGUI/26_more_worksheets_names.py
-# 
+#
 # link to source in literate programming format:
 # https://tisnik.github.io/most-popular-python-libs/PySimpleGUI/26_more_worksheets_names.html
 
