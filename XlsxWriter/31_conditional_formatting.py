805b2c31e68592c70ae5ec82f7cd107aae5236e4
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-08-05 09:12:20 +0200

    Removed trailing whitespaces

--- XlsxWriter/31_conditional_formatting.py
+++ XlsxWriter/31_conditional_formatting.py
@@ -15,7 +15,7 @@
 #
 # link to source:
 # https://github.com/tisnik/most-popular-python-libs/blob/master/PySimpleGUI/31_conditional_formatting.py
-# 
+#
 # link to source in literate programming format:
 # https://tisnik.github.io/most-popular-python-libs/PySimpleGUI/31_conditional_formatting.html
 
