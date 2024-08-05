186aaf7081ea3df0e0f0aa5f2520cb84714ca26f
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-08-05 09:12:20 +0200

    Removed trailing whitespaces

--- XlsxWriter/35_conditional_formatting_average.py
+++ XlsxWriter/35_conditional_formatting_average.py
@@ -15,7 +15,7 @@
 #
 # link to source:
 # https://github.com/tisnik/most-popular-python-libs/blob/master/PySimpleGUI/35_conditional_formatting_average.py
-# 
+#
 # link to source in literate programming format:
 # https://tisnik.github.io/most-popular-python-libs/PySimpleGUI/35_conditional_formatting_average.html
 
