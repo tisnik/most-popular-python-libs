700a551a3154c0ca3f7305ceabef2e94491f7f43
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-08-06 09:25:49 +0200

    Removed trailing whitespaces

--- asizeof/asizeof03.py
+++ asizeof/asizeof03.py
@@ -15,7 +15,7 @@
 #
 # link to source:
 # https://github.com/tisnik/most-popular-python-libs/blob/master/asizeof/asizeof03.py
-# 
+#
 # link to source in literate programming format:
 # https://tisnik.github.io/most-popular-python-libs/asizeof/asizeof03.html
 
