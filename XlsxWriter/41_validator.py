edea637d9c70c12c9daa2e7c49e65329d8d6a326
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-08-06 09:25:49 +0200

    Removed trailing whitespaces

--- XlsxWriter/41_validator.py
+++ XlsxWriter/41_validator.py
@@ -15,7 +15,7 @@
 #
 # link to source:
 # https://github.com/tisnik/most-popular-python-libs/blob/master/PySimpleGUI/41_validator.py
-# 
+#
 # link to source in literate programming format:
 # https://tisnik.github.io/most-popular-python-libs/PySimpleGUI/41_validator.html
 
