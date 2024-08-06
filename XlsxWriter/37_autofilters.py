559210a5160f3ed4b6b819c5bd590094d39b960a
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-08-06 09:25:49 +0200

    Removed trailing whitespaces

--- XlsxWriter/37_autofilters.py
+++ XlsxWriter/37_autofilters.py
@@ -15,7 +15,7 @@
 #
 # link to source:
 # https://github.com/tisnik/most-popular-python-libs/blob/master/PySimpleGUI/37_autofilters.py
-# 
+#
 # link to source in literate programming format:
 # https://tisnik.github.io/most-popular-python-libs/PySimpleGUI/37_autofilters.html
 
