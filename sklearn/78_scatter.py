f0f32627363fdb55d6cdbcc0e614bc89939e8461
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-08-09 09:36:21 +0200

    Scatter plot

---
+++ sklearn/78_scatter.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+
+from sklearn.datasets import fetch_california_housing
+
+# nacteni datove sady
+housings = fetch_california_housing()
+
+# precteni dat z datove sady
+# urcenych pro trenink, validaci atd.
+data = housings["data"]
+
+FIRST_DIM = 6
+SECOND_DIM = 7
+
+# vykresleni korelacniho diagramu pro dvojici vybranych atributu
+# prvni sloupec: x-ove souradnice
+# druhy sloupec: y-ove souradnice
+plt.scatter(data[:, FIRST_DIM], data[:, SECOND_DIM], s=2)
+plt.title("Classes")
+
+# popisky os
+plt.xlabel(housings.feature_names[FIRST_DIM])
+plt.ylabel(housings.feature_names[SECOND_DIM])
+
+# ulozeni diagramu do souboru
+plt.savefig("78.png")
+
+# zobrazeni diagramu
+plt.show()
