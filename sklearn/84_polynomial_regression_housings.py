30436df1754bdb7cb610eea8b09c0eac76eba581
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-08-10 11:57:15 +0200

    Removed unused imports

--- sklearn/84_polynomial_regression_housings.py
+++ sklearn/84_polynomial_regression_housings.py
@@ -1,11 +1,8 @@
-import numpy as np
 import matplotlib.pyplot as plt
 
 from sklearn import linear_model
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.datasets import fetch_california_housing
-from sklearn.metrics import mean_squared_error, r2_score
-from sklearn.model_selection import train_test_split
 
 # nacteni datove sady
 housings = fetch_california_housing()
