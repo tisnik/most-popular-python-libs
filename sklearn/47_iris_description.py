968b7effd35f3a528e26ed22c617f87340703c50
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-08-14 08:57:03 +0200

    Added linkts to serie and articles

--- sklearn/47_iris_description.py
+++ sklearn/47_iris_description.py
@@ -9,6 +9,15 @@
 #  Contributors:
 #      Pavel Tisnovsky
 #
+#
+# Tento demonstracni priklad je pouzity v serialu o datove analyze
+# s vyuzitim programovaciho jazyka Python:
+# https://www.root.cz/serialy/datova-analyza-s-vyuzitim-jazyka-python/
+#
+# Clanek, kde je tento demonstracni priklad pouzit:
+# Datová analýza s využitím nástroje scikit-learn: první kroky
+# https://www.root.cz/clanky/datova-analyza-s-vyuzitim-nastroje-scikit-learn-prvni-kroky/#k19
+#
 
 from sklearn.datasets import load_iris
 
