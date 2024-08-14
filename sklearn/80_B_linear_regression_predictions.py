f45895c5ba9213e0b2e928c24a09ac16401ca694
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-08-14 08:55:44 +0200

    Linear regression model prediction

---
+++ sklearn/80_B_linear_regression_predictions.py
@@ -0,0 +1,60 @@
+#
+#  (C) Copyright 2024  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn import linear_model
+from sklearn.model_selection import train_test_split
+
+VALUES = 100
+
+x = np.linspace(0, 10, VALUES)
+y = np.linspace(-1, 1, VALUES) + 0.5*np.random.rand(VALUES)
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.97)
+
+print("Array sizes:")
+print(f"x_train: {len(x_train)}")
+print(f"y_train: {len(y_train)}")
+print(f"x_test:  {len(x_test)}")
+print(f"y_test:  {len(y_test)}")
+
+# konstrukce modelu
+lr = linear_model.LinearRegression()
+
+# trénink modelu
+lr.fit(x_train.reshape(-1, 1), y_train)
+
+# predikce modelu
+y_pred = lr.predict(x_test.reshape(-1, 1))
+
+# výpis vypočtených koeficientů modelu
+print("Coefficients: \n", lr.coef_)
+print("Intercept: \n", lr.intercept_)
+
+# vykreslení výsledku
+plt.scatter(x_test, y_test, color="black", s=2)
+plt.plot(x_test, y_pred, color="blue", linewidth=2)
+
+# titulek grafu
+plt.title("Linear regression")
+
+# osy
+plt.xticks(())
+plt.yticks(())
+
+# ulozeni diagramu do souboru
+plt.savefig("80.png")
+
+# zobrazeni diagramu
+plt.show()
