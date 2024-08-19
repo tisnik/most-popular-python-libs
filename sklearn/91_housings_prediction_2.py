bef64f40506a378fcef6d1b9b342a8028120157d
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-08-19 08:34:11 +0200

    California housings predictions, example #2: proper training

---
+++ sklearn/91_housings_prediction_2.py
@@ -0,0 +1,40 @@
+from sklearn import linear_model
+from sklearn.datasets import fetch_california_housing
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.model_selection import train_test_split
+
+# nacteni datove sady
+housings = fetch_california_housing()
+
+# precteni dat z datove sady
+# urcenych pro trenink, validaci atd.
+data = housings["data"]
+
+# ceny bloku
+targets = housings["target"]
+
+# X je matice, y je vektor
+X = data
+y = targets
+
+# rozdeleni dat na treninkovou a testovaci mnozinu
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.6)
+
+# konstrukce modelu
+lr = linear_model.LinearRegression()
+
+# trénink modelu
+lr.fit(X_train, y_train)
+
+# predikce modelu
+y_pred = lr.predict(X_test)
+
+# výpis vypočtených koeficientů modelu
+print("Coefficients: \n", lr.coef_)
+print("Intercept: \n", lr.intercept_)
+
+# chyba predikce
+print("Mean squared error: %.2f" % mean_squared_error(y_test, y_pred))
+
+# 1 = nejlepší predikce modelu
+print("Coefficient of determination: %.2f" % r2_score(y_test, y_pred))
