a8b00ee88125b45d0857fec1f08a21f401fe6e96
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-08-20 09:29:33 +0200

    California housings: basic statistic for normalized values

---
+++ sklearn/97_housing_statistic_normalized.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+from sklearn import linear_model
+from sklearn.datasets import fetch_california_housing
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing as pre
+
+# nacteni datove sady
+housings = fetch_california_housing()
+
+# precteni dat z datove sady
+data = housings["data"]
+
+print("Feature              Min         Max           Avg         Std")
+
+for i in range(8):
+    column = data[:, i]
+    column = pre.MinMaxScaler().fit_transform(column.reshape(-1,1))
+    feature = housings.feature_names[i]
+    print(f"{feature:12}   {column.min():10.3f}   {column.max():10.3f}   {np.mean(column):10.3f}  {np.std(column):10.3f}")
