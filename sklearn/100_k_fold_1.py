fd87fda5b5e8a5bb09e68f7d3bf619aacac82376
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-08-26 09:08:43 +0200

    K-folding internal function, 1st example

---
+++ sklearn/100_k_fold_1.py
@@ -0,0 +1,14 @@
+import numpy as np
+from sklearn.model_selection import KFold
+
+k_fold = KFold(n_splits=5, shuffle=False)
+
+print(k_fold)
+
+X = 1000 * np.linspace(1, 10, 10)
+print(f"Original X: {X}")
+
+for i, (train_index, test_index) in enumerate(k_fold.split(X)):
+    print(f"Fold {i}:")
+    print(f"  Train: index={train_index}")
+    print(f"  Test:  index={test_index}")
