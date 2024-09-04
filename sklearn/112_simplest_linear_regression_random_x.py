38d6b7afb8bb7334c4cd08e98ff1e7ec1e027beb
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-09-04 09:32:40 +0200

    Simplest form of linear regression, for random values

---
+++ sklearn/112_simplest_linear_regression_random_x.py
@@ -0,0 +1,50 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn import linear_model
+
+# počet vzorků ve vektorech x i y
+VALUES = 50
+
+# x je vektor
+x = np.linspace(0, 10, VALUES) + 0.5*np.random.rand(VALUES)
+
+# y je vektor
+y = np.linspace(-1, 1, VALUES) + 0.5*np.random.rand(VALUES)
+
+# převod vektoru na 2D matici
+X = x.reshape(-1, 1)
+
+# tvar matice X a vektoru y
+print("X shape:", X.shape)
+print("y shape:", y.shape)
+
+# konstrukce modelu
+lr = linear_model.LinearRegression()
+
+# trénink modelu (X musí být maticí)
+lr.fit(X, y)
+
+# predikce modelu
+y_pred = lr.predict(X)
+
+# výpis vypočtených koeficientů modelu
+print("Coefficients: \n", lr.coef_)
+print("Intercept: \n", lr.intercept_)
+
+# vykreslení výsledku
+plt.scatter(x, y, color="black", s=2)
+plt.plot(x, y_pred, color="blue", linewidth=2)
+
+# titulek grafu
+plt.title("Linear regression")
+
+# osy
+plt.xticks()
+plt.yticks()
+
+# ulozeni diagramu do souboru
+plt.savefig("112B.png")
+
+# zobrazeni diagramu
+plt.show()
