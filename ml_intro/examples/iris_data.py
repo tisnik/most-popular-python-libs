7ed73d8f47c087d6ea306ea89daf39680798cf44
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-09-09 09:13:19 +0200

    Retrieving data from Iris data set

---
+++ ml_intro/examples/iris_data.py
@@ -0,0 +1,21 @@
+# modul s datovou sadou Iris
+from sklearn.datasets import load_iris
+
+# nacteni datove sady
+iris = load_iris()
+
+# precteni dat z datove sady
+# urcenych pro trenink, validaci atd.
+data = iris["data"]
+
+print("Feature data:")
+print(data)
+print("-" * 100)
+
+# typ a "tvar" n-dimenzionalniho pole
+print("Data type:")
+print(type(data))
+print()
+
+print("Data shape:")
+print(data.shape)
