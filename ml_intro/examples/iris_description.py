e6ec955bd7b500adc3ebe6e2ac21be0edfaa70fb
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-09-09 09:13:42 +0200

    Description for Iris data set

---
+++ ml_intro/examples/iris_description.py
@@ -0,0 +1,12 @@
+# modul s datovou sadou Iris
+from sklearn.datasets import load_iris
+
+# nacteni datove sady
+iris = load_iris()
+
+print(dir(iris))
+
+print("-" * 100)
+
+# podrobny popis datove sady
+print(iris["DESCR"])
