8739840643543964b1af49a88e7e55943125aef2
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-09-09 09:14:53 +0200

    Cross validation, version 3

---
+++ sklearn/135_cross_validation_3.py
@@ -0,0 +1,29 @@
+# import tridy realizujici vyber atributu
+from sklearn.feature_selection import VarianceThreshold
+
+from sklearn import linear_model
+
+# import funkce pro nacteni datove sady, kterou pouzijeme
+from sklearn.datasets import fetch_california_housing
+from sklearn.model_selection import cross_val_score
+
+# nacteni datove sady
+housings = fetch_california_housing()
+
+# jmena jednotlivych atributu
+feature_names = housings["feature_names"]
+
+# X je matice (feature matrix)
+X = housings.data
+
+# y je vektor (response vector)
+y = housings.target
+
+sel = VarianceThreshold(threshold=0.6)
+X_new = sel.fit_transform(X, y)
+
+# konstrukce modelu pro regresi
+lr = linear_model.LinearRegression()
+scores = cross_val_score(lr, X_new, y, cv=10, scoring='r2')
+
+print(X_new.shape, sel.get_feature_names_out(input_features=feature_names), scores.mean())
