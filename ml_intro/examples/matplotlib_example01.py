d0da0aec946cf0fe80d56feb66df91b83f2d5a5b
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-09-12 12:49:17 +0200

    Matplotlib example #1

---
+++ ml_intro/examples/matplotlib_example01.py
@@ -0,0 +1,20 @@
+# - vykreslení průběhu funkce sin
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# hodnoty na x-ové ose
+x = np.linspace(0, 2 * np.pi, 100)
+
+# hodnoty na y-ové ose
+y = np.sin(x)
+
+# vykreslit průběh funkce
+plt.plot(x, y)
+
+# popis os
+plt.xlabel("x")
+plt.ylabel("sin(x)")
+
+# zobrazení grafu
+plt.show()
