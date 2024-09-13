e14794b5fea57203499c2645291784d0931b6610
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-09-13 19:02:12 +0200

    Matplotlib example #24

---
+++ ml_intro/examples/matplotlib_example24.py
@@ -0,0 +1,30 @@
+# - zobrazení 3D grafu funkce typu z=f(x,y)
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection="3d")
+
+delta = 0.1
+
+# průběh nezávislé proměnné x
+x = np.arange(-10.0, 10.0, delta)
+
+# průběh nezávislé proměnné y
+y = np.arange(-10.0, 10.0, delta)
+
+# vytvoření dvou polí se souřadnicemi [x,y]
+X, Y = np.meshgrid(x, y)
+
+# vzdálenost od bodu [0,0]
+R = np.sqrt(X * X + Y * Y)
+
+# výpočet funkce, kterou použijeme při vykreslování grafu
+Z = np.sin(R) / R
+
+# zobrazení 3D grafu
+ax.plot_wireframe(X, Y, Z, rstride=7, cstride=7)
+
+# zobrazení grafu
+plt.show()
