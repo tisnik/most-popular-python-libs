2dbd5d584359cf085f652c8e781915c06364a291
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-09-13 19:02:12 +0200

    Matplotlib example #31

---
+++ ml_intro/examples/matplotlib_example31.py
@@ -0,0 +1,27 @@
+# - vykreslení průběhu exponenciální funkce
+# - při vykreslování se jednotlivé body spojí úsečkami
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# hodnoty na x-ové ose
+x = np.linspace(0.0, 10.0, 1000)
+
+# hodnoty na y-ové ose
+y = 2 ** x
+
+# vykreslit průběh funkce
+plt.plot(x, y, color="blue", label="exp(x)")
+
+# povolení zobrazení mřížky
+plt.grid(True)
+
+# popis os
+plt.xlabel("x")
+plt.ylabel("exp(x)")
+
+# přidání legendy
+plt.legend(loc="upper left")
+
+# zobrazení grafu
+plt.show()
