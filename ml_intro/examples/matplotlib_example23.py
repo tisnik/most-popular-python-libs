3d9916a725e2a7909df9fa1e96df0f6e29da1f62
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-09-13 19:02:12 +0200

    Matplotlib example #23

---
+++ ml_intro/examples/matplotlib_example23.py
@@ -0,0 +1,42 @@
+# - zobrazení kontur funkce typu z=f(x,y)
+# - zobrazení hodnot u jednotlivých "vrstevnic"
+# - přidání legendy
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+delta = 0.1
+
+# průběh nezávislé proměnné x
+x = np.arange(-10.0, 10.0, delta)
+
+# průběh nezávislé proměnné y
+y = np.arange(-10.0, 10.0, delta)
+
+# vytvoření dvou polí se souřadnicemi [x,y]
+X, Y = np.meshgrid(x, y)
+
+# vzdálenost od bodu [0,0]
+R1 = np.sqrt(X * X + Y * Y)
+
+# vzdálenost od bodu [3,3]
+R2 = np.sqrt((X - 3) * (X - 3) + (Y - 3) * (Y - 3))
+
+# výpočet funkce, kterou použijeme při vykreslování grafu
+Z = np.sin(R1) - np.cos(R2)
+
+# povolení zobrazení mřížky
+plt.grid(True)
+
+# vytvoření grafu s konturami funkce z=f(x,y)
+CS = plt.contour(X, Y, Z)
+
+# přidání legendy (colorbar)
+CB = plt.colorbar(CS, shrink=0.7, extend="both")
+
+# popisky "vrstevnic"
+plt.clabel(CS, inline=1, fontsize=10)
+
+# zobrazení grafu
+plt.show()
