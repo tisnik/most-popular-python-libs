1a3dd90dbd5ade6308618c82d812f0b2abeb4834
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-09-16 09:42:13 +0200

    NumPy example #13

---
+++ ml_intro/examples/numpy_13_ones_1D_array.py
@@ -0,0 +1,12 @@
+# Příklady použití konstruktoru numpy.ones
+#
+# jednorozměrný vektor s deseti prvky
+
+# import hlavního balíčku knihovny Numpy
+import numpy
+
+# konstrukce pole
+a = numpy.ones(10)
+
+# tisk obsahu pole na standardní výstup
+print(a)
