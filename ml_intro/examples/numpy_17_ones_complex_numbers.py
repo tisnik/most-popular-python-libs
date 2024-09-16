a6bd69dbb86c0a6f0bfc6d1bc0467344d4c73673
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-09-16 09:42:13 +0200

    NumPy example #17

---
+++ ml_intro/examples/numpy_17_ones_complex_numbers.py
@@ -0,0 +1,12 @@
+# Příklady použití konstruktoru numpy.ones
+#
+# použití komplexních čísel
+
+# import hlavního balíčku knihovny Numpy
+import numpy
+
+# konstrukce pole
+a = numpy.ones((3, 2), dtype=numpy.complex)
+
+# tisk obsahu pole na standardní výstup
+print(a)
