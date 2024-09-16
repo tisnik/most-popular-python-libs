82815d63624c27120f30d23e3db28d9cc0b1fed8
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-09-16 09:42:13 +0200

    NumPy example #14

---
+++ ml_intro/examples/numpy_14_ones_2D_array.py
@@ -0,0 +1,12 @@
+# Příklady použití konstruktoru numpy.ones
+#
+# matice se třemi řádky a čtyřmi sloupci
+
+# import hlavního balíčku knihovny Numpy
+import numpy
+
+# konstrukce pole
+a = numpy.ones((3, 4))
+
+# tisk obsahu pole na standardní výstup
+print(a)
