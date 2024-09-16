84a868a7341cd16c512d26c57360bace977016a6
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-09-16 09:42:13 +0200

    NumPy example #20

---
+++ ml_intro/examples/numpy_20_eye_2D_array.py
@@ -0,0 +1,12 @@
+# Příklady použití konstruktoru numpy.eye
+#
+# jednotková matice s rozměry 2x10 prvků
+
+# import hlavního balíčku knihovny Numpy
+import numpy
+
+# konstrukce pole
+a = numpy.eye(2, 10)
+
+# tisk obsahu pole na standardní výstup
+print(a)
