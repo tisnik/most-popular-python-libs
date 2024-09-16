f1c6fd57834a041b1ab34a1072d755aa0a710cbc
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-09-16 09:42:13 +0200

    NumPy example #16

---
+++ ml_intro/examples/numpy_16_ones_3D_array.py
@@ -0,0 +1,12 @@
+# Příklady použití konstruktoru numpy.ones
+#
+# trojrozměrné pole s prvky typu int
+
+# import hlavního balíčku knihovny Numpy
+import numpy
+
+# konstrukce pole
+a = numpy.ones((3, 4, 5), dtype=int)
+
+# tisk obsahu pole na standardní výstup
+print(a)
