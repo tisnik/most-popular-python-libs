a7a6c8c3093615d01ebe05c982d2e8bd54b32649
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-09-16 09:41:45 +0200

    NumPy example #08

---
+++ ml_intro/examples/numpy_08_zeros_1D_array.py
@@ -0,0 +1,12 @@
+# Příklady použití konstruktoru numpy.zeros
+#
+# jednorozměrný vektor s deseti prvky
+
+# import hlavního balíčku knihovny Numpy
+import numpy
+
+# konstrukce pole
+a = numpy.zeros(10)
+
+# tisk obsahu pole na standardní výstup
+print(a)
