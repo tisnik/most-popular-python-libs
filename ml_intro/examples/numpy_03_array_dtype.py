c41c9c9d9c8ccea170b801176ed43bf0cc5258c5
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-09-16 09:41:45 +0200

    NumPy example #03

---
+++ ml_intro/examples/numpy_03_array_dtype.py
@@ -0,0 +1,13 @@
+# Konstrukce jednorozměrného pole konstruktorem numpy.array()
+#
+# explicitní specifikace typu všech prvků pole
+# (interně se provádí přetypování)
+
+# import hlavního balíčku knihovny Numpy
+import numpy
+
+# konstrukce pole
+a = numpy.array(range(10), dtype=numpy.float)
+
+# tisk obsahu pole na standardní výstup
+print(a)
