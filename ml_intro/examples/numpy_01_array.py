b62b1904d76c9908921be918a46b0ecaa72ca743
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-09-16 09:41:45 +0200

    NumPy example #01

---
+++ ml_intro/examples/numpy_01_array.py
@@ -0,0 +1,12 @@
+# Konstrukce jednorozměrného pole konstruktorem numpy.array()
+#
+# vytvoření pole se čtyřmi prvky
+
+# import hlavního balíčku knihovny Numpy
+import numpy
+
+# konstrukce pole
+a = numpy.array([1, 2, 3, 4])
+
+# tisk obsahu pole na standardní výstup
+print(a)
