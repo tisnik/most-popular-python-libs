4c75e0fab9933f4ca7ed9d1abd00578305b7fb8f
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-09-16 09:41:45 +0200

    NumPy example #02

---
+++ ml_intro/examples/numpy_02_array_range.py
@@ -0,0 +1,12 @@
+# Konstrukce jednorozměrného pole konstruktorem numpy.array()
+#
+# vytvoření pole s deseti prvky
+
+# import hlavního balíčku knihovny Numpy
+import numpy
+
+# konstrukce pole
+a = numpy.array(range(10))
+
+# tisk obsahu pole na standardní výstup
+print(a)
