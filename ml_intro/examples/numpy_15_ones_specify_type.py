888545bce30253b40c246b4458fb0ce223009a7c
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-09-16 09:42:13 +0200

    NumPy example #15

---
+++ ml_intro/examples/numpy_15_ones_specify_type.py
@@ -0,0 +1,13 @@
+# Příklady použití konstruktoru numpy.ones
+#
+# matice se třemi řádky a čtyřmi sloupci
+# s explicitní specifikací typu prvků
+
+# import hlavního balíčku knihovny Numpy
+import numpy
+
+# konstrukce pole
+a = numpy.ones((3, 4), dtype=int)
+
+# tisk obsahu pole na standardní výstup
+print(a)
