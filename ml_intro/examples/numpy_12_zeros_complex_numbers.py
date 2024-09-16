1a10094f43a13e9bfa6e2ad3815c666a5ba67a43
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-09-16 09:42:13 +0200

    NumPy example #12

---
+++ ml_intro/examples/numpy_12_zeros_complex_numbers.py
@@ -0,0 +1,12 @@
+# Příklady použití konstruktoru numpy.zeros
+#
+# použití komplexních čísel
+
+# import hlavního balíčku knihovny Numpy
+import numpy
+
+# konstrukce pole
+a = numpy.zeros((3, 2), dtype=numpy.complex)
+
+# tisk obsahu pole na standardní výstup
+print(a)
