111781e251a1aaa7d1b5652c267a572ef8b0d776
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-09-16 09:41:45 +0200

    NumPy example #06

---
+++ ml_intro/examples/numpy_06_2D_array.py
@@ -0,0 +1,12 @@
+# Příklady použití funkce numpy.array
+#
+# vytvoření dvourozměrné matice
+
+# import hlavního balíčku knihovny Numpy
+import numpy
+
+# konstrukce pole
+a = numpy.array([[1, 2, 3], [4, 5, 6]])
+
+# tisk obsahu pole na standardní výstup
+print(a)
