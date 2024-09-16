4ec6f5cce6601893b017f2b35b60370fcc01bca1
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-09-16 09:41:45 +0200

    NumPy example #10

---
+++ ml_intro/examples/numpy_10_zeros_3D_array.py
@@ -0,0 +1,12 @@
+# Příklady použití konstruktoru numpy.zeros
+#
+# matice o velikosti 3x3x3 prvků, každý prvek je typu float
+
+# import hlavního balíčku knihovny Numpy
+import numpy
+
+# konstrukce pole
+a = numpy.zeros((3, 3, 3))
+
+# tisk obsahu pole na standardní výstup
+print(a)
