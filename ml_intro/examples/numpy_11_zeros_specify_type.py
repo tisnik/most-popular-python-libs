63cfc0fad92db6f1475d75b110e2a0283e2fd04f
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-09-16 09:42:13 +0200

    NumPy example #11

---
+++ ml_intro/examples/numpy_11_zeros_specify_type.py
@@ -0,0 +1,12 @@
+# Příklady použití konstruktoru numpy.zeros
+#
+# matice o velikosti 5x5 prvků, každý prvek je typu int
+
+# import hlavního balíčku knihovny Numpy
+import numpy
+
+# konstrukce pole
+a = numpy.zeros((5, 5), dtype=int)
+
+# tisk obsahu pole na standardní výstup
+print(a)
