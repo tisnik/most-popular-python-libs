a6d074d75247425b77b1313d63648972b6297e3d
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-09-17 10:21:05 +0200

    NumPy example #29

---
+++ ml_intro/examples/numpy_29_full_inf.py
@@ -0,0 +1,12 @@
+# Příklady použití konstruktoru numpy.full
+#
+# matice s rozměry 3x4x5 prvků
+
+# import hlavního balíčku knihovny Numpy
+import numpy
+
+# konstrukce pole
+a = numpy.full((3, 4, 5), numpy.inf)
+
+# tisk obsahu pole na standardní výstup
+print(a)
