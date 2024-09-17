e65b59549f4e889ee6e475a75e763de24d4439df
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-09-17 10:21:06 +0200

    NumPy example #38

---
+++ ml_intro/examples/numpy_38_linspace_default_number_of_items.py
@@ -0,0 +1,13 @@
+# Použití funkce numpy.linspace
+#
+# pokud se nespecifikuje počet prvků, bude se předpokládat, že výsledný
+# vektor má mít padesát prvků
+
+# import hlavního balíčku knihovny Numpy
+import numpy
+
+# konstrukce pole
+a = numpy.linspace(1, 99)
+
+# tisk obsahu pole na standardní výstup
+print(a)
