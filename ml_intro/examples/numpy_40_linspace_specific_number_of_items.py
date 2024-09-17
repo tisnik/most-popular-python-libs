c6d7b266fc3e407339b40337c4d4e1f4010c2f6c
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-09-17 10:21:06 +0200

    NumPy example #40

---
+++ ml_intro/examples/numpy_40_linspace_specific_number_of_items.py
@@ -0,0 +1,13 @@
+# Použití funkce numpy.linspace
+#
+# zde explicitně specifikujeme, že výsledný vektor má mít jedenáct prvků
+# (tím, že se začíná od nuly, získáme krok 0.1)
+
+# import hlavního balíčku knihovny Numpy
+import numpy
+
+# konstrukce pole
+a = numpy.linspace(0, 1, 11)
+
+# tisk obsahu pole na standardní výstup
+print(a)
