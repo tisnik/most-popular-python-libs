00020de3dd47a0f9b0d87912f398006f2e03bb7c
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-09-17 10:21:05 +0200

    NumPy example #36

---
+++ ml_intro/examples/numpy_36_arange_complex.py
@@ -0,0 +1,13 @@
+# Funkce numpy.arange
+#
+# nemusíme zůstat pouze u celých čísel, protože pracovat je možné i s hodnotami
+# typu float a complex
+
+# import hlavního balíčku knihovny Numpy
+import numpy
+
+# konstrukce pole
+a = numpy.arange(0 + 0j, 10 + 10j, 2 + 0j)
+
+# tisk obsahu pole na standardní výstup
+print(a)
