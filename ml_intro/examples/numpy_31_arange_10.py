bca6eb8d96f05a88a6db12253c90b72b43e43fd2
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-09-17 10:21:05 +0200

    NumPy example #31

---
+++ ml_intro/examples/numpy_31_arange_10.py
@@ -0,0 +1,13 @@
+# Funkce numpy.arange
+#
+# při použití jednoho parametru má tento parametr význam hodnoty „stop“
+# vytvoří se vektor s prvky od 0 do „stop“ (kromě)
+
+# import hlavního balíčku knihovny Numpy
+import numpy
+
+# konstrukce pole
+a = numpy.arange(10)
+
+# tisk obsahu pole na standardní výstup
+print(a)
