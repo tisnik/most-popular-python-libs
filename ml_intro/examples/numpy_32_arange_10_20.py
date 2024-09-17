b817e06a03e1c64648cac6316ffd86c4f52468a5
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-09-17 10:21:05 +0200

    NumPy example #32

---
+++ ml_intro/examples/numpy_32_arange_10_20.py
@@ -0,0 +1,12 @@
+# Funkce numpy.arange
+#
+# specifikace hodnot „start“ (včetně) a „stop“ (kromě)
+
+# import hlavního balíčku knihovny Numpy
+import numpy
+
+# konstrukce pole
+a = numpy.arange(10, 20)
+
+# tisk obsahu pole na standardní výstup
+print(a)
