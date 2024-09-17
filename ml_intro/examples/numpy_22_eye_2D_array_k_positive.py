8f8ebae1eb1a37008f349f58de8867cb5d155a86
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-09-17 10:21:05 +0200

    NumPy example #22

---
+++ ml_intro/examples/numpy_22_eye_2D_array_k_positive.py
@@ -0,0 +1,12 @@
+# Příklady použití konstruktoru numpy.eye
+#
+# jednotková matice s rozměry 10x10 prvků
+
+# import hlavního balíčku knihovny Numpy
+import numpy
+
+# konstrukce pole
+a = numpy.eye(10, 10, k=1)
+
+# tisk obsahu pole na standardní výstup
+print(a)
