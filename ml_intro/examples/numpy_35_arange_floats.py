6b750aababe91bc6f51aeeba3ac2a7c21a7edcd7
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-09-17 10:21:05 +0200

    NumPy example #35

---
+++ ml_intro/examples/numpy_35_arange_floats.py
@@ -0,0 +1,12 @@
+# Funkce numpy.arange
+#
+# použít lze i hodnoty typu float
+
+# import hlavního balíčku knihovny Numpy
+import numpy
+
+# konstrukce pole
+a = numpy.arange(0, 5.5, 0.5)
+
+# tisk obsahu pole na standardní výstup
+print(a)
