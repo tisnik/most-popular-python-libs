5260cf971f19b74f658b9e68cebc7d7d2b6fc4da
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-09-17 10:21:05 +0200

    NumPy example #34

---
+++ ml_intro/examples/numpy_34_arange_negative_step.py
@@ -0,0 +1,12 @@
+# Funkce numpy.arange
+#
+# třetí nepovinný parametr určuje krok použitý při generování prvků vektoru
+
+# import hlavního balíčku knihovny Numpy
+import numpy
+
+# konstrukce pole
+a = numpy.arange(20, 10, -2)
+
+# tisk obsahu pole na standardní výstup
+print(a)
