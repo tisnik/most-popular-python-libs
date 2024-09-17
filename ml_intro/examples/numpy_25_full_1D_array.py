1018368d0abcba9ac84e437c24ccbd429a75efa0
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-09-17 10:21:05 +0200

    NumPy example #25

---
+++ ml_intro/examples/numpy_25_full_1D_array.py
@@ -0,0 +1,12 @@
+# Příklady použití konstruktoru numpy.full
+#
+# matice s rozměry 1x10 prvků
+
+# import hlavního balíčku knihovny Numpy
+import numpy
+
+# konstrukce pole
+a = numpy.full((10), 100)
+
+# tisk obsahu pole na standardní výstup
+print(a)
