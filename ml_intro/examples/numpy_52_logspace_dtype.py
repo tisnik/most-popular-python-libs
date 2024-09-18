a2e8b373191406657c22daa2aa9381fb252d53e8
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-09-18 10:19:58 +0200

    NumPy example #52

---
+++ ml_intro/examples/numpy_52_logspace_dtype.py
@@ -0,0 +1,12 @@
+# Použití funkce numpy.logspace
+#
+# zde explicitně specifikujeme, že výsledný vektor má mít pět prvků
+
+# import hlavního balíčku knihovny Numpy
+import numpy
+
+# konstrukce pole
+a = numpy.logspace(1, 3, 5, dtype="I")
+
+# tisk obsahu pole na standardní výstup
+print(a)
