d7b2dee27fda24bcd126120c4af18724953f3a6b
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-09-18 10:19:58 +0200

    NumPy example #45

---
+++ ml_intro/examples/numpy_45_geomspace_default_number_of_items.py
@@ -0,0 +1,13 @@
+# Použití funkce numpy.geomspace
+#
+# pokud se nespecifikuje počet prvků, bude se předpokládat, že výsledný
+# vektor má mít padesát prvků
+
+# import hlavního balíčku knihovny Numpy
+import numpy
+
+# konstrukce pole
+a = numpy.geomspace(1, 100)
+
+# tisk obsahu pole na standardní výstup
+print(a)
