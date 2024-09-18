95fb76c3cea43803c6012714468141121c712550
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-09-18 10:19:58 +0200

    NumPy example #44

---
+++ ml_intro/examples/numpy_44_linspace_complex_both_parts.py
@@ -0,0 +1,12 @@
+# Použití funkce numpy.linspace
+#
+# další možnost použití komplexních čísel
+
+# import hlavního balíčku knihovny Numpy
+import numpy
+
+# konstrukce pole
+a = numpy.linspace(0 + 0j, 1 + 1j, 10)
+
+# tisk obsahu pole na standardní výstup
+print(a)
