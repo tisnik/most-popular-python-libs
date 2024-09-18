87535bea2d8ee92095cd50da1035f268aa84f334
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-09-18 10:19:58 +0200

    NumPy example #46

---
+++ ml_intro/examples/numpy_46_geomspace_specific_number_of_items.py
@@ -0,0 +1,12 @@
+# Použití funkce numpy.geomspace
+#
+# zde explicitně specifikujeme, že výsledný vektor má mít deset prvků
+
+# import hlavního balíčku knihovny Numpy
+import numpy
+
+# konstrukce pole
+a = numpy.geomspace(1, 1000, 10)
+
+# tisk obsahu pole na standardní výstup
+print(a)
