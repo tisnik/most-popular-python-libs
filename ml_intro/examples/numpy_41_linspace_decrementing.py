e09cd6494dbd62501d7dd95ddfdc2789c31ece47
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-09-18 10:19:58 +0200

    NumPy example #41

---
+++ ml_intro/examples/numpy_41_linspace_decrementing.py
@@ -0,0 +1,12 @@
+# Použití funkce numpy.linspace
+#
+# sekvence hodnot samozřejmě může i klesat
+
+# import hlavního balíčku knihovny Numpy
+import numpy
+
+# konstrukce pole
+a = numpy.linspace(1, 0, 11)
+
+# tisk obsahu pole na standardní výstup
+print(a)
