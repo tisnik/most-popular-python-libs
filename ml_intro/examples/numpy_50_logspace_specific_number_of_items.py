2bbe78ce58d96ea859d24d4ee296ee0db824cf1a
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-09-18 10:19:58 +0200

    NumPy example #50

---
+++ ml_intro/examples/numpy_50_logspace_specific_number_of_items.py
@@ -0,0 +1,12 @@
+# Použití funkce numpy.logspace
+#
+# zde explicitně specifikujeme, že výsledný vektor má mít deset prvků
+
+# import hlavního balíčku knihovny Numpy
+import numpy
+
+# konstrukce pole
+a = numpy.logspace(1, 10, 10)
+
+# tisk obsahu pole na standardní výstup
+print(a)
