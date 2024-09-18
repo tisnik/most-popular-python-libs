f74f9fcbb06d0721f1a4886f28aef11f9b85a8bb
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-09-18 10:19:58 +0200

    NumPy example #60

---
+++ ml_intro/examples/numpy_60_matrix_string_2D_array_error.py
@@ -0,0 +1,20 @@
+# Příklady použití konstruktoru numpy.matrix
+#
+# matice s rozměry 2x2 prvky
+
+# import hlavního balíčku knihovny Numpy
+import numpy
+
+# konstrukce matice
+a = numpy.matrix("1 2 3; 4 5; 6")
+
+# vytvoření pole z matice
+b = numpy.array(a)
+
+# tisk obsahu matice na standardní výstup
+print(type(a))
+print(a)
+
+# tisk obsahu pole na standardní výstup
+print(type(b))
+print(b)
