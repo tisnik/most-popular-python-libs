466de8178778a97b47f5bb0a205099c5760ae917
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-09-19 09:16:01 +0200

    NumPy example #72

---
+++ ml_intro/examples/numpy_72_select_by_other_array_negative_indexes.py
@@ -0,0 +1,17 @@
+# Výběr prvků pomocí indexů uložených v jiném poli
+# (záporné indexy)
+
+# import hlavního balíčku knihovny Numpy
+import numpy
+
+# jednorozměrná pole - vektory
+a = numpy.arange(12)
+
+# tisk původního pole
+print(a)
+
+# pole indexů
+b = numpy.array([-1, -2, -9, -8, -5])
+
+# výběr celým polem
+print(a[b])
