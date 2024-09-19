a849ed289b51e75510db7786dfbd37757b586d4c
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-09-19 09:16:01 +0200

    NumPy example #76

---
+++ ml_intro/examples/numpy_76_slicing.py
@@ -0,0 +1,28 @@
+# Slicing - vynechání indexu/indexů
+
+# import hlavního balíčku knihovny Numpy
+import numpy
+
+# původní pole
+a = numpy.arange(12)
+
+# tisk původního pole
+print(a)
+
+# slicing
+b = a[:7]
+
+# tisk nového pole
+print(b)
+
+# slicing
+c = a[5:]
+
+# tisk nového pole
+print(c)
+
+# slicing
+d = a[:]
+
+# tisk nového pole
+print(d)
