805dc0087399ace679ecf5523c5c7d5aed670a9f
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-09-19 09:16:01 +0200

    NumPy example #69

---
+++ ml_intro/examples/numpy_69_selectors_1D_array.py
@@ -0,0 +1,22 @@
+# Výběr prvků v poli
+
+# import hlavního balíčku knihovny Numpy
+import numpy
+
+# jednorozměrná pole - vektory
+a = numpy.arange(12)
+
+# tisk původního pole
+print(a)
+
+# indexování prvků od nuly
+print(a[0])
+
+# indexování prvků od nuly
+print(a[5])
+
+# indexovat lze i od konce pole
+print(a[-1])
+
+# indexovat lze i od konce pole
+print(a[-5])
