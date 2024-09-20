67f6199e0124bfc8a6ef3c45465090d14ee1fe6f
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-09-20 09:23:24 +0200

    NumPy example #86

---
+++ ml_intro/examples/numpy_86_operator_pluseq.py
@@ -0,0 +1,16 @@
+# Modifikace matice s využitím operátorů += atd.
+
+# import hlavního balíčku knihovny Numpy
+import numpy
+
+# původní matice
+a1 = numpy.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+
+# tisk matice
+print(a1)
+
+# provedení operace
+a1 += 100
+
+# tisk výsledku operace
+print(a1)
