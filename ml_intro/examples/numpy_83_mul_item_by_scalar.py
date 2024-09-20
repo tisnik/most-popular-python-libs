d2f8c7fa096180ffc168ebf90e720ab01afe8630
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-09-20 09:23:24 +0200

    NumPy example #83

---
+++ ml_intro/examples/numpy_83_mul_item_by_scalar.py
@@ -0,0 +1,16 @@
+# Vynásobení prvků matice dvěma
+
+# import hlavního balíčku knihovny Numpy
+import numpy
+
+# konstrukce pole
+a = numpy.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+
+# provedení operace
+b = a * 2
+
+# tisk původního pole
+print(a)
+
+# tisk nového pole
+print(b)
