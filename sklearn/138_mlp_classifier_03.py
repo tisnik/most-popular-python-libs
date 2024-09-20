02cf12b199c5fa9d0e34d86181645e60cfe301a4
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-09-20 09:24:41 +0200

    Change MLP classifier hyperparamters

---
+++ sklearn/138_mlp_classifier_03.py
@@ -0,0 +1,42 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+
+from sklearn.metrics import accuracy_score
+
+# model zalozeny na neuronove siti
+from sklearn.neural_network import MLPClassifier
+
+
+# nacteni datove sady
+iris = load_iris()
+
+# konstrukce klasifikatoru
+# (s hyperparametrem)
+classifier = MLPClassifier(max_iter=5000, hidden_layer_sizes = (10, 10, 10))
+
+# X je matice (feature matrix)
+X = iris.data
+
+# y je vektor (response vector)
+y = iris.target
+
+# rozdělení na trénovací a testovací data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+# trening modelu
+classifier.fit(X_train, y_train)
+
+
+# výsledky modelu (predikované výsledky)
+y_pred = classifier.predict(X_test)
+
+# vypoctena presnost modelu
+print(accuracy_score(y_test, y_pred))
+
+print(f"Features: {classifier.n_features_in_}")
+print(f"Layers:   {classifier.n_layers_}")
+print(f"Outputs:  {classifier.n_outputs_}")
+print("Weights:")
+
+for layer, weights in enumerate(classifier.coefs_):
+    print("\t", layer, weights.shape)
