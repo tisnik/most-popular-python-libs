c8f7638afb56d474f11904dd03724b841a6a914e
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-09-20 09:23:24 +0200

    NumPy example #82

---
+++ ml_intro/examples/numpy_82_add_item_by_scalar.py
@@ -0,0 +1,16 @@
+# Přičtení hodnoty ke všem prvkům matice
+
+# import hlavního balíčku knihovny Numpy
+import numpy
+
+# konstrukce pole
+a = numpy.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+
+# provedení operace
+b = a + 100
+
+# tisk původního pole
+print(a)
+
+# tisk nového pole
+print(b)
