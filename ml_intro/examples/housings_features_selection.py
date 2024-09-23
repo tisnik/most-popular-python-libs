cca4576eb6b8b47b9d525d9e1d34d1c8ab4cbdb6
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-09-23 08:30:30 +0200

    California housings: features selection

---
+++ ml_intro/examples/housings_features_selection.py
@@ -0,0 +1,32 @@
+import numpy as np
+from sklearn.datasets import fetch_california_housing
+from sklearn import linear_model
+from sklearn.model_selection import cross_val_score
+
+# nacteni datove sady
+housings = fetch_california_housing()
+
+# X je matice (feature matrix)
+X = housings.data
+
+# y je vektor (response vector)
+y = housings.target
+
+feature_names = np.array(housings.feature_names)
+
+print("n", "selectors", "features")
+
+for i in range(1, 2**len(feature_names)):
+    indexes = []
+    n = i
+    for j in range(len(feature_names)):
+        if n % 2 == 1:
+            indexes.append(j)
+        n //= 2
+    selectors = np.array(indexes, dtype=int)
+    # konstrukce modelu
+    lr = linear_model.LinearRegression()
+    selected_features = X[:, selectors]
+    scores = -cross_val_score(lr, selected_features, y, cv=10, scoring='neg_mean_squared_error')
+    print(i, selectors, feature_names[selectors], scores.mean())
+
