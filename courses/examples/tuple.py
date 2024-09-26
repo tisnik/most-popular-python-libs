68f25ad4b4bd64159a2656594bb07da8c76285c2
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-09-26 16:44:43 +0200

    Tuple uage

---
+++ courses/examples/tuple.py
@@ -0,0 +1,20 @@
+# tříprvková n-tice
+t1 = (1, 2, 3)
+
+# jednoprvková n-tice
+t2 = (4, )
+
+# pozor - není tuple!
+t3 = (5)
+
+# nehomogenní n-tice
+t4 = (4, 3.14, "string", [])
+
+# prázdná n-tice
+t5 = ()
+
+print(t1)
+print(t2)
+print(t3)
+print(t4)
+print(t5)
