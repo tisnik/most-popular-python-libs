1ec6d3cb259918177b988cf7d05a0ffef22d9952
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-09-26 16:44:43 +0200

    Strings usage

---
+++ courses/examples/strings.py
@@ -0,0 +1,22 @@
+# běžný řetězec
+print("Hello")
+
+# bytové pole
+print(b"abcdef")
+
+# podpora pro Unicode
+print(u"příliš žluťoučký kůň")
+
+# řídicí znaky
+print("abc\ndef")
+
+# 'raw' řetězce
+print(r"abc\ndef")
+
+# víceřádkový řetězec
+print("""A
+B
+C
+D
+E
+F""")
