84811d11122776219de6623fcba5403fa3014f4d
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-09-26 16:44:43 +0200

    List comprehension

---
+++ courses/examples/list_comprehension.py
@@ -0,0 +1,9 @@
+seznam = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+seznam2 = [item*2 for item in seznam]
+
+seznam3 = [item for item in seznam if item % 3 == 0]
+
+print(seznam)
+print(seznam2)
+print(seznam3)
