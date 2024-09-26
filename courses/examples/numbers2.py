e8e3a61f4231247c5ffc4fbef570f87b7a8a96a0
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-09-26 16:44:43 +0200

    Number operations

---
+++ courses/examples/numbers2.py
@@ -0,0 +1,14 @@
+# int/long
+print(42)
+print(100000000000000000000000000000000000000000000000000000)
+
+# float
+print(3.1415)
+print(1.2e10)
+print(3e-3)
+print(-3e-3)
+
+# complex
+print(2 + 3j)
+print(1.5 + 2.8j)
+print(1e10 + 3e-5j)
