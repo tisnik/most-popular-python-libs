91f8b275f58186471290918eaa197b5a2c96467c
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-09-30 09:19:21 +0200

    Style improvement: double quotes

--- sklearn/108_features_selection_iris.py
+++ sklearn/108_features_selection_iris.py
@@ -43,6 +43,6 @@ for i in range(1, 2**len(feature_names)):
     selectors = np.array(indexes, dtype=int)
     knn_classifier = KNeighborsClassifier(n_neighbors=5)
     selected_features = X[:, selectors]
-    scores = cross_val_score(knn_classifier, selected_features, y, cv=10, scoring='accuracy')
+    scores = cross_val_score(knn_classifier, selected_features, y, cv=10, scoring="accuracy")
     print(i, selectors, feature_names[selectors], scores.mean())
 
