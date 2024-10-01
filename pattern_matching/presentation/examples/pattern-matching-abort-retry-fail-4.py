c13615bc027c16d9a243ecd4d81931b05733462f
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-10-01 08:40:37 +0200

    Pattern match for abort-retry-fail, solution #4

---
+++ pattern_matching/presentation/examples/pattern-matching-abort-retry-fail-4.py
@@ -0,0 +1,21 @@
+# Strukturální pattern matching:
+# - rozhodování realizované pattern matchingem
+
+print("Not ready reading drive A")
+
+
+def abort_retry_fail():
+    # získání odpovědi od uživatele
+    response = input("Abort, Retry, Fail? ")
+
+    # rozhodnutí o provedené operaci na základě odpovědi
+    match response:
+        case "a":
+            return "Abort"
+        case "r":
+            return "Retry"
+        case "f":
+            return "Fail"
+
+
+print(abort_retry_fail())
