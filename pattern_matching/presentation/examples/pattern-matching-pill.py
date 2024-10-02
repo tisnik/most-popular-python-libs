06e7c1f60900f6c2e90cf1978dccb472e42d29cd
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-10-02 08:52:25 +0200

    Red or blue pill

---
+++ pattern_matching/presentation/examples/pattern-matching-pill.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+class Color(Enum):
+    RED = 0
+    GREEN = 1
+    BLUE = 2
+
+match pill:
+    case Color.RED:
+        print("you stay in Wonderland, and I show you how deep the rabbit hole goes.")
+    case Color.BLUE:
+        print("the story ends, you wake up in your bed and believe whatever you want to believe")
+    case _:
+        print("this does not compute")
