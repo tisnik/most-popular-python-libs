8f343a41a141255cfba9f8ea89fef24a649765cd
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-10-02 08:52:25 +0200

    Pattern matchings and objects, example #2

---
+++ pattern_matching/presentation/examples/pattern-matching-object2.py
@@ -0,0 +1,60 @@
+# Strukturální pattern matching:
+# - pattern matching a objekty
+
+from fractions import Fraction
+
+
+class Complex():
+    """Třída představující komplexní čísla."""
+
+    def __init__(self, real, imag):
+        self.real = real
+        self.imag = imag
+
+    def __str__(self):
+        return f"Complex number {self.real}+i{self.imag} represented as object"
+
+
+def test_number(value):
+    """Test, o jakou variantu komplexního čísla se jedná."""
+    match value:
+        case (0, 0):
+            print("Zero")
+        case (real, 0) if real>0:
+            print(f"Positive real number {real}")
+        case (real, 0):
+            print(f"Negative real number {real}")
+        case (0, imag) if imag<0:
+            print(f"Negative imaginary number {imag}")
+        case (0, imag):
+            print(f"Positive imaginary number {imag}")
+        case (real, imag):
+            print(f"Complex number {real}+i{imag}")
+        case Complex(real=0, imag=0):
+            print("Zero complex represented as object")
+        case Complex():
+            print(value)
+        case Fraction():
+            print(f"Fraction {value}")
+        case _:
+            raise ValueError("Not a complex number")
+
+
+test_number((0,0))
+test_number((1,0))
+test_number((-1,0))
+test_number((0,1))
+test_number((0,-1))
+test_number((1,1))
+
+test_number(Complex(0,0))
+test_number(Complex(1,0))
+test_number(Complex(-1,0))
+test_number(Complex(0,1))
+test_number(Complex(0,-1))
+test_number(Complex(1,1))
+
+test_number(Fraction(0,1))
+test_number(Fraction(1,1))
+test_number(Fraction(1,2))
+test_number(Fraction(1,3))
