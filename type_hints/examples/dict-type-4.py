5922efbbc6f4e903b87d58f9e4baa9a196621f90
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-10-03 08:15:23 +0200

    Type hints in Python - elementary examples

---
+++ type_hints/examples/dict-type-4.py
@@ -0,0 +1,13 @@
+# - definice slovníku
+# - specifikace typu klíčů i typu hodnot
+
+from typing import Dict
+
+d: Dict[str, float] = {}
+
+d["foo"] = 1
+d["bar"] = 3.14
+d[10] = 10
+d[42] = "answer"
+
+print(d)
