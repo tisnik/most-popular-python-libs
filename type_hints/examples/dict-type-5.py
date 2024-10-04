af4faaf0ef8a6a5f33b6ee626325bff1d3c3ff3e
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-10-04 09:35:35 +0200

    Type hint in Python: a bit more detailed examples

---
+++ type_hints/examples/dict-type-5.py
@@ -0,0 +1,14 @@
+# - definice slovníku
+# - specifikace typu klíčů i typu hodnot
+# - u hodnot je použit typ Union
+
+from typing import Dict, Union
+
+d: Dict[str, Union[int, float, str]] = {}
+
+d["foo"] = 1
+d["bar"] = 3.14
+d[10] = 10
+d[42] = "answer"
+
+print(d)
