6684df3697a24c85694ba4ec85e8e4ffc6d56ff4
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-10-04 09:35:35 +0200

    Type hint in Python: a bit more detailed examples

---
+++ type_hints/examples/variance-6.py
@@ -0,0 +1,35 @@
+# - návratové typy jsou kovariantní
+
+from typing import Callable
+
+
+class Ovoce:
+    """Třída, která je předkem tříd Hruska i Jablko."""
+
+    pass
+
+
+class Hruska(Ovoce):
+    """Potomek třídy Ovoce."""
+
+    def __repr__(self) -> str:
+        """Tisk 'hodnoty' objektu."""
+        return "Hruska"
+
+
+class Jablko(Ovoce):
+    """Potomek třídy Ovoce."""
+
+    def __repr__(self) -> str:
+        """Tisk 'hodnoty' objektu."""
+        return "Jablko"
+
+
+def utrhni(f: Callable[[], Ovoce]) -> Ovoce:
+    """Zavolá funkci, která získá jeden kus ovoce a vrátí ho."""
+    ovoce = f()
+    return ovoce
+
+
+print(utrhni(Hruska))
+print(utrhni(Jablko))
