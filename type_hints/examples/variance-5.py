db2b41bf39441392b9df5679d0dac179ee6ae365
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-10-04 09:35:35 +0200

    Type hint in Python: a bit more detailed examples

---
+++ type_hints/examples/variance-5.py
@@ -0,0 +1,44 @@
+# - hierarchie tříd Ovoce <- Hruska a Ovoce <- Jablko
+# - funkce `tiskni` akceptuje neměnitelnou sekvenci s ovocem
+# - samotný seznam se přitom ve funkci nemění (jen se čte)
+# - funkci `tiskni` voláme s prázdným seznamem pro Hrušky
+# - tisk anotace funkce `tiskni`
+
+from typing import Sequence
+
+
+class Ovoce:
+    """Třída, která je předkem tříd Hruska i Jablko."""
+
+    pass
+
+
+class Hruska(Ovoce):
+    """Potomek třídy Ovoce."""
+
+    def __repr__(self) -> str:
+        """Tisk 'hodnoty' objektu."""
+        return "Hruska"
+
+
+class Jablko(Ovoce):
+    """Potomek třídy Ovoce."""
+
+    def __repr__(self) -> str:
+        """Tisk 'hodnoty' objektu."""
+        return "Jablko"
+
+
+def tiskni(kosik: Sequence[Ovoce]) -> None:
+    """Vytiskne obsah košíku s ovocem."""
+    for ovoce in kosik:
+        print(ovoce)
+
+
+# košík, který může obsahovat pouze hrušky
+kosik: Sequence[Hruska] = []
+
+tiskni(kosik)
+
+# tisk anotace funkce `tiskni`
+print(tiskni.__annotations__)
