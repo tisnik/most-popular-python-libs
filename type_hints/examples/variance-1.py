80b3e48cfba3f382273728a0c3a40b68739afd5a
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-10-04 09:35:35 +0200

    Type hint in Python: a bit more detailed examples

---
+++ type_hints/examples/variance-1.py
@@ -0,0 +1,43 @@
+# - hierarchie tříd Ovoce <- Hruska a Ovoce <- Jablko
+# - funkce `smichej` akceptuje seznam s ovocem
+# - přidá do tohoto seznamu Hrusku a Jablko
+# - funkci `smichej` voláme s prázdným seznamem pro Ovoce
+
+from typing import List
+
+
+class Ovoce:
+    """Třída, která je předkem tříd Hruska i Jablko."""
+
+    pass
+
+
+class Hruska(Ovoce):
+    """Potomek třídy Ovoce."""
+
+    def __repr__(self) -> str:
+        """Tisk 'hodnoty' objektu."""
+        return "Hruska"
+
+
+class Jablko(Ovoce):
+    """Potomek třídy Ovoce."""
+
+    def __repr__(self) -> str:
+        """Tisk 'hodnoty' objektu."""
+        return "Jablko"
+
+
+def smichej(kosik: List[Ovoce]) -> None:
+    """Do košíku se přidá jedna hruška a jedno jablko."""
+    kosik.append(Hruska())
+    kosik.append(Jablko())
+
+
+# košík, který může obsahovat hrušky i jablka
+kosik: List[Ovoce] = []
+
+smichej(kosik)
+
+for ovoce in kosik:
+    print(ovoce)
