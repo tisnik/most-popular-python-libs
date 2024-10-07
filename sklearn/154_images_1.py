0b77be54c75dd0ff8b631940ef0e5fea6c41a1e8
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-10-07 08:45:31 +0200

    Image recognition using scikit-learn: logistic regression model

---
+++ sklearn/154_images_1.py
@@ -0,0 +1,95 @@
+# Rozpoznání obrazu s využitím knihovny scikit-learn
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import ConfusionMatrixDisplay
+from sklearn.model_selection import cross_val_score
+
+# Databáze ručně zapsaných číslic
+from sklearn.datasets import load_digits
+
+# načtení obrázků, s nimiž budeme dále pracovat
+digits = load_digits()
+
+# zjištění základních informací o obrázcích
+print("Description:", digits.DESCR)
+
+print("Data:", digits.data.shape)
+print("Obrázky:", digits.images.shape)
+
+# X je matice (feature matrix)
+X = digits.data
+
+# y je vektor (response vector)
+y = digits.target
+
+# rozdělení dat
+trainX, testX, trainY, testY = train_test_split(X, y, test_size = 0.2)
+
+print()
+print("TrainX:", trainX.shape)
+print("TrainY:", trainY.shape)
+print("TestX:", testX.shape)
+print("TestY:", testY.shape)
+
+# provést klasifikaci
+# konstrukce klasifikatoru
+# (s hyperparametrem)
+classifier = LogisticRegression(max_iter=1000)
+
+# vypocet skore
+scores = cross_val_score(classifier, X, y, cv=10, scoring="accuracy")
+
+avg_score = scores.mean()
+
+# vypsani prumerneho skore
+print()
+print("Accuracy:", avg_score)
+print()
+
+# trening noveho modelu
+classifier = LogisticRegression(max_iter=1000)
+classifier.fit(trainX, trainY)
+
+class_names = digits.target_names
+
+# absolutni hodnoty
+disp = ConfusionMatrixDisplay.from_estimator(
+    classifier,
+    testX,
+    testY,
+    display_labels=class_names,
+    cmap=plt.cm.Blues,
+    normalize=None,
+)
+
+# zobrazeni matice
+print(disp.confusion_matrix)
+
+# ulozeni vysledku
+plt.savefig("154_1.png")
+
+# vizualizace matice
+plt.show()
+
+# relativni hodnoty
+disp = ConfusionMatrixDisplay.from_estimator(
+    classifier,
+    testX,
+    testY,
+    display_labels=class_names,
+    cmap=plt.cm.Blues,
+    normalize="true",
+)
+
+# zobrazeni matice
+print(disp.confusion_matrix)
+
+# ulozeni vysledku
+plt.savefig("154_2.png")
+
+# vizualizace matice
+plt.show()
