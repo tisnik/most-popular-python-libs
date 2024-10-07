7fff8eb979c658a25eff22bb082d5cae8a7a7e40
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-10-07 08:45:31 +0200

    Proper quotes in source code

--- sklearn/147_mlp_regression_6.py
+++ sklearn/147_mlp_regression_6.py
@@ -33,7 +33,7 @@ for layers in r:
     nn = MLPRegressor(max_iter=5000, hidden_layer_sizes = layer_sizes)
 
     # vypocet skore
-    scores = cross_val_score(nn, X, y, cv=10, scoring='r2', n_jobs=-1)
+    scores = cross_val_score(nn, X, y, cv=10, scoring="r2", n_jobs=-1)
 
     # vypsani prumerneho skore do tabulky
     avg_score = scores.mean()
