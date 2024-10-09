6e4fe1b386fe8a3eebb21beeba148eb3bfbef595
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-10-09 08:28:03 +0200

    Raising exception from the code

---
+++ courses/examples/raise_exception.py
@@ -0,0 +1,13 @@
+def function(level):
+    if level < 1:
+        raise ValueError("Invalid level!", level)
+    print("ok - function was called with parameter level set to {level}".format(level=level))
+
+
+try:
+    for i in range(10, -10, -1):
+        function(i)
+except ValueError as value:
+    print("Exception in function(): {value}".format(value=value))
+else:
+    print("Everything is OK")
