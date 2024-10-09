fb0d522d0877d6ee80008d01a5adf1d1f9202dd8
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-10-09 08:28:03 +0200

    Custom exception usage

---
+++ courses/examples/custom_exception.py
@@ -0,0 +1,18 @@
+class B(Exception):
+    pass
+
+class C(B):
+    pass
+
+class D(C):
+    pass
+
+for cls in [B, C, D]:
+    try:
+        raise cls()
+    except D:
+        print("D")
+    except C:
+        print("C")
+    except B:
+        print("B")
