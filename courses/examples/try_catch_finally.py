f4d3d0f71a6c7fd284ab47b9080f7c4a5dee579f
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-10-09 08:28:03 +0200

    Try-catch-finally

---
+++ courses/examples/try_catch_finally.py
@@ -0,0 +1,8 @@
+try:
+    file = open("testfile", "w")
+    file.write("test")
+except IOError:
+    print("soubor nelze otevrit pro zapis")
+else:
+    print("zapis uspesne proveden")
+    file.close()
