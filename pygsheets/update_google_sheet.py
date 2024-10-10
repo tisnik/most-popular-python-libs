e3568316a2656d82fe8fdbff9cff779b7cfe9f5d
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-10-10 16:38:04 +0200

    Added license header

--- pygsheets/update_google_sheet.py
+++ pygsheets/update_google_sheet.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python3
 
+#
+#  (C) Copyright 2024  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
 """Import CSV into Google sheet.
 
 In order to use this script, file with client secret to Google API must be present (symlinked etc.)
