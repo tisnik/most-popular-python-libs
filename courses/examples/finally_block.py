0b0ea7b9c89bc58ca57d91a0769094da4acbae2d
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-10-28 08:40:47 +0100

    New example for slides

---
+++ courses/examples/finally_block.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+try:
+    file = open("testfile", "w")
+    file.write("test")
+except IOError:
+    print("soubor nelze otevrit pro zapis")
+else:
+    print("zapis uspesne proveden")
+finally:
+    file.close()
