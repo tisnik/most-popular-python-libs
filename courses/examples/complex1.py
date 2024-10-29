1d8d92aeb610623f208634f041ab0754fd5be74f
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-10-29 09:35:58 +0100

    Examples for Python course

---
+++ courses/examples/complex1.py
@@ -0,0 +1,16 @@
+class Complex:
+    def __init__(self, real=0, imag=0):
+        self._real = real
+        self._imag = imag
+
+
+c1 = Complex(1, 2)
+c2 = Complex(10, 20)
+c3 = Complex(100)
+c4 = Complex()
+
+c5 = Complex(1, 2)
+print(c1)
+print(c2)
+print(c3)
+print(c4)
