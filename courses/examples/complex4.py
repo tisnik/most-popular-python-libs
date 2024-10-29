ae3223e223e1dd0b451f77a9bed8f921fe24cf80
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-10-29 09:35:58 +0100

    Examples for Python course

---
+++ courses/examples/complex4.py
@@ -0,0 +1,34 @@
+class Complex:
+    def __init__(self, real=0, imag=0):
+        self._real = real
+        self._imag = imag
+
+    def __str__(self):
+        return "{r} + {i}j".format(r=self._real, i=self._imag)
+
+    def __eq__(self, other):
+        return self._real == other._real and self._imag == other._imag
+
+    def __add__(self, other):
+        r = self._real + other._real
+        i = self._imag + other._imag
+        return Complex(r, i)
+
+
+c1 = Complex(1, 2)
+c2 = Complex(10, 20)
+c3 = Complex(100)
+c4 = Complex()
+
+c1 += c3
+print(c1)
+
+c5 = Complex(1, 2)
+print(c1)
+print(c2)
+print(c3)
+print(c4)
+
+print(c1 == c5)
+print(c2 == c5)
+print(c3 == c5)
