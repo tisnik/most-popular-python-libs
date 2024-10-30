185b0e70fc41524acc0dd9847137af2ea7769d25
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-10-30 08:53:07 +0100

    Activation function in PyTorch

---
+++ PyTorch/activation_functions.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+
+# hodnoty na x-ové ose
+x = torch.linspace(-4, 4, 200)
+
+# výpočet hodnot aktivačních funkcí
+relu = nn.ReLU()(x)
+tanh = nn.Tanh()(x)
+sigmoid = nn.Sigmoid()(x)
+ 
+# nový graf s průběhem tří aktivačních funkcí
+plt.plot(x, sigmoid, label="sigmoid")
+plt.plot(x, tanh, label="tanh")
+plt.plot(x, relu, label="ReLU")
+plt.ylim(-1.5, 2)
+
+# zobrazení legendy
+plt.legend()
+
+# uložení do souboru
+plt.savefig("activation_functions.png")
+
+# zobrazení v novém okně
+plt.show()
