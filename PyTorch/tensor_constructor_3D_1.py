8f798fe73e232b20ec7bf23de8c5375f2add0d87
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-10-31 08:16:21 +0100

    Tensor constructor for 3D structures

---
+++ PyTorch/tensor_constructor_3D_1.py
@@ -0,0 +1,9 @@
+import torch
+
+# 2D struktury - matice
+m1 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+m2 = [[10, 20, 30], [40, 50, 60], [70, 80, 90]]
+
+# konstrukce tenzoru
+c = torch.Tensor([m1, m2])
+print(c)
