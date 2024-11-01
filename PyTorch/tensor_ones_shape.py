dc1d99fab638a0775ee815c73b1ab0d563d60cb9
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-11-01 08:17:26 +0100

    Tensor filled-in by ones

---
+++ PyTorch/tensor_ones_shape.py
@@ -0,0 +1,14 @@
+import torch
+
+# konstrukce tenzoru, vyplneni jednickami
+v1 = torch.ones(1)
+print(v1)
+
+v2 = torch.ones(10)
+print(v2)
+
+m1 = torch.ones(3, 4)
+print(m1)
+
+c1 = torch.ones(3, 4, 5)
+print(c1)
