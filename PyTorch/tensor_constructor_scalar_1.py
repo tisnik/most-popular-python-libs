337989cb474bad950b88b9e47d0003807d037edb
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-11-04 08:45:19 +0100

    Updated comments in this example

--- PyTorch/tensor_constructor_scalar_1.py
+++ PyTorch/tensor_constructor_scalar_1.py
@@ -1,13 +1,13 @@
 import torch
 
-# konstrukce tenzoru
-s1 = torch.Tensor(1)
+# skutecny skalar - tenzor nulteho radu
+s1 = torch.tensor(100)
 print(s1)
+
+# konstrukce tenzoru prvniho radu (jednoprvkovy vektor)
+s2 = torch.Tensor(1)
+print(s2)
 
 # pristup k (jedine) slozce tenzoru
-s1[0] = 42
-print(s1)
-
-# skutecny skalar
-s2 = torch.tensor(100)
+s2[0] = 42
 print(s2)
