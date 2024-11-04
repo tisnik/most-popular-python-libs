47f1533d7142046c9ec4726961abebeae7223f94
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-11-04 08:45:19 +0100

    Updated comments in this example

--- PyTorch/tensor_constructor_vector_1.py
+++ PyTorch/tensor_constructor_vector_1.py
@@ -1,10 +1,10 @@
 import torch
 
-# konstrukce tenzoru
+# konstrukce tenzoru prvniho radu (vektoru)
 v1 = torch.Tensor(3)
 print(v1)
 
-# pristup k (jedine) slozce tenzoru
+# inicializace jednotlivych prvku vektoru
 v1[0] = 10
 v1[1] = 20
 v1[2] = 30
