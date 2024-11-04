0accf21f81106fb1f5a57c85f0e12c32c77250e0
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-11-04 08:45:19 +0100

    Updated comments in this example

--- PyTorch/tensor_constructor_scalar_zero.py
+++ PyTorch/tensor_constructor_scalar_zero.py
@@ -1,13 +1,15 @@
 import torch
 
-# konstrukce tenzoru
-s3 = torch.Tensor(1).zero_()
-print(s3)
+# toto je skutecny skalar
+s1 = torch.tensor(1).zero_()
+print(s1)
+
+# konstrukce tenzoru prvniho radu s jednim prvkem
+# vynulovani prvku
+s2 = torch.Tensor(1).zero_()
+print(s2)
 
 # pristup k (jedine) slozce tenzoru
-s3[0] = 42
-print(s3)
+s2[0] = 42
+print(s2)
 
-# toto je skutecny skalar
-s4 = torch.tensor(1).zero_()
-print(s4)
