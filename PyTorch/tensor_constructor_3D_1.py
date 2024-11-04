208332dbc8f9176519d42e38944b02c6cf4c215a
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-11-04 08:45:18 +0100

    Updated comments in this example

--- PyTorch/tensor_constructor_3D_1.py
+++ PyTorch/tensor_constructor_3D_1.py
@@ -4,6 +4,6 @@ import torch
 m1 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 m2 = [[10, 20, 30], [40, 50, 60], [70, 80, 90]]
 
-# konstrukce tenzoru
+# konstrukce tenzoru tretiho radu
 c = torch.Tensor([m1, m2])
 print(c)
