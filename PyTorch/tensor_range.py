36f9602d78a175e4713c68f882a259cc1d469ead
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-11-05 08:13:15 +0100

    Updated comments, more examples

--- PyTorch/tensor_range.py
+++ PyTorch/tensor_range.py
@@ -4,8 +4,15 @@ import torch
 v1 = torch.range(10, 20)
 print(v1)
 
+# specifikace kroku
 v2 = torch.range(10, 20, 2)
 print(v2)
 
+# zaporny krok
 v3 = torch.range(10, 0, -1)
 print(v3)
+
+# krok, ktery neni celociselny
+v4 = torch.range(0, 10, 0.5)
+print(v4)
+
