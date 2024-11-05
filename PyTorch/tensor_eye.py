e1ab348af90b6359d62e3c68e72b0566e328f7e8
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-11-05 08:13:56 +0100

    Eye constructor

---
+++ PyTorch/tensor_eye.py
@@ -0,0 +1,11 @@
+import torch
+
+# konstrukce jednotkove matice
+m1 = torch.eye(10)
+print(m1)
+print()
+
+# konstrukce jednotkove matice se specifikaci sloupcu
+m2 = torch.eye(10, 15)
+print(m2)
+print()
