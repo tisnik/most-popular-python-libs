762d42a802c624a0ecca31c8a5910aeb363d7036
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-11-05 08:13:23 +0100

    Casting tensor storage

---
+++ PyTorch/tensor_storage_casts.py
@@ -0,0 +1,16 @@
+import torch
+
+# konstrukce tenzoru
+v1 = torch.tensor(10)
+print(v1)
+print(type(v1))
+print(v1.dtype)
+
+print()
+
+storage = v1.untyped_storage()
+
+print(storage.int())
+print(storage.half())
+print(storage.float())
+print(storage.double())
