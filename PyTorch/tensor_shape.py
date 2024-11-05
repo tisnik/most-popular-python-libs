b2c2a878c29062a41a6b7a76d8f3e07f7921e73a
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-11-05 08:13:23 +0100

    Tensor shape

---
+++ PyTorch/tensor_shape.py
@@ -0,0 +1,22 @@
+import torch
+
+# konstrukce tenzoru + zjisteni jejich tvaru
+
+# tenzor nulteho radu - skalar
+s1 = torch.tensor(100)
+print(s1.shape)
+
+# tenzor prvniho radu - vektor
+v1 = torch.Tensor(1)
+print(v1.shape)
+
+v2 = torch.Tensor(3)
+print(v2.shape)
+
+# tenzor druheho radu - matice
+m1 = torch.Tensor(3, 4)
+print(m1.shape)
+
+# tenzor tretiho radu - 3D pole
+c1 = torch.Tensor(3, 4, 5)
+print(c1.shape)
