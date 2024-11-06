740e15506b50a8bcf66d7f2113201750ed71c8cb
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-11-06 08:27:53 +0100

    Tensors internal storage structure, example #1

---
+++ PyTorch/tensor_storage_1.py
@@ -0,0 +1,15 @@
+import torch
+
+# konstrukce tenzoru prvniho radu
+v1 = torch.tensor([1, 255, 65535, 65536])
+print(v1)
+print(type(v1))
+print(v1.dtype)
+
+print()
+
+# informace o typu Storage
+storage = v1.untyped_storage()
+
+print(storage)
+print(type(storage))
