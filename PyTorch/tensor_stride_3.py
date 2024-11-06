cb0edd6f7bb08d2f6232cf13011de99013673821
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-11-06 08:27:53 +0100

    Tensors stride(s) and offset(s), example #3

---
+++ PyTorch/tensor_stride_3.py
@@ -0,0 +1,30 @@
+import torch
+
+# konstrukce tenzoru
+m1 = torch.Tensor([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]])
+print("m1:")
+print(m1)
+print("Stride:", m1.stride())
+print("Offset:", m1.storage_offset())
+print()
+
+m2 = m1[1:3]
+print("m2:")
+print(m2)
+print("Stride:", m2.stride())
+print("Offset:", m2.storage_offset())
+print()
+
+m3 = m1[:,1:3]
+print("m3:")
+print(m3)
+print("Stride:", m3.stride())
+print("Offset:", m3.storage_offset())
+print()
+
+m4 = m1[1:3,1:3]
+print("m4:")
+print(m4)
+print("Stride:", m4.stride())
+print("Offset:", m4.storage_offset())
+print()
