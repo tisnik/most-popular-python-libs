4173f48a9d34a99d1e8c3fd37e61ac4f94dfce50
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-11-06 08:27:53 +0100

    Tensors internal storage structure, example #2

---
+++ PyTorch/tensor_storage_2.py
@@ -0,0 +1,15 @@
+import torch
+
+# konstrukce tenzoru druheho radu
+m2 = torch.tensor([[1,2,3], [4,5,6]]).type(torch.uint8)
+print(m2)
+print(type(m2))
+print(m2.dtype)
+
+print()
+
+# informace o typu Storage
+storage = m2.untyped_storage()
+
+print(storage)
+print(type(storage))
