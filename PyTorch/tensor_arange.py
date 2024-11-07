008a20636c98c90086b1058c5842c66294eef658
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-11-07 08:43:16 +0100

    Tensor constructor arange

---
+++ PyTorch/tensor_arange.py
@@ -0,0 +1,18 @@
+import torch
+
+# konstrukce tenzoru, vyplneni sekvenci
+v1 = torch.arange(10, 20)
+print(v1)
+
+# specifikace kroku
+v2 = torch.arange(10, 20, 2)
+print(v2)
+
+# zaporny krok
+v3 = torch.arange(10, 0, -1)
+print(v3)
+
+# krok, ktery neni celociselny
+v4 = torch.arange(0, 10, 0.5)
+print(v4)
+
