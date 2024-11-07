4ca44be3f85182b8fcc040ff1b16277a2626d322
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-11-07 08:43:16 +0100

    Dot product implementation

---
+++ PyTorch/tensor_dot_product.py
@@ -0,0 +1,17 @@
+import torch
+
+# konstrukce 1D tenzoru
+v1 = torch.Tensor([2, 2, 3, 5])
+print(v1)
+print()
+
+# konstrukce druheho 2D tenzoru
+v2 = torch.Tensor([2, 2, 3, 5])
+print(v2)
+print()
+
+# skalarni soucin
+s = torch.dot(v1, v2)
+print(s)
+
+# vysledkem je 42 - nahoda???
