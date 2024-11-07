ec5a43655e9882ffd1e1107157eafc27ca80b629
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-11-07 08:43:16 +0100

    Multiply two tensors

---
+++ PyTorch/tensor_operator_mul.py
@@ -0,0 +1,18 @@
+import torch
+
+# konstrukce tenzoru, vyplneni sekvenci
+v1 = torch.range(1, 12)
+print(v1)
+print()
+
+m1 = torch.reshape(v1, (4, 3))
+print(m1)
+print()
+
+m2 = torch.ones(4, 3) + torch.ones(4, 3)
+print(m2)
+print()
+
+m3 = m1 * m2
+print(m3)
+print()
