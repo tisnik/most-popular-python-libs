7aae9cb5a23b3084d2e6757922aeb0202b6b55d5
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-11-07 08:43:16 +0100

    Test if slice is just a view

---
+++ PyTorch/tensor_is_slice_view.py
@@ -0,0 +1,19 @@
+import torch
+
+# konstrukce tenzoru
+v1 = torch.arange(10, 20)
+print(v1)
+
+# konstrukce rezu
+v2 = v1[5:10]
+print(v2)
+
+# modifikace vektoru
+v1[5] = 999
+print(v1)
+print(v2)
+
+# modifikace rezu
+v2[0] = 0
+print(v1)
+print(v2)
