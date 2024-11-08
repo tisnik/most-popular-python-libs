32fc9a96a5eb225db18dc29db7ee96836cb658e7
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-11-08 08:48:27 +0100

    Download stopwords

---
+++ sklearn/173_download_stopwords.py
@@ -0,0 +1,7 @@
+# Stažení slovníku, který bude použit pro předzpracování textu v dalších
+# demonstračních příkladech.
+
+import nltk
+
+# tento příkaz zajistí stažení příslušných datových souborů
+nltk.download("stopwords")
