36f3e197984454a9eb3e53043ad30e6aaa9f7cef
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-11-11 08:48:03 +0100

    Tensor reshape operation

---
+++ PyTorch/tensor_reshape_2.py
@@ -0,0 +1,9 @@
+import torch
+
+# konstrukce tenzoru
+m1 = torch.Tensor([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]])
+print(m1)
+print()
+
+v1 = torch.reshape(m1, (16, ))
+print(v1)
