e20c38f1ecdd0ad9cbf5c92ced1f572d572e2cfb
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-11-13 09:24:42 +0100

    Using more frequent words in corpus

---
+++ sklearn/196_more_frequent_words.py
@@ -0,0 +1,35 @@
+# Výpočet relevance jednotlivých slov
+
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+
+# sada textů (korpus)
+corpus = [
+    "I'd like an apple or an apple pie",
+    "An apple a day keeps the doctor away",
+    "Never compare an apple to an orange",
+    "I prefer scikit-learn to orange",
+    "The scikit-learn docs are orange and blue",
+    "New apple logo will be orange and blue",
+]
+
+# věty pro vyšší četnosti vybraných slov
+for i in range(20):
+    corpus.append("blue day")
+
+# provedení vektorizace korpusu
+vectorizer = CountVectorizer(min_df=1, stop_words="english")
+vectorized = vectorizer.fit_transform(corpus)
+
+# slova pro dekódování vah
+feature_names = vectorizer.get_feature_names_out()
+
+# převod na běžnou matici
+as_array = vectorized.toarray()
+
+# výpočet IDF - převrácené četnosti slov
+transformer = TfidfTransformer(smooth_idf=True, use_idf=True)
+transformer.fit(vectorized)
+
+# zobrazení tabulky s převrácenými četnostmi jednotlivých slov
+for feature_name, idf in zip(feature_names, transformer.idf_):
+    print(f"{feature_name:9} {idf:5.2}")
