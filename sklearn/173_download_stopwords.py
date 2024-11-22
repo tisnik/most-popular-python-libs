8ebafa32aec9ce0170742b1344b77340a1bc9d20
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-11-22 09:01:16 +0100

    Added license header

--- sklearn/173_download_stopwords.py
+++ sklearn/173_download_stopwords.py
@@ -1,3 +1,20 @@
+#
+#  (C) Copyright 2024  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+#
+# Tento demonstracni priklad je pouzity v serialu o datove analyze
+# s vyuzitim programovaciho jazyka Python:
+# https://www.root.cz/serialy/datova-analyza-s-vyuzitim-jazyka-python/
+#
+
 # Stažení slovníku, který bude použit pro předzpracování textu v dalších
 # demonstračních příkladech.
 
