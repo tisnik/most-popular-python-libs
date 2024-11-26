091adbcf46e810ee9681ef5beace85bc03fb52a2
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-11-26 08:41:09 +0100

    Updated comments

--- PyTorch/tensor_operator_mul.py
+++ PyTorch/tensor_operator_mul.py
@@ -5,14 +5,17 @@ v1 = torch.range(1, 12)
 print(v1)
 print()
 
+# konstrukce 2D matice z puvodniho vektoru
 m1 = torch.reshape(v1, (4, 3))
 print(m1)
 print()
 
+# vytvořeni druhe matice se stejnym tvarem
 m2 = torch.ones(4, 3) + torch.ones(4, 3)
 print(m2)
 print()
 
+# soucin metodou prvek po prvku
 m3 = m1 * m2
 print(m3)
 print()
