31e9f7819f7dc85d35cf2047b1dee79b894a7c9e
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-11-26 08:40:56 +0100

    Updated comments

--- PyTorch/tensor_narrow_operation_3.py
+++ PyTorch/tensor_narrow_operation_3.py
@@ -1,14 +1,16 @@
 import torch
 
-# konstrukce tenzoru
+# konstrukce tenzoru (matice 4x4 prvky)
 m1 = torch.Tensor([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]])
 print(m1)
 print()
 
+# pohled na původní matici
 m2 = torch.narrow(m1, 1, 1, 2)
 print(m2)
 print()
 
+# pohled na pohled
 m3 = torch.narrow(m2, 0, 1, 2)
 print(m3)
 print()
