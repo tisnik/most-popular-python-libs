0ea21f2f6d05ad2b01971d0418f286510f0b396d
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-11-27 09:13:55 +0100

    Tensor and value broadcasting, 2nd example

---
+++ PyTorch/tensor_broadcast_2.py
@@ -0,0 +1,21 @@
+import torch
+
+# konstrukce tenzoru, vyplneni sekvenci
+v1 = torch.arange(1, 13)
+print(v1)
+print()
+
+# vytvoreni matice z puvodniho vektoru
+m1 = torch.reshape(v1, (4, 3))
+print(m1)
+print()
+
+# jednorozmerny vektor
+v2 = torch.Tensor([1, 2, -1])
+print(v2)
+print()
+
+# nasobeni matice a vektoru prvek po prvku
+m2 = m1 * v2
+print(m2)
+print()
