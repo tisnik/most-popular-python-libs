42ab72bf6d99ae75a326f655ddfec7f356c52317
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-11-27 09:13:55 +0100

    Updated comments

--- PyTorch/tensor_broadcast_1.py
+++ PyTorch/tensor_broadcast_1.py
@@ -1,14 +1,16 @@
 import torch
 
 # konstrukce tenzoru, vyplneni sekvenci
-v1 = torch.range(1, 12)
+v1 = torch.arange(1, 13)
 print(v1)
 print()
 
+# vytvoreni matice z puvodniho vektoru
 m1 = torch.reshape(v1, (4, 3))
 print(m1)
 print()
 
+# vynasobeni kazdeho prvku matice skalarem
 m2 = m1 * 10
 print(m2)
 print()
