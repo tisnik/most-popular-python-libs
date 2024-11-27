204766cebf8719039353022d1dcb3f7107362b7a
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-11-27 09:13:55 +0100

    Updated comments

--- PyTorch/tensor_operator_matmul.py
+++ PyTorch/tensor_operator_matmul.py
@@ -5,14 +5,16 @@ v1 = torch.range(1, 12)
 print(v1)
 print()
 
+# konstrukce prvni matice z puvodniho tenzoru
 m1 = torch.reshape(v1, (4, 3))
 print(m1)
 print()
 
+# konstrukce druhe matice z puvodniho tenzoru
 m2 = torch.reshape(v1, (3, 4))
 print(m2)
 print()
 
+# provedeni maticoveho soucinu
 m3 = m1 @ m2
 print(m3)
-print()
