ffe60b5eb20ef6162ab8909d120b7c2c647a1718
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-11-28 08:46:00 +0100

    Narrow operation on matrix

---
+++ PyTorch/tensor_narrow_operation_2_B.py
@@ -0,0 +1,20 @@
+import torch
+
+# konstrukce tenzoru (matice 4x4 prvky)
+m1 = torch.Tensor([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]])
+print(m1)
+
+print()
+
+# realizace operace narrow s výběrem dimenze číslo 1
+m2 = m1.narrow(1, 1, 2)
+print(m2)
+
+print()
+
+# modifikace puvodni matice pres jeji pohled
+m2[0, 0] = 99
+print(m1)
+print()
+print(m2)
+
