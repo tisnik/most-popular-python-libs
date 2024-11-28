95c6cc9989f45f42a6bb631e27f17a50e178b11e
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-11-28 08:46:00 +0100

    Matrix multiplication with broadcasting

---
+++ PyTorch/tensor_operator_matmul_4.py
@@ -0,0 +1,22 @@
+import torch
+
+# konstrukce tenzoru, vyplneni sekvenci
+v1 = torch.range(1, 12)
+print(v1)
+print()
+
+# konstrukce prvni matice z puvodniho tenzoru
+m1 = torch.reshape(v1, (4, 3))
+print(m1)
+print()
+
+# konstrukce druhe matice z puvodniho tenzoru
+m2 = torch.Tensor([[[ 1,  1], [ 1,  1], [ 1,  1]],
+                   [[ 2,  2], [ 2,  2], [ 2,  2]],
+                   [[-1, -1], [-1, -2], [-1, -1]]])
+print(m2)
+print()
+
+# pokus o provedeni maticoveho soucinu s broadcastem
+m3 = m1 @ m2
+print(m3)
