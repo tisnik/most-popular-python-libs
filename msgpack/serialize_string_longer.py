b41761a4d7178218ab1f19c73acdd916af6ffa15
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-12-11 09:04:03 +0100

    Serializing longer string

---
+++ msgpack/serialize_string_longer.py
@@ -0,0 +1,7 @@
+import msgpack
+
+value = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+
+with open("longer_string.bin", "wb") as outfile:
+ packed = msgpack.packb(value)
+ outfile.write(packed)
