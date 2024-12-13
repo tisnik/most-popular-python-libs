553f07ec483abbfde32e3d6c3dd824cfc354fe05
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-12-13 10:06:59 +0100

    Added license header

--- sklearn/212_basic_filtering.py
+++ sklearn/212_basic_filtering.py
@@ -1,4 +1,21 @@
 # Vektorizace textových dat, výpis výsledného slovníku
+#
+#  (C) Copyright 2024  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+#
+# Tento demonstracni priklad je pouzity v serialu o datove analyze
+# s vyuzitim programovaciho jazyka Python:
+# https://www.root.cz/serialy/datova-analyza-s-vyuzitim-jazyka-python/
+#
+
 
 import pandas as pd
 import re
