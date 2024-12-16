87ef036510d1d6ff636163d057136b80a52be42d
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-12-16 09:23:55 +0100

    Bytes type literal

---
+++ binary_structs/bytes_literal.py
@@ -0,0 +1,15 @@
+x = bytes(b"foo")
+print(type(x))
+print(x)
+
+print()
+
+y = bytes(b'Write "Hello world"')
+print(type(y))
+print(y)
+
+print()
+
+z = bytes(b"""We can use 'a' and "b" there""")
+print(type(z))
+print(z)
