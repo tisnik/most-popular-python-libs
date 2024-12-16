58987d1a7acefff8c95517deeedcbda7990b83c5
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-12-16 09:23:55 +0100

    Converting from string with hexadecimal digits to bytes

---
+++ binary_structs/bytes_fromhex.py
@@ -0,0 +1,15 @@
+x = bytes.fromhex("")
+print(type(x))
+print(x)
+
+print()
+
+y = bytes.fromhex("00 0f 1f ff")
+print(type(y))
+print(y)
+
+print()
+
+z = bytes.fromhex("000f1fff")
+print(type(z))
+print(z)
