90033dc78a5df8ae469177cea99398098dc54252
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-12-18 10:03:58 +0100

    Pack negative byte value

---
+++ binary_structs/pack_byte_negative.py
@@ -0,0 +1,11 @@
+import struct
+
+bytes1 = struct.pack("b", -42)
+print(type(bytes1))
+print(bytes1.hex(" ", 1))
+
+print()
+
+bytes2 = struct.pack("B", -42)
+print(type(bytes2))
+print(bytes2.hex(" ", 1))
