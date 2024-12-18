f8aae22c64c5f663604e4243c5bb4f7439636822
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-12-18 10:03:59 +0100

    Pack part of PNG header, 1st variant

---
+++ binary_structs/pack_png_header_1.py
@@ -0,0 +1,31 @@
+import struct
+
+# Width:              4 bytes
+# Height:             4 bytes
+# Bit depth:          1 byte
+# Color type:         1 byte
+# Compression method: 1 byte
+# Filter method:      1 byte
+# Interlace method:   1 byte
+
+width = 1024
+height = 768
+bit_depth = 8
+color_type = 6  # RGB
+compression_method = 0  # deflate
+filter_method = 0  # adaptive filter
+interlace_method = 0 # no interlace
+
+bytes1 = b"IHDR" + struct.pack(
+    "!IIBBBBB",
+    width,
+    height,
+    bit_depth,
+    color_type,
+    compression_method,
+    filter_method,
+    interlace_method,
+)
+
+print(type(bytes1))
+print(bytes1.hex(" ", 1))
