6e34f9b408cae4de5798db9c1258322c5983b5f5
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-12-18 10:03:58 +0100

    Conversion from hex string to bytearray

---
+++ binary_structs/bytearray_fromhex.py
@@ -0,0 +1,15 @@
+x = bytearray.fromhex("")
+print(type(x))
+print(x)
+
+print()
+
+y = bytearray.fromhex("00 0f 1f ff")
+print(type(y))
+print(y)
+
+print()
+
+z = bytearray.fromhex("000f1fff")
+print(type(z))
+print(z)
