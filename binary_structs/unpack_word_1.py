45bb15fab88f56b612f1dfac17e8680e23693c92
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-01-02 09:17:55 +0100

    Unpack one word from binary data

---
+++ binary_structs/unpack_word_1.py
@@ -0,0 +1,15 @@
+import struct
+
+# uložení hodnoty bajtu do binární struktury
+bytes = struct.pack("h", 42)
+print("Serialized:  ", bytes.hex(" ", 1))
+
+# přečtení binární struktury, která obsahuje jediný bajt
+s = struct.Struct("h")
+
+# první prvek z přečtené struktury
+from_struct = s.unpack(bytes)[0]
+
+# vypsat přečtenou hodnotu
+print("Deserialized:", from_struct)
+print("Type:        ", type(from_struct))
