e26a703d5528d4a98b6077f17430e6c6a665dcd9
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-01-06 09:42:21 +0100

    Added copyright header

--- FM/plot_wave.py
+++ FM/plot_wave.py
@@ -1,3 +1,16 @@
+#
+#  (C) Copyright 2024, 2025  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+#
+
 # Zobrazení průběhu audio signáku přečteného ze souborů typu WAV
 
 import matplotlib.pyplot as plt
