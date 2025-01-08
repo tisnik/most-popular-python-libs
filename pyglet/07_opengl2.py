74c1052f12ce29ae3c16fce65d17610690c39faf
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-01-08 09:38:15 +0100

    Fixed imports

--- pyglet/07_opengl2.py
+++ pyglet/07_opengl2.py
@@ -27,7 +27,15 @@
 import pyglet
 
 # druhý import s funkcemi převzatými z OpenGL
-from pyglet.gl import GL_COLOR_BUFFER_BIT, GL_TRIANGLES, glBegin, glClear, glEnd, glLoadIdentity, glVertex2f
+from pyglet.gl import (
+    GL_COLOR_BUFFER_BIT,
+    GL_TRIANGLES,
+    glBegin,
+    glClear,
+    glEnd,
+    glLoadIdentity,
+    glVertex2f,
+)
 
 # vytvoření okna
 window = pyglet.window.Window(width=640, height=480, caption="Pyglet+OpenGL")
