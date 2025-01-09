de1105428c97ed466f7a2566d0d51ebc83e274ad
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-01-09 09:09:33 +0100

    Added (c) header

--- binary_structs/bytearray_hex.py
+++ binary_structs/bytearray_hex.py
@@ -1,3 +1,15 @@
+#
+#  (C) Copyright 2025  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
 x = bytearray(b"The quick brown fox jumps over the lazy dog")
 print(x.hex())
 print(x.hex(" "))
