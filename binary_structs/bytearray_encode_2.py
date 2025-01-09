76d75d268b910ca7ad83f2526e769536b5c1ca9b
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-01-09 09:09:24 +0100

    Added (c) header

--- binary_structs/bytearray_encode_2.py
+++ binary_structs/bytearray_encode_2.py
@@ -1,5 +1,17 @@
 """Uložení řetězce do pole bajtů s volitelných kódováním."""
 
+#
+#  (C) Copyright 2025  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
 s = "ěščřžýáíéů"
 
 # nelze převést na čisté ASCII
