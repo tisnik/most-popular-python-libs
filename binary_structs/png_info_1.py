11e13bc4e81728ed1af3a4f6817f7dfcf47bfe18
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-01-16 09:15:11 +0100

    Added license header

--- binary_structs/png_info_1.py
+++ binary_structs/png_info_1.py
@@ -1,5 +1,17 @@
 """Informace o obrázku uloženého ve formátu PNG."""
 
+#
+#  (C) Copyright 2025  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
 import struct
 
 PNG_SIGNATURE = b'\x89PNG\r\n\x1a\n'
