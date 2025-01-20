54680598d5230847235c8a2d6ff0e679e2bd545a
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-01-20 21:59:00 +0100

    Strawberry demo #1

---
+++ GraphQL/strawberry-books-1/schema.py
@@ -0,0 +1,41 @@
+import typing
+import strawberry
+
+
+def get_books():
+    return [
+        Book(
+            title="Dune",
+            author="Frank Herbert",
+        ),
+        Book(
+            title="Dune Messiah",
+            author="Frank Herbert",
+        ),
+        Book(
+            title="Mythago Wood",
+            author="Robert Holdstock",
+        ),
+        Book(
+            title="Merlin's Wood",
+            author="Robert Holdstock",
+        ),
+        Book(
+            title="Lavondyss",
+            author="Robert Holdstock",
+        ),
+    ]
+
+
+@strawberry.type
+class Book:
+    title: str
+    author: str
+
+
+@strawberry.type
+class Query:
+    books: list[Book] = strawberry.field(resolver=get_books)
+
+
+schema = strawberry.Schema(query=Query)
