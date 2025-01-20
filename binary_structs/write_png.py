6ae29ee3f91f9fd3980b1d00d53ed027350dd1bb
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-01-20 09:10:39 +0100

    Added license header

--- binary_structs/write_png.py
+++ binary_structs/write_png.py
@@ -1,5 +1,18 @@
 """Zápis rastrového obrázku s RGB pixely do formátu PNG."""
 
+#
+#  (C) Copyright 2025  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
+
 # Inspirace: https://stackoverflow.com/questions/902761/saving-a-numpy-array-as-an-image
 
 import struct
