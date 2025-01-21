caf2dccb0e2ad73460d907241653ab7cee55b92b
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-01-21 10:18:54 +0100

    Book selection by author

---
+++ GraphQL/strawberry-books-4/schema.py
@@ -0,0 +1,44 @@
+from typing import Optional
+import strawberry
+
+
+def get_books_for_author(author: Optional[str] = None):
+    books = [
+        Book(
+            title="Dune",
+            author="Frank Herbert",
+        ),
+        Book(
+            title="Dune Messiah",
+            author="Frank Herbert",
+        ),
+        Book(
+            title="Mythago Wood",
+            author="Robert Holdstock",
+        ),
+        Book(
+            title="Merlin's Wood",
+            author="Robert Holdstock",
+        ),
+        Book(
+            title="Lavondyss",
+            author="Robert Holdstock",
+        ),
+    ]
+    if author is None:
+        return books
+    return [book for book in books if book.author == author]
+
+
+@strawberry.type
+class Book:
+    title: str
+    author: str
+
+
+@strawberry.type
+class Query:
+    books: list[Book] = strawberry.field(resolver=get_books_for_author)
+
+
+schema = strawberry.Schema(query=Query)
