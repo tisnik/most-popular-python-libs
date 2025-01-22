e0f1c1e34cc1a291731a7d1352550f8a0078b615
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-01-22 11:26:34 +0100

    PromptFlow example

---
+++ PromptFlow/example1/flow.py
@@ -0,0 +1,28 @@
+# See also:
+# https://microsoft.github.io/promptflow/index.html
+
+import os
+
+from dotenv import load_dotenv
+from pathlib import Path
+from promptflow.tracing import trace
+from promptflow.core import Prompty
+
+BASE_DIR = Path(__file__).absolute().parent
+print(BASE_DIR)
+
+@trace
+def chat(question: str = "What's the capital of France?") -> str:
+    """Flow entry function."""
+
+    if "OPENAI_API_KEY" not in os.environ and "AZURE_OPENAI_API_KEY" not in os.environ:
+        # load environment variables from .env file
+        load_dotenv()
+
+    prompty = Prompty.load(source=BASE_DIR / "chat.prompty")
+    # trigger a llm call with the prompty obj
+    output = prompty(question=question)
+    return output
+
+
+print(chat())
