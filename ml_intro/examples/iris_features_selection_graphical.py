f8f83612deb143d13ac5c41bc174c8c732ab6796
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-02-03 08:46:16 +0100

    Reformatted, sorted imports

--- ml_intro/examples/iris_features_selection_graphical.py
+++ ml_intro/examples/iris_features_selection_graphical.py
@@ -1,9 +1,8 @@
 import matplotlib.pyplot as plt
-
 import numpy as np
 from sklearn.datasets import load_iris
-from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import cross_val_score
+from sklearn.neighbors import KNeighborsClassifier
 
 # nacteni datove sady
 iris = load_iris()
