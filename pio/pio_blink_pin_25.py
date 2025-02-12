5f46d282db0d44bf06882b972043f9d364211cf8
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-02-12 12:56:32 +0100

    Blink LED on GPIO #25 via PIO program

---
+++ pio/pio_blink_pin_25.py
@@ -0,0 +1,19 @@
+from machine import Pin
+import rp2
+
+@rp2.asm_pio(set_init=rp2.PIO.OUT_LOW)
+def blink_1hz():
+    set(pins, 1)
+    set(x, 31)                  [6]
+    label("delay_high")
+    nop()                       [29]
+    jmp(x_dec, "delay_high")
+    set(pins, 0)
+    set(x, 31)                  [6]
+    label("delay_low")
+    nop()                       [29]
+    jmp(x_dec, "delay_low")
+
+
+sm = rp2.StateMachine(0, blink_1hz, freq=2000, set_base=Pin(25))
+sm.active(1)
