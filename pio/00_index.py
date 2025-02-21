21221e527682e228b9bc8faf360c1496193a04c4
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-02-21 15:25:52 +0100

    Updated index file

---
+++ pio/00_index.py
@@ -0,0 +1,13 @@
+# blink_gpio_25.py
+# blink_gpio_8.py
+# display_frequency.py
+# display_mem_info_1.py
+# display_mem_info_2.py
+# display_pins.py
+# display_version.py
+# pio_blink_pin_25.py
+# pio_blink_pin_8_loop_1.py
+# pio_blink_pin_8_loop_2.py
+# pio_blink_pin_8_loop_3.py
+# pio_blink_pin_8.py
+# pio_blink_pip_8_control.py
