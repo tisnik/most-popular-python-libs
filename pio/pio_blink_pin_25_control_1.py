77c0dd4cef632df2eb9a98cf583e527f97a8e090
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-02-24 08:40:39 +0100

    LED blink via PIO, controlling state machine, example #1

---
+++ pio/pio_blink_pin_25_control_1.py
@@ -0,0 +1,23 @@
+import rp2
+from machine import Pin
+
+
+@rp2.asm_pio(set_init=rp2.PIO.OUT_LOW)
+def blink4():
+    pull(block)                # přečtení hodnoty z FIFO s čekáním, uložení do OSR
+    mov(y, osr)                # přesun hodnoty z OSR do registru Y
+    wrap_target()              # cíl skoku
+    set(x, 31)                 # počitadlo smyčky
+    set(pins, 1)               # nastavit pin
+    label("blink_loop")
+    jmp(x_not_y, "skip")       # když x!=y přeskočit další instrukci
+    set(pins, 0)               # vynulovat pin
+    label("skip")
+    nop() [29]                 # zpožďovací mechanismus
+    jmp(x_dec, "blink_loop")   # a opakujeme X-krát
+    wrap()
+
+
+sm = rp2.StateMachine(0, blink4, freq=2000, set_base=Pin(25))
+sm.active(1)                   # spustit stavový stroj
+sm.put(10)                     # prahová hodnota, která se pošle do FIFO
