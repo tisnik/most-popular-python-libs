726147e29230f4cc7ebec896751812658c5ecf38
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-02-26 14:58:39 +0100

    PWM controlled by PIO on GPIO #8

---
+++ pio/pio_pwm_pin_8.py
@@ -0,0 +1,34 @@
+import rp2
+from machine import Pin
+import time
+
+
+@rp2.asm_pio(set_init=rp2.PIO.OUT_LOW)
+def blink6():
+    pull(block)                # přečtení hodnoty z FIFO s čekáním, uložení do OSR
+    mov(x, osr)                # přesun hodnoty z OSR do registru X
+    wrap_target()              # cíl skoku
+    pull(noblock)              # zkusíme přečíst novou poslanou hodnotu, ale nečekáme na ni
+                               # pokud hodnota nebyla poslána, doplní se hodnota registru X
+    mov(x, osr)                # přesun hodnoty z OSR do registru X
+    set(y, 31)                 # počitadlo smyčky
+    set(pins, 1)               # nastavit pin
+    label("blink_loop")
+    jmp(x_not_y, "skip")       # když X!=Y přeskočit další instrukci
+    set(pins, 0)               # vynulovat pin
+    label("skip")
+    nop() [29]                 # zpožďovací mechanismus
+    jmp(y_dec, "blink_loop")   # a opakujeme Y-krát
+    wrap()
+
+
+sm = rp2.StateMachine(0, blink6, freq=200000, set_base=Pin(8))
+sm.active(1)                   # spustit stavový stroj
+
+for duty_cycle in range(0, 32):
+    time.sleep(0.3)
+    sm.put(duty_cycle)         # prahová hodnota, která se pošle do FIFO
+
+for duty_cycle in range(31, 0, -1):
+    time.sleep(0.3)
+    sm.put(duty_cycle)         # prahová hodnota, která se pošle do FIFO
