1fe5373e88a4607b6249a561017cc8114237597a
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-02-28 14:38:27 +0100

    Select vectors by distance, example #2

---
+++ pgvector/select_by_distance_2.py
@@ -0,0 +1,21 @@
+import psycopg2
+from pgvector.psycopg2 import register_vector
+
+
+connection = psycopg2.connect(
+    host="", port=5432, user="tester", password="123qwe", dbname="test"
+)
+
+register_vector(connection)
+
+with connection.cursor() as cursor:
+    query = """
+        SELECT embedding
+          FROM v2
+         ORDER BY embedding <-> %s::vector
+         LIMIT 5
+    """
+    cursor.execute(query, ([3,3], ))
+    records = cursor.fetchall()
+    for record in records:
+        print(record[0])
