8b4b6ef9213e9f804b5429076492d454bc93adb4
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-02-28 14:49:04 +0100

    Fixed imports

--- pgvector/read_vectors_3.py
+++ pgvector/read_vectors_3.py
@@ -1,6 +1,6 @@
 import psycopg2
-from pgvector.psycopg2 import register_vector
 
+from pgvector.psycopg2 import register_vector
 
 connection = psycopg2.connect(
     host="", port=5432, user="tester", password="123qwe", dbname="test"
