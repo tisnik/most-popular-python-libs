9586df5f5da537049c6ee4978c2992f5acf8b28d
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-02-28 14:38:27 +0100

    Insert values into v2 table

---
+++ pgvector/insert_into_v2.py
@@ -0,0 +1,21 @@
+import numpy as np
+import psycopg2
+from pgvector.psycopg2 import register_vector
+
+
+connection = psycopg2.connect(
+    host="", port=5432, user="tester", password="123qwe", dbname="test"
+)
+
+register_vector(connection)
+
+x = [-5, -4, -3,    3,  4,  5,   3, 3, 3, 4, 4, 4, 5, 5, 5]
+y = [ 5,  3,  5,   -5, -3, -5,   3, 4, 5, 3, 4, 5, 3, 4, 5]
+
+with connection.cursor() as cursor:
+    for i in range(len(x)):
+        vector = np.array([x[i], y[i]])
+        print(vector)
+        cursor.execute("INSERT INTO v2 (embedding) VALUES (%s)", (vector, ))
+    connection.commit()
+
