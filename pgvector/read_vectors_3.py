b2cc835bb8165c65bd4351b06304c550adfa188b
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-02-28 14:38:27 +0100

    Read vectors from table, example #3

---
+++ pgvector/read_vectors_3.py
@@ -0,0 +1,15 @@
+import psycopg2
+from pgvector.psycopg2 import register_vector
+
+
+connection = psycopg2.connect(
+    host="", port=5432, user="tester", password="123qwe", dbname="test"
+)
+
+register_vector(connection)
+
+with connection.cursor() as cursor:
+    cursor.execute("SELECT * FROM v2")
+    records = cursor.fetchall()
+    for record in records:
+        print(record, type(record[1]))
