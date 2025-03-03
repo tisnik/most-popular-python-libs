080593fce87f61a02242a04f19e6ee4b7ec2572c
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-03-03 09:02:21 +0100

    Distance operators

---
+++ pgvector/various_distances.py
@@ -0,0 +1,46 @@
+import psycopg2
+
+from pgvector.psycopg2 import register_vector
+
+connection = psycopg2.connect(
+    host="", port=5432, user="tester", password="123qwe", dbname="test"
+)
+
+register_vector(connection)
+
+
+def list_by_distance(title, query):
+    print(title)
+    print("-" * 70)
+    with connection.cursor() as cursor:
+        cursor.execute(query, ([3,3], ))
+        records = cursor.fetchall()
+        for record in records:
+            print(record[0], record[1])
+    print()
+
+
+query_l2_distance = """
+        SELECT embedding, embedding <-> %s::vector as distance
+          FROM v2
+         ORDER BY distance
+    """
+
+
+query_cosine_distance = """
+        SELECT embedding, embedding <=> %s::vector as distance
+          FROM v2
+         ORDER BY distance
+    """
+
+
+query_inner_product_distance = """
+        SELECT embedding, (embedding <#> %s::vector) * -1 as distance
+          FROM v2
+         ORDER BY distance
+    """
+
+
+list_by_distance("L2", query_l2_distance)
+list_by_distance("cosine", query_cosine_distance)
+list_by_distance("inner product", query_inner_product_distance)
