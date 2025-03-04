1afa810a5d52914bf90138078d338de0e5e44eaf
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-03-04 09:29:34 +0100

    Added license header

--- pgvector/insert_normalized.py
+++ pgvector/insert_normalized.py
@@ -1,3 +1,18 @@
+#!/usr/bin/python
+# vim: set fileencoding=utf-8
+
+#
+#  (C) Copyright 2025  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
 import psycopg2
 
 import numpy as np
@@ -20,6 +35,6 @@ with connection.cursor() as cursor:
         norm = np.linalg.norm(vector)
         vector /= norm
         print(vector)
-        cursor.execute("INSERT INTO normalized (embedding) VALUES (%s)", (vector, ))
+        #cursor.execute("INSERT INTO normalized (embedding) VALUES (%s)", (vector, ))
     connection.commit()
 
