19f4c2969cf35ff5661c2736a0521c5b73f3bcd5
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-03-04 09:29:34 +0100

    Added license header

--- pgvector/insert_into_v2.py
+++ pgvector/insert_into_v2.py
@@ -1,3 +1,18 @@
+#!/usr/bin/python
+# vim: set fileencoding=utf-8
+
+#
+#  (C) Copyright 2025  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
 import psycopg2
 
 import numpy as np
