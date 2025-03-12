16110ef9095445be3c2139f963d4df663f0e602c
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-03-12 09:14:29 +0100

    Testing in Python: average computation

---
+++ courses/testing/primes2/primes.py
@@ -0,0 +1,30 @@
+"""Výpočet seznamu prvočísel až do zadaného limitu."""
+
+# originální kód lze nalézt na adrese:
+# http://www.rosettacode.org/wiki/Sieve_of_Eratosthenes#Odds-only_version_of_the_array_sieve_above
+
+
+def primes2(limit):
+    """Výpočet seznamu prvočísel až do zadaného limitu."""
+    # okrajový případ
+    if limit < 2:
+        return []
+
+    # druhý případ - 2 je speciálním prvočíslem
+    if limit < 3:
+        return [2]
+
+    lmtbf = (limit - 3) // 2
+
+    # naplnění tabulky, která se bude prosívat
+    buf = [True] * (lmtbf + 1)
+
+    # vlastní prosívání
+    for i in range((int(limit ** 0.5) - 3) // 2 + 1):
+        if buf[i]:
+            p = i + i + 3
+            s = p * (i + 1) + i
+            buf[s::p] = [False] * ((lmtbf - s) // p + 1)
+
+    # vytvoření seznamu prvočísel
+    return [2] + [i + i + 3 for i, v in enumerate(buf) if v]
