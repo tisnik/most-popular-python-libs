677b49b86c74c5551eb141ae7626422e4c002a57
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-03-12 09:14:29 +0100

    Testing in Python: average computation

---
+++ courses/testing/primes3/main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""Vstupní bod do testované aplikace."""
+
+from primes import *
+
+
+if __name__ == "__main__":
+    # pouze se ujistíme, že lze spustit funkci primes2
+    print(primes2(100))
