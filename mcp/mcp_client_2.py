41b0fe150429bc5eb4346892aff41a24cd8a3c78
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-03-28 08:28:34 +0100

    MCP client with SSE transport defined

---
+++ mcp/mcp_client_2.py
@@ -0,0 +1,30 @@
+"""MCP klient, který spustí server, se kterým se komunikuje přes SSE."""
+
+from mcp import ClientSession, types
+from mcp.client.sse import sse_client
+
+
+async def run():
+    """Realizace klienta."""
+    async with sse_client(url="http://localhost:8000/sse") as (read, write):
+        async with ClientSession(read, write) as session:
+            await session.initialize()
+
+            prompts = await session.list_prompts()
+            print(prompts)
+
+            resources = await session.list_resources()
+            print(resources)
+
+            templates = await session.list_resource_templates()
+            print(templates)
+
+            tools = await session.list_tools()
+            print(tools)
+
+
+# přímé spuštění klienta
+if __name__ == "__main__":
+    import asyncio
+
+    asyncio.run(run())
