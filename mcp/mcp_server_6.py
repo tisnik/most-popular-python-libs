6b013b1757347d28cabd34dc45783ba350ba8cf0
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-03-28 08:28:15 +0100

    MCP server that starts itself using SSE transport

---
+++ mcp/mcp_server_6.py
@@ -0,0 +1,32 @@
+"""MCP server se zdrojem,  dynamickým zdrojem a definicí nástroje (tool)."""
+
+from mcp.server.fastmcp import FastMCP
+
+# konstrukce serveru
+mcp = FastMCP("Test")
+
+
+@mcp.tool()
+def factorial(n: int) -> int:
+    """Výpočet faktoriálu ve smyčce."""
+    f = 1
+    for x in range(1, n + 1):
+        f *= x
+    return f
+
+
+@mcp.resource("pozdrav://")
+def pozdrav1() -> str:
+    """Odpověď s pozdravem."""
+    return f"Hello, dear client"
+
+
+@mcp.resource("pozdrav://{name}")
+def pozdrav2(name: str) -> str:
+    """Odpověď s osobním pozdravem."""
+    return f"Hello, {name}"
+
+
+# přímé spuštění serveru v režimu SSE (Server-Sent Events)
+if __name__ == "__main__":
+    mcp.run(transport="sse")
