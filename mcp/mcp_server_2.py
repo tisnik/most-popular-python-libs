bb5d4cc7e9a2adca0a8c80813a6ceb5d28be6735
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-03-28 08:27:24 +0100

    Added comments

--- mcp/mcp_server_2.py
+++ mcp/mcp_server_2.py
@@ -1,9 +1,12 @@
+"""Jednoduchý MCP server s jediným definovaným dynamickým zdrojem."""
+
 from mcp.server.fastmcp import FastMCP
 
+# konstrukce serveru
 mcp = FastMCP("Test")
 
 
 @mcp.resource("pozdrav://{name}")
 def pozdrav(name: str) -> str:
-    """Odpoveď s osobním pozdravem."""
+    """Odpověď s osobním pozdravem."""
     return f"Hello, {name}"
