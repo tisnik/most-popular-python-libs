8787194b408d3cc0b0b6d0e725834dc49f70adaa
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-04-07 09:50:02 +0200

    Simple MCP client written in Python

---
+++ mcp/mcp-golang/mcp_client_1.py
@@ -0,0 +1,33 @@
+"""MCP klient, který spustí server, se kterým se komunikuje přes STDIO."""
+
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
+
+# parametry pro spuštění MCP serveru
+server_params = StdioServerParameters(
+    command="go",  # spustí se tento příkaz
+    args=["run", "./mcp_server_1.go"],  # a předají se mu následující parametry
+    env=None,  # lze definovat i proměnné prostředí
+)
+
+
+async def run():
+    """Realizace klienta."""
+    async with stdio_client(server_params) as (read, write):
+        async with ClientSession(read, write) as session:
+            await session.initialize()
+
+            # přečtení zdroje
+            data = await session.read_resource("pozdrav://")
+            print("Data returned:", data)
+            print("Type:", type(data))
+
+            text = data.contents[0].text
+            print("Text:", text)
+
+
+# přímé spuštění klienta
+if __name__ == "__main__":
+    import asyncio
+
+    asyncio.run(run())
