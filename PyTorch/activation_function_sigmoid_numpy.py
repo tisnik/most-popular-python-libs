231a9db64722abef9782210867af7e96f7aced98
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-04-15 09:20:44 +0200

    Activation function plot: sigmoid computed by Numpy

---
+++ PyTorch/activation_function_sigmoid_numpy.py
@@ -0,0 +1,29 @@
+# Výpočet a vykreslení aktivační funkce sigmoid
+# Výpočet je proveden knihovnou NumPy
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# velikost obrázku s grafem
+plt.subplots(figsize=(6.4, 4.8))
+
+# hodnoty na x-ové ose
+x = np.linspace(-5, 5, 100)
+
+# výpočet aktivační funkce
+y = 1/(1 + np.exp(-x))
+
+# vykreslení aktivační funkce
+plt.plot(x, y, label="Sigmoid")
+
+# zobrazení legendy
+plt.legend()
+
+# zobrazení mřížky
+plt.grid()
+
+# uložení do souboru
+plt.savefig("activation_function_sigmoid_numpy.png")
+
+# zobrazení v novém okně
+plt.show()
