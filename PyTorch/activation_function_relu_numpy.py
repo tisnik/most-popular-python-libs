e3c01c3ac5de9e5f1e25d5860d24de06b3397517
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-04-15 09:20:23 +0200

    Activation function plot: ReLU computed by Numpy

---
+++ PyTorch/activation_function_relu_numpy.py
@@ -0,0 +1,29 @@
+# Výpočet a vykreslení aktivační funkce ReLU
+# Výpočet je proveden knihovnou NumPy
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# velikost obrázku s grafem
+plt.subplots(figsize=(6.4, 4.8))
+
+# hodnoty na x-ové ose
+x = np.linspace(-5, 5, 100)
+
+# výpočet aktivační funkce
+y = [max(0, i) for i in x]
+
+# vykreslení aktivační funkce
+plt.plot(x, y, label="ReLU")
+
+# zobrazení legendy
+plt.legend()
+
+# zobrazení mřížky
+plt.grid()
+
+# uložení do souboru
+plt.savefig("activation_function_relu_numpy.png")
+
+# zobrazení v novém okně
+plt.show()
