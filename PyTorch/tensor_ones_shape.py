848637475880e617ec2fcdd48eeaf87c0299aff5
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-04-15 09:21:18 +0200

    Reformatted

--- PyTorch/tensor_ones_shape.py
+++ PyTorch/tensor_ones_shape.py
@@ -5,17 +5,17 @@ v1 = torch.ones(1)
 print(v1)
 print()
 
-# konstrukce tenzoru prvniho radu, vyplneni jednickami 
+# konstrukce tenzoru prvniho radu, vyplneni jednickami
 v2 = torch.ones(10)
 print(v2)
 print()
 
-# konstrukce tenzoru druheho radu, vyplneni jednickami 
+# konstrukce tenzoru druheho radu, vyplneni jednickami
 m1 = torch.ones(3, 4)
 print(m1)
 print()
 
-# konstrukce tenzoru tretiho radu, vyplneni jednickami 
+# konstrukce tenzoru tretiho radu, vyplneni jednickami
 c1 = torch.ones(3, 4, 5)
 print(c1)
 print()
