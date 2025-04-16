58daad56429cf5c4bc9c57eb7a7ee2d4822b3275
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-04-16 09:22:45 +0200

    Activation function tanh computed by NumPy

---
+++ PyTorch/activation_function_tanh_numpy.py
@@ -0,0 +1,29 @@
+# Výpočet a vykreslení aktivační funkce tanh
+# Výpočet je proveden knihovnou NumPy
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# velikost obrázku s grafem
+plt.subplots(figsize=(6.4, 4.8))
+
+# hodnoty na x-ové ose
+x = np.linspace(-5, 5, 100)
+
+# výpočet aktivační funkce
+y = np.tanh(x)
+
+# vykreslení aktivační funkce
+plt.plot(x, y, label="Tanh")
+
+# zobrazení legendy
+plt.legend()
+
+# zobrazení mřížky
+plt.grid()
+
+# uložení do souboru
+plt.savefig("activation_function_tanh_numpy.png")
+
+# zobrazení v novém okně
+plt.show()
