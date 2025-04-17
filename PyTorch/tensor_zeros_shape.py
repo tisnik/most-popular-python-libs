30f3d8262c2628582b02c4a090b41fb6b29aebd8
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-04-17 09:29:41 +0200

    Reformatted

--- PyTorch/tensor_zeros_shape.py
+++ PyTorch/tensor_zeros_shape.py
@@ -1,21 +1,21 @@
 import torch
 
-# konstrukce tenzoru prvniho radu, vyplneni nulami 
+# konstrukce tenzoru prvniho radu, vyplneni nulami
 v1 = torch.zeros(1)
 print(v1)
 print()
 
-# konstrukce tenzoru prvniho radu, vyplneni nulami 
+# konstrukce tenzoru prvniho radu, vyplneni nulami
 v2 = torch.zeros(10)
 print(v2)
 print()
 
-# konstrukce tenzoru druheho radu, vyplneni nulami 
+# konstrukce tenzoru druheho radu, vyplneni nulami
 m1 = torch.zeros(3, 4)
 print(m1)
 print()
 
-# konstrukce tenzoru tretiho radu, vyplneni nulami 
+# konstrukce tenzoru tretiho radu, vyplneni nulami
 c1 = torch.zeros(3, 4, 5)
 print(c1)
 print()
