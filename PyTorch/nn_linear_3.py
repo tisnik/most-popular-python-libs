f6ba8c288972cc69682466894e15586ca5913f12
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-04-22 09:03:03 +0200

    nn.Linear, example #1

---
+++ PyTorch/nn_linear_3.py
@@ -0,0 +1,10 @@
+from torch import tensor, nn
+
+l = nn.Linear(1, 1)
+
+l.weight = nn.Parameter(data=tensor([[2.0]]))
+l.bias = nn.Parameter(data=tensor([10.0]))
+
+print("Weights:", l.weight)
+print()
+print("Bias:", l.bias)
