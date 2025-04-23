2f158b97d07aa6e4b7397623836411a9bf041884
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-04-23 11:49:54 +0200

    2D affine transformation

---
+++ PyTorch/nn_linear_7.py
@@ -0,0 +1,24 @@
+import math
+import torch
+from torch import nn
+
+l = nn.Linear(2, 2)
+
+phi = 90
+cos_phi = math.cos(math.radians(phi))
+sin_phi = math.sin(math.radians(phi))
+
+rotation_matrix = [
+        [cos_phi, -sin_phi],
+        [sin_phi,  cos_phi],
+        ]
+
+translation_vector = [0.0, 0.0]
+
+l.weight = nn.Parameter(data=torch.tensor(rotation_matrix))
+l.bias = nn.Parameter(data=torch.tensor(translation_vector))
+
+x = torch.tensor([1.0, 2.0])
+y = l(x)
+
+print(y)
