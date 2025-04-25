988dabf5b1365ad2f9d94c07678b2dff4521c0c0
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-04-25 08:29:38 +0200

    Updated comments

--- PyTorch/activation_function_sigmoid_pytorch.py
+++ PyTorch/activation_function_sigmoid_pytorch.py
@@ -1,5 +1,8 @@
-# Výpočet a vykreslení aktivační funkce sigmoid
-# Výpočet je proveden knihovnou PyTorch
+# Knihovna PyTorch
+#
+# - výpočet a vykreslení aktivační funkce sigmoid
+# - výpočet je proveden knihovnou PyTorch
+# - vykreslení je provedeno knihovnou Matplotlib
 
 import matplotlib.pyplot as plt
 import torch
