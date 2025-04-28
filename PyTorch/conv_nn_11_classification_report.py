d7da49c62b77dffe21adb3671864641f8f2829b3
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-04-28 09:38:24 +0200

    Classification report

---
+++ PyTorch/conv_nn_11_classification_report.py
@@ -0,0 +1,8 @@
+from sklearn.metrics import classification_report
+
+
+y_pred = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+y_test = [0, 1, 2, 8, 4, 5, 6, 7, 6, 9, 0, 1, 2, 2, 4, 5, 8, 7, 8, 9]
+
+# tisk zprávy o výsledcích klasifikace
+print(classification_report(y_test, y_pred))
