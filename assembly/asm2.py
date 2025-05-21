d072e762323114e610f12ff9be65ea0ae5bb09e0
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-05-21 09:09:54 +0200

    Assembly and Python: example #2

---
+++ assembly/asm2.py
@@ -0,0 +1,26 @@
+import ctypes
+import mmap
+
+# konstrukce bufferu
+with mmap.mmap(-1, mmap.PAGESIZE, prot=mmap.PROT_WRITE | mmap.PROT_EXEC) as buffer:
+    # zapis strojoveho kodu do bufferu
+    buffer.write(
+        b'\xB8\x2A\x00\x00\x00'
+        b'\xC3'
+    )
+
+    # deklarace typu nativni funkce
+    function_type = ctypes.CFUNCTYPE(ctypes.c_int)
+
+    # ziskani adresy strojoveho kodu
+    function_pointer = ctypes.c_void_p.from_buffer(buffer)
+
+    # reference na volatelnou funkci
+    function_42 = function_type(ctypes.addressof(function_pointer))
+
+    # zavolani funkce a vypis vysledku
+    result = function_42()
+    print(result)
+
+    # pred uzavrenim bufferu je nutne odstranit ukazatele
+    del function_pointer
