a1c227b3eb8cbb833c3506bccee2d1d272de396c
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-05-21 09:09:54 +0200

    Assembly and Python: example #5

---
+++ assembly/asm5.py
@@ -0,0 +1,23 @@
+import ctypes
+import mmap
+
+# konstrukce bufferu
+with mmap.mmap(-1, mmap.PAGESIZE, prot=mmap.PROT_WRITE | mmap.PROT_EXEC) as buffer:
+    with open("double.bin", "rb") as fin:
+        buffer.write(fin.read())
+
+    # deklarace typu nativni funkce
+    function_type = ctypes.CFUNCTYPE(ctypes.c_int, ctypes.c_int)
+
+    # ziskani adresy strojoveho kodu
+    function_pointer = ctypes.c_void_p.from_buffer(buffer)
+
+    # reference na volatelnou funkci
+    double = function_type(ctypes.addressof(function_pointer))
+
+    # zavolani funkce a vypis vysledku
+    result = double(10000000000)
+    print(result)
+
+    # pred uzavrenim bufferu je nutne odstranit ukazatele
+    del function_pointer
