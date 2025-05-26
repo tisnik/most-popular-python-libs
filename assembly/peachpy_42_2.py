c321220e5c83491922ecf275ac3f7fa50fc18297
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-05-26 08:55:28 +0200

    Printing generated instructions in assembly

---
+++ assembly/peachpy_42_2.py
@@ -0,0 +1,17 @@
+# datové typy
+from peachpy import int32_t
+
+# základní konstruktory atd.
+from peachpy.x86_64 import Function, GeneralPurposeRegister32, abi
+
+# konstruktory instrukcí
+from peachpy.x86_64 import MOV, RETURN
+
+# vytvoření nové subrutiny ve strojovém kódu
+with Function("Function_42", (), int32_t) as asm_function:
+    reg_x = GeneralPurposeRegister32()
+    MOV(reg_x, 42)
+    RETURN(reg_x)
+
+# výpis disassemblovaného strojového kódu
+print(asm_function.finalize(abi.detect()).format_code())
