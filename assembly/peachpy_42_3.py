55ffbce287cdbe2f13978fb85f2d2b1df568c12b
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-05-26 08:55:28 +0200

    Subroutine returning constant value, based on PeachPy, bobtimal solution

---
+++ assembly/peachpy_42_3.py
@@ -0,0 +1,26 @@
+# datové typy
+from peachpy import int32_t
+
+# základní konstruktory atd.
+from peachpy.x86_64 import Function, GeneralPurposeRegister32, abi
+
+# registry
+from peachpy.x86_64 import eax
+
+# konstruktory instrukcí
+from peachpy.x86_64 import MOV, RETURN
+
+# vytvoření nové subrutiny ve strojovém kódu
+with Function("Function_42", (), int32_t) as asm_function:
+    MOV(eax, 42)
+    RETURN()
+
+# obalení strojového kódu tak, aby se dal volat z interpretru Pythonu
+function_42 = asm_function.finalize(abi.detect()).encode().load()
+
+# typ hodnoty
+print(type(function_42))
+print()
+
+# zavolání nové funkce
+print(function_42())
