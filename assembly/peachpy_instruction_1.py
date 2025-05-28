e41a6345f7381afc30f6feece3c8aafb3cacc479
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-05-28 09:41:13 +0200

    Instruction coding and decoding

---
+++ assembly/peachpy_instruction_1.py
@@ -0,0 +1,26 @@
+# Základy použití knihovny PeachPy
+#
+# - definice instrukce
+# - tisk strojového kódu této instrukce
+# - tisk instrukce ve formátu Netwide Assembleru
+# - tisk instrukce ve formátu PeachPy
+# - tisk instrukce ve formátu GNU Assembleru
+# - tisk instrukce ve formátu Go Assembleru
+
+
+# registry
+from peachpy.x86_64 import rax, rdi
+
+# konstruktory instrukcí
+from peachpy.x86_64 import ADD
+
+
+def print_instruction(instruction):
+    print(" ".join(format(byte, "02x") for byte in instruction.encode()))
+    print(instruction.format(assembly_format="nasm"))
+    print(instruction.format(assembly_format="peachpy"))
+    print(instruction.format(assembly_format="gas"))
+    print(instruction.format(assembly_format="go"))
+
+
+print_instruction(ADD(rax, rdi))
