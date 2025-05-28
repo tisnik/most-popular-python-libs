ce55740a9f7690408d569d09cd03bd88621edcc9
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-05-28 09:41:41 +0200

    Instruction coding and decoding

---
+++ assembly/peachpy_instruction_2.py
@@ -0,0 +1,27 @@
+# Základy použití knihovny PeachPy
+#
+# - definice instrukce ADD s různými kombinacemi registrů
+# - tisk strojového kódu této instrukce
+# - tisk instrukce ve formátu Netwide Assembleru
+# - tisk instrukce ve formátu PeachPy
+# - tisk instrukce ve formátu GNU Assembleru
+# - tisk instrukce ve formátu Go Assembleru
+
+
+# registry
+from peachpy.x86_64 import rax, rbx, rcx, rdx, rsi, rdi, rbp
+from peachpy.x86_64 import r8, r9, r10, r11, r12, r13, r14, r15
+
+# konstruktory instrukcí
+from peachpy.x86_64 import ADD
+
+
+def print_instruction(instruction):
+    print(" ".join(format(byte, "02x") for byte in instruction.encode()), end="\t")
+    print(instruction.format(assembly_format="peachpy"), end="\t")
+    print(instruction.format(assembly_format="gas"))
+
+
+for r1 in  rax, rbx, rcx, rdx, rsi, rdi, rbp, r8, r9, r10, r11, r12, r13, r14, r15:
+    for r2 in  rax, rbx, rcx, rdx, rsi, rdi, rbp, r8, r9, r10, r11, r12, r13, r14, r15:
+        print_instruction(ADD(r1, r2))
