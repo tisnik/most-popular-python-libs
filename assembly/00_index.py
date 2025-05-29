e10da1c948c76d8a060ec1272b2ba4d0f3e6d3e0
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-05-29 08:31:29 +0200

    Updated generated index

---
+++ assembly/00_index.py
@@ -0,0 +1,96 @@
+# asm1.py:
+#
+# asm2.py:
+#
+# asm3.py:
+#
+# asm4.py:
+#
+# asm5.py:
+#
+# asm6.py:
+#
+# asm7.py:
+#
+# asm8.py:
+#
+# peachpy_42_1.py:
+# - vytvoření subrutiny vracející konstantu
+# - neoptimalizovaná varianta
+#
+# peachpy_42_2.py:
+# - vytvoření subrutiny vracející konstantu
+# - neoptimalizovaná varianta
+# - tisk vytvořeného strojového kódu
+#
+# peachpy_42_3.py:
+# - vytvoření subrutiny vracející konstantu
+# - optimalizovaná varianta
+#
+# peachpy_42_4.py:
+# - vytvoření subrutiny vracející konstantu
+# - optimalizovaná varianta
+# - tisk vytvořeného strojového kódu
+#
+# peachpy_abi.py:
+# - výpis ABI na aktuálně používaném počítači
+#
+# peachpy_add_1.py:
+# - definice subrutiny add pro součet dvou 64bitových hodnot
+# - založeno na znalosti ABI
+# - tisk vytvořeného strojového kódu
+# - otestování funkcionality této subrutiny
+#
+# peachpy_add_2.py:
+# - definice subrutiny add pro součet dvou 64bitových hodnot
+# - použití pojmenovaných argumentů
+# - tisk vytvořeného strojového kódu
+# - otestování funkcionality této subrutiny
+#
+# peachpy_add_3.py:
+# - definice subrutiny add pro součet dvou 64bitových hodnot
+# - použití pojmenovaných argumentů
+# - odlišná práce s registry
+# - tisk vytvořeného strojového kódu
+# - otestování funkcionality této subrutiny
+#
+# peachpy_double32_1.py:
+# - definice subrutiny double vracející dvojnásobnou hodnotu argumentu
+# - použití pojmenovaných argumentů
+# - tisk vytvořeného strojového kódu
+# - otestování funkcionality této subrutiny
+#
+# peachpy_double32_2.py:
+# - definice subrutiny double vracející dvojnásobnou hodnotu argumentu
+# - založeno na znalosti ABI
+# - tisk vytvořeného strojového kódu
+# - otestování funkcionality této subrutiny
+#
+# peachpy_double64_1.py:
+# - definice subrutiny double vracející dvojnásobnou hodnotu argumentu
+# - pokus o výpočty s 64bitovými hodnotami
+# - tisk vytvořeného strojového kódu
+# - otestování funkcionality této subrutiny
+#
+# peachpy_double64_2.py:
+# - definice subrutiny double vracející dvojnásobnou hodnotu argumentu
+# - korektní výpočty s 64bitovými hodnotami
+# - tisk vytvořeného strojového kódu
+# - otestování funkcionality této subrutiny
+#
+# peachpy_instruction_1.py:
+# - definice instrukce
+# - tisk strojového kódu této instrukce
+# - tisk instrukce ve formátu Netwide Assembleru
+# - tisk instrukce ve formátu PeachPy
+# - tisk instrukce ve formátu GNU Assembleru
+# - tisk instrukce ve formátu Go Assembleru
+#
+# peachpy_instruction_2.py:
+# - definice instrukce ADD s různými kombinacemi registrů
+# - tisk strojového kódu této instrukce
+# - tisk instrukce ve formátu Netwide Assembleru
+# - tisk instrukce ve formátu PeachPy
+# - tisk instrukce ve formátu GNU Assembleru
+# - tisk instrukce ve formátu Go Assembleru
+#
