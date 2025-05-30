b19807f462fc54d3321a793c95e8d6d8e9bddb3b
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-05-30 08:37:26 +0200

    Index file

---
+++ PyTorch/00_index.py
@@ -0,0 +1,294 @@
+# Seznam zdrojových kódů v tomto podadresáři
+# ------------------------------------------
+#
+# activation_function_relu_numpy.py:
+# - výpočet a vykreslení aktivační funkce ReLU
+# - výpočet je proveden knihovnou NumPy
+# - vykreslení je provedeno knihovnou Matplotlib
+#
+# activation_function_relu_pytorch.py:
+# - výpočet a vykreslení aktivační funkce ReLU
+# - výpočet je proveden knihovnou PyTorch
+# - vykreslení je provedeno knihovnou Matplotlib
+#
+# activation_function_sigmoid_numpy.py:
+# - výpočet a vykreslení aktivační funkce sigmoid
+# - výpočet je proveden knihovnou NumPy
+# - vykreslení je provedeno knihovnou Matplotlib
+#
+# activation_function_sigmoid_pytorch.py:
+# - výpočet a vykreslení aktivační funkce sigmoid
+# - výpočet je proveden knihovnou PyTorch
+# - vykreslení je provedeno knihovnou Matplotlib
+#
+# activation_function_tanh_numpy.py:
+# - výpočet a vykreslení aktivační funkce tanh
+# - výpočet je proveden knihovnou NumPy
+# - vykreslení je provedeno knihovnou Matplotlib
+#
+# activation_function_tanh_pytorch.py:
+# - výpočet a vykreslení aktivační funkce tanh
+# - výpočet je proveden knihovnou PyTorch
+# - vykreslení je provedeno knihovnou Matplotlib
+#
+# activation_functions_numpy.py:
+# - výpočet a vykreslení všech aktivačních funkcí
+# - výpočet je proveden knihovnou NumPy
+# - vykreslení je provedeno knihovnou Matplotlib
+#
+# activation_functions_pytorch.py:
+# - výpočet a vykreslení všech aktivačních funkcí
+# - výpočet je proveden knihovnou PyTorch
+# - vykreslení je provedeno knihovnou Matplotlib
+#
+# classification_report.py:
+#
+# compute_train_and_test_data.py:
+# - výpočet trénovacích a testovacích dat pro neuronové sítě
+#
+# confusion_matrix.py:
+#
+# conv_nn_01_digits.py:
+#
+# conv_nn_02_digits_as_bitmaps.py:
+#
+# conv_nn_03_show_digits.py:
+#
+# conv_nn_04_show_digits.py:
+#
+# conv_nn_05_show_all_digits.py:
+#
+# conv_nn_06_noise.py:
+#
+# conv_nn_07_noise_levels.py:
+#
+# conv_nn_08_5_6_or_8.py:
+#
+# conv_nn_09_shift.py:
+#
+# conv_nn_10_shifts.py:
+#
+# conv_nn_11_classification_report.py:
+#
+# conv_nn_12_confusion_matrix.py:
+#
+# conv_nn_13_input_data.py:
+#
+# conv_nn_14_real_input_data.py:
+#
+# conv_nn_15_train_test_data.py:
+#
+# conv_nn_16_simple_nn.py:
+#
+# conv_nn_17_noise_nn.py:
+#
+# conv_nn_18_shift_nn.py:
+#
+# conv_nn_19_noise_and_shift_nn.py:
+#
+# conv_nn_20_confusion_matrix.py:
+#
+# is_cuda_available.py:
+# - zjištění, jestli je možné výpočty použít pro platformu CUDA
+#
+# is_mps_available.py:
+# - zjištění, jestli je možné výpočty použít pro platformu MPS
+#
+# make_circles.py:
+# - vykreslení bodů generovaných funkcí make_circles
+#
+# make_circles_labels.py:
+# - vykreslení bodů (a jejich skupiny) generovaných funkcí make_circles
+#
+# make_data_set.py:
+# - výpočet trénovacích a testovacích dat pro neuronové sítě
+#
+# make_more_noise_circles.py:
+# - vykreslení bodů generovaných funkcí make_circles
+# - zvýšený faktor náhodnosti
+#
+# nn_01.py:
+#
+# nn_02.py:
+#
+# nn_03.py:
+#
+# nn_04.py:
+#
+# nn_04_vs_05.py:
+#
+# nn_04_vs_05_07.py:
+#
+# nn_05.py:
+#
+# nn_06.py:
+#
+# nn_07.py:
+#
+# nn_08.py:
+#
+# nn_09.py:
+#
+# nn_10.py:
+#
+# nn_forward_1.py:
+#
+# nn_forward_2.py:
+#
+# nn_forward_3.py:
+#
+# nn_forward_4.py:
+#
+# nn_linear_1.py:
+#
+# nn_linear_2.py:
+#
+# nn_linear_3.py:
+#
+# nn_linear_4.py:
+#
+# nn_linear_5.py:
+#
+# nn_linear_6.py:
+#
+# nn_linear_7.py:
+#
+# nn_linear_help.py:
+#
+# nn_linear_zeros.py:
+#
+# prepare_for_training.py:
+#
+# print_train_and_test_data.py:
+#
+# tensor_arange.py:
+#
+# tensor_broadcast_1.py:
+#
+# tensor_broadcast_2.py:
+#
+# tensor_broadcast_3.py:
+#
+# tensor_broadcast_4.py:
+#
+# tensor_constructor_3D_1.py:
+#
+# tensor_constructor_3D_2.py:
+#
+# tensor_constructor_3D_zero.py:
+#
+# tensor_constructor_matrix_1.py:
+#
+# tensor_constructor_matrix_2.py:
+#
+# tensor_constructor_matrix_zero.py:
+#
+# tensor_constructor_scalar_1.py:
+#
+# tensor_constructor_scalar_2.py:
+#
+# tensor_constructor_scalar_zero.py:
+#
+# tensor_constructor_vector_1.py:
+#
+# tensor_constructor_vector_2.py:
+#
+# tensor_constructor_vector_3.py:
+#
+# tensor_constructor_vector_zero.py:
+#
+# tensor_dot_product.py:
+#
+# tensor_eye.py:
+#
+# tensor_is_slice_view.py:
+#
+# tensor_narrow_operation_0.py:
+#
+# tensor_narrow_operation_0_B.py:
+#
+# tensor_narrow_operation_1.py:
+#
+# tensor_narrow_operation_1_B.py:
+#
+# tensor_narrow_operation_2.py:
+#
+# tensor_narrow_operation_2_B.py:
+#
+# tensor_narrow_operation_3.py:
+#
+# tensor_narrow_operation_3_B.py:
+#
+# tensor_narrow_operation_4.py:
+#
+# tensor_narrow_operation_5.py:
+#
+# tensor_ones_shape.py:
+#
+# tensor_operator_add.py:
+#
+# tensor_operator_div.py:
+#
+# tensor_operator_matmul.py:
+#
+# tensor_operator_matmul_2.py:
+#
+# tensor_operator_matmul_3.py:
+#
+# tensor_operator_matmul_4.py:
+#
+# tensor_operator_matmul_5.py:
+#
+# tensor_operator_mul.py:
+#
+# tensor_operator_sub.py:
+#
+# tensor_range.py:
+#
+# tensor_read_dtype.py:
+#
+# tensor_reshape.py:
+#
+# tensor_reshape_2.py:
+#
+# tensor_set_dtype.py:
+#
+# tensor_shape.py:
+#
+# tensor_slice_operation_1.py:
+# - konstrukce řezu z tenzoru prvního řádu
+#
+# tensor_slice_operation_2.py:
+# - konstrukce řezu z tenzoru druhého řádu (přes řádky a sloupce)
+#
+# tensor_slice_operation_3.py:
+# - konstrukce řezu s jeho následnou modifikací
+#
+# tensor_slice_operation_4.py:
+# - konstrukce řezu s jeho následnou modifikací (odlišné operace od předchozího příkladu)
+#
+# tensor_sparse.py:
+#
+# tensor_storage_1.py:
+# - získání datové struktury se zdrojem dat pro tenzor
+#
+# tensor_storage_2.py:
+# - získání datové struktury se zdrojem dat pro tenzor
+#
+# tensor_storage_3.py:
+# - získání datové struktury se zdrojem dat pro tenzor
+#
+# tensor_storage_casts.py:
+# - přetypování datové struktury se zdrojem dat pro tenzor
+#
+# tensor_stride_1.py:
+# - význam atributů stride a storage_offset
+#
+# tensor_stride_2.py:
+# - význam atributů stride a storage_offset
+#
+# tensor_stride_3.py:
+# - význam atributů stride a storage_offset
+#
+# tensor_zeros_shape.py:
+#
