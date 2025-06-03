18d138ecf96694ecfbc69e076c45be0b1fb28898
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-06-03 09:14:48 +0200

    Index file

---
+++ FM/00_index.py
@@ -0,0 +1,15 @@
+# Seznam zdrojových kódů v tomto podadresáři
+# ------------------------------------------
+#
+# feedback.py:
+# - pro vykreslení je použita knihovna Matplotlib
+#
+# opl3_waveforms.py:
+# - vykreslení průběhů signálů generovaných čipem OPL3
+# - pro vykreslení je použita knihovna Matplotlib
+#
+# plot_wave.py:
+# - vykreslení průběhu signálu uloženého ve formátu WAV
+# - konverze z osmibitových samplů
+# - pro vykreslení je použita knihovna Matplotlib
+#
