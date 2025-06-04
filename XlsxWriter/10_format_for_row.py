399ad82db6ad4d6133d3e08a6868381e61fd592e
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-06-04 09:09:35 +0200

    Added comments

--- XlsxWriter/10_format_for_row.py
+++ XlsxWriter/10_format_for_row.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 # vim: set fileencoding=utf-8
+#
+# Knihovna XLSXWriter
+#
+# - vytvoření sešitu s delší tabulkou
+# - nastavení stylů řádků
 
 #
 #  (C) Copyright 2021  Pavel Tisnovsky
