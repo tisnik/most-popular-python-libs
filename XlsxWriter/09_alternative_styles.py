6fad39a6fdbc1309c903edb85ed8545ae5a67092
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-06-04 09:09:35 +0200

    Added comments

--- XlsxWriter/09_alternative_styles.py
+++ XlsxWriter/09_alternative_styles.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 # vim: set fileencoding=utf-8
+#
+# Knihovna XLSXWriter
+#
+# - vytvoření sešitu s delší tabulkou
+# - nastavení stylů vybraných buněk
 
 #
 #  (C) Copyright 2021  Pavel Tisnovsky
