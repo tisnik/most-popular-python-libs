9e037729ad79421bcc3e233fdc042bb17b34d8fe
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-06-16 08:42:13 +0200

    Removed unused import

--- assembly/peachpy_42_3.py
+++ assembly/peachpy_42_3.py
@@ -8,13 +8,9 @@
 from peachpy import int32_t
 
 # základní konstruktory atd.
-from peachpy.x86_64 import Function, GeneralPurposeRegister32, abi
-
 # registry
-from peachpy.x86_64 import eax
-
 # konstruktory instrukcí
-from peachpy.x86_64 import MOV, RETURN
+from peachpy.x86_64 import MOV, RETURN, Function, abi, eax
 
 # vytvoření nové subrutiny ve strojovém kódu
 with Function("Function_42", (), int32_t) as asm_function:
