aa31d1a065f93b924e57fe64cce9cf8e25c66f91
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-06-19 08:36:07 +0200

    Fixed imports

--- PyTorch/classification_report.py
+++ PyTorch/classification_report.py
@@ -1,9 +1,10 @@
-import torch
-from torch import nn
-from torch import optim
-from torch.utils.data import DataLoader
-import numpy as np
 import itertools
+
+import torch
+from torch import nn, optim
+from torch.utils.data import DataLoader
+
+import numpy as np
 from sklearn.metrics import classification_report
 
 
