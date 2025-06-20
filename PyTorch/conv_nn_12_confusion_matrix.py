3c50713ddf9f20efd764027b9895904e3e68e4f3
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-06-20 08:43:49 +0200

    Fixed imports

--- PyTorch/conv_nn_12_confusion_matrix.py
+++ PyTorch/conv_nn_12_confusion_matrix.py
@@ -1,7 +1,6 @@
 import matplotlib.pyplot as plt
 from sklearn.metrics import ConfusionMatrixDisplay
 
-
 y_pred = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 y_test = [0, 1, 2, 8, 4, 5, 6, 7, 6, 9, 0, 1, 2, 2, 4, 5, 8, 7, 8, 9]
 
