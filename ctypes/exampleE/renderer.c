0dd7091c5b43ac6edd928563db61f4f1c8f38d14
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-06-27 08:19:16 +0200

    Example calling double free() by mistake

---
+++ ctypes/exampleE/renderer.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+
+typedef struct {
+    unsigned int width;
+    unsigned int height;
+} image_size_t;
+
+typedef struct {
+    unsigned int maxiter;
+    double cx;
+    double cy;
+} rendering_params_t;
+
+
+void render_mandelbrot(image_size_t *image_size,
+                       const unsigned char *palette, unsigned char *pixels,
+                       rendering_params_t *rendering_params) {
+    free(pixels);
+}
+
+void render_julia(image_size_t *image_size,
+                  const unsigned char *palette, unsigned char *pixels,
+                  rendering_params_t *rendering_params) {
+    free(pixels);
+}
