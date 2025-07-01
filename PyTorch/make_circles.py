9ef4f4bbd6f8c2092cf6e3c059638dd1394d28c0
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-07-01 08:00:51 +0200

    Fixed imports

--- PyTorch/make_circles.py
+++ PyTorch/make_circles.py
@@ -4,7 +4,6 @@
 
 # budeme provádět vykreslování de facto standardní knihovnou Matplotlib
 import matplotlib.pyplot as plt
-
 from sklearn.datasets import make_circles
 
 # velikost obrázku s grafem
