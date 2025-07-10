de7d8709bdff0d455f4f2ce3561394ad4cfb5923
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-07-10 09:57:53 +0200

    Index file

---
+++ faiss/00_index.py
@@ -0,0 +1,39 @@
+# Seznam zdrojových kódů v tomto podadresáři
+# ------------------------------------------
+#
+# faiss-1.py:
+# - konstrukce vektoru se souřadnicemi bodů v rovině
+#
+# faiss-2.py:
+# - konstrukce 2D matice se souřadnicemi bodů v rovině
+#
+# faiss-3.py:
+# - konstrukce indexu knihovnou FAISS
+# - tisk základních informací o indexu
+#
+# faiss-4.py:
+# - nalezení nejpodobnějších vektorů
+# - použití L2 metriky
+# - výpis indexů nejpodobnějších vektorů
+#
+# faiss-5.py:
+# - nalezení nejpodobnějších vektorů
+# - použití L2 metriky
+# - výpis souřadnic nejpodobnějších vektorů
+#
+# faiss-6.py:
+# - nalezení nejpodobnějších vektorů
+# - použití metriky založené na skalárním součinu
+# - výpis souřadnic nejpodobnějších vektorů
+# - vektory nejsou normalizovány
+#
+# faiss-7.py:
+# - nalezení nejpodobnějších vektorů
+# - použití metriky založené na skalárním součinu
+# - výpis souřadnic nejpodobnějších vektorů
+# - vektory jsou normalizovány
+#
+# faiss-8.py:
+# - benchmark rychlosti nalezení nejpodobnějších vektorů
+# - výpis výsledků v tabulkové formě
+# - vizualizace výsledků formou grafu
