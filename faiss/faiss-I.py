cbd53f042f51ca8888186683cb30c0d57750dffc
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-07-15 09:34:48 +0200

    Benchmark: differences between float16 and float32

---
+++ faiss/faiss-I.py
@@ -0,0 +1,71 @@
+# Knihovna FAISS
+#
+# - benchmark rychlosti nalezení nejpodobnějších vektorů
+# - vizualizace výsledků formou grafu
+# - porovnání float16 a float32
+
+from time import time
+import faiss
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+
+def similarity_search(n, k, float_type):
+    # pocet dimenzi
+    DIMENSIONS=128
+
+    # nahodne vektory
+    data = np.random.rand(n, 128).astype(float_type)
+
+    # konstrukce indexu pro vyhledavani na zaklade vzdalenosti
+    index = faiss.IndexFlatL2(DIMENSIONS)
+    index.add(data)
+
+    t1 = time()
+
+    # vektor, ke kteremu budeme pocitat vzdalenost
+    query_vector = np.random.rand(1, DIMENSIONS).astype(float_type)
+
+    # pocet nejblizsich bodu
+    distances, indices = index.search(query_vector, k)
+    t2 = time()
+
+    return n, t2-t1
+
+
+def benchmark(from_n, to_n, steps, float_type):
+    ns = []
+    ts_search = []
+
+    for n in np.linspace(from_n, to_n, steps):
+        print(n)
+        n, t_search = similarity_search(int(n), 1, float_type)
+        ns.append(n)
+        ts_search.append(t_search)
+
+    return ns, ts_search
+
+
+#for n in np.linspace(1000000, 10000000, 10):
+from_n = 1000000
+to_n = 10000000
+steps = 10
+
+ns, float16_times = benchmark(from_n, to_n, steps, "float16")
+_, float32_times = benchmark(from_n, to_n, steps, "float32")
+
+plt.plot(ns, float16_times, "r-", label="float16")
+plt.plot(ns, float32_times, "b-", label="float32")
+
+# přidání legendy
+plt.legend(loc="upper left")
+
+# povolení zobrazení mřížky
+plt.grid(True)
+
+plt.savefig(f"faiss_benchmark_3.png")
+
+# zobrazení grafu
+plt.show()
+
