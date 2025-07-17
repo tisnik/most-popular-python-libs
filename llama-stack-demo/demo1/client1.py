1b66fa32079afe3146cc7f6e7c900a7ebfd9b7d7
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-07-17 08:40:47 +0200

    Refactored a bit

--- llama-stack-demo/demo1/client1.py
+++ llama-stack-demo/demo1/client1.py
@@ -4,9 +4,12 @@ PROMPT = "Say Hello"
 
 client = LlamaStackClient(base_url="http://localhost:8321")
 
+print(f"Using Llama Stack version {client._version}")
+
 response = client.inference.chat_completion(
     messages=[{"role": "user", "content": PROMPT}],
     model_id=client.models.list()[0].identifier,
 )
 
-print(response.completion_message.content)
+text = response.completion_message.content
+print(f"LLM response: {text}")
