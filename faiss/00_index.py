7fb441da1302ba2d1f7a08739f4a028eb1f45dbb
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-07-21 08:04:53 +0200

    Updated list of examples

--- faiss/00_index.py
+++ faiss/00_index.py
@@ -37,3 +37,46 @@
 # - benchmark rychlosti nalezení nejpodobnějších vektorů
 # - výpis výsledků v tabulkové formě
 # - vizualizace výsledků formou grafu
+#
+# faiss-9.py:
+# - vizualizace koncových bodů vektorů v rovině
+#
+# faiss-A.py:
+# - vykreslení nejpodobnějších vektorů získaných na základě L2 metriky
+# - vektory nejsou normalizovány
+#
+# faiss-B.py:
+# - vykreslení nejpodobnějších vektorů získaných na základě skalárního součinu
+# - vektory nejsou normalizovány
+#
+# faiss-C.py:
+# - vykreslení nejpodobnějších vektorů získaných na základě skalárního součinu
+# - vektory jsou normalizovány
+#
+# faiss-D.py:
+# - vykreslení nejpodobnějších vektorů získaných na základě skalárního součinu
+# - vektory jsou normalizovány
+# - vykresleny jsou ovšem původní vektory
+#
+# faiss-E.py:
+# - nalezení nejpodobnějších vektorů získaných na základě skalárního součinu
+# - vektory jsou normalizovány
+# - vykreslení všech vektorů formou orientovaných šipek
+#
+# faiss-F.py:
+# - nalezení nejpodobnějších vektorů získaných na základě skalárního součinu
+# - vektory jsou normalizovány
+# - vykreslení všech vektorů po normalizaci formou orientovaných šipek
+#
+# faiss-G.py:
+# - vyhledání a vykreslení nejvíce NEpodobných vektorů
+#
+# faiss-H.py:
+# - vykreslení nejpodobnějších vektorů získaných na základě jejich vzdálenosti
+# - vykresleny jsou původní vektory
+# - složky všech vektorů jsou typu float16
+#
+# faiss-I.py:
+# - benchmark rychlosti nalezení nejpodobnějších vektorů
+# - vizualizace výsledků formou grafu
+# - porovnání float16 a float32
