970faf3ae51e3115f5520383e320760225aec448
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-07-28 08:44:01 +0200

    Construct embeddings and display its shape

---
+++ faiss_transformers/transformer4.py
@@ -0,0 +1,22 @@
+from sentence_transformers import SentenceTransformer
+
+model = SentenceTransformer("paraphrase-MiniLM-L6-v2")
+
+print(model)
+
+sentences = [
+    "The rain in Spain falls mainly on the plain",
+    "The tesselated polygon is a special type of polygon",
+    "The quick brown fox jumps over the lazy dog",
+    "To be or not to be, that is the question",
+    "It is a truth universally acknowledged...",
+    "The goat ran down the hill"
+]
+
+embeddings = model.encode(sentences)
+print(f"Embeddings shape: {embeddings.shape}")
+
+print(f"Data type: {type(embeddings)}")
+
+print(embeddings)
+
