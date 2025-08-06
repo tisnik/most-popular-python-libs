d09934b68f9e0a1bb6eadfe4c44ea53345a8cb21
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-08-06 08:07:22 +0200

    Double-free example (will crash the process)

---
+++ ctypes/exampleF/renderer.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+
+#include "renderer.h"
+
+
+void render_mandelbrot(image_size_t *image_size,
+                       const unsigned char *palette, unsigned char *pixels,
+                       rendering_params_t *rendering_params) {
+    free(pixels);
+}
+
+void render_julia(image_size_t *image_size,
+                  const unsigned char *palette, unsigned char *pixels,
+                  rendering_params_t *rendering_params) {
+    free(pixels);
+}
---
+++ ctypes/exampleF/renderer.h
@@ -0,0 +1,10 @@
+typedef struct {
+    unsigned int width;
+    unsigned int height;
+} image_size_t;
+
+typedef struct {
+    unsigned int maxiter;
+    double cx;
+    double cy;
+} rendering_params_t;
