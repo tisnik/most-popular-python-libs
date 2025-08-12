acea701fd82b89cbad9435965003833f4ef04846
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-08-12 08:28:38 +0200

    Insert embeddings (once sentence) into table

---
+++ pgvector/sentence_into_v384.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+# vim: set fileencoding=utf-8
+
+#
+#  (C) Copyright 2025  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
+import psycopg2
+
+import numpy as np
+from pgvector.psycopg2 import register_vector
+from sentence_transformers import SentenceTransformer
+
+model = SentenceTransformer("paraphrase-MiniLM-L6-v2")
+
+print(model)
+
+sentence = "The rain in Spain falls mainly on the plain"
+
+embeddings = model.encode(sentence)
+print(f"Embeddings shape: {embeddings.shape}")
+
+vector = embeddings
+
+connection = psycopg2.connect(
+    host="", port=5432, user="tester", password="123qwe", dbname="test"
+)
+
+register_vector(connection)
+
+with connection.cursor() as cursor:
+    cursor.execute("INSERT INTO v384 (embedding) VALUES (%s)", (vector, ))
+    connection.commit()
+
