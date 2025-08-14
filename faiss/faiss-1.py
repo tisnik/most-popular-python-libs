40c0db71eb157ff2e282952c76ebde15968f70ee
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-08-14 08:24:46 +0200

    Updated comments

--- faiss/faiss-1.py
+++ faiss/faiss-1.py
@@ -1,6 +1,8 @@
 # Knihovna FAISS
 #
-# - konstrukce vektoru se souřadnicemi bodů v rovině
+# - konstrukce dvou vektorů se souřadnicemi bodů v rovině
+# - výpis obsahu obou vektorů na standardní výstup
+# - konstrukce seznamu souřadnic vytvořených z obou vektorů
 
 # x-ove souradnice bodu v rovine
 x = [-5, -4, -3,    3,  4,  5,   3, 3, 3,  4, 4, 4,  5, 5, 5]
@@ -8,8 +10,9 @@ x = [-5, -4, -3,    3,  4,  5,   3, 3, 3,  4, 4, 4,  5, 5, 5]
 # y-ove souradnice bodu v rovine
 y = [ 5,  3,  5,   -5, -3, -5,   3, 4, 5,  3, 4, 5,  3, 4, 5]
 
+# vypis obsahu obou vektoru
 print(x)
 print(y)
 
-# seznam souradnic
+# konstrukce seznamu souradnic
 print(list(zip(x, y)))
