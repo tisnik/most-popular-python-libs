e204dabe1072b44c479bf3bf994da2833751a0e8
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-08-18 08:43:22 +0200

    FAISS inded benchmark: basic FlatL2 index

---
+++ faiss/faiss-index-benchmark-1.py
@@ -0,0 +1,73 @@
+# Knihovna FAISS
+#
+# - benchmark rychlosti nalezení nejpodobnějších vektorů
+# - je použit index FlatL2
+# - výpis výsledků v tabulkové formě
+# - vizualizace výsledků formou grafu
+
+from time import time
+import faiss
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+
+def similarity_search(n, k):
+    """Nalezeni k nejblizsich vektoru v mnozine n vektoru."""
+    # pocet slozek vektoru
+    DIMENSIONS=128
+
+    t1 = time()
+
+    # nahodne vektory
+    data = np.random.rand(n, 128).astype('float32')
+
+    t2 = time()
+
+    # konstrukce indexu pro vyhledavani na zaklade vzdalenosti
+    index = faiss.IndexFlatL2(DIMENSIONS)
+    index.add(data)
+
+    t3 = time()
+    REP_COUNT = 20
+
+    for _ in range(REP_COUNT):
+        # vektor, ke kteremu budeme pocitat vzdalenost
+        query_vector = np.random.rand(1, DIMENSIONS).astype("float32")
+
+        # pocet nejblizsich bodu
+        distances, indices = index.search(query_vector, k)
+
+        # test, kolik vektoru se nalezlo
+        assert len(distances) == k
+        assert len(indices) == k
+
+    t4 = time()
+
+    return n, t2-t1, t3-t2, (t4-t3)/REP_COUNT
+
+
+ns = []
+ts_search = []
+
+for n in np.linspace(0, 1000000, 11):
+    print(n)
+    n, t_rand, t_index, t_search = similarity_search(int(n), 1)
+    ns.append(n)
+    ts_search.append(t_search)
+
+
+plt.xlabel("# vectors")
+plt.ylabel("Time/sec")
+plt.plot(ns, ts_search, "m-", label="similarity search")
+
+# přidání legendy
+plt.legend(loc="upper left")
+
+# povolení zobrazení mřížky
+plt.grid(True)
+
+plt.savefig("faiss_benchmark_1.png")
+
+# zobrazení grafu
+plt.show()
