513724e916c9d85a9118c509b326e5f33c5d64ab
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-08-19 09:28:06 +0200

    Proper User class construction - but how to support optional attributes

---
+++ pydantic/user_04.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel
+
+
+class User(BaseModel):
+    name: str
+    surname: str
+    age: int
+    registered: bool
+
+
+user1 = User(name="John", surname="Doe", age=None, registered=None)
+print(user1)
