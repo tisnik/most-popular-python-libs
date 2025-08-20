f9c22558d1e5070830d4fd204bf1e731957b7826
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-08-20 08:35:55 +0200

    Character class with optional address with default value

---
+++ pydantic/character_4.py
@@ -0,0 +1,35 @@
+from pydantic import BaseModel, Field, PositiveInt, field_validator
+
+
+class Address(BaseModel):
+    street: str
+    house_number: PositiveInt | str
+    city: str
+
+
+class User(BaseModel):
+    name: str = Field(..., max_length=10)
+    surname: str = Field(..., max_length=10)
+    age: PositiveInt | None
+    registered: bool = False
+
+    @field_validator("age")
+    def check_age(cls, value):
+        if value < 18:
+            raise ValueError("You are too young to register")
+        return value
+
+
+class Character(BaseModel):
+    role: str
+    user: User
+    address: Address | None = None
+
+
+character = Character(
+    role="Detective",
+    user=User(name="Sherlock", surname="Holmes", age=42)
+)
+
+as_json = character.model_dump_json(indent=4)
+print(as_json)
