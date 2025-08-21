c55ec4ac46618f88e27ea73ba113a72767798daa
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-08-21 07:55:28 +0200

    Max length specification for strings

---
+++ pydantic/user_08.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel, Field, PositiveInt, field_validator
+
+
+class User(BaseModel):
+    name: str = Field(..., max_length=10)
+    surname: str = Field(..., max_length=10)
+    age: PositiveInt | None
+    registered: bool = False
+
+    @field_validator("age")
+    def check_age(cls, value):
+        if value < 18:
+            raise ValueError("You are too young to register")
+        return value
+
+
+user1 = User(name="Nabuchodonozor", surname="II", age=18)
+print(user1)
