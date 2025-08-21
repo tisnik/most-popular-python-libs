008ce057bb1921fa091451e5865db0395dc0a797
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-08-21 07:55:28 +0200

    Default attribute values, attributes that can be None

---
+++ pydantic/user_07.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel, PositiveInt, field_validator
+
+
+class User(BaseModel):
+    name: str
+    surname: str
+    age: PositiveInt | None
+    registered: bool = False
+
+    @field_validator("age")
+    def check_age(cls, value):
+        if value < 18:
+            raise ValueError("You are too young to register")
+        return value
+
+
+user1 = User(name="John", surname="Doe", age=17)
+print(user1)
