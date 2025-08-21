4c8bb5ccea711be76fc5ff34c858964f4f644429
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-08-21 07:55:28 +0200

    Deserialization from JSON format

---
+++ pydantic/user_09.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel, Field, PositiveInt, field_validator
+
+
+class User(BaseModel):
+    name: str = Field(..., max_length=10)
+    surname: str = Field(..., max_length=10)
+    age: PositiveInt | None
+    registered: bool = False
+
+    @field_validator("age")
+    def check_age(cls, value):
+        if value < 18:
+            raise ValueError("You are too young to register")
+        return value
+
+
+data = """
+    {"name": "John",
+     "surname": "Doe",
+     "age": 18
+    }
+"""
+
+user1 = User.model_validate_json(data)
+print(user1)
