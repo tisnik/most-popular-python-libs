2ab3dc69dfbf5891fd120d37c5855018aebd4719
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-08-26 09:54:03 +0200

    Index file

---
+++ pygame/00_index.py
@@ -0,0 +1,68 @@
+# Seznam zdrojových kódů v tomto podadresáři
+# ------------------------------------------
+#
+# pygame01.py:
+#
+# pygame02.py:
+#
+# pygame03.py:
+#
+# pygame04.py:
+#
+# pygame05.py:
+#
+# pygame06.py:
+#
+# pygame07.py:
+#
+# pygame08.py:
+#
+# pygame09.py:
+#
+# pygame10.py:
+#
+# pygame11.py:
+#
+# pygame12.py:
+#
+# pygame13.py:
+#
+# pygame14.py:
+#
+# pygame15.py:
+#
+# pygame16.py:
+#
+# pygame17.py:
+#
+# pygame18.py:
+#
+# pygame19.py:
+#
+# pygame20.py:
+#
+# pygame21.py:
+#
+# pygame22.py:
+#
+# pygame23.py:
+#
+# pygame24.py:
+#
+# pygame25.py:
+#
+# pygame26.py:
+#
+# pygame27.py:
+#
+# pygame28.py:
+#
+# pygame_matplotlib_1.py:
+#
+# pygame_matplotlib_2.py:
+#
+# pygame_matplotlib_3.py:
+#
+# pygame_matplotlib_4.py:
+#
+# pygame_matplotlib_5.py:
