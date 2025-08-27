5bcf6db8cefba666886aba8913e0cb3d9f8be191
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-08-27 10:41:05 +0200

    IO exceptions

---
+++ courses/rest/exception_file_write2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# vim: set fileencoding=utf-8
+
+try:
+    file = open("testfile", "w")
+    file.write("test")
+except IOError:
+    print("soubor nelze otevrit pro zapis")
+else:
+    print("zapis uspesne proveden")
+finally:
+    file.close()
