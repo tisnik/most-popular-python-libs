d3f36d78b40b659ea8a00bf1815666701fc6ff2f
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-09-01 09:27:36 +0200

    Pydantic model for service configuration, example #4

---
+++ pydantic/config_04.py
@@ -0,0 +1,38 @@
+"""Global configuration."""
+
+
+import yaml
+
+from pydantic import (
+    BaseModel,
+    ConfigDict,
+)
+
+
+class ConfigurationBase(BaseModel):
+    """Base class for all configuration models that rejects unknown fields."""
+
+    model_config = ConfigDict(extra="forbid")
+
+
+class Configuration(ConfigurationBase):
+    """Global configuration."""
+
+    name: str
+
+    def dump(self, filename: str = "configuration.json") -> None:
+        """Dump actual configuration into JSON file."""
+        with open(filename, "w", encoding="utf-8") as fout:
+            fout.write(self.model_dump_json(indent=4))
+
+
+def load_configuration(filename: str) -> Configuration:
+    """Load configuration from YAML file."""
+    with open(filename, encoding="utf-8") as fin:
+        config_dict = yaml.safe_load(fin)
+        return Configuration(**config_dict)
+
+
+configuration = load_configuration("config_04.yaml")
+
+configuration.dump("config_04.json")
