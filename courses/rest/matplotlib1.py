e736e743e1c41cf07d0b5cca29d00335a12adce3
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-09-03 09:30:40 +0200

    Matplotlib invocation

---
+++ courses/rest/matplotlib1.py
@@ -0,0 +1,23 @@
+# Knihovny Numpy a matplotlib
+#
+# První demonstrační příklad:
+# - vykreslení průběhu funkce sin
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# hodnoty na x-ové ose
+x = np.linspace(0, 2 * np.pi, 100)
+
+# hodnoty na y-ové ose
+y = np.sin(x)
+
+# vykreslit průběh funkce
+plt.plot(x, y)
+
+# popis os
+plt.xlabel("x")
+plt.ylabel("sin(x)")
+
+# zobrazení grafu
+plt.show()
