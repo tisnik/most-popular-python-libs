81225d802cb3ef59dade1bc5ed585a7db94f5596
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-09-05 08:41:08 +0200

    List sort

---
+++ courses/rest/list_sort.py
@@ -0,0 +1,12 @@
+seznam = [5, 4, 1, 3, 4, 100, -1]
+print(seznam)
+
+seznam.sort()
+print(seznam)
+
+seznam = [5, 4, 1, 3, 4, 100, -1]
+print(seznam)
+
+seznam2 = sorted(seznam)
+print(seznam)
+print(seznam2)
