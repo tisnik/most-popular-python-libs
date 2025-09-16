92992c161dd3bcfbb63e4b4bbf62a3d6634961d8
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-09-16 08:47:21 +0200

    Pre-generated index file

---
+++ pydantic/00_index.py
@@ -0,0 +1,49 @@
+# Seznam zdrojových kódů v tomto podadresáři
+# ------------------------------------------
+#
+# character_1.py:
+#
+# character_2.py:
+#
+# character_3.py:
+#
+# character_4.py:
+#
+# config_01.py:
+#
+# config_02.py:
+#
+# config_03.py:
+#
+# config_04.py:
+#
+# config_05.py:
+#
+# config_06.py:
+#
+# config_07.py:
+#
+# config_08.py:
+#
+# config_09.py:
+#
+# user_01.py:
+#
+# user_02.py:
+#
+# user_03.py:
+#
+# user_04.py:
+#
+# user_05.py:
+#
+# user_06.py:
+#
+# user_07.py:
+#
+# user_08.py:
+#
+# user_09.py:
+#
+# user_10.py:
+#
