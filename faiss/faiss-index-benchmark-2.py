d27023720cafadba89c7ef8e0c9387d536553d11
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-10-09 08:59:32 +0200

    Fixed import sorting

--- faiss/faiss-index-benchmark-2.py
+++ faiss/faiss-index-benchmark-2.py
@@ -7,10 +7,10 @@
 # - vizualizace výsledků formou grafu
 
 from time import time
-import faiss
-import numpy as np
 
+import faiss
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 def similarity_search(dimensions, n, k):
