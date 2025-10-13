bba25f5be911abbb71dff04badaf7315b1bfa182
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-10-13 09:13:57 +0200

    PEP-765: motivation, example #1

---
+++ python3.14/pep-765-motivation-1.py
@@ -0,0 +1,13 @@
+def test_fn():
+    try:
+        with open("pep-758-motivation.py", "r") as fin:
+            content = fin.read()
+        with open("bar", "w") as fout:
+            fout.write(content)
+    except (FileNotFoundError, PermissionError, IsADirectoryError, IOError) as e:
+        print(type(e))
+    finally:
+        return
+
+
+test_fn()
