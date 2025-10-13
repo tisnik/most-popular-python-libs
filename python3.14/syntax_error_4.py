96ebe4091871964aae1572bd4129414391d9750f
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-10-13 09:13:57 +0200

    Syntax error to be detected by Python interpreter, example #4

---
+++ python3.14/syntax_error_4.py
@@ -0,0 +1,12 @@
+def test_fn():
+    for x in range(10):
+        try:
+            y = 1 / x
+            print(f"{x:2}  {y:4.2f}")
+        except ZeroDivisionError:
+            print(f"{x:2}  divide by zero")
+        finale:
+            pass
+
+
+test_fn()
