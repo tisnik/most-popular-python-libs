afd15d2b7e0e615c720c2a0c158e645e764b6965
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-10-27 09:01:36 +0100

    Communication between threads via queue

---
+++ python3.14/interpreters/queue-multithreading-2.py
@@ -0,0 +1,56 @@
+# Multiprocesing a multithreading v Pythonu:
+# - spuštění více úloh v nových vláknech
+# - komunikace mezi vlákny s využitím fronty
+
+
+from queue import Queue
+from threading import Thread
+import time
+
+
+def worker(name, q):
+    """Worker spuštěný několikrát v samostatných vláknech."""
+    while True:
+        # čtení příkazů z fronty
+        cmd = q.get()
+        q.task_done()
+        print(f"Thread '{name}' received command '{cmd}'")
+        if cmd == "quit":
+            print(f"Thread '{name}' is about to quit")
+            return
+        time.sleep(1)
+
+
+if __name__ == "__main__":
+    print("Starting")
+
+    # vytvoření fronty pro komunikaci mezi vlákny
+    q = Queue()
+
+    # spuštění tří vláken
+    names = ("foo", "bar", "baz")
+    ts = [Thread(target=worker, daemon=True, name=name, args=[name, q]) for name in names]
+
+    # spuštění tří vláken
+    for t in ts:
+        t.start()
+
+    print("Sending data to other threads")
+
+    # komunikace s vlákny přes frontu
+    for i in range(10):
+        print(f"Sending 'command {i}'")
+        q.put("command {}".format(i))
+
+    print("Asking other threads to finish")
+
+    # příkaz pro ukončení vláken
+    for i in range(3):
+        q.put("quit")
+
+    print("Waiting for other threads")
+
+    # čekání na zpracování všech zpráv ve frontě
+    q.join()
+
+    print("All work done!")
