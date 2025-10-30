aa1a98a5e64a183a4bbec281349f32d8f6bb9838
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-10-30 08:30:15 +0100

    Benchmark results

---
+++ python3.14/julia-renderer/run_anim.sh
@@ -0,0 +1,6 @@
+python3.14 julia_seq_anim.py
+
+for i in `seq 1 16`
+do
+    python3.14 julia_parallel_anim.py $i
+done
---
+++ python3.14/julia-renderer/run_seq.sh
@@ -0,0 +1,9 @@
+python3.11 julia_seq_anim.py > 11A
+python3.11 julia_seq_anim.py > 11B
+python3.12 julia_seq_anim.py > 12A
+python3.12 julia_seq_anim.py > 12B
+python3.13 julia_seq_anim.py > 13A
+python3.13 julia_seq_anim.py > 13B
+python3.14 julia_seq_anim.py > 14A
+python3.14 julia_seq_anim.py > 14B
+
