674339c0d7aed7a7de6c308272cad75b0c6c3c15
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-10-30 08:28:10 +0100

    Sequential bubble sort impementation

---
+++ python3.14/bubble-sort/bubble_sort_seq.py
@@ -0,0 +1,27 @@
+import random
+from time import perf_counter
+
+
+def bubble_sort(size):
+    a = [random.randrange(0, 10000) for i in range(size)]
+
+    t1 = perf_counter()
+
+    for i in range(size - 1, 0, -1):
+        for j in range(i):
+            if a[j] > a[j + 1]:
+                a[j], a[j + 1] = a[j + 1], a[j]
+
+    t2 = perf_counter()
+
+    print(f"Sorted in {t2-t1} seconds")
+
+
+t1 = perf_counter()
+
+for i in range(100):
+    bubble_sort(5000)
+
+t2 = perf_counter()
+
+print(f"Total time: {t2-t1} seconds")
