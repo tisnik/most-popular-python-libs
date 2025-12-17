40894d00a95925b13f2a73141e405f0271b07bb8
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-12-17 09:35:38 +0100

    Second 'Hello world' example using Kv language for GUI specification

---
+++ kivy/hello_kv_spec_2.py
@@ -0,0 +1,10 @@
+from kivy.app import App
+from kivy.lang import Builder
+
+class TestApp(App):
+    def build(self):
+        builder = Builder.load_file("TestApp2.kv")
+        return builder
+
+application = TestApp()
+application.run()
