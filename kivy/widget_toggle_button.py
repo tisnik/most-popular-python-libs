1baa4d0dd453301e198f1d16147d262cddc6bf1a
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-12-19 13:23:16 +0100

    Toggle button in Kivy

---
+++ kivy/widget_toggle_button.py
@@ -0,0 +1,33 @@
+from kivy.app import App
+from kivy.core.window import Window
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.togglebutton import ToggleButton
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+
+
+class Application(App):
+    def build(self):
+        layout = GridLayout(cols=1, padding=10, spacing=10)
+
+        # prvni radek mrizky
+        label1 = Label(text="ToggleButton")
+        layout.add_widget(label1)
+
+        # druhy radek mrizky
+        toggle_button = ToggleButton(text="Toggle")
+        layout.add_widget(toggle_button)
+
+        # treti radek mrizky
+        button = Button(text="Ok")
+        button.on_release=self.stop
+        layout.add_widget(button)
+
+        return layout
+
+    def on_start(self):
+        Window.size = (400, 180)
+
+
+if __name__ == "__main__":
+    Application().run()
