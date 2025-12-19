634b51090c81e5fe0f7c5069229fb76e82366ec0
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-12-19 13:23:16 +0100

    Checkbox in Kivy

---
+++ kivy/widget_checkbox_2.py
@@ -0,0 +1,37 @@
+from kivy.app import App
+from kivy.core.window import Window
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.checkbox import CheckBox
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+
+
+class Application(App):
+    def build(self):
+        layout = GridLayout(cols=2, padding=10, spacing=10)
+
+        # prvni radek mrizky
+        label1 = Label(text="I really want to receive spam")
+        layout.add_widget(label1)
+        ack1 = CheckBox(active=True)
+        layout.add_widget(ack1)
+
+        # druhy radek mrizky
+        label2 = Label(text="Share my data with FAANG")
+        layout.add_widget(label2)
+        ack2 = CheckBox(active=True)
+        layout.add_widget(ack2)
+
+        # treti radek mrizky
+        button = Button(text="Ok")
+        button.on_release=self.stop
+        layout.add_widget(button)
+
+        return layout
+
+    def on_start(self):
+        Window.size = (420, 160)
+
+
+if __name__ == "__main__":
+    Application().run()
