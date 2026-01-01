11379c5cc77d47a3cdf11f4872c092af26d7bd6c
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-01-01 11:09:25 +0100

    Box layout: 2nd example

---
+++ kivy/box_layout_2.py
@@ -0,0 +1,45 @@
+from kivy.app import App
+from kivy.core.window import Window
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+
+
+class Application(App):
+    def build(self):
+        layout = BoxLayout(orientation="vertical")
+
+        button = Button(text="1")
+        layout.add_widget(button)
+
+        button = Button(text="2")
+        layout.add_widget(button)
+
+        button = Button(text="3")
+        layout.add_widget(button)
+
+        button = Button(text="4")
+        layout.add_widget(button)
+
+        button = Button(text="5")
+        layout.add_widget(button)
+
+        button = Button(text="6")
+        layout.add_widget(button)
+
+        button = Button(text="7")
+        layout.add_widget(button)
+
+        button = Button(text="8")
+        layout.add_widget(button)
+
+        button = Button(text="9")
+        layout.add_widget(button)
+
+        return layout
+
+    def on_start(self):
+        Window.size = (400, 400)
+
+
+if __name__ == "__main__":
+    Application().run()
