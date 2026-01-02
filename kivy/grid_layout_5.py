13997b32987f04f385114a978b3b770a808fa8a8
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-01-02 10:13:08 +0100

    Grid layout, example #5

---
+++ kivy/grid_layout_5.py
@@ -0,0 +1,16 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.core.window import Window
+
+
+class Application(App):
+    def build(self):
+        builder = Builder.load_file("grid_layout.kv")
+        return builder
+
+    def on_start(self):
+        Window.size = (200, 200)
+
+
+if __name__ == "__main__":
+    Application().run()
