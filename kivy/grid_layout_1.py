9600c85f5a1678193db2ed94f62a68bdf529e5b5
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-01-02 10:12:55 +0100

    Grid layout, example #1

---
+++ kivy/grid_layout_1.py
@@ -0,0 +1,45 @@
+from kivy.app import App
+from kivy.core.window import Window
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.button import Button
+
+
+class Application(App):
+    def build(self):
+        layout = GridLayout(cols=3)
+
+        button = Button(text="1")
+        layout.add_widget(button)
+
+        button = Button(text="2")
+        layout.add_widget(button)
+
+        button = Button(text="3")
+        layout.add_widget(button)
+
+        button = Button(text="4")
+        layout.add_widget(button)
+
+        button = Button(text="5")
+        layout.add_widget(button)
+
+        button = Button(text="6")
+        layout.add_widget(button)
+
+        button = Button(text="7")
+        layout.add_widget(button)
+
+        button = Button(text="8")
+        layout.add_widget(button)
+
+        button = Button(text="9")
+        layout.add_widget(button)
+
+        return layout
+
+    def on_start(self):
+        Window.size = (200, 200)
+
+
+if __name__ == "__main__":
+    Application().run()
