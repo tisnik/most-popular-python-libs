d8c1f34215d67ae68ae39ba8dae09498592a5892
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-01-06 09:15:12 +0100

    Fixed imports

--- faiss_transformers/transformerD.py
+++ faiss_transformers/transformerD.py
@@ -1,7 +1,9 @@
-from sentence_transformers import SentenceTransformer
-import faiss
-from datasets import load_dataset
 from time import time
+
+from datasets import load_dataset
+from sentence_transformers import SentenceTransformer
+
+import faiss
 
 MODEL_NAME = "paraphrase-MiniLM-L6-v2"
 DATASET_ID = "polygraf-ai/human-sentences-1M-sample-v2"
