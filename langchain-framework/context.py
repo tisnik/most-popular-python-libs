d13908fe76286a776d8245dcab0cd820a328ca4b
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-01-09 09:19:52 +0100

    Context: conversation memory

---
+++ langchain-framework/context.py
@@ -0,0 +1,18 @@
+# Framework LangChain
+#
+# - práce s pamětí, ve které je uložena konverzace
+# - poslání fakta do jazykového modelu
+# - získání dvou odpovědí, které závisí na faktech v konverzaci
+
+from langchain_openai import ChatOpenAI
+from langchain.chains import ConversationChain
+from langchain.memory import ConversationBufferMemory
+
+llm = ChatOpenAI(model_name="gpt-4o-mini")
+
+memory = ConversationBufferMemory()
+chatbot = ConversationChain(llm=llm, memory=memory)
+
+print(chatbot.invoke("Hi, I'm Pavel and I live in Czechia."))
+print(chatbot.invoke("What's my name?"))
+print(chatbot.invoke("Where I live"))
