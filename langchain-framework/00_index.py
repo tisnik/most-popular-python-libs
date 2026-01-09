ee0ac9ea3204c479ef3249bbf149b87cf2d58f69
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-01-09 09:20:04 +0100

    List of examples

---
+++ langchain-framework/00_index.py
@@ -0,0 +1,28 @@
+# Seznam zdrojových kódů v tomto podadresáři
+# ------------------------------------------
+#
+# context.py:
+# - práce s pamětí, ve které je uložena konverzace
+# - poslání fakta do jazykového modelu
+# - získání dvou odpovědí, které závisí na faktech v konverzaci
+#
+# list_models.py:
+# - tisk všech dostupných modelů získaných od poskytovatele
+#
+# openai_query.py:
+# - inicializace rozhraní k modelu poskytovaného přes OpenAI
+# - poslání dotazu do jazykového modelu
+# - tisk odpovědi
+#
+# prompt.py:
+# - konstrukce jednoduchého řetězce
+# - předání parametru do výzvy
+# - poslání dotazu do jazykového modelu
+# - tisk odpovědi
+#
+# temperature.py:
+# - inicializace rozhraní k modelu poskytovaného přes OpenAI
+# - nastavení parametrů jazykového modelu
+# - poslání dotazu do jazykového modelu
+# - tisk odpovědi
+#
