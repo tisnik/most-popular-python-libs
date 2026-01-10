77616b57931892b23c98ae645d7b26d0f798a630
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-01-10 09:21:50 +0100

    Index file for Kivy examples

---
+++ kivy/00_index.py
@@ -0,0 +1,85 @@
+# Seznam zdrojových kódů v tomto podadresáři
+# ------------------------------------------
+#
+# box_layout_1.py:
+#
+# box_layout_2.py:
+#
+# box_layout_3.py:
+#
+# box_layout_4.py:
+#
+# box_layout_5.py:
+#
+# grid_layout_1.py:
+#
+# grid_layout_2.py:
+#
+# grid_layout_3.py:
+#
+# grid_layout_4.py:
+#
+# grid_layout_5.py:
+#
+# hello_kv_spec.py:
+#
+# hello_kv_spec_2.py:
+#
+# hello_kv_spec_3.py:
+#
+# hello_world.py:
+#
+# hello_world_2.py:
+#
+# login_form_1.py:
+#
+# login_form_2.py:
+#
+# login_form_3.py:
+#
+# login_form_4.py:
+#
+# login_form_5.py:
+#
+# login_form_6.py:
+#
+# stack_layout_1.py:
+#
+# stack_layout_2.py:
+#
+# stack_layout_3.py:
+#
+# stack_layout_4.py:
+#
+# stack_layout_5.py:
+#
+# widget_basic_1.py:
+#
+# widget_basic_2.py:
+#
+# widget_button_attributes.py:
+#
+# widget_checkbox.py:
+#
+# widget_checkbox_2.py:
+#
+# widget_checkbox_3.py:
+#
+# widget_colors.py:
+#
+# widget_icon.py:
+#
+# widget_label_attributes.py:
+#
+# widget_radio_buttons_1.py:
+#
+# widget_radio_buttons_2.py:
+#
+# widget_rgb.py:
+#
+# widget_rgb_2.py:
+#
+# widget_slider.py:
+#
+# widget_toggle_button.py:
+#
