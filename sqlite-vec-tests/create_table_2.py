19b783e5abd90962bd3bca2e4e5f1387ed4f9640
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-01-12 09:07:07 +0100

    Create table with vector column, based on context manager

---
+++ sqlite-vec-tests/create_table_2.py
@@ -0,0 +1,31 @@
+# Knihovna SQLite s rozšířením SQLite-vec
+#
+# - připojení k databázi uložené v souboru
+# - databáze je v případě potřeby vytvořena
+# - načtení rozšíření SQLite-vec
+# - vytvoření virtuální tabulky s jedním sloupcem
+# - využití kontext manažeru
+# - nepoužívá se kurzor (zkrácený zápis)
+
+import sqlite3
+import sqlite_vec
+
+# připojení k databázi uložené v souboru
+# v případě potřeby je databáze vytvořena
+with sqlite3.connect("v1.db") as db:
+    # načtení rozšíření SQLite-vec
+    db.enable_load_extension(True)
+    sqlite_vec.load(db)
+    db.enable_load_extension(False)
+
+    TABLE_STATEMENT = """
+        CREATE virtual TABLE v1 USING vec0(
+            embedding float[2]
+        );
+    """
+
+    # vytvoření virtuální tabulky
+    result = db.execute(TABLE_STATEMENT)
+    print(result)
+
+    db.commit()
