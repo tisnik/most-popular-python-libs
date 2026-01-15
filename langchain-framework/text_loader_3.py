58817a6e7a13d8712dead477079deb53623dad7d
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-01-15 09:21:23 +0100

    Text loader

---
+++ langchain-framework/text_loader_3.py
@@ -0,0 +1,26 @@
+# Framework LangChain
+#
+# - inicializace objektu pro načítání textových dokumentů
+# - načtení a zpracování jednoho textového dokumentu
+# - tisk typu dokumentu a metadat
+# - tisk obsahu objektu představujícího dokument
+
+from langchain_community.document_loaders import TextLoader
+
+DOCUMENT_PATH = "documents/assemblery.txt"
+
+loader = TextLoader(DOCUMENT_PATH)
+print(loader)
+
+text_documents= loader.load()
+
+print(f"Number of documents: {len(text_documents)}")
+
+for text_document in text_documents:
+    print()
+    print("-"*80)
+    print()
+    print("Type:", text_document.type)
+    print("Metadata:", text_document.metadata)
+    print()
+    print(text_document.page_content)
