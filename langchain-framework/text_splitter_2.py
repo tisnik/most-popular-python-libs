70f08be5fb7a564dea05f4ed04d4675d9de1c2f4
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-01-15 09:21:24 +0100

    Text splitter, basic example

---
+++ langchain-framework/text_splitter_2.py
@@ -0,0 +1,24 @@
+# Framework LangChain
+#
+# - inicializace objektu pro načítání textových dokumentů
+# - načtení a zpracování jednoho textového dokumentu
+# - rozdělení dokumentu do menších částí
+# - tisk všech částí dokumentu
+
+from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+DOCUMENT_PATH = "documents/assemblery.txt"
+
+loader = TextLoader(DOCUMENT_PATH)
+print(loader)
+print()
+
+text_documents= loader.load()
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+splitted=text_splitter.split_documents(text_documents)
+
+for chunk in splitted:
+    print(chunk.page_content)
+    print("-"*50)
