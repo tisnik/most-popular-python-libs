3f9a12218d09c8d956a82b753fb9573f6bb0555c
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-01-19 08:23:37 +0100

    Bubble sort: old Python variant

---
+++ python0.9.1/bubble_sort_old.py
@@ -0,0 +1,23 @@
+import rand
+import time
+
+
+def bubble_sort(size):
+    a = []
+    for i in range(size):
+        a.append(rand.rand())
+    for i in range(size - 1, 0, -1):
+        for j in range(i):
+            if a[j] > a[j + 1]:
+                a[j], a[j + 1] = a[j + 1], a[j]
+    for i in range(size-1):
+        if a[i] > a[i+1]:
+            print('FAIL')
+
+t1 = time.time()
+
+bubble_sort(30000)
+
+t2 = time.time()
+
+print 'Total time:', t2-t1
