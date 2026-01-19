22e4a45ccab0315599e3618a2cbcb1600fe2c896
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-01-19 08:23:37 +0100

    Class constructor: modern Python variant

---
+++ python0.9.1/class_modern.py
@@ -0,0 +1,8 @@
+class Employee:
+
+    def __init__(self, first_name, surname, salary):
+        self._first_name = first_name
+        self._surname = surname
+        self._salary = salary
+
+e = Employee()
