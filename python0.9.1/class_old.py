0a4016034188a987a3df47f335ae666c2f3e2580
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-01-19 08:23:37 +0100

    Class constructor: old Python variant

---
+++ python0.9.1/class_old.py
@@ -0,0 +1,7 @@
+class Employee(Object):
+    def __init__(self, first_name, surname, salary):
+        self._first_name = first_name
+        self._surname = surname
+        self._salary = salary
+
+e = Employee()
