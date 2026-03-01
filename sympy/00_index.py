54f244d8091a0c9e4cb774cbc23f18e043b96a63
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-03-01 09:51:44 +0100

    Stub for index file

---
+++ sympy/00_index.py
@@ -0,0 +1,159 @@
+# Seznam zdrojových kódů v tomto podadresáři
+# ------------------------------------------
+#
+# sympy01.py:
+#
+# sympy02.py:
+#
+# sympy03.py:
+#
+# sympy04.py:
+#
+# sympy05.py:
+#
+# sympy06.py:
+#
+# sympy07.py:
+#
+# sympy08.py:
+#
+# sympy09.py:
+#
+# sympy10.py:
+#
+# sympy11.py:
+#
+# sympy12.py:
+#
+# sympy13.py:
+#
+# sympy14.py:
+#
+# sympy15.py:
+#
+# sympy16.py:
+#
+# sympy17.py:
+#
+# sympy18.py:
+#
+# sympy19.py:
+#
+# sympy20.py:
+#
+# sympy21.py:
+#
+# sympy22.py:
+#
+# sympy23.py:
+#
+# sympy24.py:
+#
+# sympy25.py:
+#
+# sympy26.py:
+#
+# sympy27.py:
+#
+# sympy28.py:
+#
+# sympy29.py:
+#
+# sympy30.py:
+#
+# sympy31.py:
+#
+# sympy32.py:
+#
+# sympy33.py:
+#
+# sympy34.py:
+#
+# sympy35.py:
+#
+# sympy36.py:
+#
+# sympy37.py:
+#
+# sympy38.py:
+#
+# sympy39.py:
+#
+# sympy40.py:
+#
+# sympy41.py:
+#
+# sympy42.py:
+#
+# sympy43.py:
+#
+# sympy44.py:
+#
+# sympy45.py:
+#
+# sympy46.py:
+#
+# sympy47.py:
+#
+# sympy48.py:
+#
+# sympy49.py:
+#
+# sympy50.py:
+#
+# sympy51.py:
+#
+# sympy52.py:
+#
+# sympy53.py:
+#
+# sympy54.py:
+#
+# sympy55.py:
+#
+# sympy56.py:
+#
+# sympy57.py:
+#
+# sympy58.py:
+#
+# sympy59.py:
+#
+# sympy60.py:
+#
+# sympy61.py:
+#
+# sympy62.py:
+#
+# sympy63.py:
+#
+# sympy64.py:
+#
+# sympy65.py:
+#
+# sympy66.py:
+#
+# sympy67.py:
+#
+# sympy68.py:
+#
+# sympy69.py:
+#
+# sympy70.py:
+#
+# sympy71.py:
+#
+# sympy72.py:
+#
+# sympy73.py:
+#
+# sympy74.py:
+#
+# sympy75.py:
+#
+# sympy76.py:
+#
+# sympy77.py:
+#
+# sympy78.py:
+#
