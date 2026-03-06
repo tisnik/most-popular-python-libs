6988b4609dedc8f0573cd83c5946709ee4824b6c
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-03-06 09:15:18 +0100

    Anchor layout: example #2

---
+++ kivy/anchor_layout_2.py
@@ -0,0 +1,27 @@
+# Knihovna Kivy
+#
+# - GUI aplikace s jediným dialogem
+# - definice dialogu využívajícího AnchorLayout
+# - dialog obsahuje jediné tlačítko, které je horizontálně vycentrované
+
+from kivy.app import App
+from kivy.core.window import Window
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.button import Button
+
+
+class Application(App):
+    def build(self):
+        layout = AnchorLayout(anchor_y="bottom")
+
+        button = Button(text="Click!", size_hint=(0.5, 0.5))
+        layout.add_widget(button)
+
+        return layout
+
+    def on_start(self):
+        Window.size = (400, 300)
+
+
+if __name__ == "__main__":
+    Application().run()
