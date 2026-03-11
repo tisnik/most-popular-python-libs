d92c6c513e30d18b0190cb76c92ecd2013a3ca90
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-03-11 08:21:03 +0100

    Matrix inversion with complex numbers

---
+++ scipy-lib/inverse_4.py
@@ -0,0 +1,11 @@
+import numpy as np
+from scipy import linalg
+
+m = np.array([[0+0j, 1+0j, 0+1j], [1+0j, 1+1j, 1-1j], [0+0j, 1+0j, 1+2j]])
+print(m)
+print()
+
+inv = linalg.inv(m)
+print("Inverse matrix:" )
+print(inv)
+
