bc13095290d928870e21cb6f2ee2a33351642d67
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-03-12 08:32:58 +0100

    Matrix norm, example #3

---
+++ scipy-lib/matrix_norm_3.py
@@ -0,0 +1,13 @@
+import numpy as np
+from scipy import linalg
+
+m1 = np.array([[0, 0], [1, 1]])
+print("Matrix M1")
+print(m1)
+print()
+
+n = linalg.norm(m1, ord=2)
+print("Norm (max):", n)
+
+n = linalg.norm(m1, ord=-2)
+print("Norm (min):", n)
