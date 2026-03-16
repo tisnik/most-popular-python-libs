ff57b755028773c5ffb04c77cfa93cea8880a5e0
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-03-16 08:12:10 +0100

    Cubic spline computation and display, example #2

---
+++ scipy-lib/cubic_spline_2.py
@@ -0,0 +1,42 @@
+from scipy.interpolate import CubicSpline
+
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+
+def func(x, a, b):
+    return a * x + b
+
+
+# hodnoty na x-ové ose
+x = np.arange(0, 10)
+
+# generátor pseudonáhodných dat
+rng = np.random.default_rng(seed=42)
+
+# hodnoty na y-ové ose
+y = x + 10 * rng.random((len(x))) - 5
+
+# vykreslení bodů
+plt.plot(x, y, "go", label="data")
+
+# vytvoření spline
+spline = CubicSpline(x, y)
+
+x = np.arange(0, 10, 0.01)
+
+# vykreslení křivky
+plt.plot(x, spline(x), "r-", label="Spline")
+plt.plot(x, spline(x, nu=1), '--', label='1st derivative')
+
+# popis os a legenda
+plt.xlabel("x")
+plt.ylabel("y")
+plt.legend()
+
+# uložení grafu do rastrového obrázku
+plt.savefig("cubic_spline_2.png")
+
+# zobrazení grafu
+plt.show()
