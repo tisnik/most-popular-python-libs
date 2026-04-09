1c8b1718b379bee60ced2d102c98e5d2ca85baa9
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-04-09 08:58:00 +0200

    Signal/function resampling

---
+++ scipy-lib/resample_2.py
@@ -0,0 +1,24 @@
+import numpy as np
+import scipy.datasets as datasets
+import scipy.signal as signal
+
+import matplotlib.pyplot as plt
+
+# načtení signálu
+ekg = datasets.electrocardiogram()
+
+# jen prvních 250 vzorků
+ekg = ekg[0:250]
+
+# resampling
+resampled = signal.resample(ekg, 50)
+
+# vykreslení výsledného signálu
+plt.plot(np.linspace(0, 1000, len(ekg)), ekg, "b")
+plt.plot(np.linspace(0, 1000, len(resampled)), resampled, "r")
+
+# uložení grafu s průběhem signálu
+plt.savefig("resample_2.png")
+
+# zobrazení grafu
+plt.show()
