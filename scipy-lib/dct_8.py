06f35cc00d50352d053a2a85ff1837e86b2665ab
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-04-11 09:53:01 +0200

    DCT: example #8

---
+++ scipy-lib/dct_8.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+import scipy.datasets as datasets
+from scipy.fft import dct
+
+import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
+
+
+# načtení matice
+ascent = datasets.ascent()
+
+# výpočet 2D DCT
+ascent_dct = dct(ascent)
+
+# zobrazení výsledku, změna měřítka
+plt.imshow(ascent_dct, norm=LogNorm(vmin=5))
+
+plt.title("2D DCT")
+
+# uložení matice do rastrového obrázku
+plt.savefig("dct_8.png")
+
+# zobrazení grafu
+plt.show()
