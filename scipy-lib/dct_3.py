6714019c848d0ab4b225288f1cb8996ba4d7f218
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-04-11 09:53:01 +0200

    DCT: example #3

---
+++ scipy-lib/dct_3.py
@@ -0,0 +1,13 @@
+from scipy.fft import dct
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# signál
+t = np.cos(np.linspace(0, 2.0*np.pi, 100)) + 0.5*np.cos(np.linspace(0, 20*np.pi, 100))
+
+# vykreslení signálu
+plt.plot(t)
+
+# uložení grafu s průběhem signálu
+plt.savefig("dct_3.png")
