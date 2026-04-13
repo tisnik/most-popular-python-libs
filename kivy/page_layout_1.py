606fe76c10fc094f166acef45f5ef0053bf0b18b
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-04-13 11:04:23 +0200

    Page layout in Kivy, example #1

---
+++ kivy/page_layout_1.py
@@ -0,0 +1,52 @@
+# Knihovna Kivy
+#
+# - GUI aplikace s jediným dialogem
+# - definice dialogu využívajícího PageLayout
+# - PageLayout využívá výchozí (horizontální) orientaci
+# - dialog obsahuje devět tlačítek se stejnými vlastnostmi
+
+from kivy.app import App
+from kivy.core.window import Window
+from kivy.uix.pagelayout import PageLayout
+from kivy.uix.button import Button
+
+
+class Application(App):
+    def build(self):
+        layout = PageLayout()
+
+        button = Button(text="1")
+        layout.add_widget(button)
+
+        button = Button(text="2")
+        layout.add_widget(button)
+
+        button = Button(text="3")
+        layout.add_widget(button)
+
+        button = Button(text="4")
+        layout.add_widget(button)
+
+        button = Button(text="5")
+        layout.add_widget(button)
+
+        button = Button(text="6")
+        layout.add_widget(button)
+
+        button = Button(text="7")
+        layout.add_widget(button)
+
+        button = Button(text="8")
+        layout.add_widget(button)
+
+        button = Button(text="9")
+        layout.add_widget(button)
+
+        return layout
+
+    def on_start(self):
+        Window.size = (400, 300)
+
+
+if __name__ == "__main__":
+    Application().run()
