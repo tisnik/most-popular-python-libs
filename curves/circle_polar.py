4fc3f1bd9b5917d6a6f414c91da1a6482ce01c80
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-04-15 09:34:28 +0200

    Parametric circle, polar coordinates

---
+++ curves/circle_polar.py
@@ -0,0 +1,43 @@
+"""Kružnice vykreslená v polárním grafu."""
+
+#
+# Použito v článku:
+#
+# Křivky v přírodě, architektuře, stavitelství i v počítačové grafice
+# https://www.root.cz/clanky/krivky-v-prirode-architekture-stavitelstvi-i-v-pocitacove-grafice/
+#
+# Součást seriálu:
+#
+# Křivky nejen v počítačové grafice
+# https://www.root.cz/serialy/krivky-nejen-v-pocitacove-grafice/
+#
+# Zdrojový kód tohoto skriptu ve stylu literate programming
+# https://tisnik.github.io/presentations/appendix/lit_sources/basic/circle_polar.html
+#
+
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+# úhel v polárním grafu
+theta = np.arange(0.00, 2 * np.pi, 0.05)
+
+a = 1.0
+
+# funkce: vzdálenost od středu
+radius = np.repeat(a, len(theta))
+
+# rozměry grafu při uložení: 640x480 pixelů
+fig, ax = plt.subplots(1, figsize=(6.4, 4.8), subplot_kw={"projection": "polar"})
+
+# titulek grafu
+fig.suptitle("Kružnice", fontsize=15)
+
+# vrcholy na křivce pospojované úsečkami
+ax.plot(theta, radius, "g-")
+
+# uložení grafu do rastrového obrázku
+plt.savefig("circle_polar.png")
+
+# zobrazení grafu
+plt.show()
