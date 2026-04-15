666e8b33156e1ef21abd51d06046c905a1ab8317
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-04-15 09:34:28 +0200

    Hypocycloid, example #2

---
+++ curves/hypocycloid2.py
@@ -0,0 +1,50 @@
+"""Parametrická křivka: hypocykloida."""
+
+#
+# Použito v článku:
+#
+# Křivky v přírodě, architektuře, stavitelství i v počítačové grafice: matematický popis křivek
+# https://www.root.cz/clanky/krivky-v-prirode-architekture-stavitelstvi-i-v-pocitacove-grafice-matematicky-popis-krivek/
+#
+# Součást seriálu:
+#
+# Křivky nejen v počítačové grafice
+# https://www.root.cz/serialy/krivky-nejen-v-pocitacove-grafice/
+#
+# Zdrojový kód tohoto skriptu ve stylu literate programming
+# https://tisnik.github.io/presentations/appendix/lit_sources/basic/hypocycloid2.html
+#
+
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+# hodnoty parametru t
+t = np.arange(0, 14 * np.pi, 0.01)
+
+# parametry
+a = 2
+b = 7
+
+# výpočet bodů ležících na křivce
+x = (a - b) * np.cos(t) + b * np.cos((a / b - 1) * t)
+y = (a - b) * np.sin(t) - b * np.sin((a / b - 1) * t)
+
+# rozměry grafu při uložení: 640x480 pixelů
+fig, ax = plt.subplots(1, figsize=(6.4, 4.8))
+
+# titulek grafu
+fig.suptitle("Hypocykloida", fontsize=15)
+
+# určení rozsahů na obou souřadných osách
+ax.set_xlim(-18, 18)
+ax.set_ylim(-12, 12)
+
+# vrcholy na křivce pospojované úsečkami
+ax.plot(x, y, "g-")
+
+# uložení grafu do rastrového obrázku
+plt.savefig("hypocycloid2.png")
+
+# zobrazení grafu
+plt.show()
