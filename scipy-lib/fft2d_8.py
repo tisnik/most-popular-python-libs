dc5c744be58454954ddee4ad67c366cefbddbd8b
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-04-16 08:38:13 +0200

    Removing high and some low frequencies from the image

---
+++ scipy-lib/fft2d_8.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+import scipy.datasets as datasets
+from scipy import fftpack
+
+import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
+
+
+# načtení matice
+ascent = datasets.ascent()
+
+# výpočet 2D FFT
+ascent_fft = fftpack.fft2(ascent)
+
+high_freq_limit = 25
+
+ascent_fft[high_freq_limit:512-high_freq_limit] = 0
+ascent_fft[:, high_freq_limit:512-high_freq_limit] = 0
+
+ascent_time = fftpack.ifft2(ascent_fft)
+
+# zobrazení výsledku
+plt.imshow(ascent_time.real, cmap="gray")
+
+# uložení matice do rastrového obrázku
+plt.savefig("fft2d_8.png")
+
+# zobrazení grafu
+plt.show()
