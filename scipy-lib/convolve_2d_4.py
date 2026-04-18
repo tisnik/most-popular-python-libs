5352ef98a4d2507df85819f27ce844b4a79ad49d
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-04-18 08:58:22 +0200

    2D convolution: example 4

---
+++ scipy-lib/convolve_2d_4.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+from scipy.signal import convolve
+import scipy.datasets as datasets
+
+
+# načtení matice
+ascent = datasets.ascent()
+
+kernel = np.array([
+    [-1,  0,  1],
+    [-1,  0,  1],
+    [-1,  0,  1],
+])
+
+print("Kernel:")
+print(kernel)
+
+# výpočet konvoluce
+filtered = convolve(ascent, kernel)
+
+# zobrazení výsledku
+plt.imshow(filtered, cmap="gray")
+
+plt.title("2D convolution")
+
+# uložení grafu s průběhem signálu
+plt.savefig("convolve_2d_4.png")
+
+# zobrazení grafu
+plt.show()
