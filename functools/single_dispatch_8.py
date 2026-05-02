bcfc5982b9ee5e4fe4f638be4bf6e4dbf91b5e48
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-05-02 09:43:00 +0200

    @singledispatch decorator, example #8

---
+++ functools/single_dispatch_8.py
@@ -0,0 +1,32 @@
+from typing import Any
+import dis
+
+from functools import singledispatch
+
+
+@singledispatch
+def function(arg: Any) -> None:
+    print("Original function with argument", arg, "that has type", type(arg))
+
+
+@function.register
+def _(arg: int) -> None:
+    print("Integer variant with argument:", arg)
+
+
+@function.register
+def _(arg: str) -> None:
+    print("String variant with argument:", arg)
+
+
+@function.register(list)
+def _(arg: list[Any]) -> None:
+    print("List variant with argument:", arg)
+
+
+@function.register
+def _(arg: None) -> None:
+    print("None variant with argument:", arg)
+
+
+dis.dis(function)
