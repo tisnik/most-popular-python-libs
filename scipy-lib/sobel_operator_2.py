bc777e6330a6c55b17dc92578d4152ad5fe618d7
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-05-05 09:51:12 +0200

    Sobel operator usage, example #2

---
+++ scipy-lib/sobel_operator_2.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+import scipy.datasets as datasets
+from scipy import ndimage
+
+import matplotlib.pyplot as plt
+
+
+# načtení matice
+ascent = datasets.ascent()
+
+# aplikace filtru
+filtered = ndimage.sobel(ascent, axis=1)
+
+# zobrazení výsledku, změna měřítka
+plt.imshow(filtered, cmap="gray")
+
+plt.title("Sobel operator")
+
+# uložení matice do rastrového obrázku
+plt.savefig("sobel_operator_2.png")
+
+# zobrazení grafu
+plt.show()
