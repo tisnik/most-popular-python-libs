6236c094902aa7cca3305347caeb3d2e5d3fff90
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-05-09 09:36:01 +0200

    Ellipsis in Numpy, example #2

---
+++ ellipsis/numpy_2.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+a=np.arange(24)
+print(a)
+print()
+
+b=a.reshape((3, 4, 2))
+print(b)
+print()
+
+print(b[1])
+print()
+
+print(b[..., 1])
+print()
+
+print(b[..., 1, 1])
+print()
