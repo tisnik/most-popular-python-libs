21cda29fa5832e89e340a4a590c67a83774835df
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-05-13 11:27:41 +0200

    Selecting vectors by distance from B1

---
+++ pgvector/select_by_distance_b1_3.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# vim: set fileencoding=utf-8
+
+#
+#  (C) Copyright 2025  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
+import psycopg2
+
+from pgvector.psycopg2 import register_vector
+
+connection = psycopg2.connect(
+    host="localhost", port=54321, user="tester", password="123qwe", dbname="test"
+)
+
+register_vector(connection)
+
+with connection.cursor() as cursor:
+    cursor.execute("SELECT embedding FROM b1 ORDER BY embedding <%%> %s LIMIT 5", ("0100", ))
+    records = cursor.fetchall()
+    for record in records:
+        print(record[0])
