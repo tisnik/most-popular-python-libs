30d6a50a37c777927127ed5c303c388054680fc2
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-05-20 09:04:54 +0200

    Updated index file

--- kivy/00_index.py
+++ kivy/00_index.py
@@ -22,24 +22,87 @@
 # - dialog je postaven na kontejneru typu AnchorLayout
 #
 # box_layout_1.py:
+# - GUI aplikace s jediným dialogem
+# - definice dialogu využívajícího BoxLayout
+# - BoxLayout využívá výchozí (horizontální) orientaci
+# - dialog obsahuje devět tlačítek se stejnými vlastnostmi
 #
 # box_layout_2.py:
+# - GUI aplikace s jediným dialogem
+# - definice dialogu využívajícího BoxLayout
+# - BoxLayout využívá vertikální orientaci
+# - dialog obsahuje devět tlačítek se stejnými vlastnostmi
 #
 # box_layout_3.py:
+# - GUI aplikace s jediným dialogem
+# - definice dialogu využívajícího BoxLayout
+# - uvnitř kontejneru BoxLayout jsou umístěny další kontejnery stejného typu
+# - tlačítka jsou vložena až do těchto vnitřních kontejnerů
 #
 # box_layout_4.py:
+# - GUI aplikace s jediným dialogem
+# - načtení definice dialogu popsaného v jazyku Kv
+# - dialog je postaven na kontejneru typu BoxLayout
 #
 # box_layout_5.py:
+# - GUI aplikace s jediným dialogem
+# - definice dialogu využívajícího BoxLayout
+# - specifikace mezer mezi prvky vloženými do kontejneru
+# - uvnitř kontejneru BoxLayout jsou umístěny další kontejnery stejného typu
+# - tlačítka jsou vložena až do těchto vnitřních kontejnerů
+#
+# canvas_1.py:
+# - GUI aplikace s jediným oknem
+# - v okně je umístěna kreslicí plocha
+#
+# float_layout_1.py:
+# - GUI aplikace s jediným dialogem
+# - definice dialogu využívajícího FloatLayout
+# - dialog obsahuje devět tlačítek se stejnými vlastnostmi
+#
+# float_layout_2.py:
+# - GUI aplikace s jediným dialogem
+# - definice dialogu využívajícího FloatLayout
+# - dialog obsahuje devět tlačítek se stejnými vlastnostmi
+# - explicitní rozmístění tlačítek
+#
+# float_layout_3.py:
+# - GUI aplikace s jediným dialogem
+# - definice dialogu využívajícího FloatLayout
+# - dialog obsahuje devět tlačítek se stejnými vlastnostmi
+# - explicitní rozmístění tlačítek
 #
 # grid_layout_1.py:
+# - GUI aplikace s jediným dialogem
+# - definice dialogu využívajícího GridLayout
+# - použita je mřížka se třemi sloupci
+# - dialog obsahuje devět tlačítek se stejnými vlastnostmi
 #
 # grid_layout_2.py:
+# - GUI aplikace s jediným dialogem
+# - definice dialogu využívajícího GridLayout
+# - specifikace mezer mezi prvky vloženými do kontejneru
+# - použita je mřížka se třemi sloupci
+# - dialog obsahuje devět tlačítek se stejnými vlastnostmi
 #
 # grid_layout_3.py:
+# - GUI aplikace s jediným dialogem
+# - definice dialogu využívajícího GridLayout
+# - specifikace mezer mezi prvky vloženými do kontejneru
+# - použita je mřížka se třemi sloupci
+# - dialog obsahuje devět tlačítek s různýmí šířkami
 #
 # grid_layout_4.py:
+# - GUI aplikace s jediným dialogem
+# - definice dialogu využívajícího GridLayout
+# - specifikace mezer mezi prvky vloženými do kontejneru
+# - použita je mřížka se třemi sloupci
+# - dialog obsahuje devět tlačítek s různýmí šířkami
 #
 # grid_layout_5.py:
+# - GUI aplikace s jediným dialogem
+# - načtení definice dialogu popsaného v jazyku Kv
+# - dialog je postaven na kontejneru typu GridLayout
 #
 # hello_kv_spec.py:
 #
@@ -63,15 +126,58 @@
 #
 # login_form_6.py:
 #
+# page_layout_1.py:
+# - GUI aplikace s jediným dialogem
+# - definice dialogu využívajícího PageLayout
+# - PageLayout využívá výchozí (horizontální) orientaci
+# - dialog obsahuje devět tlačítek se stejnými vlastnostmi
+#
+# page_layout_2.py:
+# - GUI aplikace s jediným dialogem
+# - definice dialogu využívajícího PageLayout
+# - PageLayout využívá výchozí (horizontální) orientaci
+# - dialog obsahuje trojici stránek s tlačítky
+#
+# page_layout_3.py:
+# - GUI aplikace s jediným dialogem
+# - definice dialogu využívajícího PageLayout
+# - PageLayout využívá výchozí (horizontální) orientaci
+# - dialog obsahuje trojici stránek s tlačítky
+#
+# relative_layout_1.py:
+# - GUI aplikace s jediným dialogem
+# - definice dialogu využívajícího RelativeLayout
+# - dialog obsahuje devět tlačítek se stejnými vlastnostmi
+# - explicitní rozmístění tlačítek
+#
 # stack_layout_1.py:
+# - GUI aplikace s jediným dialogem
+# - definice dialogu využívajícího StackLayout
+# - StackLayout využívá výchozí orientaci
+# - dialog obsahuje devět tlačítek se specifikací výšky
 #
 # stack_layout_2.py:
+# - GUI aplikace s jediným dialogem
+# - definice dialogu využívajícího StackLayout
+# - StackLayout využívá výchozí orientaci
+# - dialog obsahuje devět tlačítek se specifikací výšky
 #
 # stack_layout_3.py:
+# - GUI aplikace s jediným dialogem
+# - definice dialogu využívajícího StackLayout
+# - StackLayout využívá výchozí orientaci
+# - dialog obsahuje devět tlačítek se specifikací šířky i výšky
 #
 # stack_layout_4.py:
+# - GUI aplikace s jediným dialogem
+# - definice dialogu využívajícího StackLayout
+# - StackLayout využívá orientaci rl-tb
+# - dialog obsahuje devět tlačítek se specifikací šířky i výšky
 #
 # stack_layout_5.py:
+# - GUI aplikace s jediným dialogem
+# - načtení definice dialogu popsaného v jazyku Kv
+# - dialog je postaven na kontejneru typu StackLayout
 #
 # widget_basic_1.py:
 #
