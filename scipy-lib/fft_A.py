f89518ac76d94602b88f67d07cc7f2a161ec52aa
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-06-02 09:20:56 +0200

    FFT in Scipy, example #10

---
+++ scipy-lib/fft_A.py
@@ -0,0 +1,25 @@
+import scipy.signal as signal
+from scipy.fft import fft, fftshift
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# signal
+t = np.zeros(50) + np.ones(50) + np.zeros(50)
+
+# rychlá Fourierova transformace
+f = fft(t, 1024) / (len(t)/2.0)
+
+# rozsah normalizovaných frekvencí na x-ové ose
+freq = np.linspace(-0.5, 0.5, len(f))
+
+#response = np.abs(fftshift(f / abs(f).max()))
+
+# vykreslení výsledného signálu
+plt.plot(f)
+
+# uložení grafu s průběhem signálu
+plt.savefig("fft_A.png")
+
+# zobrazení grafu
+plt.show()
