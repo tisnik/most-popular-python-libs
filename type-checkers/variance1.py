c6849315fa10785ef590398d8c9444b4bc64d05f
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-07-12 09:40:49 +0200

    Variance: example #1

---
+++ type-checkers/variance1.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+#
+#  (C) Copyright 2023  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
+from typing import List
+
+
+class Ovoce:
+    pass
+
+
+class Hruska(Ovoce):
+    def __repr__(self) -> str:
+        return "Hruska"
+
+
+class Jablko(Ovoce):
+    def __repr__(self) -> str:
+        return "Jablko"
+
+
+def smichej(kosik : List[Ovoce]) -> None:
+    kosik.append(Hruska())
+    kosik.append(Jablko())
+
+
+kosik : List[Ovoce] = []
+
+smichej(kosik)
+
+for ovoce in kosik:
+    print(ovoce)
