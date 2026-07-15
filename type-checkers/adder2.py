e12cee818615c99337bb8ffe9509430208b62ab2
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-07-15 10:09:43 +0200

    Adder and type checking, example #2

---
+++ type-checkers/adder2.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+#
+#  (C) Copyright 2023  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
+def add(a:int, b:int) -> int:
+    return a+b
+
+
+print(add(1, 2))
+print(add(1, True))
+print(add(1, False))
+print(add(1.2, 3.4))
+print(add("foo", "bar"))
+print(add((1, 2), (3, 4)))
+print(add([1, 2], [3, 4]))
