6fc4ef21625b8ac764ad2a2a265d6775c6da6407
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-09-03 09:41:50 +0200

    Lua to Python: passing tables, example #3

---
+++ lua-python/lua_table_5.py
@@ -0,0 +1,14 @@
+from lupa import LuaRuntime
+
+lua = LuaRuntime()
+
+with open("table5.lua", "r", encoding="utf-8") as fin:
+    lua_script = fin.read()
+
+lua.execute(lua_script)
+full_name = lua.globals().full_name
+
+d = {"id": 42, "name": "John", "surname": "Doe", "address": None}
+n = full_name(d)
+
+print(n)
