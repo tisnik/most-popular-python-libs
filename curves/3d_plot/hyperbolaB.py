7bc7d1149cae7cd49875287fae0172f2fc1e9491
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-09-04 08:20:45 +0200

    Hyperbola mesh in 3D

---
+++ curves/3d_plot/hyperbolaB.py
@@ -0,0 +1,67 @@
+"""Hyperbola zadaná implicitní funkcí."""
+
+#
+# Použito v článku:
+#
+# Křivky popsané implicitní funkcí, animace křivek
+# https://www.root.cz/clanky/krivky-popsane-implicitni-funkci-animace-krivek/
+#
+# Součást seriálu:
+#
+# Křivky nejen v počítačové grafice
+# https://www.root.cz/serialy/krivky-nejen-v-pocitacove-grafice/
+#
+# Zdrojový kód tohoto skriptu ve stylu literate programming
+# https://tisnik.github.io/presentations/appendix/lit_sources/3d_plot/hyperbolaA.html
+#
+
+import numpy as np
+
+import matplotlib.cm as cm
+import matplotlib.pyplot as plt
+
+# příprava vektorů pro konstrukci mřížky
+x = np.linspace(-4, 4, 50)
+y = np.linspace(-4, 4, 50)
+
+# konstrukce mřížky
+x, y = np.meshgrid(x, y)
+
+# implicitní funkce hyperboly
+a = 1
+z = (x ** 2 - y ** 2) / a ** 2 - 1
+
+# inicializace grafu
+fig = plt.figure()
+
+# nastavení 3D projekce
+ax = fig.gca(projection="3d")
+
+# zobrazení 3D grafu formou plochy
+surface = ax.plot_surface(
+    x, y, z, rstride=2, cstride=2, cmap=cm.coolwarm, linewidth=0, antialiased=False
+)
+
+# titulek grafu
+fig.suptitle("Hyperbola", fontsize=15)
+
+# kontura: průmět na rovinu x-y
+cset = ax.contour(x, y, z, zdir="z", offset=10)
+
+# rozměry grafu ve směru osy x
+ax.set_xlabel("X")
+ax.set_xlim(-4, 4)
+
+# rozměry grafu ve směru osy y
+ax.set_ylabel("Y")
+ax.set_ylim(-4, 4)
+
+# rozměry grafu ve směru osy z
+ax.set_zlabel("Z")
+ax.set_zlim(-10, 10)
+
+# uložení grafu do rastrového obrázku
+plt.savefig("hyperbola.png")
+
+# zobrazení grafu
+plt.show()
