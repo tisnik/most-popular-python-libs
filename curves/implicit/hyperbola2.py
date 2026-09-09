48c662210dd05c594ef0e09cd15f981d3146a76e
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-09-09 10:43:59 +0200

    Implicit curve: hyperbola, example #2

---
+++ curves/implicit/hyperbola2.py
@@ -0,0 +1,61 @@
+"""Hyperbola zadaná implicitní funkcí."""
+
+#
+# Použito v článku:
+#
+# Křivky popsané implicitní funkcí, animace křivek
+# https://www.root.cz/clanky/krivky-popsane-implicitni-funkci-animace-krivek/
+#
+# Součást seriálu:
+#
+# Křivky nejen v počítačové grafice
+# https://www.root.cz/serialy/krivky-nejen-v-pocitacove-grafice/
+#
+# Zdrojový kód tohoto skriptu ve stylu literate programming
+# https://tisnik.github.io/presentations/appendix/lit_sources/implicit/hyperbola2.html
+#
+
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+# příprava vektorů pro konstrukci mřížky
+x = np.linspace(-10, 10, 50)
+y = np.linspace(-10, 10, 50)
+
+# konstrukce mřížky
+x, y = np.meshgrid(x, y)
+
+# koeficient hyperboly
+a = 4
+
+# implicitní funkce hyperboly
+z = a / x - y
+
+# hodnota, která se má zvýraznit na isoploše
+levels = [0]
+
+# rozměry grafu při uložení: 640x480 pixelů
+fig, ax = plt.subplots(1, figsize=(6.4, 4.8))
+
+# titulek grafu
+fig.suptitle("Hyperbola", fontsize=15)
+
+# vykreslení implicitní funkce
+ax.contour(x, y, z, levels)
+
+# zobrazit mřížku
+ax.grid(True)
+
+# zachovat poměr stran
+ax.axis("scaled")
+
+# popisek os
+plt.xlabel("Osa x")
+plt.ylabel("Osa y")
+
+# uložení grafu do rastrového obrázku
+plt.savefig("hyperbola2.png")
+
+# zobrazení grafu
+plt.show()
