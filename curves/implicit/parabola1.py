6f6e5f46664518c58e63c8681d171aa21127bfdc
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-09-09 10:43:59 +0200

    Implicit curve: parabola, example #1

---
+++ curves/implicit/parabola1.py
@@ -0,0 +1,62 @@
+"""Parabola zadaná implicitní funkcí."""
+
+#
+# Použito v článku:
+#
+# Křivky popsané implicitní funkcí, animace křivek
+# https://www.root.cz/clanky/krivky-popsane-implicitni-funkci-animace-krivek/
+#
+# Součást seriálu:
+#
+# Křivky nejen v počítačové grafice
+# https://www.root.cz/serialy/krivky-nejen-v-pocitacove-grafice/
+#
+# Zdrojový kód tohoto skriptu ve stylu literate programming
+# https://tisnik.github.io/presentations/appendix/lit_sources/implicit/parabola1.html
+#
+
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+# příprava vektorů pro konstrukci mřížky
+x = np.linspace(-4, 4, 50)
+y = np.linspace(0, 8, 50)
+
+# konstrukce mřížky
+x, y = np.meshgrid(x, y)
+
+# koeficienty paraboly
+f = 0.5
+p = 2 * f
+
+# implicitní funkce paraboly
+z = x ** 2 - 2 * p * y
+
+# hodnota, která se má zvýraznit na isoploše
+levels = [0]
+
+# rozměry grafu při uložení: 640x480 pixelů
+fig, ax = plt.subplots(1, figsize=(6.4, 4.8))
+
+# titulek grafu
+fig.suptitle("Parabola", fontsize=15)
+
+# vykreslení implicitní funkce
+ax.contour(x, y, z, levels)
+
+# zobrazit mřížku
+ax.grid(True)
+
+# zachovat poměr stran
+ax.axis("scaled")
+
+# popisek os
+plt.xlabel("Osa x")
+plt.ylabel("Osa y")
+
+# uložení grafu do rastrového obrázku
+plt.savefig("parabola1.png")
+
+# zobrazení grafu
+plt.show()
