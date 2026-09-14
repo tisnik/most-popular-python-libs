b1240d413741052d7b0e8313d61c204740514d33
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-09-14 09:54:55 +0200

    Linear interpolation: basis polynoms

---
+++ curves/parametric/linear_basis.py
@@ -0,0 +1,49 @@
+"""Bázové polynomy pro lineární interpolaci."""
+
+#
+# Použito v článku:
+#
+# Parametrické křivky používané v designu i při tvorbě animací
+# https://www.root.cz/clanky/parametricke-krivky-pouzivane-v-designu-i-pri-tvorbe-animaci/
+#
+# Součást seriálu:
+#
+# Křivky nejen v počítačové grafice
+# https://www.root.cz/serialy/krivky-nejen-v-pocitacove-grafice/
+#
+# Zdrojový kód tohoto skriptu ve stylu literate programming
+# https://tisnik.github.io/presentations/appendix/lit_sources/parametric/linear_basis.html
+#
+
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+# hodnoty parametru t
+t = np.linspace(0, 1, 50)
+
+# Bernsteinovy polynomy pro lineární interpolaci
+B = [1 * (1 - t), 1 * t]
+
+# rozměry grafu při uložení: 640x480 pixelů
+fig, ax = plt.subplots(1, figsize=(6.4, 4.8))
+
+# titulek grafu
+fig.suptitle("Bázové polynomy", fontsize=15)
+
+# určení rozsahů na obou souřadných osách
+ax.set_xlim(0, 1)
+ax.set_ylim(0, 1)
+
+# bázové polynomy
+ax.plot(t, B[0], "r-", label="b0,1")
+ax.plot(t, B[1], "g-", label="b1,1")
+
+# zobrazení legendy
+ax.legend()
+
+# uložení grafu do rastrového obrázku
+plt.savefig("linear_basis.png")
+
+# zobrazení grafu
+plt.show()
